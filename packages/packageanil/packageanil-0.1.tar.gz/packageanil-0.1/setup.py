e5c6cc6b412c5fc673fa0a8b7a616264abb5b8f4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-17 23:43:24 +0000

    Add package packageanil/0.1/packageanil-0.1.tar.gz

---
+++ packages/packageanil/packageanil-0.1.tar.gz/packageanil-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+setup(name="packageanil",version="0.1",
+      description="this is code with anil package",
+      long_description="this is very very long description",
+      author="anil",
+      packages=["packageanil"],
+      install_requires=[])
