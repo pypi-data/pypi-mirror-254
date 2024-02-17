74e889c5617e11e4705f1c615eabf4aaa9030e5e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-17 23:48:21 +0000

    Add package sybil-engine/5.2.0/sybil_engine-5.2.0-py3-none-any.whl

---
+++ packages/sybil-engine/sybil_engine-5.2.0-py3-none-any.whl/sybil_engine/config/app_config.py
@@ -0,0 +1,56 @@
+network = None
+dex_retry_interval = None
+gas_prices_gwei = None
+module_data = None
+okx = None, (None, None, None, None)
+
+
+def set_network(value):
+    global network
+    network = value
+
+
+def get_network():
+    return network
+
+
+def set_dex_retry_interval(value):
+    global dex_retry_interval
+    dex_retry_interval = value
+
+
+def get_dex_retry_interval():
+    return dex_retry_interval
+
+
+def set_gas_prices(value):
+    global gas_prices_gwei
+    gas_prices_gwei = value
+
+
+def get_gas_prices():
+    return gas_prices_gwei
+
+
+def set_module_data(value):
+    global module_data
+    module_data = value
+
+
+def get_module_data():
+    return module_data
+
+
+def set_okx_config(value):
+    global okx
+    okx = value
+
+
+def get_okx():
+    return okx
+
+
+def get_okx_data():
+    _, (cex_data, _, _, _) = okx
+
+    return cex_data
