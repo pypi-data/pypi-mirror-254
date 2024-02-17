9b8387835b1f0d755092dd047cec90f970dbb89c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-17 23:53:05 +0000

    Add package pub.tools/4.3/pub.tools-4.3-py3-none-any.whl

---
+++ packages/pub.tools/pub.tools-4.3-py3-none-any.whl/pub/tools/config.py
@@ -0,0 +1,4 @@
+NO_VALUE = "<<blank>>"
+MAX_PUBS = 9000
+# Biopython will put a count greater than 200 ids into a post, so we don't need to worry about request size
+# But there does seem to be a 9999 limit either from Biopython or from NCBI
