2fcc7c3b9da36da77c0d145dfa251f0de500ae3f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-17 23:38:20 +0000

    Add package scPECA/1.8/scPECA-1.8.tar.gz

---
+++ packages/scPECA/scPECA-1.8.tar.gz/scPECA-1.8/scPECA/prior_install.py
@@ -0,0 +1,7 @@
+import subprocess
+import os
+
+def prior_install(path):
+    subprocess.run(['mkdir','{}/Prior'.format(path)])
+    subprocess.run(['wget', '-O', '{}/Prior/Opn_median_mm9.bed'.format(path),
+                    'https://github.com/SUwonglab/PECA/raw/master/Prior/Opn_median_mm9.bed'])
