be332f73071693f49fa83e29d10b47c0493979ee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-17 23:40:08 +0000

    Add package rnaglib/2.0.12/rnaglib-2.0.12.tar.gz

---
+++ packages/rnaglib/rnaglib-2.0.12.tar.gz/rnaglib-2.0.12/rnaglib/utils/misc.py
@@ -0,0 +1,33 @@
+import os
+from torch.utils.data import Subset
+
+
+def listdir_fullpath(d):
+    return [os.path.join(d, f) for f in os.listdir(d)]
+
+
+def tonumpy(torch_array):
+    """
+    Routine to get any torch tensor back to numpy without cpu or gradient problems
+
+    :param torch_array: A torch array
+
+    :return: A numpy array
+    """
+    return torch_array.detach().cpu().numpy()
+
+
+def get_dataset(loader):
+    """
+    Using torch subsets have a weird property that messes up the dataset accessing.
+     This is the recommended fix in https://discuss.pytorch.org/t/how-to-get-a-part-of-datasets/82161/7
+
+    :param dataset: either a dataset or a Subset
+    :param node_sim:
+
+    :return:
+    """
+    if isinstance(loader, Subset):
+        return loader.dataset.dataset
+    else:
+        return loader.dataset
