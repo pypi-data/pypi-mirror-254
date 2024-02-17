5766127a5b690ef37c9af31206524dcc5c4ab987
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-17 23:56:20 +0000

    Add package panel-jstree/0.3.2/panel_jstree-0.3.2-py3-none-any.whl

---
+++ packages/panel-jstree/panel_jstree-0.3.2-py3-none-any.whl/panel_jstree/dist/lib/index.js
@@ -0,0 +1,8 @@
+// export {jsTreePlot} from "./models/jstree"
+// import {register_models} from "@bokehjs/base"
+// register_models(jsTreePlot as any)
+import * as PaneljsTree from "./bokeh_extensions/";
+export { PaneljsTree };
+import { register_models } from "@bokehjs/base";
+register_models(PaneljsTree);
+//# sourceMappingURL=index.js.map
