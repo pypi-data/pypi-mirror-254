3a8a52490fd9010588951ab0f8f8fa1d0639f062
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-17 23:47:07 +0000

    Add package configwidgets/1.0.0/configwidgets-1.0.0-py3-none-any.whl

---
+++ packages/configwidgets/configwidgets-1.0.0-py3-none-any.whl/configwidgets/__init__.py
@@ -0,0 +1,8 @@
+#
+__version__ = "1.0.0"
+
+
+from .buttons import ConfigCheckBox, ConfigRadioButton
+from .datetime import ConfigDateEdit, ConfigDateTimeEdit, ConfigTimeEdit
+from .text import ConfigLineEdit
+from .numbers import ConfigDoubleSpinBox, ConfigSpinBox
