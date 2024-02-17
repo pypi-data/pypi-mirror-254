381f2114a49ca8dfe5e0da58963899ccf4775d41
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-17 23:42:50 +0000

    Add package metagentools/0.0.10/metagentools-0.0.10.tar.gz

---
+++ packages/metagentools/metagentools-0.0.10.tar.gz/metagentools-0.0.10/metagentools/__init__.py
@@ -0,0 +1,8 @@
+__version__ = "0.0.10"
+
+# import os
+# from metagentools import __file__
+
+# from metagentools import __file__
+# ROOT = os.path.split(os.path.split(__file__)[0])[0]
+# os.environ['METAGENTOOLS_ROOT'] = ROOT
