763cc8eaf3efccedac933fcc89dd0888a70e3164
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-17 23:43:11 +0000

    Add package polylith-cli/1.1.2/polylith_cli-1.1.2-py3-none-any.whl

---
+++ packages/polylith-cli/polylith_cli-1.1.2-py3-none-any.whl/polylith_cli/polylith/commands/create.py
@@ -0,0 +1,13 @@
+from pathlib import Path
+from typing import Union
+from polylith_cli.polylith import repo, workspace
+
+def create(name: Union[str, None], description: Union[str, None], fn):
+    root = repo.get_workspace_root(Path.cwd())
+    namespace = workspace.parser.get_namespace_from_config(root)
+    if not name:
+        raise ValueError('Please add a name by using --name')
+    if not namespace:
+        raise ValueError("Didn't find a namespace. Expected to find it in workspace.toml.")
+    options = {'namespace': namespace, 'package': name, 'description': description, 'modulename': 'core'}
+    fn(root, options)
