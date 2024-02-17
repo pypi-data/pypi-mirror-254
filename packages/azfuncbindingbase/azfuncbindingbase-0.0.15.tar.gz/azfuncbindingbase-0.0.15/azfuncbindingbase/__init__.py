935ae08572e43869ce5e931951f17fe0e0338d4a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-17 23:35:22 +0000

    Add package azfuncbindingbase/0.0.15/azfuncbindingbase-0.0.15.tar.gz

---
+++ packages/azfuncbindingbase/azfuncbindingbase-0.0.15.tar.gz/azfuncbindingbase-0.0.15/azfuncbindingbase/__init__.py
@@ -0,0 +1,4 @@
+from .meta import Datum, _ConverterMeta, _BaseConverter, InConverter, OutConverter, get_binding_registry
+from .sdkType import SdkType
+
+__all__ = ['Datum', '_ConverterMeta', '_BaseConverter', 'InConverter', 'OutConverter', 'SdkType', 'get_binding_registry']
