e36ae45438f38721d169e483e6ceef2461f85903
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-17 23:53:02 +0000

    Add package nnetsauce/0.16.7/nnetsauce-0.16.7-py2.py3-none-any.whl

---
+++ packages/nnetsauce/nnetsauce-0.16.7-py2.py3-none-any.whl/nnetsauce/sampling/rowsubsampling.py
@@ -0,0 +1,65 @@
+import numpy as np
+from .helpers import dosubsample
+
+
+class SubSampler:
+    """Subsampling class.
+
+    Attributes:
+
+       y: array-like, shape = [n_samples]
+           Target values.
+
+       row_sample: double
+           subsampling fraction
+
+       n_samples: int
+            subsampling by using the number of rows (supersedes row_sample)
+
+       seed: int
+           reproductibility seed
+
+       n_jobs: int
+            number of jobs to run in parallel
+
+       verbose: bool
+            print progress messages and bars
+
+    Returns:
+
+        indices of subsampled y
+
+    """
+
+    def __init__(
+        self,
+        y,
+        row_sample=0.8,
+        n_samples=None,
+        seed=123,
+        n_jobs=None,
+        verbose=False,
+    ):
+        self.y = y
+        self.n_samples = n_samples
+        if n_samples is None:
+            assert row_sample < 1 and row_sample >= 0, "'row_sample' must be provided, plus < 1 and >= 0"
+            self.row_sample = row_sample
+        else:
+            assert n_samples < len(y), "'n_samples' must be < len(y)"
+            self.row_sample = n_samples / len(y)
+        self.seed = seed
+        self.indices = None
+        self.n_jobs = n_jobs
+        self.verbose = verbose
+
+    def subsample(self):
+        self.indices = dosubsample(
+            self.y,
+            self.row_sample,
+            #self.n_samples,
+            self.seed,
+            self.n_jobs,
+            self.verbose,
+        )
+        return self.indices
