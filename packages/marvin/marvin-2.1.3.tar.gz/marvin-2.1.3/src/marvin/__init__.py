3e530a1dd52d0c2052afa47bde225bd577c1aa1a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-17 23:34:37 +0000

    Add package marvin/2.1.3/marvin-2.1.3.tar.gz

---
+++ packages/marvin/marvin-2.1.3.tar.gz/marvin-2.1.3/src/marvin/__init__.py
@@ -0,0 +1,55 @@
+from .settings import settings
+
+from .ai.text import (
+    fn,
+    cast,
+    cast_async,
+    extract,
+    extract_async,
+    classify,
+    classify_async,
+    classifier,
+    generate,
+    generate_async,
+    model,
+    Model,
+)
+from .ai.images import paint, image
+from .ai.audio import speak, speech
+from . import beta
+
+try:
+    from ._version import version as __version__
+except ImportError:
+    __version__ = "unknown"
+
+
+__all__ = [
+    # --- text ---
+    "Model",
+    "cast",
+    "cast_async",
+    "classify",
+    "classify_async",
+    "classifier",
+    "extract",
+    "extract_async",
+    "fn",
+    "generate",
+    "generate_async",
+    "model",
+    # --- images ---
+    "image",
+    "paint",
+    # --- audio ---
+    "speak",
+    "speech",
+    # --- beta ---
+    "beta",
+]
+
+
+# compatibility with Marvin v1
+ai_fn = fn
+ai_model = model
+ai_classifier = classifier
