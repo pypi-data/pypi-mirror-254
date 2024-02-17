ac17846fc61f3705534c7c073e60eb5224938155
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-17 23:47:15 +0000

    Add package nnetsauce/0.16.6/nnetsauce-0.16.6.tar.gz

---
+++ packages/nnetsauce/nnetsauce-0.16.6.tar.gz/nnetsauce-0.16.6/nnetsauce/sampling/rowsubsampling.py
@@ -0,0 +1,59 @@
+from .helpers import dosubsample
+
+
+class SubSampler:
+    """Subsampling class.
+
+    Attributes:
+
+       y: array-like, shape = [n_samples]
+           Target values.
+
+       row_sample: double
+           subsampling fraction
+
+       n_samples: int
+            subsampling by using the number of rows
+
+       seed: int
+           reproductibility seed
+
+       n_jobs: int
+            number of jobs to run in parallel
+
+       verbose: bool
+            print progress messages and bars
+
+    Returns:
+
+        indices of subsampled y
+
+    """
+
+    def __init__(
+        self,
+        y,
+        row_sample=0.8,
+        n_samples=None,
+        seed=123,
+        n_jobs=None,
+        verbose=False,
+    ):
+        self.y = y
+        self.row_sample = row_sample
+        self.n_samples = n_samples
+        self.seed = seed
+        self.indices = None
+        self.n_jobs = n_jobs
+        self.verbose = verbose
+
+    def subsample(self):
+        self.indices = dosubsample(
+            self.y,
+            self.row_sample,
+            self.n_samples,
+            self.seed,
+            self.n_jobs,
+            self.verbose,
+        )
+        return self.indices
