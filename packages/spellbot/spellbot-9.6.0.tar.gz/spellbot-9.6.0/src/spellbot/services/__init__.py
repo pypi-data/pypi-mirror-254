d99f8266e45667bd7db83c053b1f5ca02030b734
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-17 23:37:52 +0000

    Add package spellbot/9.6.0/spellbot-9.6.0.tar.gz

---
+++ packages/spellbot/spellbot-9.6.0.tar.gz/spellbot-9.6.0/src/spellbot/services/__init__.py
@@ -0,0 +1,40 @@
+# pylint: disable=too-many-instance-attributes
+from __future__ import annotations
+
+from .awards import AwardsService, NewAward
+from .channels import ChannelsService
+from .configs import ConfigsService
+from .games import GamesService
+from .guilds import GuildsService
+from .plays import PlaysService
+from .users import UsersService
+from .verifies import VerifiesService
+from .watches import WatchesService
+
+
+class ServicesRegistry:
+    def __init__(self) -> None:
+        self.awards = AwardsService()
+        self.channels = ChannelsService()
+        self.configs = ConfigsService()
+        self.games = GamesService()
+        self.guilds = GuildsService()
+        self.plays = PlaysService()
+        self.users = UsersService()
+        self.verifies = VerifiesService()
+        self.watches = WatchesService()
+
+
+__all__ = [
+    "AwardsService",
+    "ChannelsService",
+    "ConfigsService",
+    "GamesService",
+    "GuildsService",
+    "NewAward",
+    "PlaysService",
+    "ServicesRegistry",
+    "UsersService",
+    "VerifiesService",
+    "WatchesService",
+]
