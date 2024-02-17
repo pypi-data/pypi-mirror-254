da6be138b800cab8ace68c29cf8a1940a6ed64fa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-17 23:52:10 +0000

    Add package mlflow-hf-transformers/0.5/mlflow_hf_transformers-0.5.tar.gz

---
+++ packages/mlflow-hf-transformers/mlflow_hf_transformers-0.5.tar.gz/mlflow_hf_transformers-0.5/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(name='mlflow_hf_transformers',
+      version='0.5',
+      description='Mlflow flavors for pytorch huggingface transformers models',
+          install_requires=[
+        "mlflow==2.2.2",
+        "transformers[torch]==4.36.2",
+    ],
+      keywords='mlflow huggingface transformers',
+      url='https://github.com/Warra07/mlflow-hf-transformers-flavor',
+      author='Wacim Belahcel',
+      author_email='wacimbelahcel@gmail.com',
+      license='Apache 2',
+      packages=['mlflow_hf_transformers'])
