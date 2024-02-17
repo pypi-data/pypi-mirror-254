f4eb20686dde325837ba6506982bc7b133f69653
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-17 23:43:56 +0000

    Add package synapse-token-authenticator/0.4.1/synapse_token_authenticator-0.4.1-py3-none-any.whl

---
+++ packages/synapse-token-authenticator/synapse_token_authenticator-0.4.1-py3-none-any.whl/synapse_token_authenticator/utils.py
@@ -0,0 +1,33 @@
+import requests
+from urllib.parse import urljoin
+from jwcrypto.jwk import JWKSet
+
+
+class OpenIDProviderMetadata:
+    """
+    Wrapper around OpenID Provider Metadata values
+    """
+
+    def __init__(self, issuer: str):
+        response = requests.get(
+            urljoin(issuer, "/.well-known/openid-configuration"), proxies={}
+        )
+        response.raise_for_status()
+
+        configuration = response.json()
+
+        self.issuer = issuer
+        self.introspection_endpoint: str = configuration["introspection_endpoint"]
+        self.jwks_uri: str = configuration["jwks_uri"]
+        self.id_token_signing_alg_values_supported: list[str] = configuration[
+            "id_token_signing_alg_values_supported"
+        ]
+
+    def jwks(self) -> JWKSet:
+        """
+        Signing keys used to validate signatures from the OpenID Provider
+        """
+        response = requests.get(self.jwks_uri, proxies={})
+        response.raise_for_status()
+
+        return JWKSet.from_json(response.text)
