22429ba8bdf3dcb1d3c57091145078c085bf9299
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-17 23:42:30 +0000

    Add package pixel-font-builder/0.0.15/pixel_font_builder-0.0.15.tar.gz

---
+++ packages/pixel-font-builder/pixel_font_builder-0.0.15.tar.gz/pixel_font_builder-0.0.15/examples/__init__.py
@@ -0,0 +1,5 @@
+import os
+
+project_root_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+glyphs_dir = os.path.join(project_root_dir, 'assets', 'glyphs')
+build_dir = os.path.join(project_root_dir, 'build')
