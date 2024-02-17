801bee050df60e57e5a0aac61b06c88be98c4df6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-17 23:52:27 +0000

    Add package rooms-shared-services/0.1.12/rooms_shared_services-0.1.12.tar.gz

---
+++ packages/rooms-shared-services/rooms_shared_services-0.1.12.tar.gz/rooms_shared_services-0.1.12/rooms_shared_services/src/logging/http_logger.py
@@ -0,0 +1,39 @@
+import logging
+from logging.handlers import HTTPHandler
+
+from rooms_shared_services.src.logging.settings import Settings
+
+settings = Settings()
+
+
+def get_logger(name: str, use_http: bool = False, use_stream: bool = True) -> logging.Logger:
+    """Get default logger.
+
+    Args:
+        name (str): _description_
+        use_http (bool): _description_. Defaults to False.
+        use_stream (bool): _description_. Defaults to True.
+
+    Returns:
+        logging.Logger: _description_
+    """
+    logger = logging.getLogger(name)
+    if use_http:
+        http_handler = HTTPHandler(
+            "cfribc1wdc.execute-api.us-east-1.amazonaws.com",
+            "/handle-logs",
+            method="POST",
+            secure=settings.secure,
+            context=None,
+        )
+        http_handler.setLevel(logging.INFO)
+        http_handle_format = logging.Formatter("%(levelname)s - %(message)s - %(asctime)s")
+        http_handler.setFormatter(http_handle_format)
+        logger.addHandler(http_handler)
+    if use_stream:
+        stream_handler = logging.StreamHandler()
+        stream_handler.setLevel(logging.INFO)
+        stream_handle_format = logging.Formatter("%(process)s - %(message)s")
+        stream_handler.setFormatter(stream_handle_format)
+        logger.addHandler(stream_handler)
+    return logger
