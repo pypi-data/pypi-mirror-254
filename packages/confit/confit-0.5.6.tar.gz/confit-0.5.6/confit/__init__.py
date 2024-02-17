f5dd394e2b24cb5c34346b2720d9e97022bdbe59
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-17 23:52:31 +0000

    Add package confit/0.5.6/confit-0.5.6.tar.gz

---
+++ packages/confit/confit-0.5.6.tar.gz/confit-0.5.6/confit/__init__.py
@@ -0,0 +1,15 @@
+from .cli import Cli
+from .config import Config
+from .registry import (
+    validate_arguments,
+    Registry,
+    get_default_registry,
+    set_default_registry,
+    RegistryCollection,
+    VisibleDeprecationWarning,
+)
+from .autoreload import autoreload_plugin
+
+__version__ = "0.5.6"
+
+autoreload_plugin()
