a27876fa023e6f3ce7c76c84626c7b81b7c6f2c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-17 23:38:33 +0000

    Add package slyp/0.2.1/slyp-0.2.1.tar.gz

---
+++ packages/slyp/slyp-0.2.1.tar.gz/slyp-0.2.1/CHANGELOG.md
@@ -0,0 +1,62 @@
+# Unreleased
+<!-- changelog-unreleased-marker -->
+
+# 0.2.1
+
+- Fix unnecessary paren fixer aggressively fixing Comparison nodes. Add this to
+  the set of nodes which retain their innermost parens.
+
+# 0.2.0
+
+- Improve handling on non-UTF8 files under `--use-git-ls`
+- Helptext (`slyp --help`) now does not list all linting codes. Use
+  `slyp --list` to view this data.
+- Introduce autofixer behavior. Fixing is always run before linting, so that
+  emitted lint errors are accurate to the fixed file.
+  - The first autofixer has been added: remove unnecessary parentheses for
+    expressions wrapped in multiple parentheses.
+
+# 0.1.2
+
+- Add `W120` for catching unparenthesized multiline annotations on parameters
+
+# 0.1.1
+
+- If CST traversal hits recursion depth, this is now reported as an internal
+  error without aborting the entire run of `slyp`
+- `W103` now ignores a multiline string join which is the only string inside
+  of a container type.
+
+# 0.1.0
+
+- `--disable` and `--enable` now support the special string "all" to refer to
+  all codes. Explicit enables and disables are given higher precedence than "all".
+- Add `E110` for catching the return of a known-`None` variable immediately
+  after testing it (prefer to return `None`, not the variable name)
+- `--disable` and `--disable` now support categories, as in `--disable W`
+  to disable all warnings.
+- The `E101`, `E102`, and `E103` codes have each been downgraded to warnings
+  (`W` category) but are still enabled by default
+- The matching AST checker now considers the triviality and proximity of
+  matching AST nodes. By default, only the check for non-trivial adjacent
+  branches is enabled.
+  - Add `W201`, `W202`, and `W203` to describe the disabled checks
+  - Triviality is defined as a heuristic which captures simple expressions and
+    statements (e.g. `return None` is a trivial statement)
+- Add support for `--enable` to turn on disabled codes
+
+# 0.0.3
+
+- Add `--disable` for turning off specific codes
+- Various minor speed improvements, resulting in ~5% faster runs on large
+  codebases
+- Invert verbosity control by removing `-q/--quiet` and adding `-v/--verbose`.
+  Verbose output is now opt-in, not opt-out.
+
+# 0.0.2
+
+- Add pre-commit-hooks config, allowing use with pre-commit
+
+# 0.0.1
+
+- Initial release
---
+++ packages/slyp/slyp-0.2.1.tar.gz/slyp-0.2.1/src/slyp/fixers/unnecessary_parens.py
@@ -0,0 +1,104 @@
+from __future__ import annotations
+
+import typing as t
+
+import libcst
+import libcst.matchers
+
+# alphabetical order
+NODE_TYPES = t.Union[
+    libcst.Attribute,
+    libcst.Await,
+    libcst.BinaryOperation,
+    libcst.Call,
+    libcst.Comparison,
+    libcst.ConcatenatedString,
+    libcst.Dict,
+    libcst.DictComp,
+    libcst.Ellipsis,
+    libcst.Float,
+    libcst.FormattedString,
+    libcst.GeneratorExp,
+    libcst.IfExp,
+    libcst.Imaginary,
+    libcst.Integer,
+    libcst.Lambda,
+    libcst.List,
+    libcst.ListComp,
+    libcst.Name,
+    libcst.Set,
+    libcst.SetComp,
+    libcst.SimpleString,
+    libcst.StarredElement,
+    libcst.Subscript,
+    libcst.Tuple,
+    libcst.UnaryOperation,
+    libcst.Yield,
+]
+MATCH_ON = (
+    getattr(libcst.matchers, type_.__name__)() for type_ in t.get_args(NODE_TYPES)
+)
+
+# The innermost parens should not be removed in some cases, as they are
+# required in some contexts.
+PRESERVE_INNERMOST_PAREN_TYPES: tuple[type[libcst.CSTNode], ...] = (
+    # list of tuples:
+    #   [(1, 2), (3, 4)] != [1, 2, 3, 4]
+    libcst.Tuple,
+    # generator expression passed to a function call:
+    #   foo((x % 2 for x in range(10)), bar)
+    libcst.GeneratorExp,
+    # parens define precedence:
+    #   (1 + 2) * 3 != 1 + 2 * 3
+    libcst.BinaryOperation,
+    libcst.UnaryOperation,
+    # precedence also for comparisons:
+    #   (x is y) in truefalsecontainer
+    libcst.Comparison,
+    # parens are required for usage of the results:
+    #   (foo() if x else bar())["baz"]
+    #   (await foo())["bar"]
+    libcst.IfExp,
+    libcst.Await,
+    # direct access to attributes of integers and floats requires parens:
+    #   (1).bit_length()
+    libcst.Integer,
+    libcst.Float,
+)
+
+
+class UnnecessaryParenthesesFixer(libcst.matchers.MatcherDecoratableTransformer):
+    METADATA_DEPENDENCIES = (libcst.metadata.PositionProvider,)
+
+    def _how_many_parens_same_line(self, node: libcst.CSTNode) -> int:
+        lpar, rpar = node.lpar, node.rpar  # type: ignore[attr-defined]
+        if not lpar:
+            return 0
+
+        max_offset = -1
+        for offset in range(len(lpar)):
+            lpar_line = self.get_metadata(
+                libcst.metadata.PositionProvider, lpar[-(offset + 1)]
+            ).start.line
+            rpar_line = self.get_metadata(
+                libcst.metadata.PositionProvider, rpar[offset]
+            ).start.line
+
+            if lpar_line != rpar_line:
+                break
+            max_offset = offset
+        return max_offset + 1
+
+    @libcst.matchers.leave(libcst.matchers.OneOf(*MATCH_ON))
+    def modify_parenthesized_node(
+        self, original_node: NODE_TYPES, updated_node: NODE_TYPES
+    ) -> NODE_TYPES:
+        num_parens_to_unwrap = self._how_many_parens_same_line(original_node)
+        if isinstance(original_node, PRESERVE_INNERMOST_PAREN_TYPES):
+            num_parens_to_unwrap -= 1
+        if num_parens_to_unwrap <= 0:
+            return updated_node
+        return updated_node.with_changes(
+            lpar=updated_node.lpar[:-num_parens_to_unwrap],
+            rpar=updated_node.rpar[num_parens_to_unwrap:],
+        )
