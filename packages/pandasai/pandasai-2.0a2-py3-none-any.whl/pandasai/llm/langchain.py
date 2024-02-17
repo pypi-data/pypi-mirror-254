0aebb18f0b77a3c860b93823d68fd61a9bc66c28
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-17 23:52:21 +0000

    Add package pandasai/2.0a2/pandasai-2.0a2-py3-none-any.whl

---
+++ packages/pandasai/pandasai-2.0a2-py3-none-any.whl/pandasai/llm/langchain.py
@@ -0,0 +1,35 @@
+from pandasai.prompts.base import BasePrompt
+from .base import LLM
+
+"""Langchain LLM 
+
+This module is to run LLM using LangChain framework.
+
+Example:
+    Use below example to call LLM
+    >>> from pandasai.llm.langchain import LangchainLLm
+"""
+
+
+class LangchainLLM(LLM):
+    """
+    Class to wrap Langchain LLMs and make PandasAI interoperable
+    with LangChain.
+    """
+
+    langchain_llm = None
+
+    def __init__(self, langchain_llm):
+        self.langchain_llm = langchain_llm
+
+    def call(self, instruction: BasePrompt, suffix: str = "") -> str:
+        prompt = instruction.to_string() + suffix
+        return self.langchain_llm.predict(prompt)
+
+    @staticmethod
+    def is_langchain_llm(llm: LLM) -> bool:
+        return hasattr(llm, "_llm_type")
+
+    @property
+    def type(self) -> str:
+        return f"langchain_{self.langchain_llm._llm_type}"
