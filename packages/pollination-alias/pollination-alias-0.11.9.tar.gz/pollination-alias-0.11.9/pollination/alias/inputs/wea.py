6e0b04bb2c0fd784c3a344adc836e9fda9b24bf0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-17 23:46:49 +0000

    Add package pollination-alias/0.11.9/pollination-alias-0.11.9.tar.gz

---
+++ packages/pollination-alias/pollination-alias-0.11.9.tar.gz/pollination-alias-0.11.9/pollination/alias/inputs/wea.py
@@ -0,0 +1,38 @@
+from pollination_dsl.alias import InputAlias
+from queenbee.io.common import IOAliasHandler
+
+
+"""Alias for inputs that expect a .wea file as the recipe input."""
+wea_input = [
+    InputAlias.any(
+        name='wea',
+        description='A Wea object produced from the Wea components that are under '
+        'the Light Sources tab. This can also be the path to a .wea or a .epw file.',
+        platform=['grasshopper', 'rhino', 'revit'],
+        handler=[
+            IOAliasHandler(
+                language='python',
+                module='pollination_handlers.inputs.wea',
+                function='wea_handler'
+            )
+        ]
+    )
+]
+
+
+"""Alias for inputs that expect a .wea file at a timestep of 1."""
+wea_input_timestep_check = [
+    InputAlias.any(
+        name='wea',
+        description='A Wea object produced from the Wea components that are under '
+        'the Light Sources tab. This can also be the path to a .wea or a .epw file.',
+        platform=['grasshopper', 'rhino', 'revit'],
+        handler=[
+            IOAliasHandler(
+                language='python',
+                module='pollination_handlers.inputs.wea',
+                function='wea_handler_timestep_check'
+            )
+        ]
+    )
+]
