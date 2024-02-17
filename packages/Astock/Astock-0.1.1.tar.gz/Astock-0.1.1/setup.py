7ff8144bd326cf0b40b7efb481cf7432fa1938ce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-17 23:39:05 +0000

    Add package Astock/0.1.1/Astock-0.1.1.tar.gz

---
+++ packages/Astock/Astock-0.1.1.tar.gz/Astock-0.1.1/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+setup(
+    name = 'Astock',
+    version= '0.1.1',
+    description='stock data analyse',
+    author='Jopil',
+    author_email='jopil@163.com',
+    classifiers=['Programming Language :: Python'],
+    license='',
+    install_requires=['pandas>=1.3.5','numpy>=1.21.5','xlrd>=2.0.1'],
+    packages=find_packages(),
+    # packages=find_packages(where='library_manager'),
+    # package_dir={'': 'library_lanager'},
+    include_package_data=True
+    # package_data={'':['*.csv','*.xlsx','*.xls']}
+    
+)
