601efae26491812b7c9e8ec53948f81e8453ae0c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-17 23:37:39 +0000

    Add package pyBrainGenixClient/2024.1.30.1/pyBrainGenixClient-2024.1.30.1-py3-none-any.whl

---
+++ packages/pyBrainGenixClient/pyBrainGenixClient-2024.1.30.1-py3-none-any.whl/BrainGenix/NES/Models/Connections/Receptor/Configuration.py
@@ -0,0 +1,16 @@
+# BrainGenix-NES
+# AGPLv3
+
+
+class Configuration():
+
+    def __init__(self):
+        
+        # Create Attributes
+        self.Name:str = None
+        self.SourceCompartment = None
+        self.DestinationCompartment = None
+        self.Conductance_nS:float = None
+        self.TimeConstant_ms:float = None
+        self.ReceptorLocation_um:list = None
+    
