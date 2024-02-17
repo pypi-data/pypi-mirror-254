a263e59b721f43102112f820c8a61d22aa412f38
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-17 23:41:45 +0000

    Add package banglanlptoolkit/1.1.7.dev1/banglanlptoolkit-1.1.7.dev1.tar.gz

---
+++ packages/banglanlptoolkit/banglanlptoolkit-1.1.7.dev1.tar.gz/banglanlptoolkit-1.1.7.dev1/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+VERSION = '1.1.7.dev1' 
+DESCRIPTION = 'Toolkits for text processing and augmentation for Bangla NLP'
+
+setup(
+        name="banglanlptoolkit", 
+        version=VERSION,
+        
+        author="A F M Mahfuzul Kabir",
+        author_email="<afmmahfuzulkabir@gmail.com>",
+        description=DESCRIPTION,
+        long_description=open('README.md').read(),
+        long_description_content_type='text/markdown',
+        packages=find_packages(),
+        
+        classifiers = [
+        "Intended Audience :: Education",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3.8"
+        ],
+        
+        keywords=['Bangla','NLP','toolkit','punctuation','augmentation','normalizer'],
+        
+        url='https://github.com/Kabir5296/banglanlptoolkit',
+)
