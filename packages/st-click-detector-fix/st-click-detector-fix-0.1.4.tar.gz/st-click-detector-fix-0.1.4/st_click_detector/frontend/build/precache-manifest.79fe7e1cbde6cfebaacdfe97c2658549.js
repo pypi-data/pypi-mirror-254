d0c1b35c79944950cda2806cc4105c55d2583e4c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-17 23:58:08 +0000

    Add package st-click-detector-fix/0.1.4/st-click-detector-fix-0.1.4.tar.gz

---
+++ packages/st-click-detector-fix/st-click-detector-fix-0.1.4.tar.gz/st-click-detector-fix-0.1.4/st_click_detector/frontend/build/precache-manifest.79fe7e1cbde6cfebaacdfe97c2658549.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ebc415b3385a182d80cee9006fb63797",
+    "url": "./index.html"
+  },
+  {
+    "revision": "ebd8aae00e5e8690e520",
+    "url": "./static/js/2.ef58ee94.chunk.js"
+  },
+  {
+    "revision": "ea9e8a04b4530945f0b69f09f74b01e4",
+    "url": "./static/js/2.ef58ee94.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "89bb80afc1360a9e9a34",
+    "url": "./static/js/main.b653c345.chunk.js"
+  },
+  {
+    "revision": "7e9d84e346ce158d1e50",
+    "url": "./static/js/runtime-main.44d30fc2.js"
+  }
+]);
