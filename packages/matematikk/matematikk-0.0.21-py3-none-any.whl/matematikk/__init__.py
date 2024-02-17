20f4cbab35476c1a7345e20b914ea113f6dd28a7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-17 23:53:01 +0000

    Add package matematikk/0.0.21/matematikk-0.0.21-py3-none-any.whl

---
+++ packages/matematikk/matematikk-0.0.21-py3-none-any.whl/matematikk/__init__.py
@@ -0,0 +1,29 @@
+# overskudd_max()
+from .overskudd_max_fil import (
+    tmp,
+    diff)
+from .overskudd_maks_fil import (
+    tmp,
+    diff)
+
+# reggis()
+from .reggis_fil import (
+    reggis)
+from .reggis_cas_fil import (
+    reggis_cas)
+
+# superlos()
+from .superlos_fil import (
+    superløs)
+from .superlos_fil import (
+    Eq,
+    Reals,
+    core,
+    Symbol,
+    ConditionSet,
+    FiniteSet,
+    Intersection,
+    solve,
+    solveset,
+    nsolve)
+
