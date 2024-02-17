844dbc11b3f74fcce2c1f5e1a5967a6c4e38488f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-17 23:53:32 +0000

    Add package polycon/2024.1.31.0/polycon-2024.1.31.0-py3-none-any.whl

---
+++ packages/polycon/polycon-2024.1.31.0-py3-none-any.whl/polycon/__init__.py
@@ -0,0 +1,13 @@
+# cpp source directory
+# def add_cpp_flags():
+#     from pathlib import Path
+#     import vfs
+#
+#     vfs.add_global_cpp_flag( "-I" + str( Path( __file__ ).parents[ 3 ].joinpath( 'src', 'cpp' ) ) )
+#     vfs.add_global_cpp_flag( "--cpp-flag=-O3" )
+#
+# add_cpp_flags()
+#
+# # exports
+# from .ConvexFunction import ConvexFunction
+# from PowerDiagram import PowerDiagram
