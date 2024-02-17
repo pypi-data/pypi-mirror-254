b5cc5a82d625d2e81f25c021d83460e284db4616
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-17 23:56:52 +0000

    Add package geoh5py/0.9.0a1/geoh5py-0.9.0a1.tar.gz

---
+++ packages/geoh5py/geoh5py-0.9.0a1.tar.gz/geoh5py-0.9.0a1/geoh5py/data/data.py
@@ -0,0 +1,251 @@
+#  Copyright (c) 2024 Mira Geoscience Ltd.
+#
+#  This file is part of geoh5py.
+#
+#  geoh5py is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU Lesser General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  geoh5py is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU Lesser General Public License for more details.
+#
+#  You should have received a copy of the GNU Lesser General Public License
+#  along with geoh5py.  If not, see <https://www.gnu.org/licenses/>.
+
+from __future__ import annotations
+
+from abc import abstractmethod
+
+import numpy as np
+
+from ..shared import Entity
+from ..shared.utils import mask_by_extent
+from .data_association_enum import DataAssociationEnum
+from .data_type import DataType
+from .primitive_type_enum import PrimitiveTypeEnum
+
+
+class Data(Entity):
+    """
+    Base class for Data entities.
+    """
+
+    _attribute_map = Entity._attribute_map.copy()
+    _attribute_map.update({"Association": "association", "Modifiable": "modifiable"})
+    _visible = False
+
+    def __init__(
+        self,
+        data_type: DataType,
+        **kwargs,
+    ):
+        self._association = None
+        self._on_file = False
+        self._modifiable = True
+
+        if (
+            not isinstance(data_type, DataType)
+            or data_type.primitive_type != self.primitive_type()
+        ):
+            raise TypeError(
+                "Input 'data_type' must be a DataType object of primitive_type 'TEXT'."
+            )
+
+        self.entity_type = data_type
+        self._values = None
+
+        super().__init__(**kwargs)
+
+        if self.entity_type.name == "Entity":
+            self.entity_type.name = self.name
+
+        data_type.workspace._register_data(self)
+
+    def copy(
+        self,
+        parent=None,
+        copy_children: bool = True,
+        clear_cache: bool = False,
+        mask: np.ndarray | None = None,
+        **kwargs,
+    ) -> Data:
+        """
+        Function to copy data to a different parent entity.
+
+        :param parent: Target parent to copy the entity under. Copied to current
+            :obj:`~geoh5py.shared.entity.Entity.parent` if None.
+        :param copy_children: (Optional) Create copies of all children entities along with it.
+        :param clear_cache: Clear array attributes after copy.
+        :param mask: Array of bool defining the values to keep.
+        :param kwargs: Additional keyword arguments to pass to the copy constructor.
+
+        :return entity: Registered Entity to the workspace.
+        """
+        if parent is None:
+            parent = self.parent
+
+        if self.values is not None and mask is not None:
+            if not isinstance(mask, np.ndarray):
+                raise TypeError("Mask must be an array or None.")
+
+            if mask.dtype != bool or mask.shape != self.values.shape:
+                raise ValueError(
+                    f"Mask must be a boolean array of shape {self.values.shape}, not {mask.shape}"
+                )
+
+            n_values = (
+                parent.n_cells
+                if self.association is DataAssociationEnum.CELL
+                else parent.n_vertices
+            )
+
+            if n_values < self.values.shape[0]:
+                kwargs.update({"values": self.values[mask]})
+            else:
+                values = np.ones_like(self.values) * self.nan_value
+                values[mask] = self.values[mask]
+
+                kwargs.update({"values": values})
+
+        new_entity = parent.workspace.copy_to_parent(
+            self,
+            parent,
+            clear_cache=clear_cache,
+            **kwargs,
+        )
+
+        return new_entity
+
+    @property
+    def extent(self) -> np.ndarray | None:
+        """
+        Geography bounding box of the parent object.
+
+        :return: shape(2, 3) Bounding box defined by the bottom South-West and
+            top North-East coordinates.
+        """
+        return None
+
+    @property
+    def n_values(self) -> int | None:
+        """
+        :obj:`int`: Number of expected data values based on
+        :obj:`~geoh5py.data.data.Data.association`
+        """
+        if self.association is DataAssociationEnum.VERTEX:
+            return self.parent.n_vertices
+        if self.association is DataAssociationEnum.DEPTH:
+            return self.parent.n_vertices
+        if self.association is DataAssociationEnum.CELL:
+            return self.parent.n_cells
+        if self.association is DataAssociationEnum.FACE:
+            return self.parent.n_faces
+        if self.association is DataAssociationEnum.OBJECT:
+            return 1
+
+        return None
+
+    @property
+    def nan_value(self) -> None:
+        """
+        Value used to represent missing data in python.
+        """
+        return None
+
+    @property
+    def values(self):
+        """
+        Data values
+        """
+        return self._values
+
+    @property
+    def association(self) -> DataAssociationEnum | None:
+        """
+        :obj:`~geoh5py.data.data_association_enum.DataAssociationEnum`:
+        Relationship made between the
+        :func:`~geoh5py.data.data.Data.values` and elements of the
+        :obj:`~geoh5py.shared.entity.Entity.parent` object.
+        Association can be set from a :obj:`str` chosen from the list of available
+        :obj:`~geoh5py.data.data_association_enum.DataAssociationEnum` options.
+        """
+        return self._association
+
+    @association.setter
+    def association(self, value: str | DataAssociationEnum):
+        if isinstance(value, str):
+            if value.upper() not in DataAssociationEnum.__members__:
+                raise ValueError(
+                    f"Association flag should be one of {DataAssociationEnum.__members__}"
+                )
+
+            value = getattr(DataAssociationEnum, value.upper())
+
+        if not isinstance(value, DataAssociationEnum):
+            raise TypeError(f"Association must be of type {DataAssociationEnum}")
+
+        self._association = value
+
+    @property
+    def modifiable(self) -> bool:
+        """
+        :obj:`bool` Entity can be modified.
+        """
+        return self._modifiable
+
+    @modifiable.setter
+    def modifiable(self, value: bool):
+        self._modifiable = value
+        self.workspace.update_attribute(self, "attributes")
+
+    @property
+    def entity_type(self) -> DataType:
+        """
+        :obj:`~geoh5py.data.data_type.DataType`
+        """
+        return self._entity_type
+
+    @entity_type.setter
+    def entity_type(self, data_type: DataType):
+        self._entity_type = data_type
+        self.workspace.update_attribute(self, "entity_type")
+
+    @classmethod
+    @abstractmethod
+    def primitive_type(cls) -> PrimitiveTypeEnum:
+        ...
+
+    def add_file(self, file: str):
+        """
+        Alias not implemented from base Entity class.
+        """
+        raise NotImplementedError("Data entity cannot contain files.")
+
+    def mask_by_extent(
+        self, extent: np.ndarray, inverse: bool = False
+    ) -> np.ndarray | None:
+        """
+        Sub-class extension of :func:`~geoh5py.shared.entity.Entity.mask_by_extent`.
+
+        Uses the parent object's vertices or centroids coordinates.
+        """
+        if self.association is DataAssociationEnum.VERTEX:
+            return mask_by_extent(self.parent.vertices, extent, inverse=inverse)
+
+        if self.association is DataAssociationEnum.CELL:
+            if getattr(self.parent, "centroids", None) is not None:
+                return mask_by_extent(self.parent.centroids, extent, inverse=inverse)
+
+            indices = mask_by_extent(self.parent.vertices, extent, inverse=inverse)
+            if indices is not None:
+                indices = np.all(indices[self.parent.cells], axis=1)
+
+            return indices
+
+        return None
+
+    def __call__(self):
+        return self.values
---
+++ packages/geoh5py/geoh5py-0.9.0a1.tar.gz/geoh5py-0.9.0a1/geoh5py/shared/entity.py
@@ -0,0 +1,529 @@
+#  Copyright (c) 2024 Mira Geoscience Ltd.
+#
+#  This file is part of geoh5py.
+#
+#  geoh5py is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU Lesser General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  geoh5py is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU Lesser General Public License for more details.
+#
+#  You should have received a copy of the GNU Lesser General Public License
+#  along with geoh5py.  If not, see <https://www.gnu.org/licenses/>.
+
+# pylint: disable=R0904
+
+from __future__ import annotations
+
+import uuid
+import warnings
+from abc import ABC, abstractmethod
+from pathlib import Path
+from typing import TYPE_CHECKING
+
+import numpy as np
+
+from geoh5py.shared.utils import str2uuid
+
+if TYPE_CHECKING:
+    from numpy import ndarray
+
+    from .. import shared
+    from ..groups import PropertyGroup
+    from ..workspace import Workspace
+
+DEFAULT_CRS = {"Code": "Unknown", "Name": "Unknown"}
+
+
+class Entity(ABC):
+    """
+    Base Entity class
+    """
+
+    _attribute_map: dict = {
+        "Allow delete": "allow_delete",
+        "Allow move": "allow_move",
+        "Allow rename": "allow_rename",
+        "Clipping IDs": "clipping_ids: list | None",
+        "ID": "uid",
+        "Name": "name",
+        "Partially hidden": "partially_hidden",
+        "Public": "public",
+        "Visible": "visible",
+    }
+    _visible = True
+
+    def __init__(self, uid: uuid.UUID | None = None, name="Entity", **kwargs):
+        self._uid = (
+            str2uuid(uid) if isinstance(str2uuid(uid), uuid.UUID) else uuid.uuid4()
+        )
+        self._name = name
+        self._parent: Entity | None = None
+        self._children: list = []
+        self._allow_delete = True
+        self._allow_move = True
+        self._allow_rename = True
+        self._partially_hidden = False
+        self._clipping_ids: list[uuid.UUID] | None = None
+        self._public = True
+        self._on_file = False
+        self._metadata: dict | None = None
+
+        for attr, item in kwargs.items():
+            try:
+                if attr in self._attribute_map:
+                    attr = self._attribute_map[attr]
+                setattr(self, attr, item)
+            except AttributeError:
+                continue
+
+    def add_children(self, children: list[shared.Entity]):
+        """
+        :param children: Add a list of entities as
+            :obj:`~geoh5py.shared.entity.Entity.children`
+        """
+        for child in children:
+            if child not in self._children:
+                self._children.append(child)
+
+    def add_file(self, file: str):
+        """
+        Add a file to the object or group stored as bytes on a FilenameData
+
+        :param file: File name with path to import.
+        """
+        if not Path(file).is_file():
+            raise ValueError(f"Input file '{file}' does not exist.")
+
+        with open(file, "rb") as raw_binary:
+            blob = raw_binary.read()
+
+        name = Path(file).name
+        attributes = {
+            "name": name,
+            "file_name": name,
+            "association": "OBJECT",
+            "parent": self,
+            "values": blob,
+        }
+        entity_type = {"name": "UserFiles", "primitive_type": "FILENAME"}
+
+        file_data = self.workspace.create_entity(
+            None, entity=attributes, entity_type=entity_type
+        )
+
+        return file_data
+
+    @property
+    def allow_delete(self) -> bool:
+        """
+        :obj:`bool` Entity can be deleted from the workspace.
+        """
+        return self._allow_delete
+
+    @allow_delete.setter
+    def allow_delete(self, value: bool):
+        self._allow_delete = value
+        self.workspace.update_attribute(self, "attributes")
+
+    @property
+    def allow_move(self) -> bool:
+        """
+        :obj:`bool` Entity can change :obj:`~geoh5py.shared.entity.Entity.parent`
+        """
+        return self._allow_move
+
+    @allow_move.setter
+    def allow_move(self, value: bool):
+        self._allow_move = value
+        self.workspace.update_attribute(self, "attributes")
+
+    @property
+    def allow_rename(self) -> bool:
+        """
+        :obj:`bool` Entity can change name
+        """
+        return self._allow_rename
+
+    @allow_rename.setter
+    def allow_rename(self, value: bool):
+        self._allow_rename = value
+        self.workspace.update_attribute(self, "attributes")
+
+    @property
+    def attribute_map(self) -> dict:
+        """
+        :obj:`dict` Correspondence map between property names used in geoh5py and
+        geoh5.
+        """
+        return self._attribute_map
+
+    @property
+    def children(self):
+        """
+        :obj:`list` Children entities in the workspace tree
+        """
+        return self._children
+
+    @property
+    def clipping_ids(self) -> list[uuid.UUID] | None:
+        """
+        List of clipping uuids
+        """
+        return self._clipping_ids
+
+    @abstractmethod
+    def mask_by_extent(
+        self, extent: np.ndarray, inverse: bool = False
+    ) -> np.ndarray | None:
+        """
+        Get a mask array from coordinate extent.
+
+        :param extent: Bounding box extent coordinates defined by either:
+            - obj:`numpy.ndarray` of shape (2, 3)
+            3D coordinate: [[west, south, bottom], [east, north, top]]
+            - obj:`numpy.ndarray` of shape (2, 2)
+            Horizontal coordinates: [[west, south], [east, north]].
+        :param inverse: Return the complement of the mask extent. Default to False
+
+        :return: Array of bool defining the vertices or cell centers
+            within the mask extent, or None if no intersection.
+        """
+
+    @classmethod
+    def create(cls, workspace, **kwargs):
+        """
+        Function to create an entity.
+
+        :param workspace: Workspace to be added to.
+        :param kwargs: List of keyword arguments defining the properties of a class.
+
+        :return entity: Registered Entity to the workspace.
+        """
+        entity_type_kwargs = (
+            {"entity_type": {"uid": kwargs["entity_type_uid"]}}
+            if "entity_type_uid" in kwargs
+            else {}
+        )
+        entity_kwargs = {"entity": kwargs}
+        new_object = workspace.create_entity(
+            cls,
+            **{**entity_kwargs, **entity_type_kwargs},
+        )
+        return new_object
+
+    @property
+    def coordinate_reference_system(self) -> dict:
+        """
+        Coordinate reference system attached to the entity.
+        """
+        coordinate_reference_system = DEFAULT_CRS
+
+        if self.metadata is not None and "Coordinate Reference System" in self.metadata:
+            coordinate_reference_system = self.metadata[
+                "Coordinate Reference System"
+            ].get("Current", DEFAULT_CRS)
+
+        return coordinate_reference_system
+
+    @coordinate_reference_system.setter
+    def coordinate_reference_system(self, value: dict):
+        # assert value is a dictionary containing "Code" and "Name" keys
+        if not isinstance(value, dict):
+            raise TypeError("Input coordinate reference system must be a dictionary")
+
+        if value.keys() != {"Code", "Name"}:
+            raise KeyError(
+                "Input coordinate reference system must only contain a 'Code' and 'Name' keys"
+            )
+
+        # get the actual coordinate reference system
+        coordinate_reference_system = {
+            "Current": value,
+            "Previous": self.coordinate_reference_system,
+        }
+
+        # update the metadata
+        metadata = self.metadata
+        if isinstance(metadata, dict):
+            metadata["Coordinate Reference System"] = coordinate_reference_system
+        else:
+            metadata = {"Coordinate Reference System": coordinate_reference_system}
+
+        self.metadata = metadata
+
+    @abstractmethod
+    def copy(
+        self,
+        parent=None,
+        copy_children: bool = True,
+        clear_cache: bool = False,
+        mask: np.ndarray | None = None,
+        **kwargs,
+    ):
+        """
+        Function to copy an entity to a different parent entity.
+
+        :param parent: Target parent to copy the entity under. Copied to current
+            :obj:`~geoh5py.shared.entity.Entity.parent` if None.
+        :param copy_children: (Optional) Create copies of all children entities along with it.
+        :param clear_cache: Clear array attributes after copy to minimize the
+            memory footprint of the workspace.
+        :param mask: Array of indices to sub-sample the input entity.
+        :param kwargs: Additional keyword arguments to pass to the copy constructor.
+
+        :return entity: Registered Entity to the workspace.
+        """
+
+    def copy_from_extent(
+        self,
+        extent: ndarray,
+        parent=None,
+        copy_children: bool = True,
+        clear_cache: bool = False,
+        inverse: bool = False,
+        **kwargs,
+    ) -> Entity | None:
+        """
+        Function to copy an entity to a different parent entity.
+
+        :param extent: Bounding box extent requested for the input entity, as supplied for
+            :func:`~geoh5py.shared.entity.Entity.mask_by_extent`.
+        :param parent: Target parent to copy the entity under. Copied to current
+            :obj:`~geoh5py.shared.entity.Entity.parent` if None.
+        :param copy_children: (Optional) Create copies of all children entities along with it.
+        :param clear_cache: Clear array attributes after copy.
+        :param inverse: Keep the inverse (clip) of the extent selection.
+        :param kwargs: Additional keyword arguments to pass to the copy constructor.
+
+        :return entity: Registered Entity to the workspace.
+        """
+        indices = self.mask_by_extent(extent, inverse=inverse)
+        if indices is None:
+            return None
+
+        return self.copy(
+            parent=parent,
+            copy_children=copy_children,
+            clear_cache=clear_cache,
+            mask=indices,
+            **kwargs,
+        )
+
+    @property
+    @abstractmethod
+    def entity_type(self) -> shared.EntityType:
+        ...
+
+    @classmethod
+    def fix_up_name(cls, name: str) -> str:
+        """If the given  name is not a valid one, transforms it to make it valid
+        :return: a valid name built from the given name. It simply returns the given name
+        if it was already valid.
+        """
+        # TODO: implement an actual fixup
+        #  (possibly it has to be abstract with different implementations per Entity type)
+        return name
+
+    def get_entity(self, name: str | uuid.UUID) -> list[Entity | None]:
+        """
+        Get a child :obj:`~geoh5py.data.data.Data` by name.
+
+        :param name: Name of the target child data
+        :param entity_type: Sub-select entities based on type.
+        :return: A list of children Data objects
+        """
+
+        if isinstance(name, uuid.UUID):
+            entity_list = [child for child in self.children if child.uid == name]
+        else:
+            entity_list = [child for child in self.children if child.name == name]
+
+        if not entity_list:
+            return [None]
+
+        return entity_list
+
+    def get_entity_list(self, entity_type=ABC) -> list[str]:
+        """
+        Get a list of names of all children :obj:`~geoh5py.data.data.Data`.
+
+        :param entity_type: Option to sub-select based on type.
+        :return: List of names of data associated with the object.
+        """
+        name_list = [
+            child.name for child in self.children if isinstance(child, entity_type)
+        ]
+        return sorted(name_list)
+
+    @property
+    def metadata(self) -> dict | None:
+        """
+        Metadata attached to the entity.
+        """
+        if getattr(self, "_metadata", None) is None:
+            self._metadata = self.workspace.fetch_metadata(self.uid)
+
+        return self._metadata
+
+    @metadata.setter
+    def metadata(self, value: dict | None):
+        if value is not None:
+            assert isinstance(
+                value, (dict, str)
+            ), f"Input metadata must be of type {dict} or None"
+        self._metadata = value
+        self.workspace.update_attribute(self, "metadata")
+
+    @property
+    def name(self) -> str:
+        """
+        :obj:`str` Name of the entity
+        """
+        return self._name
+
+    @name.setter
+    def name(self, new_name: str):
+        self._name = self.fix_up_name(new_name)
+        self.workspace.update_attribute(self, "attributes")
+
+    @property
+    def on_file(self) -> bool:
+        """
+        Whether this Entity is already stored on
+        :obj:`~geoh5py.workspace.workspace.Workspace.h5file`.
+        """
+        return self._on_file
+
+    @on_file.setter
+    def on_file(self, value: bool):
+        self._on_file = value
+
+    @property
+    def parent(self):
+        return self._parent
+
+    @parent.setter
+    def parent(self, parent: shared.Entity):
+        current_parent = self._parent
+
+        if parent is not None:
+            parent.add_children([self])
+            self._parent = parent
+
+            if current_parent is not None and current_parent != self._parent:
+                current_parent.remove_children([self])
+                self.workspace.save_entity(self)
+
+    @property
+    def partially_hidden(self) -> bool:
+        """
+        Whether this Entity is partially hidden.
+        """
+        return self._partially_hidden
+
+    @partially_hidden.setter
+    def partially_hidden(self, value: bool):
+        self._partially_hidden = value
+        self.workspace.update_attribute(self, "attributes")
+
+    @property
+    def public(self) -> bool:
+        """
+        Whether this Entity is accessible in the workspace tree and other parts
+            of the the user interface in ANALYST.
+        """
+        return self._public
+
+    @public.setter
+    def public(self, value: bool):
+        self._public = value
+        self.workspace.update_attribute(self, "attributes")
+
+    def reference_to_uid(
+        self, value: Entity | PropertyGroup | str | uuid.UUID
+    ) -> list[uuid.UUID]:
+        """
+        General entity reference translation.
+
+        :param value: Either an `Entity`, string or uuid
+
+        :return: List of unique identifier associated with the input reference.
+        """
+        children_uid = [child.uid for child in self.children]
+        if hasattr(value, "uid"):
+            uid = [value.uid]
+        elif isinstance(value, str):
+            uid = [
+                obj.uid
+                for obj in self.workspace.get_entity(value)
+                if (obj is not None) and (obj.uid in children_uid)
+            ]
+        elif isinstance(value, uuid.UUID):
+            uid = [value]
+
+        return uid
+
+    def remove_children(self, children: list[shared.Entity] | list[PropertyGroup]):
+        """
+        Remove children from the list of children entities.
+
+        :param children: List of entities
+
+        .. warning::
+            Removing a child entity without re-assigning it to a different
+            parent may cause it to become inactive. Inactive entities are removed
+            from the workspace by
+            :func:`~geoh5py.shared.weakref_utils.remove_none_referents`.
+        """
+        if not isinstance(children, list):
+            children = [children]
+
+        self._children = [child for child in self._children if child not in children]
+        self.workspace.remove_children(self, children)
+
+    def save(self, add_children: bool = True):
+        """
+        Alias method of :func:`~geoh5py.workspace.Workspace.save_entity`.
+        WILL BE DEPRECATED AS ENTITIES ARE ALWAYS AUTOMATICALLY UPDATED.
+        :param add_children: Option to also save the children.
+        """
+        warnings.warn(
+            "Entity.save() is deprecated and will be removed in next versions.",
+            DeprecationWarning,
+        )
+        return self.workspace.save_entity(self, add_children=add_children)
+
+    @property
+    def uid(self) -> uuid.UUID:
+        return self._uid
+
+    @uid.setter
+    def uid(self, uid: str | uuid.UUID):
+        if isinstance(uid, str):
+            uid = uuid.UUID(uid)
+
+        self._uid = uid
+
+    @property
+    def visible(self) -> bool:
+        """
+        Whether the Entity is visible in camera (checked in ANALYST object tree).
+        """
+        return self._visible
+
+    @visible.setter
+    def visible(self, value: bool):
+        self._visible = value
+        self.workspace.update_attribute(self, "attributes")
+
+    @property
+    def workspace(self) -> Workspace:
+        """
+        :obj:`~geoh5py.workspace.workspace.Workspace` to which the Entity belongs to.
+        """
+        return self.entity_type.workspace
