3a93ecbccaec5344c9d90ddf2d2c6399e11df31c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-17 23:57:06 +0000

    Add package sageworks/0.4.13/sageworks-0.4.13.tar.gz

---
+++ packages/sageworks/sageworks-0.4.13.tar.gz/sageworks-0.4.13/aws_setup/sageworks_core/app.py
@@ -0,0 +1,41 @@
+import os
+import boto3
+import aws_cdk as cdk
+
+from sageworks_core.sageworks_core_stack import SageworksCoreStack, SageworksCoreStackProps
+
+
+# Grab the account and region using boto3
+session = boto3.session.Session()
+aws_account = session.client("sts").get_caller_identity().get("Account")
+aws_region = session.region_name
+print(f"Account: {aws_account}")
+print(f"Region: {aws_region}")
+
+# We'd like to set up our parameters here
+sageworks_bucket = os.environ.get("SAGEWORKS_BUCKET")
+sageworks_role_name = os.environ.get("SAGEWORKS_ROLE", "SageWorks-ExecutionRole")
+sso_group = os.environ.get("SAGEWORKS_SSO_GROUP")
+additional_buckets = os.getenv("SAGEWORKS_ADDITIONAL_BUCKETS")
+
+
+# Our CDK App
+app = cdk.App()
+
+# Note: We might want to look into this
+# env=cdk.Environment(account=os.getenv('CDK_DEFAULT_ACCOUNT'), region=os.getenv('CDK_DEFAULT_REGION')),
+env = cdk.Environment(account=aws_account, region=aws_region)
+
+sandbox_stack = SageworksCoreStack(
+    app,
+    "SageworksCore",
+    env=env,
+    props=SageworksCoreStackProps(
+        sageworks_bucket=sageworks_bucket,
+        sageworks_role_name=sageworks_role_name,
+        sso_group=sso_group,
+        additional_buckets=additional_buckets.split(",") if additional_buckets else [],
+    ),
+)
+
+app.synth()
---
+++ packages/sageworks/sageworks-0.4.13.tar.gz/sageworks-0.4.13/aws_setup/sageworks_dashboard_full/app.py
@@ -0,0 +1,42 @@
+import os
+import boto3
+import aws_cdk as cdk
+
+from sageworks_dashboard_full.sageworks_dashboard_stack import SageworksDashboardStack, SageworksDashboardStackProps
+
+# Grab the account and region using boto3
+session = boto3.session.Session()
+aws_account = session.client("sts").get_caller_identity().get("Account")
+aws_region = session.region_name
+print(f"Account: {aws_account}")
+print(f"Region: {aws_region}")
+
+# We'd like to set up our parameters here
+sageworks_bucket = os.environ.get("SAGEWORKS_BUCKET")
+sageworks_api_key = os.environ.get("SAGEWORKS_API_KEY")
+existing_vpc_id = os.environ.get("SAGEWORKS_VPC_ID")
+existing_subnet_ids = os.environ.get("SAGEWORKS_SUBNET_IDS")
+whitelist_ips = [ip.strip() for ip in os.environ.get("SAGEWORKS_WHITELIST", "").split(",") if ip.strip()]
+whitelist_prefix_lists = [ip.strip() for ip in os.environ.get("SAGEWORKS_PREFIX_LISTS", "").split(",") if ip.strip()]
+certificate_arn = os.environ.get("SAGEWORKS_CERTIFICATE_ARN")
+
+
+# TODO: Add in a security group and pass as a prop to the stack
+
+app = cdk.App()
+SageworksDashboardStack(
+    app,
+    "SageworksDashboard",
+    env={"account": aws_account, "region": aws_region},
+    props=SageworksDashboardStackProps(
+        sageworks_bucket=sageworks_bucket,
+        sageworks_api_key=sageworks_api_key,
+        existing_vpc_id=existing_vpc_id,
+        existing_subnet_ids=existing_subnet_ids,
+        whitelist_ips=whitelist_ips,
+        whitelist_prefix_lists=whitelist_prefix_lists,
+        certificate_arn=certificate_arn,
+    ),
+)
+
+app.synth()
