960ca91b327b97ffd24020426393a9b0e8a92898
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-17 23:54:16 +0000

    Add package nailplot/0.1.0/nailplot-0.1.0.tar.gz

---
+++ packages/nailplot/nailplot-0.1.0.tar.gz/nailplot-0.1.0/setup.py
@@ -0,0 +1,10 @@
+# setup.py
+
+from setuptools import setup
+
+setup(
+    name='nailplot',
+    version='0.1.0',
+    packages=[''],  # Ensure this is an empty list
+    install_requires=['requests'],  # Add any other dependencies here
+)
