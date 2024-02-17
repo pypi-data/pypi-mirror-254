60c2ab4d7ef4d41dd5ddedbe48b9ea72b676f55d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-17 23:38:53 +0000

    Add package massdash/0.0.8/massdash-0.0.8.tar.gz

---
+++ packages/massdash/massdash-0.0.8.tar.gz/massdash-0.0.8/massdash/server/PeakPickingServer.py
@@ -0,0 +1,165 @@
+"""
+massdash/server/PeakPickingServer
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+"""
+
+import numpy as np
+import streamlit as st
+from typing import Literal
+
+# UI
+from ..ui.ChromatogramPlotUISettings import ChromatogramPlotUISettings
+from ..ui.PeakPickingUISettings import PeakPickingUISettings
+# Loaders
+from ..loaders.SqMassLoader import SqMassLoader
+# Structs
+from ..structs.TransitionGroup import TransitionGroup
+# Peak Picking
+from ..peakPickers import pyMRMTransitionGroupPicker, MRMTransitionGroupPicker, ConformerPeakPicker
+# Util
+from ..util import time_block
+from .util import get_string_mslevels_from_bool
+
+class PeakPickingServer:
+    """
+    A class that performs peak picking on transition group data.
+
+    Args:
+        peak_picking_settings (object): The peak picking settings.
+        chrom_plot_settings (object, optional): The chromatogram plot settings. Defaults to None.
+    """
+
+    def __init__(self, peak_picking_settings: PeakPickingUISettings, chrom_plot_settings: ChromatogramPlotUISettings=None):
+        self.peak_picking_settings = peak_picking_settings
+        self.chrom_plot_settings = chrom_plot_settings
+
+    def perform_feature_file_peak_picking(self, tr_group_data, mzml_loader_dict, transition_list_ui: Literal['ExtractedIonChromatogramAnalysisUI', 'RawTargetedExtractionAnalysisUI']):
+        """
+        Get peak boundaries from Feature File Boundaries.
+
+        Args:
+            xic_data (object): The XIC data.
+            transition_list_ui (object): The transition list UI.
+
+        Returns:
+            dict: The transition group feature data.
+        """
+
+        tr_group_feature_data = {}
+        with time_block() as elapsed_time:
+            for file in tr_group_data.keys():
+                tr_group_feature_data[file] = mzml_loader_dict.loadTransitionGroupFeatures(transition_list_ui.transition_settings.selected_peptide,
+                transition_list_ui.transition_settings.selected_charge)[file]
+        st.write(f"Loading Feature File Peak Boundaries... Elapsed time: {elapsed_time()}")
+        return tr_group_feature_data
+
+    def perform_pypeakpicker_mrm_peak_picking(self, tr_group_data: TransitionGroup):
+        """
+        Performs peak picking using pyPeakPickerMRM algorithm.
+
+        Args:
+            tr_group_data (dict): The transition group data.
+
+        Returns:
+            dict: The transition group feature data.
+        """
+        with time_block() as elapsed_time:
+            if self.peak_picking_settings.peak_pick_on_displayed_chrom:
+                mslevel = get_string_mslevels_from_bool(
+                    {'ms1':self.chrom_plot_settings.include_ms1, 'ms2':self.chrom_plot_settings.include_ms2}
+                )
+            else:
+                mslevel = self.peak_picking_settings.peak_picker_algo_settings.mslevels
+            peak_picker_param = self.peak_picking_settings.peak_picker_algo_settings.PeakPickerMRMParams
+
+            tr_group_feature_data = {}
+            for file, tr_group in tr_group_data.items():
+                peak_picker = pyMRMTransitionGroupPicker(mslevel, peak_picker=peak_picker_param.peak_picker)
+                peak_features = peak_picker.pick(tr_group)
+                tr_group_feature_data[file] = peak_features
+
+        st.write(f"Performing pyPeakPickerMRM Peak Picking... Elapsed time: {elapsed_time()}")
+        return tr_group_feature_data
+
+    def perform_mrmtransitiongrouppicker_peak_picking(self, tr_group_data: TransitionGroup):
+        """
+        Performs peak picking using MRMTransitionGroupPicker algorithm.
+
+        Args:
+            tr_group_data (dict): The transition group data.
+
+        Returns:
+            dict: The transition group feature data.
+        """
+        with time_block() as elapsed_time:
+            tr_group_feature_data = {}
+            for file, tr_group in tr_group_data.items():
+                if self.peak_picking_settings.peak_picker_algo_settings.smoother == 'none': ## none in MassDash corresponds to 'original' in MRMTransitionGroupPicker
+                    peak_picker = MRMTransitionGroupPicker('original')
+                else:
+                    peak_picker = MRMTransitionGroupPicker(self.peak_picking_settings.peak_picker_algo_settings.smoother)
+                peak_features = peak_picker.pick(tr_group)
+                tr_group_feature_data[file] = peak_features
+
+        st.write(f"Performing MRMTransitionGroupPicker Peak Picking... Elapsed time: {elapsed_time()}")
+        return tr_group_feature_data
+
+    def perform_conformer_peak_picking(self, spec_lib_path: str, tr_group_data: TransitionGroup):
+        """
+        Performs peak picking using ConformerPeakPicker algorithm.
+
+        Args:
+            tr_group_data (dict): The transition group data.
+            spec_lib_path (str): The path to the spectral library.
+
+        Returns:
+            dict: The transition group feature data.
+        """
+        with time_block() as elapsed_time:
+            # Peak picking using Conformer
+            tr_group_feature_data = {}
+
+            for file, tr_group in tr_group_data.items():
+                #tr_group.targeted_transition_list = self.transition_list_ui.target_transition_list
+                st.write(f"Pretrained model file: {self.peak_picking_settings.peak_picker_algo_settings.pretrained_model_file}")
+                        
+                peak_picker = ConformerPeakPicker(spec_lib_path, self.peak_picking_settings.peak_picker_algo_settings.pretrained_model_file, 
+                                                  prediction_threshold=self.peak_picking_settings.peak_picker_algo_settings.conformer_prediction_threshold,
+                                                  prediction_type=self.peak_picking_settings.peak_picker_algo_settings.conformer_prediction_type)
+                
+                # get the trantition in tr_group with the max intensity
+                max_int_transition = np.max([transition.intensity for transition in tr_group.transitionData])
+                peak_features = peak_picker.pick(tr_group, max_int_transition=max_int_transition)
+                tr_group_feature_data[file] = peak_features
+        st.write(f"Performing Conformer Peak Picking... Elapsed time: {elapsed_time()}")
+        return tr_group_feature_data
+
+    def perform_peak_picking(self, tr_group_data: TransitionGroup=None, xic_data: SqMassLoader=None, mzml_loader_dict=None, transition_list_ui: Literal['ExtractedIonChromatogramAnalysisUI', 'RawTargetedExtractionAnalysisUI']=None, spec_lib: str=None):
+        """
+        Performs peak picking based on the selected method.
+
+        Args:
+            tr_group_data (dict, optional): The transition group data. Defaults to None.
+            xic_data (object, optional): The XIC data. Defaults to None.
+            transition_list_ui (object, optional): The transition list UI. Defaults to None.
+            spec_lib (object, optional): The spectral library. Defaults to None. Mandatory if peak picking using ConformerPeakPicker.
+
+        Returns:
+            dict: The transition group feature data.
+        """
+        tr_group_feature_data = {}
+
+        # Perform peak picking based on the selected method
+        if self.peak_picking_settings.do_peak_picking == 'Feature File Boundaries':
+            tr_group_feature_data = self.perform_feature_file_peak_picking(tr_group_data, mzml_loader_dict, transition_list_ui)
+        elif self.peak_picking_settings.do_peak_picking == 'pyPeakPickerMRM':
+            tr_group_feature_data = self.perform_pypeakpicker_mrm_peak_picking(tr_group_data)
+        elif self.peak_picking_settings.do_peak_picking == 'MRMTransitionGroupPicker':
+            tr_group_feature_data = self.perform_mrmtransitiongrouppicker_peak_picking(tr_group_data)
+        elif self.peak_picking_settings.do_peak_picking == 'Conformer':
+            tr_group_feature_data = self.perform_conformer_peak_picking(spec_lib, tr_group_data)
+        else:
+            tr_group_feature_data = {file: None for file in tr_group_data.keys()}
+
+        return tr_group_feature_data
+
---
+++ packages/massdash/massdash-0.0.8.tar.gz/massdash-0.0.8/massdash/ui/ConformerPickerUISettings.py
@@ -0,0 +1,56 @@
+"""
+massdash/ui/ConformerPickerUISettings
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+"""
+
+import os
+import streamlit as st
+
+# UI
+from .ChromatogramPlotUISettings import ChromatogramPlotUISettings
+# Utils
+from ..constants import URL_PRETRAINED_CONFORMER
+from ..util import download_file
+
+DIRNAME = os.path.dirname(__file__)
+
+class ConformerPickerUISettings:
+    def __init__(self, main_peak_picking_settings) -> None:
+        """
+        Initializes a new instance of the ConformerPickerUISettings class.
+
+        Parameters:
+        -----------
+        main_peak_picking_settings : PeakPickingSettings
+            The peak picking settings for the main chromatogram.
+        """
+        self.main_peak_picking_settings = main_peak_picking_settings
+        self.shipped_model = True
+        self.pretrained_model_file = None
+        self.conformer_window_size = 175
+        self.conformer_prediction_threshold = 0.5
+        self.conformer_prediction_type = "logits"
+
+    def create_ui(self, plot_settings: ChromatogramPlotUISettings):
+        """
+        Creates the user interface for the ConformerPicker app.
+
+        Parameters:
+        -----------
+        plot_settings : ChromatogramPlotUISettings
+            The plot settings for the chromatogram.
+        """
+        self.shipped_model = st.sidebar.checkbox("Use shipped model", value=True, help="Use the shipped model which picks peaks across 175 points")
+        if  self.shipped_model:
+            self.pretrained_model_file = os.path.join(DIRNAME, '..', 'assets', 'models', 'conformer', 'base_cape.onnx')
+            # Check if the model file exists
+            if not os.path.exists(self.pretrained_model_file):
+                with st.spinner(f"Downloading pretrained model: {self.pretrained_model_file}..."):
+                    tmp_download_folder = os.path.join(DIRNAME, '..', 'assets', 'models', 'conformer')
+                    download_file(URL_PRETRAINED_CONFORMER, tmp_download_folder)
+        else:
+            self.pretrained_model_file = st.sidebar.text_input("Pretrained model file", value="", help="The pretrained model file to use.")
+        
+        with st.sidebar.expander("Advanced settings"):
+            self.conformer_prediction_threshold = st.number_input("prediction score threshold", value=0.2, help="The threshold for the conformer models prediction scores to find the top peak boundary.")
+            self.conformer_prediction_type = st.selectbox("prediction type", options=["logits", "sigmoided", "binarized"], help="The type of prediction to use for finding the top peak.")
