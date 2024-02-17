a6a2f32b1bb2aedcee015a97bd642298e31e1b9a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-17 23:31:36 +0000

    Add package dlclibrary/0.0.6/dlclibrary-0.0.6-py3-none-any.whl

---
+++ packages/dlclibrary/dlclibrary-0.0.6-py3-none-any.whl/dlclibrary/__init__.py
@@ -0,0 +1,16 @@
+#
+# DeepLabCut Toolbox (deeplabcut.org)
+# © A. & M.W. Mathis Labs
+# https://github.com/DeepLabCut/DeepLabCut
+#
+# Please see AUTHORS for contributors.
+# https://github.com/DeepLabCut/DeepLabCut/blob/master/AUTHORS
+#
+# Licensed under GNU Lesser General Public License v3.0
+#
+
+from dlclibrary.dlcmodelzoo.modelzoo_download import (
+    download_huggingface_model,
+    parse_available_supermodels,
+)
+from dlclibrary.version import __version__, VERSION
