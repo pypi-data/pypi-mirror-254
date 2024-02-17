082f96d2007e81edb5a8a307372dc23e8dac2bca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-17 23:53:42 +0000

    Add package sumire/1.0.2/sumire-1.0.2-py3-none-any.whl

---
+++ packages/sumire/sumire-1.0.2-py3-none-any.whl/sumire/vectorizer/__init__.py
@@ -0,0 +1,4 @@
+from sumire.vectorizer.count import CountVectorizer  # noqa
+from sumire.vectorizer.swem import W2VSWEMVectorizer  # noqa
+from sumire.vectorizer.tfidf import TfidfVectorizer  # noqa
+from sumire.vectorizer.transformer_emb import TransformerEmbeddingVectorizer  # noqa
