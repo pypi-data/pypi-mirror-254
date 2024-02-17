588629bf01fdd074e8b03e8cdbbf9ec71866f9cf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-17 23:46:57 +0000

    Add package evilblade/1.1.4/evilblade-1.1.4.tar.gz

---
+++ packages/evilblade/evilblade-1.1.4.tar.gz/evilblade-1.1.4/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='evilblade',
+    version='1.1.4',
+    description='7 N1nEmAn\'s evilblade!!',
+    packages=['evilblade'],
+    install_requires=[
+        'pwntools',
+        'LibcSearcher'
+    ],
+)
