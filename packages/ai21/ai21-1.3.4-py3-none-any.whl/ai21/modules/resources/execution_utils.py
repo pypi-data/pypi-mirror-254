f391a5cd72edc83e22108092dd4fe98cea653bc2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-17 23:47:20 +0000

    Add package ai21/1.3.4/ai21-1.3.4-py3-none-any.whl

---
+++ packages/ai21/ai21-1.3.4-py3-none-any.whl/ai21/modules/resources/execution_utils.py
@@ -0,0 +1,53 @@
+import json
+from typing import Optional, List
+
+from ai21.modules.destination import SageMakerDestination, BedrockDestination
+from ai21.ai21_studio_client import AI21StudioClient
+from ai21.utils import validate_mandatory_field, validate_unsupported_field
+
+
+def execute_studio_request(task_url: str, params, method: str = 'POST'):
+    client = AI21StudioClient(**params)
+    return client.execute_http_request(method=method, url=task_url, params=params)
+
+
+def execute_sm_request(destination: SageMakerDestination, params, mandatory_fields=None, unsupported_fields=None):
+    call_name = f'Sagemaker {destination.endpoint_name}'
+
+    if mandatory_fields is not None:
+        for mandatory_field in mandatory_fields:
+            validate_mandatory_field(mandatory_field, call_name, params)
+
+    if unsupported_fields is not None:
+        for unsupported_field in unsupported_fields:
+            validate_unsupported_field(key=unsupported_field, call_name=call_name, params=params)
+
+    input_json = json.dumps(params)
+
+    from ai21.aws.sagemaker import invoke_sm_endpoint  # import here because boto3 exists only in AWS mode
+
+    return invoke_sm_endpoint(
+        endpoint_name=destination.endpoint_name,
+        input_json=input_json,
+        sm_session=destination.sm_session,
+    )
+
+
+def execute_bedrock_request(destination: BedrockDestination, params = None,
+                            unsupported_fields: Optional[List[str]] = None):
+    call_name = 'Bedrock'
+
+    if unsupported_fields is not None:
+        for unsupported_field in unsupported_fields:
+            validate_unsupported_field(key=unsupported_field, call_name=call_name, params=params)
+
+    input_json = json.dumps(params)
+
+    from ai21.aws.bedrock import invoke_bedrock_model  # import here because boto3 exists only in AWS mode
+
+    return invoke_bedrock_model(
+        model_id=destination.model_id,
+        input_json=input_json,
+        bedrock_session=destination.boto_session,
+    )
+
---
+++ packages/ai21/ai21-1.3.4-py3-none-any.whl/ai21/modules/sagemaker.py
@@ -0,0 +1,56 @@
+from typing import List
+
+from ai21.ai21_studio_client import AI21StudioClient
+from ai21.constants import SAGEMAKER_MODEL_PACKAGE_NAMES, STUDIO_HOST, DEFAULT_API_VERSION
+from ai21.errors import ModelPackageDoesntExistException
+from ai21.modules.resources.ai21_module import AI21Module
+
+_JUMPSTART_ENDPOINT = "jumpstart"
+_LIST_VERSIONS_ENDPOINT = f"{_JUMPSTART_ENDPOINT}/list_versions"
+_GET_ARN_ENDPOINT = f"{_JUMPSTART_ENDPOINT}/get_model_version_arn"
+
+LATEST_VERSION_STR = "latest"
+
+
+class SageMaker(AI21Module):
+    @classmethod
+    def get_model_package_arn(cls, model_name: str, region: str, version: str = LATEST_VERSION_STR) -> str:
+        _assert_model_package_exists(model_name=model_name, region=region)
+
+        client = AI21StudioClient(auth_required=False)
+
+        response = client.execute_http_request(
+            method="POST",
+            url=f'{cls.get_base_url()}/{_GET_ARN_ENDPOINT}',
+            params={
+                "modelName": model_name,
+                "region": region,
+                "version": version,
+            })
+
+        arn = response.arn
+
+        if not arn:
+            raise ModelPackageDoesntExistException(model_name=model_name, region=region, version=version)
+
+        return arn
+
+    @classmethod
+    def list_model_package_versions(cls, model_name: str, region: str) -> List[str]:
+        _assert_model_package_exists(model_name=model_name, region=region)
+        client = AI21StudioClient(auth_required=False)
+
+        response = client.execute_http_request(
+            method="POST",
+            url=f'{cls.get_base_url()}/{_LIST_VERSIONS_ENDPOINT}',
+            params={
+                "modelName": model_name,
+                "region": region,
+            })
+
+        return response["versions"]
+
+
+def _assert_model_package_exists(model_name, region):
+    if model_name not in SAGEMAKER_MODEL_PACKAGE_NAMES:
+        raise ModelPackageDoesntExistException(model_name=model_name, region=region)
