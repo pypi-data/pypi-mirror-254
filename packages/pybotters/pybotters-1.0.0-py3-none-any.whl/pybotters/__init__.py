ff06b69b60c1cae4ee66f29a5d6cc3234cee6381
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-17 23:46:55 +0000

    Add package pybotters/1.0.0/pybotters-1.0.0-py3-none-any.whl

---
+++ packages/pybotters/pybotters-1.0.0-py3-none-any.whl/pybotters/__init__.py
@@ -0,0 +1,56 @@
+from __future__ import annotations
+
+from importlib.metadata import version
+
+__version__ = version(__package__)
+
+from .auth import Auth
+from .client import Client, FetchResult, NotJSONContent
+from .models.binance import (
+    BinanceCOINMDataStore,
+    BinanceSpotDataStore,
+    BinanceUSDSMDataStore,
+)
+from .models.bitbank import bitbankDataStore
+from .models.bitflyer import bitFlyerDataStore
+from .models.bitget import BitgetDataStore
+from .models.bitmex import BitMEXDataStore
+from .models.bybit import BybitDataStore
+from .models.coincheck import CoincheckDataStore
+from .models.gmocoin import GMOCoinDataStore
+from .models.kucoin import KuCoinDataStore
+from .models.okx import OKXDataStore
+from .models.phemex import PhemexDataStore
+from .store import DataStore, DataStoreCollection, StoreChange, StoreStream
+from .ws import WebSocketApp, WebSocketQueue
+
+__all__: tuple[str, ...] = (
+    # client
+    "Client",
+    "FetchResult",
+    "NotJSONContent",
+    # ws
+    "WebSocketApp",
+    "WebSocketQueue",
+    # store
+    "DataStore",
+    "DataStoreCollection",
+    "StoreChange",
+    "StoreStream",
+    # models
+    "BinanceCOINMDataStore",
+    "BinanceSpotDataStore",
+    "BinanceUSDSMDataStore",
+    "BitMEXDataStore",
+    "BitgetDataStore",
+    "BybitDataStore",
+    "CoincheckDataStore",
+    "GMOCoinDataStore",
+    "KuCoinDataStore",
+    "OKXDataStore",
+    "PhemexDataStore",
+    "bitFlyerDataStore",
+    "bitbankDataStore",
+    # auth
+    "Auth",
+)
