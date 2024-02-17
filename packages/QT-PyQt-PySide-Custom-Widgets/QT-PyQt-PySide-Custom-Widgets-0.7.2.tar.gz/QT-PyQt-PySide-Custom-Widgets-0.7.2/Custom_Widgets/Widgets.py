c76fd55703da83b715033c93103761d396d3361b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-17 23:31:15 +0000

    Add package QT-PyQt-PySide-Custom-Widgets/0.7.2/QT-PyQt-PySide-Custom-Widgets-0.7.2.tar.gz

---
+++ packages/QT-PyQt-PySide-Custom-Widgets/QT-PyQt-PySide-Custom-Widgets-0.7.2.tar.gz/QT-PyQt-PySide-Custom-Widgets-0.7.2/Custom_Widgets/Widgets.py
@@ -0,0 +1,12 @@
+from . QCustomSlideMenu import QCustomSlideMenu
+from . QCustomQStackedWidget import QCustomQStackedWidget as QCustomStackedWidget
+from . QCustomProgressIndicator import QCustomProgressIndicator as FormProgressIndicator
+from . QAppSettings import QAppSettings
+from . QCustomQPushButton import QCustomQPushButton
+from .__init__ import *
+
+import warnings
+
+warnings.warn(
+    "Deprecated imports. For more information, refer to the documentation at: https://github.com/KhamisiKibet/QT-PyQt-PySide-Custom-Widgets"
+)
