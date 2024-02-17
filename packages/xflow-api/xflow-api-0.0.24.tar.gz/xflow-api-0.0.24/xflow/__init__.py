f1e04c1604e6afa6e54071fdeaf80cc43a5153b1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-17 23:37:47 +0000

    Add package xflow-api/0.0.24/xflow-api-0.0.24.tar.gz

---
+++ packages/xflow-api/xflow-api-0.0.24.tar.gz/xflow-api-0.0.24/xflow/__init__.py
@@ -0,0 +1,6 @@
+from xflow._private.client import init
+
+__all__ = [
+    "init"
+]
+__version__ = '0.0.24'
---
+++ packages/xflow-api/xflow-api-0.0.24.tar.gz/xflow-api-0.0.24/xflow/model/structures.py
@@ -0,0 +1,10 @@
+from dataclasses import dataclass
+
+
+@dataclass
+class ModelTypes:
+    TensorRT: str = "TENSOR_RT"
+    TensorFlow: str = "TENSORFLOW"
+    PyTorch: str = "PYTORCH"
+    ONNX: str = "ONNX"
+
