a56bbef790dadc9cd0e27607c4262c44bc2f5e5a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-17 23:42:52 +0000

    Add package labelu/0.9.2/labelu-0.9.2-py3-none-any.whl

---
+++ packages/labelu/labelu-0.9.2-py3-none-any.whl/labelu/internal/statics/backend_version.js
@@ -0,0 +1,10 @@
+
+                  (function () {
+                    window.__backend = {
+  "version": "0.9.2",
+  "name": "labelu",
+  "build_time": "2024-01-31T08:20:05.829Z",
+  "commit": "3884818bbbef428c5e94cc0595c38e67b00a2de3"
+};
+                  })();
+                
---
+++ packages/labelu/labelu-0.9.2-py3-none-any.whl/labelu/internal/statics/frontend_version.js
@@ -0,0 +1,15 @@
+(function () {
+  window.__frontend = {
+    version: '4.0.2',
+    deps: {
+      '@labelu/audio-annotator-react': '1.3.1',
+      '@labelu/components-react': '1.3.1',
+      '@labelu/formatter': '1.0.2',
+      '@labelu/image': '1.0.1',
+      '@labelu/image-annotator-react': '1.0.2',
+      '@labelu/interface': '1.3.0',
+      '@labelu/video-annotator-react': '1.3.1',
+      '@labelu/video-react': '1.2.4',
+    },
+  };
+})();
