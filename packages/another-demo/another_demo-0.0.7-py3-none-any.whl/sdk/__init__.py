c231e4fe3c2c711172cdf9fc454723efee103bfa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-17 23:47:39 +0000

    Add package another-demo/0.0.7/another_demo-0.0.7-py3-none-any.whl

---
+++ packages/another-demo/another_demo-0.0.7-py3-none-any.whl/sdk/__init__.py
@@ -0,0 +1,9 @@
+def main():
+    """Entry point for the application script"""
+    print("Call your main application code here")
+
+def add_two(number):
+    return number + 2
+
+def add_one(number):
+    return number + 1
---
+++ packages/another-demo/another_demo-0.0.7-py3-none-any.whl/sdk/start.py
@@ -0,0 +1,6 @@
+def add_three(number):
+    return number + 3
+
+def echo():
+    """Entry point for the application script"""
+    print("Call main")
