1244812c7e66006d181445984b8523e385c9b184
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-17 23:37:57 +0000

    Add package ascript/0.0.3.26/ascript-0.0.3.26.tar.gz

---
+++ packages/ascript/ascript-0.0.3.26.tar.gz/ascript-0.0.3.26/airclick/windows/client/start.py
@@ -0,0 +1,18 @@
+import airclick.windows.client.server as server
+import airclick.windows.client.ui as ui
+import threading
+
+
+def startServer():
+    server.run()
+
+if __name__ == '__main__':
+    thread_server = threading.Thread(target=startServer)
+    thread_server.start()
+    ui.enter()
+
+# home = ui.HomeWindow()
+# home.show()
+
+
+# ui.check_workspace()
