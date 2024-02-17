fe53ff3925d5505275af9ad6d072d54e020ab4bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-17 23:31:36 +0000

    Add package devicehive-plugin-yingjun/1.0.0/devicehive_plugin_yingjun-1.0.0.tar.gz

---
+++ packages/devicehive-plugin-yingjun/devicehive_plugin_yingjun-1.0.0.tar.gz/devicehive_plugin_yingjun-1.0.0/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='devicehive_plugin_yingjun',
+    version='1.0.0',
+    packages=['devicehive_plugin_yingjun'],
+    install_requires=['websocket-client>=0.47.0', 'requests>=2.18.4', 'six>=1.11.0']
+)
