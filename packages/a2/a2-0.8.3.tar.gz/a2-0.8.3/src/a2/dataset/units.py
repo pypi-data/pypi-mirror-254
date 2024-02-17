a214f976e6ef6a0cf124cd1594891872fd4724c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-17 23:47:48 +0000

    Add package a2/0.8.3/a2-0.8.3.tar.gz

---
+++ packages/a2/a2-0.8.3.tar.gz/a2-0.8.3/src/a2/dataset/units.py
@@ -0,0 +1,49 @@
+import abc
+
+import pyproj
+
+
+class UnitConverter(abc.ABC):
+    @abc.abstractmethod
+    def transform(self):
+        pass
+
+
+class ConvFromNGToLatLong(UnitConverter):
+    def __init__(self):
+        self.transformer = pyproj.Transformer.from_crs("epsg:27700", "epsg:4326")
+
+    def transform(self, easting, northing, errcheck=True):
+        """
+        Converts units in British National Grid (easting, northing) to latitude, longitude
+        Note, shift from east,north -> north,east convention
+        Parameters:
+        ----------
+        easting: Distance from western origin in m
+        northing: Distance from southern origin in m
+
+        Returns
+        -------
+        latitude, longitude
+        """
+        return self.transformer.transform(easting, northing, errcheck=errcheck)
+
+
+class ConvFromLatLongToNG(UnitConverter):
+    def __init__(self):
+        self.transformer = pyproj.Transformer.from_crs("epsg:4326", "epsg:27700")
+
+    def transform(self, latitude, longitude, errcheck=True):
+        """
+        Converts units from latitude, longitude to British National Grid (easting, northing)
+        Note, shift from north,east -> east,north convention
+        Parameters:
+        ----------
+        easting: Distance from western origin in m
+        northing: Distance from southern origin in m
+
+        Returns
+        -------
+        easting, northing
+        """
+        return self.transformer.transform(latitude, longitude, errcheck=errcheck)
