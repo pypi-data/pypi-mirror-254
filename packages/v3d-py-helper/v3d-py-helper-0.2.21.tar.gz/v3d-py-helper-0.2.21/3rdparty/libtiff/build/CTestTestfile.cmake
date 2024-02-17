767da7f0df09f6f3ca54a23535d8f86c530f1332
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-17 23:38:04 +0000

    Add package v3d-py-helper/0.2.21/v3d-py-helper-0.2.21.tar.gz

---
+++ packages/v3d-py-helper/v3d-py-helper-0.2.21.tar.gz/v3d-py-helper-0.2.21/3rdparty/libtiff/build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: D:/GitHub/v3d-py-helper/3rdparty/libtiff/build
+# Build directory: D:/GitHub/v3d-py-helper/3rdparty/libtiff/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
