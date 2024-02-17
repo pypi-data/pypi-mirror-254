edd3f059bfb13211c8130b12fd6843dbf3902d79
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-17 23:47:12 +0000

    Add package MPGrid/0.0.0/MPGrid-0.0.0.tar.gz

---
+++ packages/MPGrid/MPGrid-0.0.0.tar.gz/MPGrid-0.0.0/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, Extension
+from glob import glob
+import os
+
+if os.name == 'nt':
+    setup(
+        ext_modules=[
+            Extension(
+                name="MPGrid",
+                sources=glob("src/*.c"),
+                define_macros=[('MP_PYTHON_LIB', None),],
+                extra_compile_args=['-fopenmp'],
+                include_dirs=['zlib/include'],
+                library_dirs=['zlib/lib/x64'],
+                libraries=['zlibstat']
+            )
+        ]
+    )
+else:
+   setup(
+       ext_modules = [
+           Extension(
+               sources=glob("src/*.c"),
+               define_macros=[('MP_PYTHON_LIB', None),],
+               extra_compile_args=['-fopenmp']
+           )
+       ]
+   )
+
