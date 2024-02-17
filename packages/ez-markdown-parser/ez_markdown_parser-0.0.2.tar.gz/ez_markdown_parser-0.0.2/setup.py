ddf0f0ead461d36f44f0936d3ac0555595b918aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-17 23:38:53 +0000

    Add package ez-markdown-parser/0.0.2/ez_markdown_parser-0.0.2.tar.gz

---
+++ packages/ez-markdown-parser/ez_markdown_parser-0.0.2.tar.gz/ez_markdown_parser-0.0.2/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as f:
+    pypi_description = f.read()
+
+setup(
+    name='ez_markdown_parser',
+    version='0.0.2',
+    packages=find_packages(),
+    long_description=pypi_description,
+    long_description_content_type='text/markdown',
+)
+
+# To upload changes to pypi:
+# Have installed packages from requirements.txt
+# Upgrade Version in this file up there 
+# Compile Package: python setup.py sdist bdist_wheel
+# Publish it to pypi: twine upload dist/* (user: __token__ , pass=YOUR_API_TOKEN)
+# Push code changes to github
