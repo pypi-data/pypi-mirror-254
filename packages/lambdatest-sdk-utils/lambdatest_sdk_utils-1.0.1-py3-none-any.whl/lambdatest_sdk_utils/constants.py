9ffa48c54acbea3280f01aaa648721eddff7e4a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-17 23:56:52 +0000

    Add package lambdatest-sdk-utils/1.0.1/lambdatest_sdk_utils-1.0.1-py3-none-any.whl

---
+++ packages/lambdatest-sdk-utils/lambdatest_sdk_utils-1.0.1-py3-none-any.whl/lambdatest_sdk_utils/constants.py
@@ -0,0 +1,9 @@
+import os
+
+def get_smart_ui_server_address():
+    if not os.getenv('SMARTUI_SERVER_ADDRESS'):
+        raise Exception('SmartUI server address not found')
+    return os.getenv('SMARTUI_SERVER_ADDRESS')
+
+def get_pkg_name():
+    return "@lambdatest/python-selenium-driver"
