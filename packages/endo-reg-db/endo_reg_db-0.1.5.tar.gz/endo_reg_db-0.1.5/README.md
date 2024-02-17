31abfdb043a8ef0ea8cbe5c75bdc721b52ff08be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-17 23:58:01 +0000

    Add package endo-reg-db/0.1.5/endo_reg_db-0.1.5.tar.gz

---
+++ packages/endo-reg-db/endo_reg_db-0.1.5.tar.gz/endo_reg_db-0.1.5/README.md
@@ -0,0 +1,4 @@
+# endo-reg-db
+
+## Commands
+- build: 'nix build -o nix-endo-reg-db'
---
+++ packages/endo-reg-db/endo_reg_db-0.1.5.tar.gz/endo_reg_db-0.1.5/endo_reg_db/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class EndoRegDbConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'endo_reg_db'
