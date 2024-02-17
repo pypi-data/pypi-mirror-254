8a8f0dd83e270a0b96d8b467be4c690f114f37c7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-17 23:31:50 +0000

    Add package pqapi/4.0.0/pqapi-4.0.0-py3-none-any.whl

---
+++ packages/pqapi/pqapi-4.0.0-py3-none-any.whl/pqapi/__init__.py
@@ -0,0 +1,30 @@
+from .version import __version__  # noqa
+from .api import (
+    upload_file,
+    upload_paper,
+    agent_query,
+    get_bibliography,
+    delete_bibliography,
+    async_delete_bibliography,
+    async_get_bibliography,
+    async_agent_query,
+    async_query,
+)  # noqa
+from .models import QueryRequest, UploadMetadata, AnswerResponse  # noqa
+from .models import get_prompts
+
+__all__ = [
+    "upload_file",
+    "upload_paper",
+    "agent_query",
+    "get_bibliography",
+    "delete_bibliography",
+    "QueryRequest",
+    "UploadMetadata",
+    "AnswerResponse",
+    "async_delete_bibliography",
+    "async_get_bibliography",
+    "async_agent_query",
+    "async_query",
+    "get_prompts",
+]
