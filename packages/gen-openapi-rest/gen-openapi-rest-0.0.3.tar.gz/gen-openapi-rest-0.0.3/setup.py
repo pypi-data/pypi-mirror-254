3c4438c9ce5ad6910bf632f092b00028fd975687
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-17 23:53:50 +0000

    Add package gen-openapi-rest/0.0.3/gen-openapi-rest-0.0.3.tar.gz

---
+++ packages/gen-openapi-rest/gen-openapi-rest-0.0.3.tar.gz/gen-openapi-rest-0.0.3/setup.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+
+# pylint: skip-file
+from setuptools import find_packages, setup
+
+# read the contents of your README file and add it to the long_description
+with open("readme.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+
+setup(
+    name="gen-openapi-rest",
+    version="0.0.3",
+    packages=find_packages(),
+    description="Generate .http files from OpenAPI/Swagger JSON",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author="rangulvers",
+    url="https://github.com/rangulvers/openapi-rest-client-generator",
+    license="LICENSE",
+    install_requires=["requests"],
+    classifiers=[
+        # Trove classifiers
+        # Full list: https://pypi.org/classifiers/
+        "Development Status :: 3 - Alpha",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+    ],
+)
