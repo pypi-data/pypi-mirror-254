db2ba64836b039934a3b06882f56ec1f39205f63
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-17 23:37:17 +0000

    Add package libbash/0.1.1/libbash-0.1.1.tar.gz

---
+++ packages/libbash/libbash-0.1.1.tar.gz/libbash-0.1.1/setup.py
@@ -0,0 +1,29 @@
+import os
+
+from setuptools import setup
+from setuptools.command.build_py import build_py
+
+
+class build_libbash(build_py):
+    def run(self):
+        build_py.run(self)
+        os.system("git submodule update --init --recursive")
+        os.system("cd " + os.path.join
+                  (os.path.dirname(__file__), "libbash", "bash-5.2") +
+                  " && ./configure && make clean all")
+
+setup(name='libbash',
+      packages=['libbash', 'libbash.bash_command'],
+      cmdclass={'build_py': build_libbash},
+      package_data={'libbash': ['bash-5.2/*']},
+      version='0.1.1',
+      description="A Python library for parsing Bash scripts into an AST",
+      long_description=open('README.md').read(),
+      long_description_content_type='text/markdown',
+      url="https://github.com/binpash/libbash/",
+      author='Seth Sabar',
+      author_email='sethsabar@gmail.com',
+      license="GPL-3.0",
+      include_package_data=True,
+      has_ext_modules=lambda: True)
+
