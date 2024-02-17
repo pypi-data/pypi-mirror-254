a3284d3c584f42eeccd33a3a125eb014a99ad0c0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-17 23:56:26 +0000

    Add package pdf-outline-edit/24.2.1/pdf-outline-edit-24.2.1.tar.gz

---
+++ packages/pdf-outline-edit/pdf-outline-edit-24.2.1.tar.gz/pdf-outline-edit-24.2.1/src/pdfoe/main.py
@@ -0,0 +1,11 @@
+import importlib.util
+
+PySide6_spec = importlib.util.find_spec("PySide6")
+if PySide6_spec is not None:
+    from . import gui_pyside_qml
+
+    gui_pyside_qml.main()
+else:
+    from . import gui_tkinter
+
+    gui_tkinter.main()
