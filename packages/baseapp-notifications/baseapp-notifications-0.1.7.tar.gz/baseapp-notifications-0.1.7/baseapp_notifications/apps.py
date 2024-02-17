4a82e2d2ae0c0e1add2cbf349f001afa4bec3d86
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-17 23:56:42 +0000

    Add package baseapp-notifications/0.1.7/baseapp-notifications-0.1.7.tar.gz

---
+++ packages/baseapp-notifications/baseapp-notifications-0.1.7.tar.gz/baseapp-notifications-0.1.7/baseapp_notifications/apps.py
@@ -0,0 +1,7 @@
+from django.apps import AppConfig
+
+
+class NotificationsConfig(AppConfig):
+    default = True
+    name = "baseapp_notifications"
+    verbose_name = "BaseApp Notifications"
---
+++ packages/baseapp-notifications/baseapp-notifications-0.1.7.tar.gz/baseapp-notifications-0.1.7/baseapp_notifications/tests/factories.py
@@ -0,0 +1,20 @@
+import factory
+import swapper
+from baseapp_auth.tests.helpers import get_user_factory
+
+Notification = swapper.load_model("notifications", "Notification")
+UserFactory = get_user_factory()
+
+
+class AbstractNotificationFactory(factory.django.DjangoModelFactory):
+    recipient = factory.SubFactory(UserFactory)
+
+    class Meta:
+        abstract = True
+
+
+class NotificationFactory(AbstractNotificationFactory):
+    actor = factory.SubFactory(UserFactory)
+
+    class Meta:
+        model = Notification
