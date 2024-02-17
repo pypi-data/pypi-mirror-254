0a9e28d40cafe080158be7ecc3a01374cea67554
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-17 23:34:31 +0000

    Add package hidef/1.1.5a3/hidef-1.1.5a3.tar.gz

---
+++ packages/hidef/hidef-1.1.5a3.tar.gz/hidef-1.1.5a3/hidef/__init__.py
@@ -0,0 +1,12 @@
+__version__ = '1.1.5a3'
+__release__ = __version__
+
+__all__ = ['hidef_finder', 'weaver', 'utils', 'logger', 'dash_utils']
+
+from .logger import PackageLogger
+LOGGER = PackageLogger('hidef')
+
+# from . import hidef_finder, weaver, utils
+# from .weaver import *
+# __all__.extend(weaver.__all__)
+
