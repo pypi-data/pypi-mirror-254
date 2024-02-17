d89ee09a6611bd9e07b80ab6980ac499706ee491
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-17 23:42:58 +0000

    Add package aily-sdk/0.10.0/aily_sdk-0.10.0.tar.gz

---
+++ packages/aily-sdk/aily_sdk-0.10.0.tar.gz/aily_sdk-0.10.0/aily_sdk/openapi/__init__.py
@@ -0,0 +1,29 @@
+import requests
+
+
+class OpenAPIClient:
+    def __init__(self, user_access_token):
+        self.user_access_token = user_access_token
+
+    def get(self, url, headers=None, data=None, query=None):
+        _headers = {
+            'Authorization': f'Bearer {self.user_access_token}',
+            'Content-Type': 'application/json',
+        }
+        if headers:
+            _headers.update(headers)
+        resp = requests.get(url, headers=_headers, json=data, params=query)
+
+        print(resp.content)
+        return resp.json()
+
+    def post(self, url, headers=None, data=None):
+        _headers = {
+            'Authorization': f'Bearer {self.user_access_token}',
+            'Content-Type': 'application/json',
+        }
+        if headers:
+            _headers.update(headers)
+        resp = requests.post(url, headers=_headers, json=data)
+        print(resp.content)
+        return resp.json()
