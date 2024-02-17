7c9b9e4ee16742a76d5e8559910c6b303e25e81f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-17 23:42:55 +0000

    Add package django-allauth-ui/0.2.2/django_allauth_ui-0.2.2.tar.gz

---
+++ packages/django-allauth-ui/django_allauth_ui-0.2.2.tar.gz/django_allauth_ui-0.2.2/setup.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['allauth_ui', 'allauth_ui.templatetags']
+
+package_data = \
+{'': ['*'],
+ 'allauth_ui': ['locale/es/LC_MESSAGES/*',
+                'static/allauth_ui/*',
+                'templates/account/*',
+                'templates/socialaccount/*',
+                'templates/socialaccount/snippets/*']}
+
+install_requires = \
+['django-widget-tweaks>=1.4.12,<2.0.0']
+
+setup_kwargs = {
+    'name': 'django-allauth-ui',
+    'version': '0.2.2',
+    'description': '',
+    'long_description': 'None',
+    'author': 'Dani Hodovic',
+    'author_email': 'you@example.com',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'None',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.8,<4.2',
+}
+
+
+setup(**setup_kwargs)
