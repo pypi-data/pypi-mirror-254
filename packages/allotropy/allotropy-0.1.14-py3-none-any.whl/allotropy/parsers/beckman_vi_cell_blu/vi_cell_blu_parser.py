6ea4898bbc2bcfd41b800b10de127ef657309e80
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-17 23:56:52 +0000

    Add package allotropy/0.1.14/allotropy-0.1.14-py3-none-any.whl

---
+++ packages/allotropy/allotropy-0.1.14-py3-none-any.whl/allotropy/parsers/beckman_vi_cell_blu/vi_cell_blu_parser.py
@@ -0,0 +1,187 @@
+from __future__ import annotations
+
+from typing import Any, NamedTuple, Optional
+
+import pandas as pd
+
+from allotropy.allotrope.models.cell_counting_benchling_2023_11_cell_counting import (
+    CellCountingAggregateDocument,
+    CellCountingDetectorDeviceControlAggregateDocument,
+    CellCountingDetectorMeasurementDocumentItem,
+    CellCountingDocumentItem,
+    DataProcessingDocument,
+    DataSystemDocument,
+    DeviceControlDocumentItemModel,
+    DeviceSystemDocument,
+    MeasurementAggregateDocument,
+    Model,
+    ProcessedDataAggregateDocument1,
+    ProcessedDataDocumentItem,
+    SampleDocument,
+)
+from allotropy.allotrope.models.shared.definitions.custom import (
+    TQuantityValueCell,
+    TQuantityValueMicrometer,
+    TQuantityValueMillionCellsPerMilliliter,
+    TQuantityValuePercent,
+    TQuantityValueUnitless,
+)
+from allotropy.constants import ASM_CONVERTER_NAME, ASM_CONVERTER_VERSION
+from allotropy.exceptions import AllotropeConversionError
+from allotropy.named_file_contents import NamedFileContents
+from allotropy.parsers.beckman_vi_cell_blu.constants import (
+    DEFAULT_ANALYST,
+    DEFAULT_MODEL_NUMBER,
+    VICELL_BLU_SOFTWARE_NAME,
+)
+from allotropy.parsers.beckman_vi_cell_blu.vi_cell_blu_reader import ViCellBluReader
+from allotropy.parsers.utils.uuids import random_uuid_str
+from allotropy.parsers.vendor_parser import VendorParser
+
+property_lookup = {
+    "Average viable diameter (μm)": TQuantityValueMicrometer,
+    "Average circularity": TQuantityValueUnitless,
+    "Average diameter (μm)": TQuantityValueMicrometer,
+    "Average viable circularity": TQuantityValueUnitless,
+    "Dilution": TQuantityValueUnitless,
+    "Maximum Diameter (μm)": TQuantityValueMicrometer,
+    "Minimum Diameter (μm)": TQuantityValueMicrometer,
+    "Cell count": TQuantityValueCell,
+    "Total (x10^6) cells/mL": TQuantityValueMillionCellsPerMilliliter,
+    "Viability (%)": TQuantityValuePercent,
+    "Viable cells": TQuantityValueCell,
+    "Viable (x10^6) cells/mL": TQuantityValueMillionCellsPerMilliliter,
+}
+
+
+class _Sample(NamedTuple):
+    data_frame: pd.DataFrame
+    row: int
+
+
+def _get_value(sample: _Sample, column: str) -> Optional[Any]:
+    data_frame, row = sample
+    if column not in data_frame.columns:
+        return None
+    return data_frame[column][row]
+
+
+def _get_value_not_none(sample: _Sample, column: str) -> Any:
+    value = _get_value(sample, column)
+    if value is None:
+        msg = f"Missing value for column '{column}'."
+        raise AllotropeConversionError(msg)
+    return value
+
+
+def get_property_from_sample(sample: _Sample, property_name: str) -> Any:
+    return (
+        property_lookup[property_name](value=value)
+        if (value := _get_value(sample, property_name))
+        else None
+    )
+
+
+class ViCellBluParser(VendorParser):
+    def to_allotrope(self, named_file_contents: NamedFileContents) -> Model:
+        contents, filename = named_file_contents
+        return self._get_model(ViCellBluReader.read(contents), filename)
+
+    def _get_model(self, data: pd.DataFrame, filename: str) -> Model:
+        return Model(
+            field_asm_manifest="http://purl.allotrope.org/manifests/cell-counting/BENCHLING/2023/11/cell-counting.manifest",
+            cell_counting_aggregate_document=CellCountingAggregateDocument(
+                device_system_document=DeviceSystemDocument(
+                    model_number=DEFAULT_MODEL_NUMBER,
+                ),
+                data_system_document=DataSystemDocument(
+                    file_name=filename,
+                    software_name=VICELL_BLU_SOFTWARE_NAME,
+                    ASM_converter_name=ASM_CONVERTER_NAME,
+                    ASM_converter_version=ASM_CONVERTER_VERSION,
+                ),
+                cell_counting_document=self._get_cell_counting_document(data),
+            ),
+        )
+
+    def _get_cell_counting_document(
+        self, data: pd.DataFrame
+    ) -> list[CellCountingDocumentItem]:
+        return [
+            self._get_cell_counting_document_item(_Sample(data, i))
+            for i in range(len(data.index))
+            if _get_value(_Sample(data, i), "Cell count")
+        ]
+
+    def _get_cell_counting_document_item(
+        self, sample: _Sample
+    ) -> CellCountingDocumentItem:
+        return CellCountingDocumentItem(
+            analyst=_get_value(sample, "Analysis by") or DEFAULT_ANALYST,
+            measurement_aggregate_document=MeasurementAggregateDocument(
+                measurement_document=[
+                    CellCountingDetectorMeasurementDocumentItem(
+                        measurement_time=self._get_date_time(
+                            _get_value_not_none(sample, "Analysis date/time")
+                        ),
+                        measurement_identifier=random_uuid_str(),
+                        sample_document=SampleDocument(sample_identifier=_get_value(sample, "Sample ID")),  # type: ignore[arg-type]
+                        device_control_aggregate_document=CellCountingDetectorDeviceControlAggregateDocument(
+                            device_control_document=[
+                                DeviceControlDocumentItemModel(
+                                    device_type="brightfield imager (cell counter)",
+                                    detection_type="brightfield",
+                                )
+                            ]
+                        ),
+                        processed_data_aggregate_document=ProcessedDataAggregateDocument1(
+                            processed_data_document=[
+                                ProcessedDataDocumentItem(
+                                    data_processing_document=DataProcessingDocument(
+                                        cell_type_processing_method=_get_value(
+                                            sample, "Cell type"
+                                        ),
+                                        minimum_cell_diameter_setting=get_property_from_sample(
+                                            sample, "Minimum Diameter (μm)"
+                                        ),
+                                        maximum_cell_diameter_setting=get_property_from_sample(
+                                            sample, "Maximum Diameter (μm)"
+                                        ),
+                                        cell_density_dilution_factor=get_property_from_sample(
+                                            sample, "Dilution"
+                                        ),
+                                    ),
+                                    viability__cell_counter_=get_property_from_sample(
+                                        sample, "Viability (%)"
+                                    ),
+                                    viable_cell_density__cell_counter_=get_property_from_sample(
+                                        sample, "Viable (x10^6) cells/mL"
+                                    ),
+                                    total_cell_count=get_property_from_sample(
+                                        sample, "Cell count"
+                                    ),
+                                    total_cell_density__cell_counter_=get_property_from_sample(
+                                        sample, "Total (x10^6) cells/mL"
+                                    ),
+                                    average_total_cell_diameter=get_property_from_sample(
+                                        sample, "Average diameter (μm)"
+                                    ),
+                                    average_live_cell_diameter__cell_counter_=get_property_from_sample(
+                                        sample, "Average viable diameter (μm)"
+                                    ),
+                                    viable_cell_count=get_property_from_sample(
+                                        sample, "Viable cells"
+                                    ),
+                                    average_total_cell_circularity=get_property_from_sample(
+                                        sample, "Average circularity"
+                                    ),
+                                    average_viable_cell_circularity=get_property_from_sample(
+                                        sample, "Average viable circularity"
+                                    ),
+                                ),
+                            ]
+                        ),
+                    )
+                ],
+            ),
+        )
---
+++ packages/allotropy/allotropy-0.1.14-py3-none-any.whl/allotropy/parsers/beckman_vi_cell_xr/vi_cell_xr_reader.py
@@ -0,0 +1,69 @@
+from __future__ import annotations
+
+import re
+from typing import Any
+
+import pandas as pd
+
+from allotropy.parsers.beckman_vi_cell_xr.constants import (
+    DATE_HEADER,
+    DEFAULT_VERSION,
+    MODEL_RE,
+    XrVersion,
+)
+from allotropy.types import IOType
+
+
+class ViCellXRReader:
+    def __init__(self, contents: IOType) -> None:
+        self.contents = contents
+        self.file_info = self._get_file_info()
+        self.file_version = self._get_file_version()
+        self.data = self._read_data()
+
+    def _read_data(self) -> pd.DataFrame:
+        header_row = 4
+        if self.file_version == XrVersion._2_04:
+            header_row = 3
+
+        header = self._get_file_header(header_row)
+        skiprows = header_row + 1
+        file_data: pd.DataFrame = pd.read_excel(
+            self.contents,
+            skiprows=skiprows,
+            names=header,
+            parse_dates=[DATE_HEADER[self.file_version]],
+            date_format="%d %b %Y  %I:%M:%S %p",
+        )
+        return file_data
+
+    def _get_file_header(self, header_row: int) -> list[str]:
+        """Combine the two rows that forms the header."""
+        header = pd.read_excel(
+            self.contents,
+            nrows=2,
+            skiprows=header_row,
+            header=None,
+        ).fillna("")
+
+        header_list: list[str] = header.agg(
+            lambda x: " ".join(x).replace(" /ml", "/ml").strip()
+        ).to_list()
+        return header_list
+
+    def _get_file_info(self) -> pd.Series[Any]:
+        info: pd.Series[Any] = pd.read_excel(
+            self.contents, nrows=3, header=None, usecols=[0]
+        ).squeeze()
+        info.index = pd.Index(["model", "filepath", "serial"])
+        return info
+
+    def _get_file_version(self) -> XrVersion:
+        match = re.match(MODEL_RE, self.file_info["model"], flags=re.IGNORECASE)
+        try:
+            version = match.groupdict()["version"]  # type: ignore[union-attr]
+        except AttributeError:
+            return DEFAULT_VERSION
+        # TODO: raise exception for unsupported versions
+        version = ".".join(version.split(".")[0:2])
+        return XrVersion(version)
