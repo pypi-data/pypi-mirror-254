5b711ec1859437ccb91f6ddb46c0342c7c39bb78
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-17 23:57:42 +0000

    Add package system-monitoring-package/0.0.1/system_monitoring_package-0.0.1.tar.gz

---
+++ packages/system-monitoring-package/system_monitoring_package-0.0.1.tar.gz/system_monitoring_package-0.0.1/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+ 
+# classifiers = [
+#   'Development Status :: 5 - Production/Stable',
+#   'Intended Audience :: Education',
+#   'Operating System :: Microsoft :: Windows :: Windows 10',
+#   'License :: OSI Approved :: MIT License',
+#   'Programming Language :: Python :: 3'
+# ]
+ 
+setup(
+  name='system_monitoring_package',
+  version='0.0.1',
+  description='A very basic monitoring package',
+  # long_description=open('README.txt').read() + '\n\n' + open('CHANGELOG.txt').read(),
+  # url='',  
+  author='Ritik Saxena',
+  author_email='ritiksaxena90@gmail.com',
+  # license='MIT', 
+  # classifiers=classifiers,
+  keywords='package', 
+  packages=find_packages(),
+  install_requires=['pandas','streamlit','datetime','numpy'] 
+)
