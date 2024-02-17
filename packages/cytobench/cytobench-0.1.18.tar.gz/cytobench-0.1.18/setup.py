aa5072cc6d131b9ab674589baf2303c7b244db38
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-17 23:57:56 +0000

    Add package cytobench/0.1.18/cytobench-0.1.18.tar.gz

---
+++ packages/cytobench/cytobench-0.1.18.tar.gz/cytobench-0.1.18/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='cytobench',
+    version='0.1.18',
+    packages=find_packages(),
+    install_requires=[
+        'numpy==1.23.5',
+        'pandas==1.5.3',
+        'scipy==1.11.4',
+        'xgboost==2.0.3',
+        'scikit-learn==1.2.2',
+        'requests',
+        'mega.py',
+    ],
+    author="Nicolo' Lazzaro",
+    author_email='nlazzaro@fbk.eu',
+    description='Benchmarking library for generative algorithms in omics data',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    # url='https://github.com/username/package_name',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+)
+
+'''
+    include_package_data=True,
+    package_data={
+        'cytobench': ['config.yaml'],
+    },
+'''
