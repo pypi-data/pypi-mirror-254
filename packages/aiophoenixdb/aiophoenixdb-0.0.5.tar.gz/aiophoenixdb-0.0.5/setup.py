d9d1749d81bd753797134427d548c1f2fd7de4e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-17 23:41:33 +0000

    Add package aiophoenixdb/0.0.5/aiophoenixdb-0.0.5.tar.gz

---
+++ packages/aiophoenixdb/aiophoenixdb-0.0.5.tar.gz/aiophoenixdb-0.0.5/setup.py
@@ -0,0 +1,35 @@
+import os
+from setuptools import setup, find_packages
+
+files = []
+for item in os.walk("pyi/aiophoenixdb"):
+    base_path = item[0][4:]
+    files.append((base_path, [os.path.join(item[0], f) for f in item[-1]]))
+
+with open("README.rst", "r") as f:
+    long_desc_text = f.read()
+
+setup(
+    name="aiophoenixdb"
+    , version="0.0.5"
+    , author="Nick Hao"
+    , author_email="nickwike72@gmail.com"
+    , package_dir={"aiophoenixdb": "./src/aiophoenixdb"}
+    , description="Asyncio with phoenixdb"
+    , packages=find_packages(include=['aiophoenixdb*'], where="./src")
+    , data_files=files
+    , long_description_content_type="text/x-rst"
+    , long_description=long_desc_text
+    , install_requires=[
+        "betterproto>=2.0.0b6",
+        "aiohttp>=3.8.6",
+        "requests-gssapi",
+        "requests~=2.31.0",
+        "gssapi"
+    ]
+    , classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ]
+)
