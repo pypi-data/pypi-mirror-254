6fb1350be1645eb0739556ad6fa42f4244911b96
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-17 23:38:34 +0000

    Add package qhxc-api/0.1.2/qhxc_api-0.1.2.tar.gz

---
+++ packages/qhxc-api/qhxc_api-0.1.2.tar.gz/qhxc_api-0.1.2/setup.py
@@ -0,0 +1,10 @@
+# setup.py
+from setuptools import setup, find_packages
+
+setup(
+    name="qhxc_api",
+    version="0.1.2",
+    description="An exchange open api SDK",
+    packages=find_packages(),
+    python_requires='>=3.6',
+)
