6108423611d9f0134322fc1e334ba7e424e6f63e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-17 23:38:56 +0000

    Add package SigProfilerMatrixGenerator/1.2.23/SigProfilerMatrixGenerator-1.2.23-py3-none-any.whl

---
+++ packages/SigProfilerMatrixGenerator/SigProfilerMatrixGenerator-1.2.23-py3-none-any.whl/SigProfilerMatrixGenerator/version.py
@@ -0,0 +1,7 @@
+
+# THIS FILE IS GENERATED FROM SIGPROFILEMATRIXGENERATOR SETUP.PY
+short_version = '1.2.23'
+version = '1.2.23'
+Update = 'v1.2.23: Add SV and CNV matrix generation to CLI. Add Dockerfile.'
+
+	
