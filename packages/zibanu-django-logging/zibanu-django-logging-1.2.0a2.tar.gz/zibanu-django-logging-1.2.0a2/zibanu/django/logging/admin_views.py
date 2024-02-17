da8598cd698f95da0cf2991eb5d5cfbfc2dc1872
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-17 23:34:34 +0000

    Add package zibanu-django-logging/1.2.0a2/zibanu-django-logging-1.2.0a2.tar.gz

---
+++ packages/zibanu-django-logging/zibanu-django-logging-1.2.0a2.tar.gz/zibanu-django-logging-1.2.0a2/zibanu/django/logging/admin_views.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+
+#  Developed by CQ Inversiones SAS. Copyright ©. 2019 - 2024. All rights reserved.
+#  Desarrollado por CQ Inversiones SAS. Copyright ©. 2019 - 2024. Todos los derechos reservado
+
+# ****************************************************************
+# IDE:          PyCharm
+# Developed by: macercha
+# Date:         30/01/24 11:46
+# Project:      Zibanu - Django
+# Module Name:  admin_views
+# Description:
+# ****************************************************************
+# Default imports
+import logging
+import traceback
+from django.utils.translation import gettext_lazy as _
+from django.contrib import admin
+from django.forms import Select
+from zibanu.django.logging.models import AuditEntity
+from zibanu.django.utils import get_model_list
+
+MODEL_LIST_CHOICES = tuple(zip(get_model_list(), get_model_list()))
+
+class AuditEntityAdmin(admin.ModelAdmin):
+    list_display = ("model_name", "enabled")
+    fieldsets = (
+        (None, {"fields": ("model_name",)}),
+        (_("Actions"), {"fields": ("on_create", "on_update", "on_delete")}),
+        (None, {"fields": ("enabled",)})
+    )
+
+    def get_form(self, request, obj=None, **kwargs):
+        form = super(AuditEntityAdmin, self).get_form(request, obj, **kwargs)
+        form.base_fields["model_name"].widget = Select(choices=MODEL_LIST_CHOICES)
+        return form
