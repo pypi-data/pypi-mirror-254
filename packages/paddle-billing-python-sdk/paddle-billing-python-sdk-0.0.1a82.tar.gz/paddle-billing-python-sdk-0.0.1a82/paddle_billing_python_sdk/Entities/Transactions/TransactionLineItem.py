d9e0566f7fea2d049c41e0c5434fe6a49b54c3e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-17 23:42:52 +0000

    Add package paddle-billing-python-sdk/0.0.1a82/paddle-billing-python-sdk-0.0.1a82.tar.gz

---
+++ packages/paddle-billing-python-sdk/paddle-billing-python-sdk-0.0.1a82.tar.gz/paddle-billing-python-sdk-0.0.1a82/paddle_billing_python_sdk/Entities/Transactions/TransactionLineItem.py
@@ -0,0 +1,36 @@
+from __future__  import annotations
+from dataclasses import dataclass
+
+from paddle_billing_python_sdk.Entities.Product             import Product  # TODO changed from ProductWithIncludes
+from paddle_billing_python_sdk.Entities.ProductWithIncludes import ProductWithIncludes
+
+from paddle_billing_python_sdk.Entities.Transactions.TransactionProration import TransactionProration
+
+from paddle_billing_python_sdk.Entities.Shared.Totals     import Totals
+from paddle_billing_python_sdk.Entities.Shared.UnitTotals import UnitTotals
+
+
+@dataclass
+class TransactionLineItem:
+    id:          str
+    price_id:    str
+    quantity:    int
+    proration:   TransactionProration | None
+    tax_rate:    str
+    unit_totals: UnitTotals
+    totals:      Totals
+    product:     Product  # TODO changed from ProductWithIncludes
+
+
+    @staticmethod
+    def from_dict(data: dict) -> TransactionLineItem:
+        return TransactionLineItem(
+            id          = data['id'],
+            price_id    = data['price_id'],
+            quantity    = data['quantity'],
+            proration   = TransactionProration.from_dict(data['proration']) if data.get('proration') else None,
+            tax_rate    = data['tax_rate'],
+            unit_totals = UnitTotals.from_dict(data['unit_totals']),
+            totals      = Totals.from_dict(data['totals']),
+            product     = Product.from_dict(data['product']),  # TODO changed from ProductWithIncludes
+        )
