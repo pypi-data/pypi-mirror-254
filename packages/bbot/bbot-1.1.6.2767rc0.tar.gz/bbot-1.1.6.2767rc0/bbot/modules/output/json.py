c2d29ba4d364b901d7255e1d8769f0311c7d3c0f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-17 23:31:08 +0000

    Add package bbot/1.1.6.2767rc0/bbot-1.1.6.2767rc0.tar.gz

---
+++ packages/bbot/bbot-1.1.6.2767rc0.tar.gz/bbot-1.1.6.2767rc0/bbot/modules/output/json.py
@@ -0,0 +1,33 @@
+import json
+from contextlib import suppress
+
+from bbot.modules.output.base import BaseOutputModule
+
+
+class JSON(BaseOutputModule):
+    watched_events = ["*"]
+    meta = {"description": "Output to Newline-Delimited JSON (NDJSON)"}
+    options = {"output_file": "", "console": False}
+    options_desc = {"output_file": "Output to file", "console": "Output to console"}
+    _preserve_graph = True
+
+    async def setup(self):
+        self._prep_output_dir("output.ndjson")
+        return True
+
+    async def handle_event(self, event):
+        event_str = json.dumps(dict(event))
+        if self.file is not None:
+            self.file.write(event_str + "\n")
+            self.file.flush()
+        if self.config.get("console", False) or "human" not in self.scan.modules:
+            self.stdout(event_str)
+
+    async def cleanup(self):
+        if getattr(self, "_file", None) is not None:
+            with suppress(Exception):
+                self.file.close()
+
+    async def report(self):
+        if self._file is not None:
+            self.info(f"Saved JSON output to {self.output_file}")
