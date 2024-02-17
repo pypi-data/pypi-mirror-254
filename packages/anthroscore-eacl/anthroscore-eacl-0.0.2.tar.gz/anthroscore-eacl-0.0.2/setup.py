bebd3ac11f1b94437ac9f995cc2b20a61f875940
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-17 23:32:10 +0000

    Add package anthroscore-eacl/0.0.2/anthroscore-eacl-0.0.2.tar.gz

---
+++ packages/anthroscore-eacl/anthroscore-eacl-0.0.2.tar.gz/anthroscore-eacl-0.0.2/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
+setup(
+    name='anthroscore-eacl',
+    version = "0.0.2",
+
+
+    url='https://github.com/myracheng/anthroscore',
+    author='Myra Cheng',
+    author_email='myra@cs.stanford.edu',
+description = "Package to compute AnthroScore, a computational linguistic measure of anthropomorphism in text",
+    packages=['src'],
+    license='BSD 2-clause',
+    install_requires=['regex',
+                      'spacy==3.7.2',
+                      'numpy',
+                      'scipy',
+                      'transformers',
+                      'pandas',
+                      'torch',
+                     ],
+	long_description=long_description,
+    long_description_content_type='text/markdown',
+    classifiers=[     
+        'Programming Language :: Python :: 3.11',
+    ],
+)
