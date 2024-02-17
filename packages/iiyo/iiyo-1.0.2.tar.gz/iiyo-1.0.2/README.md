1569dbc4d6f00dd2a857018a27ded9349c2ddc1e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-17 23:46:55 +0000

    Add package iiyo/1.0.2/iiyo-1.0.2.tar.gz

---
+++ packages/iiyo/iiyo-1.0.2.tar.gz/iiyo-1.0.2/README.md
@@ -0,0 +1,10 @@
+﻿iiyo sinhala Prasthawa Pirulu
+
+Developed by @dotLK from sasindu (c) 3:43 AM 1/31/2024
+
+
+Check out: https://www.youtube.com/@sasindu
+
+You can use
+
+* github.com/dotlk/iiyo
---
+++ packages/iiyo/iiyo-1.0.2.tar.gz/iiyo-1.0.2/iiyo/random_names/__init__.py
@@ -0,0 +1,54 @@
+from __future__ import unicode_literals
+from os.path import abspath, join, dirname
+import random
+
+full_path = lambda filename: abspath(join(dirname(__file__), filename))
+
+#path = os.path.abspath("new.txt")
+#print(path)
+def name_array(file):
+    with open(file) as fp:
+        new_list = []
+        for line in fp:
+            new_list.append(line.strip())
+    return new_list
+
+
+file = full_path("male_first_names.txt")
+print(file)
+male_names_file = name_array(file)
+
+file = full_path("female_first_names.txt")
+female_names_file = name_array(file)
+
+file = full_path("last_names.txt")
+last_names_file = name_array(file)
+
+file = full_path("home_address.txt")
+home_address_file = name_array(file)
+
+file = full_path("city.txt")
+city_name_file = name_array(file)
+
+
+def quin():
+    result = f"['Avihinsaka uwathy','Nihada Mawatha ','Palu Niwahana']"
+    return result
+
+
+
+def femalename():
+    result = f"[{random.choice(female_names_file)} {random.choice(last_names_file)}]"
+    return result
+
+
+
+def femaleaddress():
+    result = f"[{random.choice(female_names_file)} {random.choice(last_names_file)},{random.choice(home_address_file)},{random.choice(city_name_file)}]"
+    return result
+
+
+
+def maleaddress():
+    result = f"['{random.choice(male_names_file)} {random.choice(last_names_file)}','{random.choice(home_address_file)}','{random.choice(city_name_file)}']"
+    return result
