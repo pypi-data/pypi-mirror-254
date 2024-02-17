23565f7da8dbfe0319cdb5237ff95e3140359556
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-17 23:38:18 +0000

    Add package systemq/1.0.0/systemq-1.0.0-cp38-cp38-win_amd64.whl

---
+++ packages/systemq/systemq-1.0.0-cp38-cp38-win_amd64.whl/systemq/qos_tools/__about__.py
@@ -0,0 +1,25 @@
+'''Text about Quantum Operating System (QOS): Tools'''
+
+__title__ = 'Quantum Operating System (QOS): Tools'
+
+__package_name__ = 'qos_tools'
+
+__author__ = 'Li Zhiyuan'
+
+__contributor__=[
+
+]
+
+__description__ = ' '.join('''
+Quantum Operating System (QOS): Tools.
+'''.strip().split())
+
+__email__ = 'lizhiyuan@baqis.ac.cn'
+
+__version__ = "0.0.1"
+
+__license__ = ''
+
+__copyright__ = ''
+
+__url__ = ''
---
+++ packages/systemq/systemq-1.0.0-cp38-cp38-win_amd64.whl/systemq/src/systemq.h
@@ -0,0 +1,4 @@
+#ifndef __SYSTEMQ_H__
+#define __SYSTEMQ_H__
+
+#endif // __SYSTEMQ_H__
