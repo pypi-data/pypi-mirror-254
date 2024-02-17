bad86492df59205890d56db018dc749e23482f3a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-17 23:37:19 +0000

    Add package gaussian-adaptive-attention/0.1.3/gaussian_adaptive_attention-0.1.3.tar.gz

---
+++ packages/gaussian-adaptive-attention/gaussian_adaptive_attention-0.1.3.tar.gz/gaussian_adaptive_attention-0.1.3/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='gaussian_adaptive_attention',
+    version='0.1.3',
+    author='Georgios Ioannides',
+    author_email='gioannid@alumni.cmu.edu',
+    description='A Gaussian Adaptive Attention module for PyTorch',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/yourusername/GaussianAdaptiveAttention',
+    packages=find_packages(),
+    install_requires=[
+        'torch',
+    ],
+    license='Apache 2.0',
+    classifiers=[
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+    ],
+)
+
