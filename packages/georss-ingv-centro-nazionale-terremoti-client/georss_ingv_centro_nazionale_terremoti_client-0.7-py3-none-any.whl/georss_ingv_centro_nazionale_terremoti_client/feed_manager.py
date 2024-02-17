5a1033f1f810e46b7c448f0c3ebdd9a650df7360
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-17 23:43:37 +0000

    Add package georss-ingv-centro-nazionale-terremoti-client/0.7/georss_ingv_centro_nazionale_terremoti_client-0.7-py3-none-any.whl

---
+++ packages/georss-ingv-centro-nazionale-terremoti-client/georss_ingv_centro_nazionale_terremoti_client-0.7-py3-none-any.whl/georss_ingv_centro_nazionale_terremoti_client/feed_manager.py
@@ -0,0 +1,27 @@
+"""INGV Centro Nazionale Terremoti (Earthquakes) feed manager."""
+from __future__ import annotations
+
+from georss_client.feed_manager import FeedManagerBase
+
+from .feed import IngvCentroNazionaleTerremotiFeed
+
+
+class IngvCentroNazionaleTerremotiFeedManager(FeedManagerBase):
+    """Feed Manager for INGV Centro Nazionale Terremoti feed."""
+
+    def __init__(
+        self,
+        generate_callback,
+        update_callback,
+        remove_callback,
+        coordinates: tuple[float, float],
+        filter_radius: float = None,
+        filter_minimum_magnitude: float = None,
+    ):
+        """Initialize the INGV Centro Nazionale Terremoti Feed Manager."""
+        feed = IngvCentroNazionaleTerremotiFeed(
+            coordinates,
+            filter_radius=filter_radius,
+            filter_minimum_magnitude=filter_minimum_magnitude,
+        )
+        super().__init__(feed, generate_callback, update_callback, remove_callback)
