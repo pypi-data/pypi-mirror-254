4fcefec043a76ccee7e8dd1a2b8272a557a84195
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-17 23:58:08 +0000

    Add package prettyPlot/0.0.24/prettyPlot-0.0.24.tar.gz

---
+++ packages/prettyPlot/prettyPlot-0.0.24.tar.gz/prettyPlot-0.0.24/setup.py
@@ -0,0 +1,20 @@
+import os
+from setuptools import setup
+
+setup(
+    name='prettyPlot',
+    version='0.0.24',
+    description="Plotting tools for journal quality figures",
+    url="https://github.com/malihass/prettyPlot",
+    author="Malik Hassanaly",
+    license="BSD 3-Clause",
+    package_dir={"prettyPlot": "prettyPlot"},
+    classifiers=[
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: BSD License",
+        "Natural Language :: English",
+        "Programming Language :: Python :: 3.10",
+    ],
+    python_requires='>=3.10',
+    install_requires=["numpy","matplotlib>=3.7.3", "imageio"],
+)
