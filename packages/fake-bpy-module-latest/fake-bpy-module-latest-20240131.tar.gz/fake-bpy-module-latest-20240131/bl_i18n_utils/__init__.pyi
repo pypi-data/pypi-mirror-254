d8d160c6a107b173e9c94c02a010aaff4141418a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-17 23:38:47 +0000

    Add package fake-bpy-module-latest/20240131/fake-bpy-module-latest-20240131.tar.gz

---
+++ packages/fake-bpy-module-latest/fake-bpy-module-latest-20240131.tar.gz/fake-bpy-module-latest-20240131/bl_i18n_utils/__init__.pyi
@@ -0,0 +1,11 @@
+import sys
+import typing
+from . import utils_cli
+from . import settings
+from . import utils
+from . import bl_extract_messages
+from . import merge_po
+from . import utils_rtl
+from . import utils_languages_menu
+
+GenericType = typing.TypeVar("GenericType")
