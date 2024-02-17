530094b9dbf9a5045d214a0e3b5b8b16a644f1cb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-17 23:42:30 +0000

    Add package ioproc/2.1.1/ioproc-2.1.1-py3-none-any.whl

---
+++ packages/ioproc/ioproc-2.1.1-py3-none-any.whl/ioproc/exceptions.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+__author__ = ["Benjamin Fuchs"]
+__copyright__ = "Copyright 2021, German Aerospace Center (DLR)"
+__credits__ = []
+
+__license__ = "MIT"
+__version__ = "1.0.2"
+__maintainer__ = "Felix Nitsch"
+__email__ = "ioProc@dlr.de"
+__status__ = "Production"
+
+
+class UnknownActionModule(Exception):
+    pass
+
+
+class UnknownAction(Exception):
+    pass
+
+
+class CheckPointError(Exception):
+    pass
+
+
+class ActionParamsError(Exception):
+    pass
+
+
+class MissingOverridesError(Exception):
+    pass
