cc22ad6fcbe9802ee2e58c40d6a47b7b18ec6861
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-17 23:46:44 +0000

    Add package invenio-users-resources/4.0.0/invenio-users-resources-4.0.0.tar.gz

---
+++ packages/invenio-users-resources/invenio-users-resources-4.0.0.tar.gz/invenio-users-resources-4.0.0/invenio_users_resources/records/models.py
@@ -0,0 +1,127 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2022 TU Wien.
+#
+# Invenio-Users-Resources is free software; you can redistribute it and/or
+# modify it under the terms of the MIT License; see LICENSE file for more
+# details.
+
+"""Base model classes for user and group management in Invenio."""
+
+from abc import ABC, abstractmethod
+
+from invenio_accounts.proxies import current_datastore
+from invenio_db import db
+
+
+class MockModel(dict, ABC):
+    """Model class that does not correspondond to a database table.
+
+    Since we already have all information about the required entities
+    stored in the database and just want to provide a central API to
+    manage these entities as one, we do not want to store any of the
+    information in the database again.
+    Instead, we provide a mock model that provides the model interface
+    that's expected by API classes as far as necessary.
+    """
+
+    def __init__(self, data=None, model_obj=None, **kwargs):
+        """Constructor."""
+        data.update(kwargs)
+        super().__init__(data)
+        self._model_obj = model_obj
+
+    @property
+    @abstractmethod
+    def model_obj(self):
+        """The actual model object behind this mock model."""
+        return None
+
+    @property
+    def id(self):
+        """User identifier."""
+        return self.data["id"]
+
+    @property
+    def created(self):
+        """When the user was created."""
+        return self.model_obj.created
+
+    @property
+    def updated(self):
+        """When the user was last updated."""
+        return self.model_obj.updated
+
+    @property
+    def version_id(self):
+        """Used by SQLAlchemy for optimistic concurrency control."""
+        return self.model_obj.version_id
+
+    @property
+    def data(self):
+        """Get the user's data by decoding the JSON."""
+        return dict(self)
+
+    @property
+    def json(self):
+        """Provide the user's data as a JSON/dict blob."""
+        return dict(self)
+
+    @property
+    def is_deleted(self):
+        """Boolean flag to determine if a user is soft deleted."""
+        # TODO
+        return False
+
+    @property
+    def blocked_at(self):
+        """Date when the user was blocked, if any."""
+        return self.model_obj.blocked_at
+
+    @blocked_at.setter
+    def blocked_at(self, value):
+        self.model_obj.blocked_at = value
+
+    @property
+    def verified_at(self):
+        """Date when the user was verified, if any."""
+        return self.model_obj.verified_at
+
+    @verified_at.setter
+    def verified_at(self, value):
+        self.model_obj.verified_at = value
+
+    @property
+    def active(self):
+        """Boolean flag to determine if a user is active."""
+        return self.model_obj.active
+
+    @active.setter
+    def active(self, value):
+        self.model_obj.active = value
+
+
+class UserAggregateModel(MockModel):
+    """Mock model for glueing together various parts of user info."""
+
+    @property
+    def model_obj(self):
+        """The actual model object behind this mock model."""
+        if self._model_obj is None:
+            id_ = self.data.get("id")
+            email = self.data.get("email")
+            with db.session.no_autoflush:
+                self._model_obj = current_datastore.get_user(id_ or email)
+        return self._model_obj
+
+
+class GroupAggregateModel(MockModel):
+    """Mock model for glueing together various parts of user group info."""
+
+    @property
+    def model_obj(self):
+        """The actual model object behind this mock model."""
+        if self._model_obj is None:
+            name = self.data.get("id")
+            self._model_obj = current_datastore.find_role(name)
+        return self._model_obj
---
+++ packages/invenio-users-resources/invenio-users-resources-4.0.0.tar.gz/invenio-users-resources-4.0.0/invenio_users_resources/services/users/config.py
@@ -0,0 +1,153 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2022 TU Wien.
+# Copyright (C) 2022 CERN.
+#
+# Invenio-Users-Resources is free software; you can redistribute it and/or
+# modify it under the terms of the MIT License; see LICENSE file for more
+# details.
+
+"""Users service configuration."""
+
+from invenio_records_resources.services import RecordServiceConfig, pagination_links
+from invenio_records_resources.services.base.config import (
+    ConfiguratorMixin,
+    FromConfig,
+    FromConfigSearchOptions,
+    SearchOptionsMixin,
+)
+from invenio_records_resources.services.records.config import SearchOptions
+from invenio_records_resources.services.records.params import (
+    FacetsParam,
+    PaginationParam,
+    QueryStrParam,
+    SortParam,
+)
+from invenio_records_resources.services.records.queryparser import (
+    QueryParser,
+    SearchFieldTransformer,
+)
+
+from ...records.api import UserAggregate
+from ..common import Link
+from ..params import FixedPagination
+from ..permissions import UsersPermissionPolicy
+from ..schemas import UserSchema
+from ..users import facets
+from .results import UserItem, UserList
+from .search_params import ModerationFilterParam
+
+
+class UserSearchOptions(SearchOptions, SearchOptionsMixin):
+    """Search options."""
+
+    pagination_options = {
+        "default_results_per_page": 10,
+        "default_max_results": 10,
+    }
+    query_parser_cls = QueryParser.factory(
+        tree_transformer_cls=SearchFieldTransformer,
+        fields=["username^2", "email^2", "profile.full_name^3", "profile.affiliations"],
+        allow_list=["username", "email"],  # mapped fields are added on the query parser
+        mapping={
+            "affiliation": "profile.affiliations",
+            "affiliations": "profile.affiliations",
+            "full_name": "profile.full_name",
+            "fullname": "profile.full_name",
+            "name": "profile.full_name",
+        },
+    )
+
+    params_interpreters_cls = [
+        QueryStrParam,
+        SortParam,
+        FixedPagination,
+        FacetsParam,
+        ModerationFilterParam.factory(param="is_blocked", field="blocked_at"),
+        ModerationFilterParam.factory(param="is_verified", field="verified_at"),
+        ModerationFilterParam.factory(param="is_active", field="active"),
+    ]
+
+    facets = {
+        "email_domain": facets.email_domain,
+        "affiliations": facets.affiliations,
+    }
+
+
+class AdminUserSearchOptions(UserSearchOptions):
+    """Admin Search options."""
+
+    pagination_options = {
+        "default_results_per_page": 10,
+        "default_max_results": 100,
+    }
+
+    query_parser_cls = QueryParser.factory(
+        tree_transformer_cls=SearchFieldTransformer,
+        fields=[
+            "id",
+            "username^2",
+            "email^2",
+            "profile.full_name^3",
+            "profile.affiliations",
+        ],
+        allow_list=["id", "username", "email"],
+        mapping={
+            "affiliation": "profile.affiliations",
+            "affiliations": "profile.affiliations",
+            "full_name": "profile.full_name",
+            "fullname": "profile.full_name",
+            "name": "profile.full_name",
+        },
+    )
+
+    params_interpreters_cls = [
+        QueryStrParam,
+        SortParam,
+        PaginationParam,
+        FacetsParam,
+        ModerationFilterParam.factory(param="is_blocked", field="blocked_at"),
+        ModerationFilterParam.factory(param="is_verified", field="verified_at"),
+        ModerationFilterParam.factory(param="is_active", field="active"),
+    ]
+
+
+class UsersServiceConfig(RecordServiceConfig, ConfiguratorMixin):
+    """Requests service configuration."""
+
+    # common configuration
+    permission_policy_cls = UsersPermissionPolicy
+    result_item_cls = UserItem
+    result_list_cls = UserList
+    search = FromConfigSearchOptions(
+        "USERS_RESOURCES_SEARCH",
+        "USERS_RESOURCES_SORT_OPTIONS",
+        "USERS_RESOURCES_SEARCH_FACETS",
+        search_option_cls=UserSearchOptions,
+    )
+    # For admin user
+    search_all = FromConfigSearchOptions(
+        "USERS_RESOURCES_SEARCH",
+        "USERS_RESOURCES_SORT_OPTIONS",
+        "USERS_RESOURCES_SEARCH_FACETS",
+        search_option_cls=AdminUserSearchOptions,
+    )
+    # search = UserSearchOptions
+    # specific configuration
+    service_id = "users"
+    record_cls = UserAggregate
+    schema = FromConfig("USERS_RESOURCES_SERVICE_SCHEMA", UserSchema)
+    indexer_queue_name = "users"
+    index_dumper = None
+
+    # links configuration
+    links_item = {
+        "self": Link("{+api}/users/{id}"),
+        "avatar": Link("{+api}/users/{id}/avatar.svg"),
+        # TODO missing moderation actions based on permissions
+    }
+    links_search = pagination_links("{+api}/users{?args*}")
+
+    components = [
+        # order of components are important!
+    ]
