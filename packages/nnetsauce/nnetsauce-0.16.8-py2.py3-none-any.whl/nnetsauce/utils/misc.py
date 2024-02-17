75c12d081e3544783e4d88d0488b95bfd22a00a6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-17 23:53:51 +0000

    Add package nnetsauce/0.16.8/nnetsauce-0.16.8-py2.py3-none-any.whl

---
+++ packages/nnetsauce/nnetsauce-0.16.8-py2.py3-none-any.whl/nnetsauce/utils/misc.py
@@ -0,0 +1,43 @@
+import numpy as np
+
+
+# merge two dictionaries
+def merge_two_dicts(x, y):
+    z = x.copy()
+    z.update(y)
+    return z
+
+
+# check if x is int
+def is_int(x):
+    try:
+        return int(x) == x
+    except:
+        return False
+
+
+# check if x is float
+def is_float(x):
+    return isinstance(x, float)
+
+
+# check if the response contains only integers
+def is_factor(y):
+    # return not np.mod(y, 1).any()
+    return not np.array([is_float(y[i]) for i in range(len(y))]).all()
+
+
+# flatten list of lists
+# flatten = lambda l: [item for sublist in l for item in sublist]
+def flatten(x):
+    res = []
+
+    for elt1 in x:
+        try:
+            for elt2 in elt1:
+                res.append(elt2)
+
+        except:
+            res.append(elt1)
+
+    return res
