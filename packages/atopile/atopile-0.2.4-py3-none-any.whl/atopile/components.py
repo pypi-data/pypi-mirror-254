709eca3b4e75dc4053caee15671fe8e1a957d40a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-17 23:38:46 +0000

    Add package atopile/0.2.4/atopile-0.2.4-py3-none-any.whl

---
+++ packages/atopile/atopile-0.2.4-py3-none-any.whl/atopile/components.py
@@ -0,0 +1,280 @@
+import logging
+from functools import cache
+from pathlib import Path
+
+import pandas as pd
+import pint
+
+from atopile import address, errors, instance_methods
+from atopile.address import AddrStr
+from atopile.front_end import Physical
+
+log = logging.getLogger(__name__)
+
+
+@cache
+def _get_pandas_data() -> pd.DataFrame:
+    current_file = Path(__file__)
+    current_dir = current_file.parent
+    data_file = current_dir / "jlc_parts.csv"
+    return pd.read_csv(data_file)
+
+
+# TODO: currently a hack until we develop the required infrastructure
+_generics_to_db_fp_map = {
+    "R01005": "01005",
+    "R0201": "0201",
+    "R0402": "0402",
+    "R0603": "0603",
+    "R0805": "0805",
+    "C01005": "01005",
+    "C0201": "0201",
+    "C0402": "0402",
+    "C0603": "0603",
+    "C0805": "0805",
+    "C1206": "1206",
+}
+
+
+_GENERIC_RESISTOR = "generic_resistor"
+_GENERIC_CAPACITOR = "generic_capacitor"
+_GENERICS_MPNS = [_GENERIC_RESISTOR, _GENERIC_CAPACITOR]
+
+
+_generic_to_type_map = {
+    _GENERIC_RESISTOR: "Resistor",
+    _GENERIC_CAPACITOR: "Capacitor",
+}
+
+
+_generic_to_unit_map = {
+    _GENERIC_RESISTOR: pint.Unit("ohm"),
+    _GENERIC_CAPACITOR: pint.Unit("farad"),
+}
+
+
+def _get_specd_mpn(addr: AddrStr) -> str:
+    """
+    Return the MPN for a component given its address
+    """
+    comp_data = instance_methods.get_data_dict(addr)
+
+    try:
+        return comp_data["mpn"]
+    except KeyError as ex:
+        raise MissingData("$addr has no MPN", title="No MPN", addr=addr) from ex
+
+
+def _is_generic(addr: AddrStr) -> bool:
+    """
+    Return whether a component is generic
+    """
+    specd_mpn = _get_specd_mpn(addr)
+    return specd_mpn in _GENERICS_MPNS
+
+
+class NoMatchingComponent(errors.AtoError):
+    """
+    Raised when there's no component matching the given parameters in jlc_parts.csv
+    """
+
+    title = "No component matches parameters"
+
+
+@cache
+def _get_generic_from_db(component_addr: str) -> dict:
+    """
+    Return the MPN for a component given its address
+    """
+    specd_mpn = _get_specd_mpn(component_addr)
+    specd_data = instance_methods.get_data_dict(component_addr)
+
+    df = _get_pandas_data()
+    filters = []
+
+    specd_type = _generic_to_type_map[specd_mpn]
+    filters.append(f"type == '{specd_type}'")
+
+    # Apply filters we know how to process
+    try:
+        value_range = specd_data["value"]
+    except KeyError as ex:
+        raise KeyError("Generics are missing data - internal error") from ex
+
+    if not isinstance(value_range, Physical):
+        raise errors.AtoTypeError(f"Value must be a Physical, not {type(value_range)} for $addr", addr=component_addr)
+
+    # Ensure the component's value is completely contained within the specd value
+    try:
+        generic_unit = _generic_to_unit_map[specd_mpn]
+        min_float_val = (value_range.min_val * value_range.unit).to(generic_unit).magnitude
+        max_float_val = (value_range.max_val * value_range.unit).to(generic_unit).magnitude
+    except pint.DimensionalityError as ex:
+        raise errors.AtoTypeError(
+            f"{value_range.unit} cannot be converted to {generic_unit} for $addr", addr = component_addr,
+            title="Invalid unit",
+        ) from ex
+
+    filters.append(f"min_value >= {min_float_val}")
+    filters.append(f"max_value <= {max_float_val}")
+
+    # Ensure the component's footprint is correct
+    try:
+        database_footprint = _generics_to_db_fp_map[specd_data['footprint']]
+    except KeyError as ex:
+        raise errors.AtoKeyError(f"Can't find generic part that matches'{specd_data['footprint']}' for $addr. Change the part's mpn or footprint.", addr=component_addr) from ex
+
+    filters.append(f"Package == '{database_footprint}'")
+
+    # Combine filters using reduce
+    combined_filter = " & ".join(filters)
+    filtered_df = df.query(combined_filter)
+    if filtered_df.empty:
+        msg = "No component matching spec for $addr \n"
+        msg += "\n & ".join(filters)
+        raise NoMatchingComponent(msg, addr=component_addr)
+
+    # FIXME: Currently our cost function is dumb - it only knows dollars
+    # In the future this cost function should incorporate other things the user is
+    # likely to care about
+    idx_min = filtered_df["Price (USD)"].idxmin(skipna=True)
+
+    # In this case we seem to hit NaN, which implies we don't have
+    # cost info - which is really a bug in the db, but for the users' sake
+    # we'll just return the first component
+    if pd.isna(idx_min):
+        return filtered_df.iloc[0].to_dict()
+
+    return filtered_df.loc[idx_min].to_dict()
+
+
+class MissingData(errors.AtoError):
+    """
+    Raised when a component is missing data in the Basic_Parts.csv file.
+    """
+
+
+# We cache the MPNs to ensure we select the same component if it's hit multiple times
+# in a build
+@cache
+def get_mpn(addr: AddrStr) -> str:
+    """
+    Return the MPN for a component
+    """
+    specd_mpn = _get_specd_mpn(addr)
+    if specd_mpn in _GENERICS_MPNS:
+        return _get_generic_from_db(addr)["LCSC Part #"]
+
+    return specd_mpn
+
+
+def get_specd_value(addr: AddrStr) -> str:
+    """
+    Return the MPN for a component given its address
+    """
+    comp_data = instance_methods.get_data_dict(addr)
+    if not _is_generic(addr):
+        # it's cool if there's no value for non-generics
+        return str(comp_data.get("value", ""))
+
+    try:
+        return str(comp_data["value"])
+    except KeyError as ex:
+        raise MissingData(
+            "$addr has no value spec'd",
+            title="No value",
+            addr=addr
+        ) from ex
+
+
+# Values come from the finally selected
+# component, so we need to arbitrate via that data
+@cache
+def get_user_facing_value(addr: AddrStr) -> str:
+    """
+    Return a "value" of a component we can slap in things like
+    the BoM and netlist. Doesn't need to be perfect, just
+    something to look at.
+    """
+    if _is_generic(addr):
+        db_data = _get_generic_from_db(addr)
+        return f"{db_data['value']} {db_data['unit']}"
+
+    comp_data = instance_methods.get_data_dict(addr)
+    # The default is okay here, because we're only generics
+    # must have a value
+    return str(comp_data.get("value", ""))
+
+
+# Footprints come from the users' code, so we reference that directly
+@cache
+def get_footprint(addr: AddrStr) -> str:
+    """
+    Return the footprint for a component
+    """
+    comp_data = instance_methods.get_data_dict(addr)
+    try:
+        return comp_data["footprint"]
+    except KeyError as ex:
+        raise MissingData(
+            "$addr has no footprint", title="No Footprint", addr=addr
+        ) from ex
+
+
+class DesignatorManager:
+    """TODO:"""
+
+    def __init__(self) -> None:
+        self._designators: dict[AddrStr, str] = {}
+
+    def _make_designators(self, root: str) -> dict[str, str]:
+        designators: dict[str, str] = {}
+        unnamed_components = []
+        used_designators = set()
+
+        # first pass: grab all the designators from the lock data
+        for component in filter(
+            instance_methods.match_components, instance_methods.all_descendants(root)
+        ):
+            designator = instance_methods.get_lock_data_dict(component).get(
+                "designator"
+            )
+            if designator:
+                used_designators.add(designator)
+                designators[component] = designator
+            else:
+                unnamed_components.append(component)
+
+        # second pass: assign designators to the unnamed components
+        for component in unnamed_components:
+            prefix = instance_methods.get_data_dict(component).get(
+                "designator_prefix", "U"
+            )
+
+            i = 1
+            while f"{prefix}{i}" in used_designators:
+                i += 1
+
+            designators[component] = f"{prefix}{i}"
+            used_designators.add(designators[component])
+            instance_methods.get_lock_data_dict(component)["designator"] = designators[
+                component
+            ]
+
+        return designators
+
+    def get_designator(self, addr: str) -> str:
+        """Return a mapping of instance address to designator."""
+        if addr not in self._designators:
+            self._designators = self._make_designators(address.get_entry(addr))
+        return self._designators[addr]
+
+
+designator_manager = DesignatorManager()
+
+
+def get_designator(addr: str) -> str:
+    """
+    Return the designator for a component
+    """
+    return designator_manager.get_designator(addr)
---
+++ packages/atopile/atopile-0.2.4-py3-none-any.whl/atopile/config.py
@@ -0,0 +1,238 @@
+# pylint: disable=too-few-public-methods
+
+"""Schema and utils for atopile config files."""
+
+import collections.abc
+import fnmatch
+from pathlib import Path
+from typing import Any
+
+import yaml
+from attrs import Factory, define
+from omegaconf import MISSING, OmegaConf
+
+from atopile import address
+import atopile.errors
+
+CONFIG_FILENAME = "ato.yaml"
+ATO_DIR_NAME = ".ato"
+MODULE_DIR_NAME = "modules"
+BUILD_DIR_NAME = "build"
+
+
+@define
+class UserPaths:
+    """Config grouping for all the paths in a project."""
+
+    src: Path = "./"
+    layout: Path = "elec/layout"
+
+
+@define
+class UserBuildConfig:
+    """Config for a build."""
+
+    entry: str = MISSING
+    targets: list[str] = ["__default__"]
+
+
+@define
+class UserConfig:
+    """
+    The config object for atopile.
+    """
+
+    location: Path = MISSING
+
+    ato_version: str = "0.1.0"
+    paths: UserPaths = Factory(UserPaths)
+    builds: dict[str, UserBuildConfig] = Factory(dict)
+    dependencies: list[str] = []
+
+
+KEY_CONVERSIONS = {
+    "ato-version": "ato_version",
+}
+
+
+def _sanitise_key(key: str) -> str:
+    """Sanitize a key."""
+    return KEY_CONVERSIONS.get(key, key)
+
+
+def _sanitise_item(item: tuple[Any, Any]) -> tuple[Any, Any]:
+    """Sanitise the key of a dictionary item to be a valid python identifier."""
+    k, v = item
+    if isinstance(v, collections.abc.Mapping):
+        return _sanitise_key(k), _sanitise_dict_keys(v)
+    return _sanitise_key(k), v
+
+
+def _sanitise_dict_keys(d: collections.abc.Mapping) -> collections.abc.Mapping:
+    """Sanitise the keys of a dictionary to be valid python identifiers."""
+    if d is None:
+        return {}
+    return dict(_sanitise_item(item) for item in d.items())
+
+
+def make_config(project_config: Path) -> UserConfig:
+    """
+    Make a config object for a project.
+
+    The typing on this is a little white lie... because they're really OmegaConf objects.
+    """
+    structure = UserConfig()
+
+    with project_config.open() as f:
+        config_data = yaml.safe_load(f)
+
+    structure.location = project_config.parent.expanduser().resolve().absolute()
+
+    return OmegaConf.merge(
+        OmegaConf.structured(structure),  # structure
+        OmegaConf.create(_sanitise_dict_keys(config_data)),  # project config
+    )
+
+
+def get_project_dir_from_path(path: Path) -> Path:
+    """
+    Resolve the project directory from the specified path.
+    """
+    for p in [path] + list(path.parents):
+        clean_path = p.resolve().absolute()
+        if (clean_path / CONFIG_FILENAME).exists():
+            return clean_path
+    raise FileNotFoundError(
+        f"Could not find {CONFIG_FILENAME} in {path} or any parents"
+    )
+
+
+_loaded_configs: dict[Path, str] = {}
+
+
+def get_project_config_from_path(path: Path) -> UserConfig:
+    """
+    Get the project config from an address.
+    """
+    project_dir = get_project_dir_from_path(path)
+    project_config_file = project_dir / CONFIG_FILENAME
+    if project_config_file not in _loaded_configs:
+        _loaded_configs[project_config_file] = make_config(project_config_file)
+    return _loaded_configs[project_config_file]
+
+
+def get_project_config_from_addr(addr: str) -> UserConfig:
+    """
+    Get the project config from an address.
+    """
+    return get_project_config_from_path(Path(address.get_file(addr)))
+
+
+# FIXME: we need factory constructors for these classes
+@define
+class ProjectContext:
+    """A class to hold the arguments to a project."""
+
+    layout_path: Path  # eg. path/to/project/layouts/default/default.kicad_pcb
+    project_path: Path  # abs path to the project directory
+    src_path: Path  # abs path to the source directory
+    module_path: Path  # abs path to the module directory
+
+    @classmethod
+    def from_config(cls, config: UserConfig) -> "ProjectContext":
+        """Create a BuildArgs object from a Config object."""
+
+        return ProjectContext(
+            layout_path=Path(config.location) / config.paths.layout,
+            project_path=Path(config.location),
+            src_path=Path(config.location) / config.paths.src,
+            module_path=Path(config.location) / ATO_DIR_NAME / MODULE_DIR_NAME,
+        )
+
+    @classmethod
+    def from_path(cls, path: Path) -> "ProjectContext":
+        """Create a BuildArgs object from a Config object."""
+        return cls.from_config(get_project_config_from_path(path))
+
+
+def match_user_layout(path: Path) -> bool:
+    """Check whether a given filename is a KiCAD autosaved layout."""
+    autosave_patterns = [
+        "_autosave-*",
+        "*-save.kicad_pcb",
+    ]
+
+    name = path.name
+
+    for pattern in autosave_patterns:
+        if fnmatch.fnmatch(name, pattern):
+            return False
+    return True
+
+
+@define
+class BuildContext:
+    """A class to hold the arguments to a build."""
+
+    name: str
+
+    entry: address.AddrStr  # eg. "path/to/project/src/entry-name.ato:module.path"
+    targets: list[str]
+    layout_path: Path  # eg. path/to/project/layouts/default/default.kicad_pcb
+
+    layout_path: Path  # eg. path/to/project/layouts/default/default.kicad_pcb
+    project_path: Path  # abs path to the project directory
+    src_path: Path  # abs path to the source directory
+    module_path: Path  # abs path to the module directory
+    build_path: Path  # eg. path/to/project/build/<build-name>
+
+    output_base: Path  # eg. path/to/project/build/<build-name>/entry-name
+
+    @classmethod
+    def from_config(cls, config: UserConfig, build_name: str) -> "BuildContext":
+        """Create a BuildArgs object from a Config object."""
+        try:
+            build_config = config.builds[build_name]
+        except KeyError as ex:
+            raise atopile.errors.AtoError(
+                f"Build {build_name} not found for project {config.location}\n"
+                f"Available builds: {list(config.builds.keys())}"
+            ) from ex
+
+        abs_entry = address.AddrStr(config.location / build_config.entry)
+
+        build_path = Path(config.location) / BUILD_DIR_NAME
+
+        layout_base = config.location / config.paths.layout / build_name
+        if layout_base.with_suffix(".kicad_pcb").exists():
+            layout_path = layout_base.with_suffix(".kicad_pcb")
+        elif layout_base.is_dir():
+            layout_candidates = list(
+                filter(
+                    match_user_layout,
+                    layout_base.glob("*.kicad_pcb")
+                )
+            )
+
+            if len(layout_candidates) == 1:
+                layout_path = layout_candidates[0]
+
+            else:
+                raise atopile.errors.AtoError(
+                    "Layout directories must contain exactly 1 layout,"
+                    f" but {len(layout_candidates)} found in {layout_base}"
+                )
+        else:
+            raise atopile.errors.AtoError("Layout file not found")
+
+        return BuildContext(
+            name=build_name,
+            entry=abs_entry,
+            targets=build_config.targets,
+            layout_path=layout_path,
+            project_path=Path(config.location),
+            src_path=Path(config.location) / config.paths.src,
+            module_path=Path(config.location) / ATO_DIR_NAME / MODULE_DIR_NAME,
+            build_path=build_path,
+            output_base=build_path / build_name,
+        )
