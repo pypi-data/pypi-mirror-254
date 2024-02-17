67c9bd7cf56810a623863c5ab08545566786a83d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-17 23:42:31 +0000

    Add package translate-shell/0.0.50/translate_shell-0.0.50-py3-none-any.whl

---
+++ packages/translate-shell/translate_shell-0.0.50-py3-none-any.whl/translate_shell/external/readline/__main__.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+"""This module can be called by
+`python -m <https://docs.python.org/3/library/__main__.html>`_
+to check if it imports any variable don't owned by any true module.
+
+Because Windows don't have
+`readline <https://docs.python.org/3/library/readline.html>`_.
+"""
+
+
+def read_history_file(unused_path: str) -> None:
+    """Read history file.
+
+    :param unused_path:
+    :type unused_path: str
+    :rtype: None
+    """
+
+
+def write_history_file(unused_path: str) -> None:
+    """Write history file.
+
+    :param unused_path:
+    :type unused_path: str
+    :rtype: None
+    """
+
+
+def add_history(unused_path: str) -> None:
+    """Add history.
+
+    :param unused_path:
+    :type unused_path: str
+    :rtype: None
+    """
+
+
+if __name__ == "__main__":
+    from ..__main__ import main_once as _main
+
+    _main(__file__, vars())
