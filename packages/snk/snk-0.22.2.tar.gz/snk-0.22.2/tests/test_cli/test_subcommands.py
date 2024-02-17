76c9e3087055ac1f0439a8dec938664b5eb9e570
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-17 23:38:25 +0000

    Add package snk/0.22.2/snk-0.22.2.tar.gz

---
+++ packages/snk/snk-0.22.2.tar.gz/snk-0.22.2/tests/test_cli/test_subcommands.py
@@ -0,0 +1,19 @@
+import pytest
+
+@pytest.mark.parametrize("cmd,expected_in_response", [
+    (["script", "run", "hello.py"], ["hello world"]),
+    (["script", "list"], ["hello.py"]),
+    (["script", "show", "hello.py"], ["print('hello world')"]),
+    (["env", "list"], ["python"]),
+    (["env", "show", "python"], ["python"]),
+    (["env", "create"], ["All conda environments created!"]),
+    (["env", "create", "python"], ["Created environment python!"]),
+    (["env", "run", "-v", "python", "which python"], [".snakemake"]),
+    (["env", "activate", "python"], ["Activating python environment...", "Exiting python environment..."]),
+    (["env", "remove", "-f"], ["Deleted"]),
+])
+def test_snk_cli_command(local_runner, cmd, expected_in_response):
+    res = local_runner(cmd)
+    assert res.code == 0, res.stderr
+    for expected in expected_in_response:
+        assert expected in res.stdout, res.stderr
