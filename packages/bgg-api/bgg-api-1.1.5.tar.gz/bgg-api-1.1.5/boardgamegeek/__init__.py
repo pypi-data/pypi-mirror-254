ebd75c7c52f20e43bc63a9f0790e7403a76f1b54
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-17 23:48:34 +0000

    Add package bgg-api/1.1.5/bgg-api-1.1.5.tar.gz

---
+++ packages/bgg-api/bgg-api-1.1.5.tar.gz/bgg-api-1.1.5/boardgamegeek/__init__.py
@@ -0,0 +1,45 @@
+"""
+.. module:: boardgamegeek
+   :platform: Unix, Windows
+   :synopsis: interface to boardgamegeek.com
+
+.. moduleauthor:: Cosmin Luță <q4break@gmail.com>
+"""
+
+from .api import (
+    BGGChoose,
+    BGGClient,
+    BGGRestrictCollectionTo,
+    BGGRestrictDomainTo,
+    BGGRestrictPlaysTo,
+    BGGRestrictSearchResultsTo,
+)
+from .cache import CacheBackendMemory, CacheBackendNone, CacheBackendSqlite
+from .exceptions import (
+    BGGApiError,
+    BGGApiRetryError,
+    BGGApiTimeoutError,
+    BGGError,
+    BGGItemNotFoundError,
+    BGGValueError,
+)
+
+__all__ = [
+    "BGGClient",
+    "BGGChoose",
+    "BGGRestrictSearchResultsTo",
+    "BGGRestrictPlaysTo",
+    "BGGRestrictDomainTo",
+    "BGGRestrictCollectionTo",
+    "BGGError",
+    "BGGValueError",
+    "BGGApiRetryError",
+    "BGGApiError",
+    "BGGApiTimeoutError",
+    "BGGItemNotFoundError",
+    "CacheBackendNone",
+    "CacheBackendSqlite",
+    "CacheBackendMemory",
+]
+
+__version__ = "1.1.5"
---
+++ packages/bgg-api/bgg-api-1.1.5.tar.gz/bgg-api-1.1.5/boardgamegeek/loaders/__init__.py
@@ -0,0 +1,18 @@
+from .collection import add_collection_items_from_xml, create_collection_from_xml
+from .game import add_game_comments_from_xml, create_game_from_xml
+from .guild import add_guild_members_from_xml, create_guild_from_xml
+from .hotitems import add_hot_items_from_xml, create_hot_items_from_xml
+from .plays import add_plays_from_xml, create_plays_from_xml
+
+__all__ = [
+    create_collection_from_xml,
+    create_guild_from_xml,
+    create_hot_items_from_xml,
+    create_plays_from_xml,
+    create_game_from_xml,
+    add_collection_items_from_xml,
+    add_guild_members_from_xml,
+    add_hot_items_from_xml,
+    add_plays_from_xml,
+    add_game_comments_from_xml,
+]
