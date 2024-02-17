d4f8de061abf34cfb74af97c8ce8412695932b8f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-17 23:34:31 +0000

    Add package MDSdata/0.1.2/mdsdata-0.1.2-py3-none-any.whl

---
+++ packages/MDSdata/mdsdata-0.1.2-py3-none-any.whl/mdsdata/__init__.py
@@ -0,0 +1,8 @@
+from mdsdata.MDS1_tensile_test.dataset import MDS1
+from mdsdata.MDS2_Ising_model_light.dataset import MDS2_light
+from mdsdata.MDS2_Ising_model.dataset import MDS2
+from mdsdata.MDS3_CahnHilliard.dataset import MDS3
+from mdsdata.MDS4_chemical_elements.dataset import MDS4
+from mdsdata.MDS5_Nanoindentation.dataset import MDS5
+
+from mdsdata.DS2_MNIST.dataset import DS2
