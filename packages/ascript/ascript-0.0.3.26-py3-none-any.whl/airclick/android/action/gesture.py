e1b6011e581e9548353221ce5f5b757218a88274
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-17 23:37:56 +0000

    Add package ascript/0.0.3.26/ascript-0.0.3.26-py3-none-any.whl

---
+++ packages/ascript/ascript-0.0.3.26-py3-none-any.whl/airclick/android/action/gesture.py
@@ -0,0 +1,12 @@
+from airclick.action.path import path
+from typing import List
+from airclick.action.path import path
+
+
+def perform(*paths: List[path]) -> None:
+    """
+    执行手势操作
+
+    :param paths: 需要执行的路径对象列表
+    """
+    pass
