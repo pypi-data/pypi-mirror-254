83aff05ccd9e70abb055875fda31e645b78d5f86
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-17 23:46:55 +0000

    Add package openframe-criteria-set-protocol/1.2.0/openframe_criteria_set_protocol-1.2.0-py3-none-any.whl

---
+++ packages/openframe-criteria-set-protocol/openframe_criteria_set_protocol-1.2.0-py3-none-any.whl/openframe_criteria_set_protocol/v1/schemas.py
@@ -0,0 +1,10 @@
+from marshmallow import Schema, fields
+
+criteria_set_id = r'^[a-zA-Z0-9.\-_]+$'
+version = r'^(0|[1-9][0-9]*)\.(0|[1-9][0-9]*)\.(0|[1-9][0-9]*)$'
+
+
+class TreeAndMatrixBodySchema(Schema):
+    locale = fields.Str(required=False)
+    values = fields.Dict(required=False, default=dict())
+    parameters = fields.Dict(required=False, default=dict())
