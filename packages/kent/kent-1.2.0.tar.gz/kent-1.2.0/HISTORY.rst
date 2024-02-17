31cc1105e7ae9ca8702a20539c4dbd4ff2a1d842
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-17 23:53:12 +0000

    Add package kent/1.2.0/kent-1.2.0.tar.gz

---
+++ packages/kent/kent-1.2.0.tar.gz/kent-1.2.0/HISTORY.rst
@@ -0,0 +1,116 @@
+=======
+HISTORY
+=======
+
+1.2.0 (January 31st, 2024)
+==========================
+
+Changes:
+
+* Feature: Improve logging. (#69)
+* Maintenance: Switch to ruff for formatting. (#66)
+
+Kent found a rock shaped like two rocks.
+
+
+1.1.0 (November 6th, 2023)
+==========================
+
+Changes:
+
+* Feature: Support Python 3.12. (#50)
+* Maintenance: Switch everything to ``pyproject.toml`` (#62)
+* Feature: Add ``loggingerror`` to testpost command (#48)
+* Maintenance: Drop support for Python 3.7. (#49)
+* Feature: Rework app logging. All lines are now prefixed with the error id to
+  make tracing easier.
+* Feature: Add support for security reports like CSP security reports. (#59)
+
+Kent wants to know if the sky is green sometimes.
+
+
+1.0.0 (November 9th, 2022)
+==========================
+
+Fixes:
+
+* Feature: Rework logging to be more helpful. (#32)
+* Maintenance: Redid dev requirements.
+* Feature: Support Python 3.11. (#33)
+
+Kent is building a bird house and asked me for my popsicle stick.
+
+
+0.5.0 (June 13th, 2022)
+=======================
+
+Fixes:
+
+* Maintenance: Update pico.css to 1.5.3.
+* Feature: Add Summary column. (#27)
+
+Kent munched on popcorn quietly.
+
+
+0.4.1 (March 29th, 2022)
+========================
+
+Fixes:
+
+* Bug: kent-server didn't work with Flask 2.1.0. (#23)
+
+Kent can make croissants disappear.
+
+
+0.4.0 (January 24th, 2022)
+==========================
+
+Fixes:
+
+* Bug: Fix handling so Kent works with raven-python which was deprecated many
+  moons ago.
+
+Kent handed me a cookie.
+
+
+0.3.0 (January 19th, 2022)
+==========================
+
+Backwards incompatible changes:
+
+* Changed ``/api/flush/`` from a GET to a POST.
+* Changed response for ``/api/error/<ERRORID>`` to return a ``project_id`` key
+  as well.
+
+Fixes:
+
+* Feature: Support multiple project ids. (#7)
+* Feature: Add timestamp to event list. (#8)
+* Feature: Add link to flush events to index page. (#9)
+
+Kent eyed my slice of ice cream cake with interest.
+
+
+0.2.0 (January 5th, 2022)
+=========================
+
+Bug fix release.
+
+* Bug: Fix ``/api/errorlist/`` AttributeError bug. (#5)
+* Feature: Add ``/api/flush/`` endpoint to flush the error manager. (#4)
+* Add tests. (#1)
+* Improve API docs.
+
+Tried to feed Kent a banana. Kent peered at it and then put it down.
+
+
+0.1.0 (January 4th, 2022)
+=========================
+
+Initial release with minimally viable feature set.
+
+* Capture errors and keep them in memory.
+* API endpoint to list errors.
+* API endpoint to fetch error.
+
+Fed Kent some pears. Kent loves pears.
---
+++ packages/kent/kent-1.2.0.tar.gz/kent-1.2.0/tests/test_app.py
@@ -0,0 +1,111 @@
+import pytest
+import uuid
+
+from kent.app import create_app, Error
+
+
+@pytest.fixture
+def client():
+    app = create_app({"TESTING": True})
+
+    with app.test_client() as client:
+        yield client
+
+
+class TestError:
+    @pytest.mark.parametrize(
+        "payload, expected",
+        [
+            # Empty error payload
+            ({}, "no summary"),
+            # Error payload for an exception
+            (
+                {
+                    "exception": {
+                        "values": [
+                            {"type": "Exception", "value": "Intentional exception"}
+                        ]
+                    }
+                },
+                "Exception: Intentional exception",
+            ),
+            # Message payload
+            (
+                {
+                    "message": "some message",
+                },
+                "some message",
+            ),
+        ],
+    )
+    def test_summary(self, payload, expected):
+        error = Error(
+            project_id="0",
+            error_id="9884b351-1e8f-4a28-8a9a-fc0033467e4e",
+            payload=payload,
+        )
+        assert error.summary == expected
+
+
+def test_index_view(client):
+    resp = client.get("/")
+    assert b"Kent" in resp.data
+
+
+def test_store_view(client):
+    resp = client.post("/api/1/store/", json={"id": "xyz"})
+    assert resp.status_code == 200
+
+
+def test_security_view(client):
+    resp = client.post("/api/1/security/", json=[{"id": "xyz"}])
+    assert resp.status_code == 200
+
+
+def test_api_flush_view(client):
+    resp = client.post("/api/flush/")
+    assert resp.status_code == 200
+    assert resp.json == {"success": True}
+
+
+class TestAPIErrorListView:
+    def test_empty_errorlist(self, client):
+        resp = client.get("/api/errorlist/")
+        assert resp.json == {"errors": []}
+
+    def test_nonempty_errorlist(self, client):
+        # Store an error
+        resp = client.post("/api/1/store/", json={"id": "xyz"})
+        assert resp.status_code == 200
+
+        resp = client.get("/api/errorlist/")
+        assert len(resp.json) == 1
+
+
+class TestAPIErrorView:
+    def test_404(self, client):
+        error_id = str(uuid.uuid4())
+        resp = client.get(f"/api/error/{error_id}")
+        assert resp.status_code == 404
+        assert resp.json == {"error": f"Error {error_id} not found"}
+
+    def test_error_exists(self, client):
+        error_payload = {"id": "new error"}
+
+        # Store an error
+        resp = client.post("/api/1/store/", json=error_payload)
+        assert resp.status_code == 200
+
+        # Get all the errors
+        resp = client.get("/api/errorlist/")
+        assert resp.status_code == 200
+        error_id = resp.json["errors"][0]
+
+        # Get the error by error_id
+        resp = client.get(f"/api/error/{error_id}")
+        assert resp.status_code == 200
+        assert resp.json == {
+            "project_id": 1,
+            "error_id": error_id,
+            "payload": error_payload,
+        }
