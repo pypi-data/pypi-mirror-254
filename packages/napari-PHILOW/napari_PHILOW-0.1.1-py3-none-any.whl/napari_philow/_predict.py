8ec0f971e033002e865788249b130582a1649dfb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 22:04:42 +0000

    Add package napari-PHILOW/0.1.1/napari_PHILOW-0.1.1-py3-none-any.whl

---
+++ packages/napari-PHILOW/napari_PHILOW-0.1.1-py3-none-any.whl/napari_philow/_predict.py
@@ -0,0 +1,98 @@
+import glob
+import os
+
+import dask_image.imread
+import dask
+import numpy as np
+from PIL import Image
+from skimage import io
+from tifffile import natural_sorted
+from tqdm import tqdm
+
+from napari_philow.segmentation.predict import pred_large_image
+from napari_philow._utils import renormalize_8bit
+
+
+def predict_and_save(dask_arr, net, out_dir_axis, size, device):
+    """
+    Args:
+        dask_arr (dask.array.Array): 3D input image
+        net (torch.nn.Module): model
+        out_dir_axis (str): output directory for the prediction of the current axis
+        size (int): patch size
+        device (str): e.g. 'cpu', 'cuda:0'
+    """
+    for i in tqdm(range(len(dask_arr))):
+        pred = 255 * pred_large_image(Image.fromarray(dask_arr[i].compute()), net, device, size)
+        io.imsave(os.path.join(out_dir_axis, str(i).zfill(6) + '.png'), pred.astype(np.uint8))
+
+
+def predict_3ax(o_path, net, out_dir, size, device):
+    """
+    predict 3 axes (TAP) and merge the prediction
+    Args:
+        o_path (str): original image directory path
+        net (torch.nn.Module): model
+        out_dir (str): output directory
+        size (int): patch size
+        device (str): e.g. 'cpu', 'cuda:0'
+    """
+    os.makedirs(out_dir, exist_ok=True)
+    out_dir_merge = os.path.join(out_dir, 'merged_prediction')
+    os.makedirs(out_dir_merge, exist_ok=True)
+    os.makedirs(f"{out_dir_merge}_raw", exist_ok=True)
+    filenames = natural_sorted(glob.glob(os.path.join(o_path, '*.png')))
+    xy_imgs = dask_image.imread.imread(os.path.join(o_path, '*.png'))
+    xy_imgs = dask.array.asarray([ renormalize_8bit(xy_imgs[z]) for z in range(xy_imgs.shape[0]) ])
+    print(f"xy_imgs.shape: {xy_imgs.shape}")
+    yz_imgs = xy_imgs.transpose(2, 0, 1)
+    zx_imgs = xy_imgs.transpose(1, 2, 0)
+    os.makedirs(os.path.join(out_dir, 'pred_xy'), exist_ok=True)
+    os.makedirs(os.path.join(out_dir, 'pred_yz'), exist_ok=True)
+    os.makedirs(os.path.join(out_dir, 'pred_zx'), exist_ok=True)
+    predict_and_save(xy_imgs, net, os.path.join(out_dir, 'pred_xy'), size, device)
+    predict_and_save(yz_imgs, net, os.path.join(out_dir, 'pred_yz'), size, device)
+    predict_and_save(zx_imgs, net, os.path.join(out_dir, 'pred_zx'), size, device)
+    pred_xy_imgs = dask_image.imread.imread(os.path.join(out_dir, 'pred_xy', '*png'))
+    pred_yz_imgs = dask_image.imread.imread(os.path.join(out_dir, 'pred_yz', '*png'))
+    pred_zx_imgs = dask_image.imread.imread(os.path.join(out_dir, 'pred_zx', '*png'))
+    print(pred_xy_imgs.shape)
+    print(pred_yz_imgs.shape)
+    print(pred_zx_imgs.shape)
+    pred_yz_imgs_xy = pred_yz_imgs.transpose(1, 2, 0)
+    pred_zx_imgs_xy = pred_zx_imgs.transpose(2, 0, 1)
+    for i in tqdm(range(len(pred_xy_imgs))):
+        mito_img_ave = pred_xy_imgs[i].compute() // 3 + pred_yz_imgs_xy[i].compute() // 3 + pred_zx_imgs_xy[i].compute() // 3
+        img = np.where(mito_img_ave >= 127, 1, 0)
+        io.imsave(f'{out_dir_merge}/{os.path.basename(filenames[i])}', img.astype(np.uint8))
+        img_ = np.where(mito_img_ave >= 127, mito_img_ave, 0)
+        io.imsave(f'{out_dir_merge}_raw/{os.path.basename(filenames[i])}', img_.astype(np.uint8))
+
+
+def predict_1ax(ori_filenames, net, out_dir, size, device):
+    """
+    predict 1 axis and merge the prediction
+    Args:
+        ori_filenames (List[Path]):
+        net (torch.nn.Module): model
+        out_dir (str): output directory
+        size (int):  patch size
+        device (str): e.g. 'cpu', 'cuda:0'
+    """
+    os.makedirs(out_dir, exist_ok=True)
+    out_dir_merge = os.path.join(out_dir, 'merged_prediction')
+    os.makedirs(out_dir_merge, exist_ok=True)
+    os.makedirs(f"{out_dir_merge}_raw", exist_ok=True)
+
+    for filename in ori_filenames:
+        image = Image.open(str(filename))
+        image = renormalize_8bit(np.array(image))
+        image = Image.fromarray(image)
+        mito_imgs_ave = 255 * pred_large_image(image, net, device, size)
+        # threshed
+        img = np.where(mito_imgs_ave >= 127, 1, 0)
+        io.imsave(f"{out_dir_merge}/{filename.name}", img.astype("uint8"))
+
+        # raw
+        img_ = np.where(mito_imgs_ave[:, :] >= 127, mito_imgs_ave, 0)
+        io.imsave(f'{out_dir_merge}_raw/{filename.name}', img_.astype('uint8'))
---
+++ packages/napari-PHILOW/napari_PHILOW-0.1.1-py3-none-any.whl/napari_philow/_prediction.py
@@ -0,0 +1,155 @@
+import os
+import shutil
+from pathlib import Path
+
+import pandas as pd
+import torch
+from qtpy.QtWidgets import (QWidget, QVBoxLayout, QPushButton, QSizePolicy, QLabel, QFileDialog,
+                            QCheckBox)
+from segmentation_models_pytorch import UnetPlusPlus
+
+from napari_philow._utils import combine_blocks
+
+from napari_philow._predict import predict_3ax, predict_1ax
+
+
+class Predicter(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.opath = ""
+        self.labelpath = ""
+        self.modelpath = ""
+        self.outpath = ""
+        self.btn1 = QPushButton('open', self)
+        self.btn1.setSizePolicy(QSizePolicy.Fixed, QSizePolicy.Fixed)
+        self.btn1.clicked.connect(self.show_dialog_o)
+        self.btn2 = QPushButton('open', self)
+        self.btn2.setSizePolicy(QSizePolicy.Fixed, QSizePolicy.Fixed)
+        self.btn2.clicked.connect(self.show_dialog_label)
+        self.btn3 = QPushButton('open', self)
+        self.btn3.setSizePolicy(QSizePolicy.Fixed, QSizePolicy.Fixed)
+        self.btn3.clicked.connect(self.show_dialog_model)
+        self.btn4 = QPushButton('open', self)
+        self.btn4.setSizePolicy(QSizePolicy.Fixed, QSizePolicy.Fixed)
+        self.btn4.clicked.connect(self.show_dialog_outdir)
+
+        self.checkBox = QCheckBox("Check the box if you want to use TAP (Three-Axis-Prediction")
+        self.checkBox.setSizePolicy(QSizePolicy.Fixed, QSizePolicy.Fixed)
+        self.checkBox.toggle()
+
+        self.btn5 = QPushButton('predict', self)
+        self.btn5.setSizePolicy(QSizePolicy.Fixed, QSizePolicy.Fixed)
+        self.btn5.clicked.connect(self.predicter)
+        self.lbl = QLabel('original dir', self)
+        self.lbl2 = QLabel('label dir', self)
+        self.lbl3 = QLabel('model path (.pth)', self)
+        self.lbl4 = QLabel('output dir', self)
+        self.build()
+
+        self.net = None
+        self.worker_pred = None
+        self.ori_filenames = None
+        self.device = None
+        self.size = 512
+
+    def build(self):
+        vbox = QVBoxLayout()
+        vbox.addWidget(combine_blocks(self.btn1, self.lbl))
+        vbox.addWidget(combine_blocks(self.btn2, self.lbl2))
+        vbox.addWidget(combine_blocks(self.btn3, self.lbl3))
+        vbox.addWidget(combine_blocks(self.btn4, self.lbl4))
+        vbox.addWidget(self.checkBox)
+        vbox.addWidget(self.btn5)
+
+        self.setLayout(vbox)
+        self.show()
+
+    def show_dialog_o(self):
+        default_path = max(self.opath, self.labelpath, os.path.expanduser('~'))
+        f_name = QFileDialog.getExistingDirectory(self, 'Open Directory', default_path)
+        if f_name:
+            self.opath = f_name
+            self.lbl.setText(self.opath)
+
+    def show_dialog_label(self):
+        default_path = max(self.opath, self.labelpath, os.path.expanduser('~'))
+        f_name = QFileDialog.getExistingDirectory(self, 'Open Directory', default_path)
+        if f_name:
+            self.labelpath = f_name
+            self.lbl2.setText(self.labelpath)
+
+    def show_dialog_model(self):
+        default_path = max(self.opath, self.labelpath, os.path.expanduser('~'))
+        f_name, _ = QFileDialog.getOpenFileName(self, 'Select weight file', default_path)
+        if f_name:
+            self.modelpath = f_name
+            self.lbl3.setText(self.modelpath)
+
+    def show_dialog_outdir(self):
+        default_path = max(self.opath, self.labelpath, os.path.expanduser('~'))
+        f_name = QFileDialog.getExistingDirectory(self, 'Open Directory', default_path)
+        if f_name:
+            self.outpath = f_name
+            self.lbl4.setText(self.outpath)
+
+    def get_newest_csv(self):
+        csvs = sorted(list(Path(self.labelpath).glob('./*csv')))
+        try:
+            csv = pd.read_csv(str(csvs[-1]), index_col=0)
+        except:
+            csv = None
+        return csv, str(csvs[-1])
+
+    def predicter(self):
+        self.ori_filenames = sorted(list(Path(self.opath).glob('./*.png')))
+
+        self.net = UnetPlusPlus(encoder_name="efficientnet-b0", encoder_weights="imagenet", in_channels=1, classes=1,
+                                activation='sigmoid')
+        self.device = torch.device("cuda:0" if torch.cuda.is_available() else "mps" if torch.backends.mps.is_available() else "cpu")
+        state_dict = torch.load(self.modelpath, map_location=torch.device(self.device))
+        self.net.load_state_dict(state_dict)
+        self.net.to(self.device)
+
+        self.btn5.setText('predicting')
+
+        if self.checkBox.isChecked() is True:
+
+            self.predict()
+        else:
+            self.predict_single()
+
+    def predict(self):
+        try:
+            predict_3ax(self.opath, self.net, self.outpath, self.size, self.device)
+        except Exception as e:
+            print(e)
+        if self.labelpath != "":
+            self.copy_previous_mask()
+        self.btn5.setText('predict')
+
+    def predict_single(self):
+        try:
+            predict_1ax(self.ori_filenames, self.net, self.outpath, self.size, self.device)
+        except Exception as e:
+            print(e)
+        if self.labelpath != "":
+            self.copy_previous_mask()
+        self.btn5.setText('predict')
+
+    def copy_previous_mask(self):
+        print('copy previous mask')
+        try:
+            csv, csv_path = self.get_newest_csv()
+            print('find csv', csv_path)
+            if csv is None:
+                pass
+            else:
+                label_names = [node.filename for node in csv.itertuples() if node.train == "Checked"]
+                print(label_names)
+                for ln in label_names:
+                    print('copy ln')
+                    shutil.copy(os.path.join(self.labelpath, ln), os.path.join(self.outpath, 'merged_prediction'))
+                shutil.copy(str(csv_path), os.path.join(self.outpath, 'merged_prediction'))
+                print('csv copied')
+        except Exception as e:
+            print(e)
