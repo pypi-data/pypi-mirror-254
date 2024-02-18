cb5444fb6b98bbff5d64a3c8758aabff6d4f3db0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 12:04:01 +0000

    Add package topsis-3561/0.1.3/topsis-3561-0.1.3.tar.gz

---
+++ packages/topsis-3561/topsis-3561-0.1.3.tar.gz/topsis-3561-0.1.3/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='topsis-3561',
+    version='0.1.3',
+    author="Mannat Sadana",
+    author_email="sadana.mannat@gmail.com",
+    license='MIT License',
+    # description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    # long_description=long_description,
+    packages=find_packages(),
+    install_requires=['pandas','numpy'],
+    project_urls={
+        'Project Link': 'https://github.com/moxie814/Topsis-102103561.git'
+    },
+)
