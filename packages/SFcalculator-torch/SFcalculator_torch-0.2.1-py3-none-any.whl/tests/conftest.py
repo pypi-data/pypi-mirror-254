66f23a2d71f4a83516ef47efe8f09d59ffe80861
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 23:08:21 +0000

    Add package SFcalculator-torch/0.2.1/SFcalculator_torch-0.2.1-py3-none-any.whl

---
+++ packages/SFcalculator-torch/SFcalculator_torch-0.2.1-py3-none-any.whl/tests/conftest.py
@@ -0,0 +1,26 @@
+import pytest
+from os.path import dirname, abspath, join
+
+@pytest.fixture
+def data_pdb():
+    datapath = "data/1dur.pdb"
+    filename = abspath(join(dirname(__file__), datapath))
+    return filename
+
+@pytest.fixture
+def data_mtz_exp():
+    datapath = "data/1dur.mtz"
+    filename = abspath(join(dirname(__file__), datapath))
+    return filename
+
+@pytest.fixture
+def data_mtz_fmodel_ksol0():
+    datapath = "data/1dur_vanilla.mtz"
+    filename = abspath(join(dirname(__file__), datapath))
+    return filename
+
+@pytest.fixture
+def data_mtz_fmodel_ksol1():
+    datapath = "data/1dur_ksol1.mtz"
+    filename = abspath(join(dirname(__file__), datapath))
+    return filename
