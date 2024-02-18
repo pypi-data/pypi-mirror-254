25e46c56041ebecce92f89f8516bd9395943a4b8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 04:06:40 +0000

    Add package fyodorov-utils/0.1.8/fyodorov_utils-0.1.8-py3-none-any.whl

---
+++ packages/fyodorov-utils/fyodorov_utils-0.1.8-py3-none-any.whl/fyodorov_utils/auth/endpoints.py
@@ -0,0 +1,16 @@
+from fastapi import FastAPI
+from .auth import sign_up, sign_in, create_api_key
+
+users_app = FastAPI()
+
+@users_app.post('/sign_up')
+def sign_up_endpoint(email: str = Body(...), password: str = Body(...), invite_code: str = Body(...)):
+    return sign_up(email, password, invite_code)
+
+@users_app.post('/sign_in')
+def sign_in_endpoint(email: str = Body(...), password: str = Body(...)):
+    return sign_in(email, password)
+
+@users_app.post('/create_api_key')
+def create_api_key_endpoint(expiration: int = 15, user = Depends(authenticate)):
+    return create_api_key(expiration, user)
