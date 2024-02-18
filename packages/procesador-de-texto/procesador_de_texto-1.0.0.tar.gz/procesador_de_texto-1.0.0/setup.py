95873a3ddfc1fad3f43d82fce5e66166410b46ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 03:04:03 +0000

    Add package procesador-de-texto/1.0.0/procesador_de_texto-1.0.0.tar.gz

---
+++ packages/procesador-de-texto/procesador_de_texto-1.0.0.tar.gz/procesador_de_texto-1.0.0/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+
+
+def read_file(file_name):
+    with open(file_name, 'r', encoding='utf-8') as file:
+        return file.read()
+
+
+setup(
+    name="procesador_de_texto",
+    version="1.0.0",
+    packages=find_packages(include=['procesamiento', 'procesamiento.*']),
+    url="",
+    author="Carlos Estrada",
+    description="This is my text processor that will process text using 9 NLP processing tehcniques and two display",
+    long_description=read_file(file_name="README.md"),
+    long_description_content_type="text/markdown",
+    python_requires="==3.12",
+    install_requires=[
+        'pandas==2.1.4',
+        'pytest==6.2.4',
+        'nltk==3.6.5',
+        'setuptools==69.0.2',
+        'jupyter==1.0.0',
+        'matplotlib==3.4.3',
+        'spacy==3.1.3',
+        'wordcloud==1.8.1',
+
+
+    ]
+)
