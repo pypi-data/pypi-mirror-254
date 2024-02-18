69207a41edee853b333edc246fe44f7c8309589c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 16:04:59 +0000

    Add package datashaper/0.0.34/datashaper-0.0.34-py3-none-any.whl

---
+++ packages/datashaper/datashaper-0.0.34-py3-none-any.whl/datashaper/engine/verbs/ungroup.py
@@ -0,0 +1,15 @@
+#
+# Copyright (c) Microsoft. All rights reserved.
+# Licensed under the MIT license. See LICENSE file in the project.
+#
+
+from datashaper.engine.verbs.verb_input import VerbInput
+from datashaper.engine.verbs.verbs_mapping import verb
+from datashaper.table_store import TableContainer
+
+
+@verb(name="ungroup", treats_input_tables_as_immutable=True)
+def ungroup(input: VerbInput):
+    input_table = input.get_input()
+    output = input_table.obj
+    return TableContainer(table=output)
