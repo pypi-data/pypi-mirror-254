2dee6529c5c38abea5c8df51e34b9eacca981d27
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 14:04:08 +0000

    Add package fpcmci/4.4.1/fpcmci-4.4.1.tar.gz

---
+++ packages/fpcmci/fpcmci-4.4.1.tar.gz/fpcmci-4.4.1/fpcmci/basics/logger.py
@@ -0,0 +1,19 @@
+import sys
+from fpcmci.basics.utils import cls
+
+
+class Logger(object):
+    def __init__(self, path, clean_console = True):
+        self.terminal = sys.stdout
+        self.log = open(path, "w")
+        if clean_console: cls()
+        
+    def write(self, message):
+        self.terminal.write(message)
+        self.log.write(message)
+
+    def flush(self):
+        # this flush method is needed for python 3 compatibility.
+        # this handles the flush command by doing nothing.
+        # you might want to specify some extra behavior here.
+        pass
