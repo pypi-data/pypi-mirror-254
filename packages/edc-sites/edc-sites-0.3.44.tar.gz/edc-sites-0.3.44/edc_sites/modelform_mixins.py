03e7a1ef39ca199b2efde037f156c0663ab49fb3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 02:10:47 +0000

    Add package edc-sites/0.3.44/edc-sites-0.3.44.tar.gz

---
+++ packages/edc-sites/edc-sites-0.3.44.tar.gz/edc-sites-0.3.44/edc_sites/modelform_mixins.py
@@ -0,0 +1,48 @@
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+
+from django import forms
+
+if TYPE_CHECKING:
+    from django.contrib.sites.models import Site
+
+__all__ = ["SiteModelFormMixin"]
+
+
+class SiteModelFormMixin:
+
+    """Validate the current site against a form question.
+
+    This should be used sparingly. A good place is on the screening and/or consent form.
+
+    Declare the modeladmin class with `SiteModelAdminMixin` to have
+    the current site set on the form from the request object.
+
+    Declare a `site` field with widget on the ModeForm:
+
+    site = SiteField()
+
+    You will also need to re-declare the `site` model field as `editable`.
+    """
+
+    def clean(self) -> dict:
+        cleaned_data = super().clean()
+        self.validate_with_current_site()
+        return cleaned_data
+
+    @property
+    def site(self) -> Site:
+        return self.cleaned_data.get("site") or self.instance.site or self.related_visit.site
+
+    def validate_with_current_site(self) -> None:
+        current_site = getattr(self, "current_site", None)
+        if current_site and self.site and current_site.id != self.site.id:
+            raise forms.ValidationError(
+                {
+                    "site": (
+                        "Invalid. Please check you are logged into the correct site "
+                        "before continuing"
+                    )
+                }
+            )
