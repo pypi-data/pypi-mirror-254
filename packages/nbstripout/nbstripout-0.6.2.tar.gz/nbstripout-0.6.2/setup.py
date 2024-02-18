98f2e5478dc0d2b41ca600361d75fa8e8eb600e0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 22:05:56 +0000

    Add package nbstripout/0.6.2/nbstripout-0.6.2.tar.gz

---
+++ packages/nbstripout/nbstripout-0.6.2.tar.gz/nbstripout-0.6.2/setup.py
@@ -0,0 +1,46 @@
+from setuptools import setup, find_packages
+
+with open('README.rst') as f:
+    long_description = f.read()
+
+install_requires = [
+    'nbformat'
+]
+
+setup(name='nbstripout',
+      version='0.6.2',
+
+      author='Florian Rathgeber',
+      author_email='florian.rathgeber@gmail.com',
+      url='https://github.com/kynan/nbstripout',
+
+      license="License :: OSI Approved :: MIT License",
+
+      description='Strips outputs from Jupyter and IPython notebooks',
+      long_description=long_description,
+      long_description_content_type='text/x-rst',
+      packages=find_packages(),
+      provides=['nbstripout'],
+      entry_points={
+          'console_scripts': [
+              'nbstripout = nbstripout._nbstripout:main'
+          ],
+      },
+
+      install_requires=install_requires,
+      python_requires='>=3.6',
+
+      classifiers=[
+          "Development Status :: 4 - Beta",
+          "Environment :: Other Environment",
+          "Framework :: IPython",
+          "Intended Audience :: Developers",
+          "Programming Language :: Python",
+          "Programming Language :: Python :: 3",
+          "Programming Language :: Python :: 3.6",
+          "Programming Language :: Python :: 3.7",
+          "Programming Language :: Python :: 3.8",
+          "Programming Language :: Python :: 3.9",
+          "Programming Language :: Python :: 3.10",
+          "Topic :: Software Development :: Version Control",
+      ])
