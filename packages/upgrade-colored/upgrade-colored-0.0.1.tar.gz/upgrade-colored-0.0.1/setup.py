33a15109c2164d8be5c41182a5d9bc2f91b78e55
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 20:05:53 +0000

    Add package upgrade-colored/0.0.1/upgrade-colored-0.0.1.tar.gz

---
+++ packages/upgrade-colored/upgrade-colored-0.0.1.tar.gz/upgrade-colored-0.0.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="upgrade-colored",
+    version="0.0.1",
+    packages=['colored'],
+    install_requires = [
+        "requests==2.31.0"
+    ],
+    author="upgrade-colored",
+    description="upgrade-colored"
+)
