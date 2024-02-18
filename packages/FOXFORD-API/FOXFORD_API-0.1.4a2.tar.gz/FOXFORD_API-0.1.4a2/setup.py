c8553f7b81ac2f2077b79010dd656f0fbb8d84bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 05:06:00 +0000

    Add package FOXFORD-API/0.1.4a2/FOXFORD_API-0.1.4a2.tar.gz

---
+++ packages/FOXFORD-API/FOXFORD_API-0.1.4a2.tar.gz/FOXFORD_API-0.1.4a2/setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='FOXFORD_API',
+    version='0.1.4a2',
+    description='FOXFORD Student API\nБиблиотека для Работы с Бекэндом Платформы FOXFORD',
+    long_description="""
+FOXFORD Student API
+
+Библиотека для взаимодействия с бекендом платформы FOXFORD, предназначенная для удобной работы с данными и функциональностью, предоставляемой этой образовательной платформой.
+
+Основные функции:
+- Получение информации о студентах, курсах и других объектах системы FOXFORD.
+- Взаимодействие с данными бекенда для автоматизации процессов.
+
+Эта библиотека позволяет легко интегрировать возможности FOXFORD в ваши приложения, обеспечивая быстрый и эффективный доступ к данным и функциям этой платформы.
+Документация: https://volt-diamond.gitbook.io/foxford-api-docs/
+""",
+    author="VOLT_DIAMOND",
+    packages=find_packages(),
+    install_requires=[
+        'undetected_chromedriver',
+        'selenium',
+        'requests',
+        'aiohttp',
+        'packaging',
+        'asyncio'
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    zip_safe=False,
+    license="MIT License"
+)
