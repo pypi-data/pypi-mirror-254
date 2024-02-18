10992d28db178403a132cc4bdd1709a927bd32a6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 23:09:00 +0000

    Add package neuromeka-clients3/3.0.2/neuromeka-clients3-3.0.2.tar.gz

---
+++ packages/neuromeka-clients3/neuromeka-clients3-3.0.2.tar.gz/neuromeka-clients3-3.0.2/neuromeka/__init__.py
@@ -0,0 +1,7 @@
+from .indydcp3 import IndyDCP3
+# from .ecat import EcatClient
+# from .indy import IndyClient
+# from .proto import shared_msgs_pb2 as IndyClient3CommonMsgs
+# from .moby import MobyClient
+# from .motor import MotorClient
+# from .eye import IndyEyeClient
