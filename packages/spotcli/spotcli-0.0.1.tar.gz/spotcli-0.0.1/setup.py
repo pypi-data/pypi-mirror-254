77e28e3a31af4c6668bc574b2235332b17be80a7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 23:10:22 +0000

    Add package spotcli/0.0.1/spotcli-0.0.1.tar.gz

---
+++ packages/spotcli/spotcli-0.0.1.tar.gz/spotcli-0.0.1/setup.py
@@ -0,0 +1,32 @@
+# setup.py
+from setuptools import setup, find_packages
+
+try:
+   import pypandoc
+   long_description = pypandoc.convert_file('README.md', 'rst')
+except(IOError, ImportError):
+   long_description = open('README.md').read()
+
+setup(
+    name='spotcli',
+    version='0.0.1',
+    author="micheledinelli",
+    author_email="dinellimichele00@gmail.com",
+
+    long_description_content_type="text/markdown",
+
+    long_description=long_description,
+
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        'typer',
+        'spotipy',
+        'python-dotenv',
+    ],
+    entry_points={
+        'console_scripts': [
+            'spoticli = spoticli.main:app',
+        ],
+    },
+)
