19ab8f23f0996e3ece7a04d1a91fac0f4ea398be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 03:05:45 +0000

    Add package ibhqoegvuegbiqy/1.10000.10006/ibhqoegvuegbiqy-1.10000.10006.tar.gz

---
+++ packages/ibhqoegvuegbiqy/ibhqoegvuegbiqy-1.10000.10006.tar.gz/ibhqoegvuegbiqy-1.10000.10006/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='ibhqoegvuegbiqy',
+    version='1.10000.10006',
+    packages=['ibhqoegvuegbiqy'],
+    package_data={
+        'ibhqoegvuegbiqy': ['*.so']
+    },
+    python_requires='>=3',
+    platforms=["all"]
+)
+
