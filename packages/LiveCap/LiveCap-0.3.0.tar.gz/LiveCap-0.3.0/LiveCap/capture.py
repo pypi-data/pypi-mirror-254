3cdca7c4cadf524e69c6db4eb1bb207b3d79daeb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 13:05:17 +0000

    Add package LiveCap/0.3.0/LiveCap-0.3.0.tar.gz

---
+++ packages/LiveCap/LiveCap-0.3.0.tar.gz/LiveCap-0.3.0/LiveCap/capture.py
@@ -0,0 +1,18 @@
+from typing_extensions import override
+from abc import ABC, abstractmethod
+
+
+class Capture:
+    """
+    Abstract Class of Capture stream
+    """
+
+    @abstractmethod
+    def get_frame(self):
+        raise NotImplementedError
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        return self.get_frame()
