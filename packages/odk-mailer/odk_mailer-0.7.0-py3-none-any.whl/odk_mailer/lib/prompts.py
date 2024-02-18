d679c51c29b824b7af4add304eb3a4e2ce8c277a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 03:04:32 +0000

    Add package odk-mailer/0.7.0/odk_mailer-0.7.0-py3-none-any.whl

---
+++ packages/odk-mailer/odk_mailer-0.7.0-py3-none-any.whl/odk_mailer/lib/prompts.py
@@ -0,0 +1,160 @@
+import inquirer
+from odk_mailer.lib import validators
+
+def prompt_source():
+    questions = [
+        inquirer.List('type', 
+                    message="Select source type:",
+                    choices= [
+                        ("File from path", "path"),
+                        ("File from url", "url"),
+                        ("ODK API", "api")
+                    ],                      
+                    carousel=True
+        ),
+        inquirer.Path('location',
+                    message="Input local path to CSV file",
+                    path_type=inquirer.Path.FILE,
+                    exists=True,
+                    normalize_to_absolute_path=True,
+                    ignore=lambda x: x["type"] != "path"
+        ),
+        inquirer.Text('location',
+                    message="Input URL to CSV file",
+                    ignore=lambda x: x["type"] != "url",
+                    validate=validators.is_url
+        ),        
+        inquirer.Text('location',
+                    message="Input form/attachment name",                      
+                    ignore=lambda x: x["type"] != "api"
+                    #tbd: add regex valdation
+        ),
+        inquirer.Text('api_proj',
+                    message="Input project id",
+                    validate= validators.int_only,
+                    ignore=lambda x: x["type"] != "api"
+        ),
+        inquirer.Text('api_host',
+                    message="Input ODK host url",
+                    ignore=lambda x: x["type"] != "api",
+                    # tbd validate url, https
+        ),
+        inquirer.Text('api_user',
+                    message="Input ODK username",
+                    ignore=lambda x: x["type"] != "api"
+        ),
+        inquirer.Password('api_pass',
+                    message="Input ODK password",
+                    ignore=lambda x: x["type"] != "api"
+        )
+    ]
+    return inquirer.prompt(questions, raise_keyboard_interrupt=True)
+
+def prompt_fields(headers=[]):
+    questions = [
+        inquirer.List('email',
+                    message="Select email field",
+                    choices=headers,
+                    default='email' if 'email' in headers else "",
+                    carousel=True
+        ),
+        inquirer.Checkbox("data", 
+                    message="Select data field(s)",
+                    choices= lambda x: filter(lambda y: y != x["email"], headers)
+        )        
+    ]
+    return inquirer.prompt(questions, raise_keyboard_interrupt=True)
+
+def prompt_message():
+    questions = [
+        inquirer.Text('sender',
+                    message="Input message sender",
+                    default="odk-mailer@freesmtpservers.com",
+                    validate=validators.email_address                      
+        ),
+        inquirer.Text('subject',
+                    message="Input message subject",
+                    default="ODK-Mailer: New Email"                    
+        ),
+        inquirer.List('source', 
+                    message="Select message source:",
+                    choices= [
+                        ("Text from stdin", "stdin"),
+                        ("File from path", "path"),
+                        ("File from url", "url")
+                    ],                      
+                    carousel=True
+        ),        
+        inquirer.Text('location',
+                    message="Input string",
+                    ignore=lambda x: x["source"] != "stdin",
+                    validate= validators.not_empty
+        ),     
+        inquirer.Path('location',
+                    message="Input file path",
+                    path_type=inquirer.Path.FILE,
+                    exists=True,
+                    normalize_to_absolute_path=True,
+                    ignore=lambda x: x["source"] != "path"
+        ),
+        inquirer.Text('location',
+                    message="Input file url",
+                    ignore=lambda x: x["source"] != "url",
+                    validate= validators.is_url
+        ),        
+        inquirer.List('type', 
+                    message="Select message type:",
+                    choices= [
+                        ("Plain", "plain"),
+                        ("HTML", "html")
+                    ],                      
+                    carousel=True
+        ),
+    ]
+    return inquirer.prompt(questions, raise_keyboard_interrupt=True)
+
+def prompt_schedule():
+    questions = [
+        inquirer.List('now',
+                    message="Send now or schedule a date and time?",
+                    choices=[
+                        ("Send now",True),
+                        ("Schedule", False)
+                    ]
+        ),
+        inquirer.Text('datetime',
+                    message="Input date and time",
+                    validate= validators.date_format,
+                    ignore=lambda x: x["now"] == True
+
+        )
+    ]
+
+    return inquirer.prompt(questions, raise_keyboard_interrupt=True)
+
+def prompt_confirmRun():
+    questions = [
+        inquirer.Confirm('confirmRun',
+                         message="Would you like to run evaluated mailjobs?"
+        )
+    ]
+
+    return inquirer.prompt(questions, raise_keyboard_interrupt=True)
+
+def prompt_confirmCronReInit():
+    questions = [
+        inquirer.Confirm('confirmCronReInit',
+                         message="Would you like to re-initialize crontabs?"
+        )
+    ]
+
+    return inquirer.prompt(questions, raise_keyboard_interrupt=True)
+
+def prompt_confirmRetryRun():
+    questions = [
+        inquirer.Confirm('confirmRetryRun',
+                         message="Would you like to retry to run this mailjob?"
+        )
+    ]
+
+    return inquirer.prompt(questions, raise_keyboard_interrupt=True)    
---
+++ packages/odk-mailer/odk_mailer-0.7.0-py3-none-any.whl/odk_mailer/lib/utils.py
@@ -0,0 +1,84 @@
+from rich import print
+from rich.console import Console
+from rich.table import Table
+from rich.panel import Panel
+from rich.padding import Padding
+from datetime import datetime
+import typer
+import time
+import base64
+
+# return unformatted string instead of raising error
+# when key is missing within dictionary
+# https://stackoverflow.com/a/17215533/3127170
+class SafeDict(dict):
+    def __missing__(self, key):
+        return '{' + key + '}' 
+
+def abort(msg):
+    print("[bold red]Error:[/bold red] "+msg)
+    raise typer.Abort()
+
+def now():
+    return int(time.time())
+
+def ts_to_str(ts):
+    dt = datetime.fromtimestamp(ts)
+    return dt.strftime("%c")
+
+def base64_encode_str(input_str: str):
+    input_bytes = input_str.encode("ascii") 
+    base64_bytes = base64.b64encode(input_bytes)
+
+    return base64_bytes.decode("ascii")
+
+def base64_decode_str(base64_str: str):
+    base64_bytes = base64_str.encode("ascii") 
+    output_bytes = base64.b64decode(base64_bytes)
+    
+    return output_bytes.decode("ascii")
+
+def safe_format_map(text, dict):
+    return text.format_map(SafeDict(vars(dict)))
+
+def render_state(state):
+    match state:
+        case 0:
+            return "pending"
+        case 1: 
+            return "success"
+        case _:
+            return "failure"
+
+def print_jobs(jobsMeta):
+    console = Console()
+    header = ["Job Id", "Created","Scheduled", "Recipients", "State", "Note"]
+    table = Table(*header, expand=False, highlight=True, box=None, title_justify="left", show_lines="True")
+    console.print()
+    for job in jobsMeta:
+        row = []
+        for key,val in job.items():
+            if key == 'hash':
+                row.append(val[:10])
+            if key in ['created','scheduled']:
+                row.append((ts_to_str(val)))
+            if key== 'recipients':
+                row.append(str(val))
+            if key == 'state':
+                row.append(render_state(val))
+            if key == 'note':
+                row.append(val)
+        # data = [job["hash"], job["created"], job["scheduled"], job["state"], job["source"], job["title"]]
+        # row.extend(data)
+        table.add_row(*row)
+    console.print(table)
+    console.print()
+
+def print_panel(data):
+    print(Panel(data))
+
+def print_mail(content):
+    print(Padding(content, 1))
+
+def print_success(msg):
+        print(f"[bold green]✅ {msg}[/bold green]")
