ccd65330360cef2b6c865ee849f5c676a731534e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 08:05:52 +0000

    Add package sev-snp-measure/0.0.8/sev-snp-measure-0.0.8.tar.gz

---
+++ packages/sev-snp-measure/sev-snp-measure-0.0.8.tar.gz/sev-snp-measure-0.0.8/sevsnpmeasure/cli.py
@@ -0,0 +1,118 @@
+#
+# Copyright 2022- IBM Inc. All rights reserved
+# SPDX-License-Identifier: Apache-2.0
+#
+
+import argparse
+import base64
+import sys
+
+from sevsnpmeasure import guest
+from sevsnpmeasure import vcpu_types
+from sevsnpmeasure import vmm_types
+from .sev_mode import SevMode
+
+VERSION = '0.0.8'
+
+
+def auto_base_int(s: str) -> int:
+    return int(s, 0)
+
+
+def print_measurement(ld: bytes, sev_mode: SevMode, output_format: str, verbose: bool):
+    if output_format == "hex":
+        measurement = ld.hex()
+    elif output_format == "base64":
+        measurement = base64.b64encode(ld).decode()
+
+    if verbose:
+        print(f"Calculated {sev_mode.name} guest measurement: {measurement}")
+    else:
+        print(measurement)
+
+
+def get_vcpu_sig(parser, args, vmm_type):
+    if args.mode == 'sev':
+        return 0
+    elif args.vcpu_family:
+        return vcpu_types.cpu_sig(args.vcpu_family, args.vcpu_model, args.vcpu_stepping)
+    elif args.vcpu_sig:
+        return args.vcpu_sig
+    elif args.vcpu_type:
+        return vcpu_types.CPU_SIGS[args.vcpu_type]
+    elif vmm_type == vmm_types.VMMType.QEMU:
+        parser.error(f"missing --vcpu-type or --vcpu-sig or --vcpu-family in guest mode '{args.mode}'")
+
+
+def main() -> int:
+    parser = argparse.ArgumentParser(prog='sev-snp-measure',
+                                     description='Calculate AMD SEV/SEV-ES/SEV-SNP guest launch measurement')
+    parser.add_argument('--version', action='version', version=f'%(prog)s {VERSION}')
+    parser.add_argument('-v', '--verbose', action='store_true')
+    parser.add_argument('--mode', choices=['sev', 'seves', 'snp', 'snp:ovmf-hash', 'snp:svsm'], help='Guest mode',
+                        required=True)
+    parser.add_argument('--vcpus', metavar='N', type=int, help='Number of guest vcpus', default=None)
+    parser.add_argument('--vcpu-type', metavar='CPUTYPE', choices=list(vcpu_types.CPU_SIGS.keys()),
+                        help=f"Type of guest vcpu ({', '.join(vcpu_types.CPU_SIGS.keys())})",
+                        default=None)
+    parser.add_argument('--vcpu-sig', metavar='VALUE', type=auto_base_int, help='Guest vcpu signature value', default=None)
+    parser.add_argument('--vcpu-family', metavar='FAMILY', type=int, help='Guest vcpu family', default=None)
+    parser.add_argument('--vcpu-model', metavar='MODEL', type=int, help='Guest vcpu model', default=None)
+    parser.add_argument('--vcpu-stepping', metavar='STEPPING', type=int, help='Guest vcpu stepping', default=None)
+    parser.add_argument('--vmm-type', metavar='VMMTYPE', type=str,
+                        help=f"Type of guest vmm ({', '.join(vmm_types.VMMType.__members__.keys())})", default='QEMU')
+    parser.add_argument('--ovmf', metavar='PATH',
+                        help='OVMF file to calculate hash from', required=True)
+    parser.add_argument('--kernel', metavar='PATH',
+                        help='Kernel file to calculate hash from')
+    parser.add_argument('--initrd', metavar='PATH',
+                        help='Initrd file to calculate hash from (use with --kernel)')
+    parser.add_argument('--append', metavar='CMDLINE',
+                        help='Kernel command line to calculate hash from (use with --kernel)')
+    parser.add_argument('--output-format', choices=['hex', 'base64'], help='Measurement output format', default='hex')
+    parser.add_argument('--snp-ovmf-hash', metavar='HASH', help='Precalculated hash of the OVMF binary (hex string)')
+    parser.add_argument('--dump-vmsa', action='store_true',
+                        help='Write measured VMSAs to vmsa<N>.bin (seves, snp, and snp:svsm modes only)')
+    parser.add_argument('--vars-size', type=int, help='OVMF_VARS size in bytes (snp:svsm mode only)')
+    parser.add_argument('--svsm', type=str, help='SVSM binary (snp:svsm mode only)')
+    args = parser.parse_args()
+
+    if args.mode == 'snp:ovmf-hash':
+        print(guest.calc_snp_ovmf_hash(args.ovmf).hex())
+        return 0
+
+    if args.initrd and args.kernel is None:
+        parser.error("--kernel required when using --initrd")
+
+    if args.append and args.kernel is None:
+        parser.error("--kernel required when using --append")
+
+    if args.mode != 'sev' and args.vcpus is None:
+        parser.error(f"missing --vcpus N in guest mode '{args.mode}'")
+
+    if args.vmm_type in vmm_types.VMMType.__members__.keys():
+        vmm_type = vmm_types.VMMType[args.vmm_type]
+    else:
+        parser.error(f"unknown VMM type '{args.vmm_type}'")
+
+    vcpu_sig = get_vcpu_sig(parser, args, vmm_type)
+
+    try:
+        sev_mode = SevMode.from_str(args.mode)
+
+        if args.dump_vmsa is True and sev_mode not in [SevMode.SEV_ES, SevMode.SEV_SNP, SevMode.SEV_SNP_SVSM]:
+            parser.error("--dump-vmsa is not availibe in the selected mode")
+
+        ld = guest.calc_launch_digest(sev_mode, args.vcpus, vcpu_sig, args.ovmf, args.kernel, args.initrd, args.append,
+                                      args.snp_ovmf_hash, vmm_type, args.dump_vmsa, args.svsm, args.vars_size)
+
+        print_measurement(ld, sev_mode, args.output_format, args.verbose)
+    except RuntimeError as e:
+        print(f"Error: {e}", file=sys.stderr)
+        return 1
+
+    return 0
+
+
+if __name__ == '__main__':
+    sys.exit(main())
---
+++ packages/sev-snp-measure/sev-snp-measure-0.0.8.tar.gz/sev-snp-measure-0.0.8/tests/test_guest.py
@@ -0,0 +1,341 @@
+#
+# Copyright 2022- IBM Inc. All rights reserved
+# SPDX-License-Identifier: Apache-2.0
+#
+
+import unittest
+from sevsnpmeasure import guest
+from sevsnpmeasure import vcpu_types
+from sevsnpmeasure import vmm_types
+from sevsnpmeasure.sev_mode import SevMode
+import pathlib
+import tempfile
+import contextlib
+import os
+
+
+class TestGuest(unittest.TestCase):
+
+    # Test of we can generate a good OVMF hash
+    def test_snp_ovmf_hash_gen(self):
+        ovmf_hash = 'cab7e085874b3acfdbe2d96dcaa3125111f00c35c6fc9708464c2ae74bfdb048a198cb9a9ccae0b3e5e1a33f5f249819'
+        ld = guest.calc_launch_digest(
+                SevMode.SEV_SNP,
+                1,
+                vcpu_types.CPU_SIGS["EPYC-v4"],
+                "tests/fixtures/ovmf_AmdSev_suffix.bin",
+                "/dev/null",
+                "/dev/null",
+                "",
+                snp_ovmf_hash_str=ovmf_hash)
+        self.assertEqual(
+                ld.hex(),
+                'db06fb267824b1ccb56edbe2a9c2ce88841bca5090dc6dac'
+                '91d9cd30f3c2c0bf42daccb30d55d6625bfbf0dae5c50c6d')
+
+    # Test of we can a full LD from the OVMF hash
+    def test_snp_ovmf_hash_full(self):
+        ovmf_hash = guest.calc_snp_ovmf_hash("tests/fixtures/ovmf_AmdSev_suffix.bin").hex()
+        self.assertEqual(
+                ovmf_hash,
+                'edcf6d1c57ce868a167c990f58c8667c698269ef9e080324'
+                '6419eea914186343054d557e1f17acd93b032c106bc70d25')
+
+        ld = guest.calc_launch_digest(
+                SevMode.SEV_SNP,
+                1,
+                vcpu_types.CPU_SIGS["EPYC-v4"],
+                "tests/fixtures/ovmf_AmdSev_suffix.bin",
+                "/dev/null",
+                "/dev/null",
+                "console=ttyS0 loglevel=7",
+                snp_ovmf_hash_str=ovmf_hash)
+        self.assertEqual(
+                ld.hex(),
+                'f07864303ad8243132029e8110b92805c78d1135a15da75f'
+                '67abb9a711d78740347f24ee76f603e650ec4adf3611cc1e')
+
+    def test_snp_ec2(self):
+        ld = guest.calc_launch_digest(
+                SevMode.SEV_SNP,
+                1,
+                vcpu_types.CPU_SIGS["EPYC-v4"],
+                "tests/fixtures/ovmf_AmdSev_suffix.bin",
+                "/dev/null",
+                "/dev/null",
+                "",
+                vmm_type=vmm_types.VMMType.ec2)
+        self.assertEqual(
+                ld.hex(),
+                '760b6e51039d2d6c1fc6d38ca5c387967d158e0294883e45'
+                '22c36f89bd61bfc9cdb975cd1ceedffbe1b23b1daf4e3f42')
+
+    def test_snp(self):
+        ld = guest.calc_launch_digest(
+                SevMode.SEV_SNP,
+                1,
+                vcpu_types.CPU_SIGS["EPYC-v4"],
+                "tests/fixtures/ovmf_AmdSev_suffix.bin",
+                "/dev/null",
+                "/dev/null",
+                "console=ttyS0 loglevel=7")
+        self.assertEqual(
+                ld.hex(),
+                'f07864303ad8243132029e8110b92805c78d1135a15da75f'
+                '67abb9a711d78740347f24ee76f603e650ec4adf3611cc1e')
+
+    def test_snp_without_kernel(self):
+        ld = guest.calc_launch_digest(
+                SevMode.SEV_SNP,
+                1,
+                vcpu_types.CPU_SIGS["EPYC-v4"],
+                "tests/fixtures/ovmf_AmdSev_suffix.bin",
+                None,
+                None,
+                None)
+        self.assertEqual(
+                ld.hex(),
+                'e5e6be5a8fa6256f0245666bb237e2d028b7928148ce78d5'
+                '1b8a64dc9506c377709a5b5d7ab75554593bced304fcff93')
+
+    def test_snp_with_multiple_vcpus(self):
+        ld = guest.calc_launch_digest(
+                SevMode.SEV_SNP,
+                4,
+                vcpu_types.CPU_SIGS["EPYC-v4"],
+                "tests/fixtures/ovmf_AmdSev_suffix.bin",
+                "/dev/null",
+                "/dev/null",
+                "")
+        self.assertEqual(
+                ld.hex(),
+                '1c784beb8c49aa604b7fd57fbc73b36ec53a3f5fb48a2b89'
+                '5ad6cc2ea15d18ee7cc15e3e57c792766b45f944c3e81cfe')
+
+    def test_snp_with_ovmfx64_without_kernel(self):
+        ld = guest.calc_launch_digest(
+                SevMode.SEV_SNP,
+                1,
+                vcpu_types.CPU_SIGS["EPYC-v4"],
+                "tests/fixtures/ovmf_OvmfX64_suffix.bin",
+                None,
+                None,
+                None)
+        self.assertEqual(
+                ld.hex(),
+                '7ef631fa7f659f7250de96c456a0eb7354bd3b9461982f38'
+                '6a41c6a6aede87870ad020552a5a0716672d5d6e5b86b8f9')
+
+    def test_snp_with_ovmfx64_and_kernel_should_fail(self):
+        with self.assertRaises(RuntimeError) as c:
+            guest.calc_launch_digest(
+                    SevMode.SEV_SNP,
+                    1,
+                    vcpu_types.CPU_SIGS["EPYC-v4"],
+                    "tests/fixtures/ovmf_OvmfX64_suffix.bin",
+                    "/dev/null",
+                    "/dev/null",
+                    "")
+        self.assertEqual(str(c.exception),
+                         "Kernel specified but OVMF metadata doesn't include SNP_KERNEL_HASHES section")
+
+    def test_seves(self):
+        ld = guest.calc_launch_digest(
+                SevMode.SEV_ES,
+                1,
+                vcpu_types.CPU_SIGS["EPYC-v4"],
+                "tests/fixtures/ovmf_AmdSev_suffix.bin",
+                "/dev/null",
+                "/dev/null",
+                "")
+        self.assertEqual(
+                ld.hex(),
+                '2e91d54814445ad178180af09f881efe4079fc54bfddd0ec1179ecd3cdbdf772')
+
+    def test_seves_with_multiple_vcpus(self):
+        ld = guest.calc_launch_digest(
+                SevMode.SEV_ES,
+                4,
+                vcpu_types.CPU_SIGS["EPYC-v4"],
+                "tests/fixtures/ovmf_AmdSev_suffix.bin",
+                "/dev/null",
+                "/dev/null",
+                "")
+        self.assertEqual(
+                ld.hex(),
+                'c05d37600072dc5ff24bafc49410f0369ba3a37c130a7bb7055ac6878be300f7')
+
+    def test_seves_dump_vmsa(self):
+        """Test that SEV-ES mode creates vmsa files if requrested."""
+        fixtures_dir = pathlib.Path('tests/fixtures').absolute()
+        with tempfile.TemporaryDirectory() as tmp:
+            with push_dir(tmp):
+                guest.calc_launch_digest(
+                        SevMode.SEV_ES,
+                        4,
+                        vcpu_types.CPU_SIGS["EPYC-v4"],
+                        fixtures_dir / "ovmf_AmdSev_suffix.bin",
+                        "/dev/null",
+                        "/dev/null",
+                        "",
+                        dump_vmsa=True)
+                self.assertTrue(pathlib.Path("vmsa0.bin").exists())
+                self.assertTrue(pathlib.Path("vmsa1.bin").exists())
+                self.assertTrue(pathlib.Path("vmsa2.bin").exists())
+                self.assertTrue(pathlib.Path("vmsa3.bin").exists())
+                self.assertFalse(pathlib.Path("vmsa4.bin").exists())
+
+    def test_seves_with_ovmfx64_and_kernel_should_fail(self):
+        with self.assertRaises(RuntimeError) as c:
+            guest.calc_launch_digest(
+                    SevMode.SEV_ES,
+                    1,
+                    None,
+                    "tests/fixtures/ovmf_OvmfX64_suffix.bin",
+                    "/dev/null",
+                    "/dev/null",
+                    "")
+        self.assertEqual(str(c.exception),
+                         "Kernel specified but OVMF doesn't support kernel/initrd/cmdline measurement")
+
+    def test_sev(self):
+        ld = guest.calc_launch_digest(
+                SevMode.SEV,
+                1,
+                None,
+                "tests/fixtures/ovmf_AmdSev_suffix.bin",
+                "/dev/null",
+                "/dev/null",
+                "console=ttyS0 loglevel=7")
+        self.assertEqual(
+                ld.hex(),
+                'f0d92a1fda00249e008820bd40def6abbed2ee65fea8a8bc47e532863ca0cc6a')
+
+    def test_sev_with_kernel_without_initrd_and_append(self):
+        ld = guest.calc_launch_digest(
+                SevMode.SEV,
+                1,
+                None,
+                "tests/fixtures/ovmf_AmdSev_suffix.bin",
+                "/dev/null",
+                None,
+                None)
+        self.assertEqual(
+                ld.hex(),
+                '7332f6ef294f79919b46302e4541900a2dfc96714e2b7b4b5ccdc1899b78a195')
+
+    def test_sev_with_ovmfx64_and_kernel_should_fail(self):
+        with self.assertRaises(RuntimeError) as c:
+            guest.calc_launch_digest(
+                    SevMode.SEV,
+                    1,
+                    None,
+                    "tests/fixtures/ovmf_OvmfX64_suffix.bin",
+                    "/dev/null",
+                    "/dev/null",
+                    "")
+        self.assertEqual(str(c.exception),
+                         "Kernel specified but OVMF doesn't support kernel/initrd/cmdline measurement")
+
+    def test_snp_dump_vmsa(self):
+        """Test that SEV-SNP mode creates vmsa files if requrested."""
+        fixtures_dir = pathlib.Path('tests/fixtures').absolute()
+        with tempfile.TemporaryDirectory() as tmp:
+            with push_dir(tmp):
+                ovmf_hash = 'cab7e085874b3acfdbe2d96dcaa3125111f00c35c6fc9708464c2ae74bfdb048a198cb9a9ccae0b3e5e1a33f5f249819'
+                guest.calc_launch_digest(
+                        SevMode.SEV_SNP,
+                        1,
+                        vcpu_types.CPU_SIGS["EPYC-v4"],
+                        fixtures_dir / "ovmf_AmdSev_suffix.bin",
+                        "/dev/null",
+                        "/dev/null",
+                        "",
+                        snp_ovmf_hash_str=ovmf_hash,
+                        dump_vmsa=True)
+                self.assertTrue(pathlib.Path("vmsa0.bin").exists())
+                self.assertFalse(pathlib.Path("vmsa1.bin").exists())
+
+    def test_sev_with_ovmfx64_without_kernel(self):
+        ld = guest.calc_launch_digest(
+                SevMode.SEV,
+                1,
+                None,
+                "tests/fixtures/ovmf_OvmfX64_suffix.bin",
+                None,
+                None,
+                None)
+        self.assertEqual(
+                ld.hex(),
+                'af9d6c674b1ff04937084c98c99ca106b25c37b2c9541ac313e6e0c54426314f')
+
+    def test_snp_svsm_4_vcpus(self):
+        """Test that SNP-SVSM mode produces correct measurement value when using 4 vCPUs"""
+        ld = guest.calc_launch_digest(
+                SevMode.SEV_SNP_SVSM,
+                4,
+                vcpu_types.CPU_SIGS["EPYC-v4"],
+                'tests/fixtures/svsm_ovmf.fd',
+                None,
+                None,
+                None,
+                None,
+                vmm_types.VMMType.QEMU,
+                False,
+                'tests/fixtures/svsm.bin',
+                540672)
+        self.assertEqual(
+                ld.hex(),
+                '265aed64f2454253f6b19e49e17041ef052dedf964dd8d2740794a06aee3a720c475e3643a1b7e26b9efce7a107bf77f')
+
+    def test_snp_svsm_2_vcpus(self):
+        """Test that SNP-SVSM mode produces correct measurement value when using 2 vCPUs"""
+        ld = guest.calc_launch_digest(
+                SevMode.SEV_SNP_SVSM,
+                2,
+                vcpu_types.CPU_SIGS["EPYC-v4"],
+                'tests/fixtures/svsm_ovmf.fd',
+                None,
+                None,
+                None,
+                None,
+                vmm_types.VMMType.QEMU,
+                False,
+                'tests/fixtures/svsm.bin',
+                540672)
+        self.assertEqual(
+                ld.hex(),
+                '4ae03ca013690e956bc1eb16bc1c6465788dba64ad88a650db09216fa9cdddf06167bfce810eba8988108e71458dd68b')
+
+    def test_snp_svsm_dump_vmsa(self):
+        """Test that SNP-SVSM mode creates vmsa files if requrested."""
+        fixtures_dir = pathlib.Path('tests/fixtures').absolute()
+        with tempfile.TemporaryDirectory() as tmp:
+            with push_dir(tmp):
+                guest.calc_launch_digest(
+                        SevMode.SEV_SNP_SVSM,
+                        2,
+                        vcpu_types.CPU_SIGS["EPYC-v4"],
+                        fixtures_dir / 'svsm_ovmf.fd',
+                        None,
+                        None,
+                        None,
+                        None,
+                        vmm_types.VMMType.QEMU,
+                        True,
+                        fixtures_dir / 'svsm.bin',
+                        540672)
+                self.assertTrue(pathlib.Path("vmsa0.bin").exists())
+                self.assertTrue(pathlib.Path("vmsa1.bin").exists())
+                self.assertFalse(pathlib.Path("vmsa2.bin").exists())
+
+
+@contextlib.contextmanager
+def push_dir(dir: str):
+    """Context managed switching of the working directory"""
+    previous = os.getcwd()
+    os.chdir(dir)
+    try:
+        yield
+    finally:
+        os.chdir(previous)
