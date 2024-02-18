31296bb500bee358ff088e9040cacc6068d5c8aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 07:04:00 +0000

    Add package pyxsim/4.3.1/pyxsim-4.3.1.tar.gz

---
+++ packages/pyxsim/pyxsim-4.3.1.tar.gz/pyxsim-4.3.1/setup.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python
+import os
+
+import numpy as np
+from setuptools import find_packages, setup
+from setuptools.extension import Extension
+
+if os.name == "nt":
+    std_libs = []
+else:
+    std_libs = ["m"]
+
+cython_extensions = [
+    Extension(
+        "pyxsim.lib.sky_functions",
+        ["pyxsim/lib/sky_functions.pyx"],
+        language="c",
+        libraries=std_libs,
+        include_dirs=[np.get_include()],
+    ),
+    Extension(
+        "pyxsim.lib.spectra",
+        ["pyxsim/lib/spectra.pyx"],
+        language="c",
+        libraries=std_libs,
+        include_dirs=[np.get_include()],
+    ),
+    Extension(
+        "pyxsim.lib.interpolate",
+        ["pyxsim/lib/interpolate.pyx"],
+        language="c",
+        libraries=std_libs,
+        include_dirs=[np.get_include()],
+    ),
+]
+
+
+setup(
+    name="pyxsim",
+    packages=find_packages(),
+    description="Python package for simulating X-ray observations of astrophysical sources",
+    author="John ZuHone",
+    author_email="jzuhone@gmail.com",
+    url="http://github.com/jzuhone/pyxsim",
+    install_requires=[
+        "numpy",
+        "astropy>=4.0",
+        "h5py>=3.0",
+        "scipy",
+        "yt>=4.3",
+        "unyt>=2.9.5",
+        "soxs>=4.6.0",
+        "tqdm",
+        "cython",
+    ],
+    include_package_data=True,
+    ext_modules=cython_extensions,
+    classifiers=[
+        "Intended Audience :: Science/Research",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "Topic :: Scientific/Engineering :: Visualization",
+    ],
+)
