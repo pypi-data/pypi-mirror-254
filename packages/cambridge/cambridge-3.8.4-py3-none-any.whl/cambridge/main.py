cd38051a3cf0a213cb0b2e9f2c8f23261080ae82
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 18:04:23 +0000

    Add package cambridge/3.8.4/cambridge-3.8.4-py3-none-any.whl

---
+++ packages/cambridge/cambridge-3.8.4-py3-none-any.whl/cambridge/main.py
@@ -0,0 +1,40 @@
+"""
+`cambridge` is a terminal version of Cambridge Dictionary.
+The dictionary data comes from https://dictionary.cambridge.org
+If you're not satisfied with the result, you can try with "-w" flag to look up the word in Merriam-Webster Dictionary.
+"""
+
+import sqlite3
+
+def main():
+    try:
+        con = sqlite3.connect(
+            DB, detect_types=sqlite3.PARSE_DECLTYPES | sqlite3.PARSE_COLNAMES
+        )
+        cur = con.cursor()
+
+        args = parse_args()
+        if args is not None:
+            args.func(args, con, cur)
+
+        cur.close()
+        con.close()
+
+    except KeyboardInterrupt:
+        print("\nStopped by user")
+
+
+if __name__ == "__main__":
+    import os
+    import sys
+
+    sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+    from cambridge.args import parse_args
+    from cambridge.cache import DB
+
+    main()
+
+else:
+    from .args import parse_args
+    from .cache import DB
