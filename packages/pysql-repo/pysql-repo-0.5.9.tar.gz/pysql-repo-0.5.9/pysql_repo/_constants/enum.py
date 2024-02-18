f3d59660b118cd424fb065d8950b9995a14f5466
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 15:06:01 +0000

    Add package pysql-repo/0.5.9/pysql-repo-0.5.9.tar.gz

---
+++ packages/pysql-repo/pysql-repo-0.5.9.tar.gz/pysql-repo-0.5.9/pysql_repo/_constants/enum.py
@@ -0,0 +1,35 @@
+# MODULES
+import enum
+
+
+class Operators(enum.Enum):
+    LIKE = "LIKE"
+    NOT_LIKE = "NOT_LIKE"
+    ILIKE = "ILIKE"
+    NOT_ILIKE = "NOT_ILIKE"
+    EQUAL = "EQUAL"
+    IEQUAL = "IEQUAL"
+    DIFFERENT = "DIFFERENT"
+    IDIFFERENT = "IDIFFERENT"
+    BETWEEN = "BETWEEN"
+    BETWEEN_OR_EQUAL = "BETWEEN_OR_EQUAL"
+    SUPERIOR = "SUPERIOR"
+    SUPERIOR_OR_EQUAL = "SUPERIOR_OR_EQUAL"
+    INFERIOR = "INFERIOR"
+    INFERIOR_OR_EQUAL = "INFERIOR_OR_EQUAL"
+    IN = "IN"
+    IIN = "IIN"
+    NOT_IN = "NOT_IN"
+    NOT_IIN = "NOT_IIN"
+    HAS = "HAS"
+    ANY = "ANY"
+
+
+class LoadingTechnique(enum.Enum):
+    CONTAINS_EAGER = "contains_eager"
+    LAZY = "select"
+    JOINED = "joined"
+    SUBQUERY = "subquery"
+    SELECTIN = "selectin"
+    RAISE = "raise"
+    NOLOAD = "noload"
