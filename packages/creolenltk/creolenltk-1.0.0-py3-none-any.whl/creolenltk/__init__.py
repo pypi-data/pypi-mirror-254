f154634e4365b8940fa01464c14616158ee2f65a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 02:11:24 +0000

    Add package creolenltk/1.0.0/creolenltk-1.0.0-py3-none-any.whl

---
+++ packages/creolenltk/creolenltk-1.0.0-py3-none-any.whl/creolenltk/__init__.py
@@ -0,0 +1,6 @@
+import os
+from .text_cleaner import TextCleaner
+from .contraction_expansion import ContractionToExpansion
+from .spelling_checker import SpellingChecker
+from .stopwords import Stopword
+from .tokenizer import Tokenizer
---
+++ packages/creolenltk/creolenltk-1.0.0-py3-none-any.whl/creolenltk/contraction_expansion.py
@@ -0,0 +1,57 @@
+import re
+
+
+class ContractionToExpansion:
+    """
+    A class for expanding contractions in Creole Haitian text.
+
+    Methods:
+    - expand_contractions(text): Expands contractions in the input text.
+    """
+
+    CONTRACTIONS_MAPPING = {
+        "m": "mwen",
+        "mw": "mwen",
+        "n": "nou",
+        "ka": "kapab",
+        "l": "li",
+        "pot": "te poko",
+        "t": "te",
+        "w": "ou",
+        "pral": "prale",
+        "fin": "fini",
+        "sot": "soti",
+        "k": "ki",
+        "al": "ale",
+        "konn": "konnen",
+        "kab": "kapab",
+        "lap": "li ap",
+        "paka": "pa kapab",
+        "al": "ale",
+        "jan": "kijan",
+        "diw": "di ou",
+        "poum": "pou mwen",
+        "yok": "yo ki",
+        "yap": "yo ap",
+        "tap": "te ap",
+        "kap": "ki ap"
+    }
+
+    @staticmethod
+    def expand_contractions(text):
+        """
+        Expands contractions in the input text.
+
+        Parameters:
+        text (str): Input text containing contractions.
+
+        Returns:
+        str: Text with contractions expanded.
+        """
+        if isinstance(text, str):
+            text = text.replace("'", " ")
+            for key, value in ContractionToExpansion.CONTRACTIONS_MAPPING.items():
+                text = re.sub(r'\b' + re.escape(key.lower()) + r'\b', value, text)
+            return text
+        else:
+            return text
