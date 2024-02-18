0bb39bade88ffd9fd1e570ad9a20362de8f8a3c1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 06:04:47 +0000

    Add package typed-ffmpeg/0.1.13/typed_ffmpeg-0.1.13.tar.gz

---
+++ packages/typed-ffmpeg/typed_ffmpeg-0.1.13.tar.gz/typed_ffmpeg-0.1.13/src/ffmpeg/schema.py
@@ -0,0 +1,21 @@
+import enum
+
+
+class StreamType(enum.Enum):
+    """
+    The type of a stream. (audio or video)
+    """
+
+    audio = "audio"
+    """it is an audio stream"""
+    video = "video"
+    """it is a video stream"""
+
+
+class Default(str):
+    """
+    This is the default value for an option. It is used for annotation purposes only
+    and will not be passed to the ffmpeg command line.
+    """
+
+    ...
