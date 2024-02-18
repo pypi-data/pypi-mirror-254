f731eac49d316de0040433072fd84fb9215cc3ee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 12:05:08 +0000

    Add package gsn-bnml-dist/0.2/gsn-bnml-dist-0.2.tar.gz

---
+++ packages/gsn-bnml-dist/gsn-bnml-dist-0.2.tar.gz/gsn-bnml-dist-0.2/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='gsn-bnml-dist',
+      version='0.2',
+      description='Gaussian and binomial distributions',
+      packages=['gsn-bnml-dist'],
+      author='Shaun Bishop',
+      zip_safe=False)
