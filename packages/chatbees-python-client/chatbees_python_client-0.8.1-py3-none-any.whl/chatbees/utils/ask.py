d462f35af191a21855745db4eaa7aebaf21b66c1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 17:04:38 +0000

    Add package chatbees-python-client/0.8.1/chatbees_python_client-0.8.1-py3-none-any.whl

---
+++ packages/chatbees-python-client/chatbees_python_client-0.8.1-py3-none-any.whl/chatbees/utils/ask.py
@@ -0,0 +1,44 @@
+from typing import List, Tuple
+
+from chatbees.server_models.doc_api import AskRequest, AskResponse, AnswerReference
+from chatbees.utils.config import Config
+
+
+def ask(
+    namespace_name: str,
+    collection_name: str,
+    question: str,
+    top_k: int = 5,
+    doc_name: str = None,
+    history_messages: List[Tuple[str, str]] = None,
+) -> (str, List[AnswerReference]):
+    url = f'{Config.get_base_url()}/docs/ask'
+
+    req = AskRequest(
+        namespace_name=namespace_name,
+        collection_name=collection_name,
+        question=question,
+        top_k=top_k,
+        doc_name=doc_name,
+        history_messages=history_messages,
+    )
+    enforce_api_key = True
+
+    # Only allow openai-web collection to be accessed without API key to
+    # simplify demo.
+    if collection_name == 'openai-web':
+        enforce_api_key = False
+
+    resp = Config.post(
+        url=url,
+        data=req.model_dump_json(),
+        enforce_api_key=enforce_api_key
+    )
+    resp = AskResponse.model_validate(resp.json())
+
+    return (
+        resp.answer,
+        [AnswerReference(doc_name=ref.doc_name,
+                         page_num=ref.page_num,
+                         sample_text=ref.sample_text) for ref in resp.refs]
+    )
