2a4f81d18da59b66eef3db868402212531620aed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 00:06:57 +0000

    Add package naive-shapelet-CF/0.1.3/naive_shapelet_CF-0.1.3.tar.gz

---
+++ packages/naive-shapelet-CF/naive_shapelet_CF-0.1.3.tar.gz/naive_shapelet_CF-0.1.3/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='naive_shapelet_CF',
+    version='0.1.3',
+    packages=find_packages(),
+    install_requires=[
+        'pandas',
+        'numpy',
+        'tslearn',
+        'pyts',
+        'scikit-learn',
+        'matplotlib',
+        'scipy',
+    ],
+    author='Peiyu Li',
+    author_email='peiyu.li@usu.edu',
+)
