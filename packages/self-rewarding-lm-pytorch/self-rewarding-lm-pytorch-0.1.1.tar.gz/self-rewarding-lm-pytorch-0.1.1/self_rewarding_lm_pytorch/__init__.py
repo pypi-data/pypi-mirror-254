d8f015e7697eeaea849a2a30f487821dea958739
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 06:04:38 +0000

    Add package self-rewarding-lm-pytorch/0.1.1/self-rewarding-lm-pytorch-0.1.1.tar.gz

---
+++ packages/self-rewarding-lm-pytorch/self-rewarding-lm-pytorch-0.1.1.tar.gz/self-rewarding-lm-pytorch-0.1.1/self_rewarding_lm_pytorch/__init__.py
@@ -0,0 +1,16 @@
+from self_rewarding_lm_pytorch.self_rewarding_lm_pytorch import (
+    SelfRewardingTrainer,
+    RewardConfig
+)
+
+from self_rewarding_lm_pytorch.spin import (
+    SPIN,
+    SPINTrainer,
+)
+
+from self_rewarding_lm_pytorch.dpo import (
+    DPO,
+    DPOTrainer,
+)
+
+from self_rewarding_lm_pytorch.mocks import create_mock_dataset
