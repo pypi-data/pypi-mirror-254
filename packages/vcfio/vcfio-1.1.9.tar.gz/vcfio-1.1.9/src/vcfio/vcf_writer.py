69887603152a24a152632d2e003e2414fc766839
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 23:05:51 +0000

    Add package vcfio/1.1.9/vcfio-1.1.9.tar.gz

---
+++ packages/vcfio/vcfio-1.1.9.tar.gz/vcfio-1.1.9/src/vcfio/vcf_writer.py
@@ -0,0 +1,81 @@
+from __future__ import annotations
+
+from pathlib import Path
+from typing import TYPE_CHECKING
+import logging
+from vcfio.utils.file_utils import open_file
+from vcfio.variant.variant import Variant
+
+if TYPE_CHECKING:
+    from typing import Union
+    from typing import AnyStr
+    from typing import Iterable
+
+REQUIRED_HEADERS = (
+    '##fileformat=VCFv4.2',
+    '#CHROM	POS	ID	REF	ALT	QUAL	FILTER	INFO	FORMAT	{sample_names}'
+)
+
+
+class VcfWriter:
+    def __init__(self, output_file: Union[AnyStr, Path]):
+        """
+        output_file -- /path/to/output
+        headers -- array of vcf headers - should comply with VCF specifications
+        variants -- array of Variants
+        """
+        self.output_file = Path(output_file)
+
+        self._file_descriptor = open_file(self.output_file, mode='w')
+        self._write_last_header = False
+
+    def write(self, headers: Iterable[AnyStr] = (), variants: Iterable[Variant] = ()):
+        """
+        Write the headers and variants to self.output_path
+        If headers is empty - will write REQUIRED HEADERS (using sample names from `variants` parameter
+        """
+        self.write_headers(headers)
+        self.write_variants(variants)
+
+    def write_headers(self, headers: Iterable[AnyStr] = ()):
+        if not headers:
+            headers = [REQUIRED_HEADERS[0]]
+            self._write_last_header = True
+        sorted_headers = sorted(headers, key=self.__sort_headers)
+        for header in sorted_headers:
+            self._file_descriptor.write(header)
+            self._file_descriptor.write('\n')
+
+    def __sort_headers(self, header):
+        if header.startswith("##fileformat"):
+            return 0
+        elif header.startswith("##"):
+            return 1
+        elif header.startswith("#CHROM"):
+            return 2
+        else:
+            logging.warning(f"This line is not a header. It will be added after the headers.\n{header}")
+            return 3
+
+    def write_variants(self, variants: Iterable[Variant]):
+        for variant in variants:
+            self.write_variant(variant)
+
+    def write_variant(self, variant: Variant):
+        if self._write_last_header:
+            self._file_descriptor.write(REQUIRED_HEADERS[-1].format(sample_names='\t'.join(variant.sample_names)))
+            self._file_descriptor.write('\n')
+            self._write_last_header = False
+
+        self._file_descriptor.write(variant.to_vcf_line())
+        self._file_descriptor.write('\n')
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        self.close()
+        return False
+
+    def close(self):
+        self._file_descriptor.close()
