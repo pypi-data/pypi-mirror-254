bedc1710587b66ef75a999dada618f58ff43ce13
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 12:04:54 +0000

    Add package envserv/1.0.2/envserv-1.0.2-py3-none-any.whl

---
+++ packages/envserv/envserv-1.0.2-py3-none-any.whl/envserv/errors.py
@@ -0,0 +1,7 @@
+class EnvfileError(ImportError):
+    def __init__(self, *args: object, name: str | None = ..., path: str | None = ...) -> None:
+        super().__init__(*args, name=name, path=path)
+        
+class EnvVariableError(NameError):
+    def __init__(self, *args: object) -> None:
+        super().__init__(*args)
