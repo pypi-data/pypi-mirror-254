d9c7a21ff9ddfe55495754beb9beb821272f63a7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 07:04:15 +0000

    Add package SplitticAPI/1.3.1/SplitticAPI-1.3.1.tar.gz

---
+++ packages/SplitticAPI/SplitticAPI-1.3.1.tar.gz/SplitticAPI-1.3.1/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+with open("README.rst") as f:
+    long_description = f.read()
+
+setup(
+    name='SplitticAPI',
+    author='CutyCat2000',
+    license='MIT',
+    url='https://github.com/Mike-FreeAI/splitticapi',
+    version='1.3.1',
+    packages=find_packages(),
+    install_requires=[
+        'asyncio',
+        'httpx',
+        'Pillow',
+        'requests',
+    ],
+    description='A Python wrapper for the SplitticAPI',
+    long_description=long_description,
+    long_description_content_type='text/x-rst',
+)
