1f9ea5acf5a63f17eb3e65f964f3622011cb187c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 21:05:33 +0000

    Add package discord-ext-i17n/0.0.8/discord_ext_i17n-0.0.8-py3-none-any.whl

---
+++ packages/discord-ext-i17n/discord_ext_i17n-0.0.8-py3-none-any.whl/discord/ext/i17n/agent.py
@@ -0,0 +1,445 @@
+from inspect import getfullargspec
+from typing import Any, Callable, Optional, Union, Coroutine
+from discord import Interaction, Webhook
+from discord.types.snowflake import Snowflake
+from discord.abc import Messageable
+from discord.message import Message
+from discord.http import HTTPClient
+from discord.interactions import InteractionResponse
+from discord.webhook.async_ import AsyncWebhookAdapter, WebhookMessage
+from discord.ui import Modal
+
+from discord.ext.i17n.language import Language
+from discord.ext.i17n.preprocess import (
+    TranslationAgent,
+    Translator,
+)
+
+Messageable_send = Messageable.send
+Message_edit = Message.edit
+HTTPClient_edit_message = HTTPClient.edit_message
+HTTPClient_send_message = HTTPClient.send_message
+InteractionResponse_send_message = InteractionResponse.send_message
+InteractionResponse_edit_message = InteractionResponse.edit_message
+InteractionResponse_send_modal = InteractionResponse.send_modal
+AsyncWebhookAdapter_create_interaction_response = (
+    AsyncWebhookAdapter.create_interaction_response
+)
+AsyncWebhookAdapter_execute_webhook = AsyncWebhookAdapter.execute_webhook
+Webhook_send = Webhook.send
+WebhookMessage_edit = WebhookMessage.edit
+
+InterfaceOverridable = Callable[
+    ..., Coroutine[Any, Any, Union[Message, Interaction, None]]
+]
+
+
+async def try_defer(obj: Any):
+    if isinstance(obj, InteractionResponse):
+        if not obj.is_done():
+            await obj.defer()
+            return True
+        return False
+    
+def wrap_i17n_editer(edit_func: InterfaceOverridable):
+    """
+    Returns a wrapped edit function that appends a language suffix into the
+    content field as an entry point to the translation system.
+    """
+    
+    async def wrapped_i17n_edit(self: Union[Message, InteractionResponse], **kwds):
+        dest_lang = await AutoI17nAgent.detector.first_language_or(self)
+        if dest_lang:
+            kwds["content"] = TranslationAgent.encode_lang_str(
+                AutoI17nAgent.source_lang, kwds.get("content", None) or "", dest_lang
+            )
+        return await edit_func(self, **kwds)
+    
+    return wrapped_i17n_edit
+
+
+def i17n_HTTPClient_edit_message(
+    self: HTTPClient, channel_id: Snowflake, message_id: Snowflake, **fields: Any
+):
+    """
+    An override for `HTTPClient.edit_message` whereas it extracts extra
+    language data if exists and translates accordingly.
+    
+    language data is only appended when the recieving channel has affiliations
+    """
+    if fields["content"]:
+        content, lang = TranslationAgent.decode_lang_str(fields["content"])
+        if lang:
+            fields, fields["content"] = TranslationAgent.translate_payload(
+                lang, fields, content, **AutoI17nAgent.get_config()
+            )
+    return HTTPClient_edit_message(self, channel_id, message_id, **fields)
+
+def wrap_i17n_sender(send_func: InterfaceOverridable):
+    """
+    Returns a wrapped message sending function that appends a language suffix
+    into the content field as an entry point to the translation system.
+    """
+    
+    async def wrapped_i17n_send(
+        self: Union[Messageable, InteractionResponse], content=None, *_, **kwds
+    ):
+        dest_lang = await AutoI17nAgent.detector.first_language_or(self)
+        if dest_lang:
+            content = TranslationAgent.encode_lang_str(
+                AutoI17nAgent.source_lang, content, dest_lang
+            )
+            if await try_deferself:
+                return await self._parent.followup.send(content, **kwds)
+            return await send_func(self, content, **kwds)
+    return wrapped_i17n_send
+
+def i17n_HTTPClient_send_message(
+    self: HTTPClient,
+    channel_id: Snowflake,
+    content: Optional[str],
+    *_,
+    **kwds,
+):
+    """
+    Intercepts the excess language suffix from the content field if it exists
+    and performs translation.
+    """
+    if content:
+        payload, lang = TranslationAgent.decode_lang_str(content)
+        if lang:
+            kwds, content = TranslationAgent.translate_payload(
+                lang, kwds, payload, **AutoI17nAgent.get_config()
+            )
+    return HTTPClient_send_message(self, channel_id, content, **kwds)
+
+
+def i17n_Adapter_create_interaction_response(self: AsyncWebhookAdapter, *args, **kwds):
+    """
+    An override for `AsyncWebhookAdapter.create_interaction_response` whereby
+    extracting language encoding if it exists and translates accordingly.
+    """
+    if kwds["data"] and ("content" in kwds["data"] or "title" in kwds["data"]):
+        content_type = "content" if "content" in kwds["data"] else "title"
+        content, lang = TranslationAgent.decode_lang_str(kwds["data"][content_type])
+
+        if lang:
+            (
+                kwds["data"],
+                kwds["data"][content_type],
+            ) = TranslationAgent.translate_payload(
+                lang, kwds["data"], content, **AutoI17nAgent.get_config()
+            )
+    return AsyncWebhookAdapter_create_interaction_response(self, *args, **kwds)
+
+
+def i17n_AsyncWebhookAdapter_execute_webhook(self: AsyncWebhookAdapter, *args, **kwds):
+    if "payload" in kwds and kwds["payload"]:
+        if "content" in kwds["payload"] and kwds["payload"]["content"].strip():
+            content, lang = TranslationAgent.decode_lang_str(kwds["payload"]["content"])
+            if lang:
+                (
+                    kwds["payload"],
+                    kwds["payload"]["content"],
+                ) = TranslationAgent.translate_payload(
+                    lang, kwds["payload"], content, **AutoI17nAgent.get_config()
+                )
+    return AsyncWebhookAdapter_execute_webhook(self, *args, **kwds)
+
+
+async def i17n_InteractionResponse_send_modal(self: InteractionResponse, modal: Modal):
+    """
+    Language code is appended to the modal title if there is a language
+    preference.
+
+    TODO: Very problematic if translation functions are slow, often ends up
+    timing out on responses.
+    """
+    dest_lang = await AutoI17nAgent.detector.first_language_of(self)
+    if dest_lang:
+        modal.title = TranslationAgent.encode_lang_str(
+            AutoI17nAgent.source_lang, modal.title, dest_lang
+        )
+    return await InteractionResponse_send_modal(self, modal)
+
+
+def isinstancemethod(func: Callable) -> bool:
+    """
+    Returns whether if a given function is a staticmethod or an
+    instancemethod/classmethod.
+    """
+    return "self" in getfullargspec(func)[0]
+
+
+class Detector:
+    def __new__(cls, *_, **__):
+        obj = super().__new__(cls)
+        for item_name in dir(obj):
+            item = getattr(obj, item_name, None)
+            if callable(item):
+                if hasattr(item, "__lang_getter__"):
+                    Detector.language_of = item  # type: ignore
+        return obj
+
+    async def first_language_of(
+        self, ctx: Union[Message, InteractionResponse, Messageable, Webhook]
+    ):
+        """
+        Resolves the most precedent destination language from a context object.
+        Other forms of checks are performed here.
+        """
+        guild_id = channel_id = author_id = dest_lang = None
+        if isinstance(ctx, Message):
+            author_id = ctx.author.id
+            channel_id = ctx.channel.id
+            if ctx.guild:
+                guild_id = ctx.guild.id
+        elif isinstance(ctx, Messageable):
+            ch = await ctx._get_channel()
+            channel_id = ch.id
+            try:
+                guild_id = ch.guild.id  # type: ignore
+            except AttributeError:
+                guild_id = None
+        elif isinstance(ctx, Webhook):
+            channel_id = ctx.channel_id
+            guild_id = ctx.guild_id
+        else:
+            if ctx._parent.message and ctx._parent.message.guild:
+                author_id = ctx._parent.message.author.id
+                channel_id = ctx._parent.message.channel.id
+                guild_id = ctx._parent.message.guild.id
+            else:
+                if ctx._parent.user:
+                    author_id = ctx._parent.user.id
+                channel_id = ctx._parent.channel_id
+                guild_id = ctx._parent.guild_id
+
+        if author_id:
+            dest_lang = await self.language_of(author_id)
+        if not dest_lang and channel_id:
+            dest_lang = await self.language_of(channel_id)
+        if not dest_lang and guild_id:
+            dest_lang = await self.language_of(guild_id)
+        return dest_lang
+
+    @staticmethod
+    async def language_of(snowflake: Snowflake) -> Optional[Language]:
+        """
+        Resolves a destination language for a snowflake.
+        """
+        return None
+
+    @staticmethod
+    def lang_getter(fn: Any):
+        fn.__lang_getter__ = fn
+        if not isinstancemethod(fn):
+            Detector.language_of = staticmethod(fn)  # type: ignore
+        return fn
+
+
+class AgentSession:
+    def __init__(
+        self,
+        translate_all: Optional[bool] = None,
+        translate_messages: Optional[bool] = None,
+        translate_embeds: Optional[bool] = None,
+        translate_buttons: Optional[bool] = None,
+        translate_selects: Optional[bool] = None,
+        translate_modals: Optional[bool] = None,
+        source_lang: Optional[Language] = None,
+    ):
+        """
+        A context menu to temporarily reconfigure the translation settings until
+        it has been exited.
+
+        Parameters
+        ----------
+
+        translate_{x}: bool
+            Temporary flag for translating x interface
+        source_lang: Language
+            Language in which the source text is written in the bot. Defaults
+            to `Language.English`
+
+        E.g.
+        ```py
+        with AgentSession(translate_embeds=False):
+            await ctx.reply(
+                embed=Embed(
+                    title="Theoretical Physics",
+                    description="This description will never be translated.",
+                )
+            )
+        ```
+        Technically equivalent to:
+        ```py
+        DEFAULT = AutoI17nAgent.translate_embeds
+        AutoI17nAgent.translate_embeds = False
+        await ctx.reply(
+            embed=Embed(
+                title="Theoretical Physics",
+                description="This description will never be translated.",
+            )
+        )
+        AutoI17nAgent.translate_embeds = DEFAULT
+        ```
+        """
+
+        vmap = {
+            "translate_messages": translate_messages,
+            "translate_embeds": translate_embeds,
+            "translate_buttons": translate_buttons,
+            "translate_selects": translate_selects,
+            "translate_modals": translate_modals,
+            "source_lang": source_lang,
+        }
+        self.initial_config = {key: getattr(AutoI17nAgent, key, None) for key in vmap}
+
+        for key, val in vmap.items():
+            if translate_all and key.startswith("translate_"):
+                setattr(AutoI17nAgent, key, True)
+            elif val is not None:
+                setattr(AutoI17nAgent, key, val)
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, *arg, **kwds):
+        for key, val in self.initial_config.items():
+            if val is not None:
+                setattr(AutoI17nAgent, key, val)
+
+
+class AutoI17nAgent:
+    translator = Translator()
+    detector = Detector()
+
+    translate_messages = True
+    translate_embeds = False
+    translate_buttons = False
+    translate_selects = False
+    translate_modals = False
+    source_lang = Language.English
+    _instantiated = False
+
+    def __init__(
+        self,
+        translator: Optional[Translator] = None,
+        detector: Optional[Detector] = None,
+        translate_all: Optional[bool] = None,
+        translate_messages: Optional[bool] = None,
+        translate_embeds: Optional[bool] = None,
+        translate_buttons: Optional[bool] = None,
+        translate_selects: Optional[bool] = None,
+        translate_modals: Optional[bool] = None,
+        source_lang: Optional[Language] = None,
+        handle_webhooks: bool = True,
+    ):
+        """
+        Sets initialized injectors to override high and low level.
+        At any given case, you cannot initialize this class more than
+        once.
+
+        Parameters
+        ----------
+        translate_{x}: bool
+            Flag for translating x interface, defaults to `True` for
+            `translate_messages` and `False` for everything else
+        source_lang: Language
+            Language in which the source text is written in the bot. Defaults
+            to `Language.English`
+        handle_webhooks: bool
+            enable webhook translations (not a static property and
+            is only settable through this parameter)
+        """
+        if AutoI17nAgent._instantiated:
+            raise TypeError("this class should only be instantiated once")
+        else:
+            AutoI17nAgent._instantiated = True
+
+        setattr(
+            AsyncWebhookAdapter,
+            "create_interaction_response",
+            i17n_Adapter_create_interaction_response,
+        )
+        setattr(
+            AsyncWebhookAdapter,
+            "execute_webhook",
+            i17n_AsyncWebhookAdapter_execute_webhook,
+        )
+        if handle_webhooks:
+            setattr(Webhook, "send", wrap_i17n_sender(Webhook_send))
+        setattr(Messageable, "send", wrap_i17n_sender(Messageable_send))
+        setattr(
+            InteractionResponse,
+            "send_message",
+            wrap_i17n_sender(InteractionResponse_send_message),
+        )
+        setattr(HTTPClient, "send_message", i17n_HTTPClient_send_message)
+        setattr(Message, "edit", wrap_i17n_editer(Message_edit))
+        setattr(
+            InteractionResponse,
+            "edit_message",
+            wrap_i17n_editer(InteractionResponse_edit_message),
+        )
+        setattr(WebhookMessage, "edit", wrap_i17n_editer(WebhookMessage_edit))
+        setattr(HTTPClient, "edit_message", i17n_HTTPClient_edit_message)
+        setattr(InteractionResponse, "send_modal", i17n_InteractionResponse_send_modal)
+
+        for key, val in {
+            "translator": translator,
+            "detector": detector,
+            "translate_messages": translate_messages,
+            "translate_embeds": translate_embeds,
+            "translate_buttons": translate_buttons,
+            "translate_selects": translate_selects,
+            "translate_modals": translate_modals,
+            "source_lang": source_lang,
+        }.items():
+            if translate_all and key.startswith("translate_"):
+                setattr(AutoI17nAgent, key, True)
+            elif val is not None:
+                setattr(AutoI17nAgent, key, val)
+
+    @staticmethod
+    def get_config():
+        """
+        Returns a dictionary of configurations needed for translation.
+        """
+        return {
+            "source_lang": AutoI17nAgent.source_lang,
+            "translator": AutoI17nAgent.translator,
+            "translate_components": (
+                AutoI17nAgent.translate_buttons
+                or AutoI17nAgent.translate_selects
+                or AutoI17nAgent.translate_modals
+            ),
+            "translate_messages": AutoI17nAgent.translate_messages,
+            "translate_embeds": AutoI17nAgent.translate_embeds,
+            "translate_buttons": AutoI17nAgent.translate_buttons,
+            "translate_selects": AutoI17nAgent.translate_selects,
+            "translate_modals": AutoI17nAgent.translate_modals,
+        }
+
+
+# alias for backwards compatibility
+Agent = AutoI17nAgent
+
+
+def no_translate(text: str):
+    """
+    Wraps the text with \u200b (zero width space) characters to tells
+    the translator to skip the text.
+
+    Parameters
+    ----------
+    text : str
+        The text to be skipped.
+
+    Returns
+    -------
+    str
+        The text wrapped with special characters.
+    """
+    return f"\u200b{text}\u200b"
