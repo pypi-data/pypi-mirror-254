a508e6f1aa8a22fed896b24e5eb4d42110cda5d3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 13:04:24 +0000

    Add package imerit-ango/1.3.5/imerit_ango-1.3.5-py3-none-any.whl

---
+++ packages/imerit-ango/imerit_ango-1.3.5-py3-none-any.whl/imerit_ango/models/enums.py
@@ -0,0 +1,19 @@
+from enum import Enum
+
+class Metrics(Enum):
+    LabelStageGroups = 'LabelStageGroups'
+    TimePerTask = 'TimePerTask'
+    AnnotationStatus = 'AnnotationStatus'
+    AnswerDistribution = 'AnswerDistribution'
+    ConsensusRanges = 'ConsensusRanges'
+    AssetSize = 'AssetSize'
+
+class OrganizationRoles(Enum):
+    Member = 'member'
+    Admin = 'admin'
+
+class ProjectRoles(Enum):
+    Manager = 'Manager'
+    Labeler = 'Labeler'
+    Reviewer = 'Reviewer'
+    Lead = 'Lead'
