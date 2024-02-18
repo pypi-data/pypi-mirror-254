ecf3c5a187cf0475600c41d3c54e185f7f9c2a45
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 19:05:23 +0000

    Add package xarray-units/0.4.0/xarray_units-0.4.0.tar.gz

---
+++ packages/xarray-units/xarray_units-0.4.0.tar.gz/xarray_units-0.4.0/xarray_units/operator.py
@@ -0,0 +1,406 @@
+__all__ = [
+    "take",
+    # any-units operators
+    "mul",  # *
+    "pow",  # **
+    "matmul",  # @
+    "truediv",  # /
+    # same-units operators
+    "add",  # +
+    "sub",  # -
+    "floordiv",  # //
+    "mod",  # %
+    "lt",  # <
+    "le",  # <=
+    "eq",  # ==
+    "ne",  # !=
+    "ge",  # >=
+    "gt",  # >
+]
+
+
+# standard library
+import operator as opr
+from typing import Any, Literal, Union, get_args
+
+
+# dependencies
+from astropy.units import Quantity
+from xarray import DataArray
+from .quantity import apply_any, set, to, unset
+from .utils import TESTER, TDataArray, UnitsConversionError, units_of
+
+
+# type hints
+AnyUnitsOperator = Literal[
+    "mul",  # *
+    "pow",  # **
+    "matmul",  # @
+    "truediv",  # /
+]
+SameUnitsOperator = Literal[
+    "add",  # +
+    "sub",  # -
+    "floordiv",  # //
+    "mod",  # %
+    "lt",  # <
+    "le",  # <=
+    "eq",  # ==
+    "ne",  # !=
+    "ge",  # >=
+    "gt",  # >
+]
+Operator = Union[AnyUnitsOperator, SameUnitsOperator]
+
+
+def take(left: TDataArray, operator: Operator, right: Any, /) -> TDataArray:
+    """Perform an operation between left and right data considering units.
+
+    Args:
+        left: DataArray with units on the left side of the operator.
+        operator: Name of the operator (e.g. ``"add"``, ``"gt"``).
+        right: Any data on the right side of the operator.
+
+    Returns:
+        DataArray of the result of the operation. Units are
+        the same as ``left`` in a numerical operation (e.g. ``"add"``)
+        or nothing in a relational operation (e.g. ``"gt"``).
+
+    Raises:
+        UnitsConversionError: Raised if units cannot be converted.
+        UnitsNotFoundError: Raised if units are not found.
+        UnitsNotValidError: Raised if units are not valid.
+
+    """
+    left_units = units_of(left, strict=True)
+    right_units = units_of(right, strict=False)
+
+    if operator == "pow":
+        method = f"__{operator}__"
+        args = (Quantity(right, right_units),)
+    elif operator == "matmul":
+        method = "__mul__"
+        args = (Quantity(TESTER, right_units),)
+    elif operator == "eq" or operator == "ne":
+        method = "__lt__"
+        args = (Quantity(TESTER, right_units),)
+    else:
+        method = f"__{operator}__"
+        args = (Quantity(TESTER, right_units),)
+
+    try:
+        test = apply_any(TESTER, left_units, method, *args)
+    except Exception as error:
+        raise UnitsConversionError(error)
+
+    if operator in get_args(SameUnitsOperator):
+        if isinstance(right, Quantity):
+            right = right.to(left_units).value  # type: ignore
+
+        if isinstance(right, DataArray):
+            right = to(right, left_units)
+
+    try:
+        result = getattr(opr, operator)(left, right)
+    except Exception as error:
+        raise UnitsConversionError(error)
+
+    if (units := units_of(test)) is None:
+        return unset(result)
+    else:
+        return set(result, units, overwrite=True)
+
+
+def mul(left: TDataArray, right: Any) -> TDataArray:
+    """Perform ``left * right`` considering units.
+
+    When called from an accessor, it does ``accessed * right``.
+
+    Args:
+        left: DataArray with units on the left side of the operator.
+        right: Any data on the right side of the operator.
+
+    Returns:
+        DataArray of the result of the operation.
+
+    Raises:
+        UnitsConversionError: Raised if units cannot be converted.
+        UnitsNotFoundError: Raised if units are not found.
+        UnitsNotValidError: Raised if units are not valid.
+
+    """
+    return take(left, "mul", right)
+
+
+def pow(left: TDataArray, right: Any) -> TDataArray:
+    """Perform ``left ** right`` considering units.
+
+    When called from an accessor, it does ``accessed ** right``.
+
+    Args:
+        left: DataArray with units on the left side of the operator.
+        right: Any data on the right side of the operator.
+
+    Returns:
+        DataArray of the result of the operation.
+
+    Raises:
+        UnitsConversionError: Raised if units cannot be converted.
+        UnitsNotFoundError: Raised if units are not found.
+        UnitsNotValidError: Raised if units are not valid.
+
+    """
+    return take(left, "pow", right)
+
+
+def matmul(left: TDataArray, right: Any) -> TDataArray:
+    """Perform ``left @ right`` considering units.
+
+    When called from an accessor, it does ``accessed @ right``.
+
+    Args:
+        left: DataArray with units on the left side of the operator.
+        right: Any data on the right side of the operator.
+
+    Returns:
+        DataArray of the result of the operation.
+
+    Raises:
+        UnitsConversionError: Raised if units cannot be converted.
+        UnitsNotFoundError: Raised if units are not found.
+        UnitsNotValidError: Raised if units are not valid.
+
+
+    """
+    return take(left, "matmul", right)
+
+
+def truediv(left: TDataArray, right: Any) -> TDataArray:
+    """Perform ``left / right`` considering units.
+
+    When called from an accessor, it does ``accessed / right``.
+
+    Args:
+        left: DataArray with units on the left side of the operator.
+        right: Any data on the right side of the operator.
+
+    Returns:
+        DataArray of the result of the operation.
+
+    Raises:
+        UnitsConversionError: Raised if units cannot be converted.
+        UnitsNotFoundError: Raised if units are not found.
+        UnitsNotValidError: Raised if units are not valid.
+
+    """
+    return take(left, "truediv", right)
+
+
+def add(left: TDataArray, right: Any) -> TDataArray:
+    """Perform ``left + right`` considering units.
+
+    When called from an accessor, it does ``accessed + right``.
+
+    Args:
+        left: DataArray with units on the left side of the operator.
+        right: Any data on the right side of the operator.
+
+    Returns:
+        DataArray of the result of the operation.
+
+    Raises:
+        UnitsConversionError: Raised if units cannot be converted.
+        UnitsNotFoundError: Raised if units are not found.
+        UnitsNotValidError: Raised if units are not valid.
+
+    """
+    return take(left, "add", right)
+
+
+def sub(left: TDataArray, right: Any) -> TDataArray:
+    """Perform ``left - right`` considering units.
+
+    When called from an accessor, it does ``accessed - right``.
+
+    Args:
+        left: DataArray with units on the left side of the operator.
+        right: Any data on the right side of the operator.
+
+    Returns:
+        DataArray of the result of the operation.
+
+    Raises:
+        UnitsConversionError: Raised if units cannot be converted.
+        UnitsNotFoundError: Raised if units are not found.
+        UnitsNotValidError: Raised if units are not valid.
+
+    """
+    return take(left, "sub", right)
+
+
+def floordiv(left: TDataArray, right: Any) -> TDataArray:
+    """Perform ``left // right`` considering units.
+
+    When called from an accessor, it does ``accessed // right``.
+
+    Args:
+        left: DataArray with units on the left side of the operator.
+        right: Any data on the right side of the operator.
+
+    Returns:
+        DataArray of the result of the operation.
+
+    Raises:
+        UnitsConversionError: Raised if units cannot be converted.
+        UnitsNotFoundError: Raised if units are not found.
+        UnitsNotValidError: Raised if units are not valid.
+
+    """
+    return take(left, "floordiv", right)
+
+
+def mod(left: TDataArray, right: Any) -> TDataArray:
+    """Perform ``left % right`` considering units.
+
+    When called from an accessor, it does ``accessed % right``.
+
+    Args:
+        left: DataArray with units on the left side of the operator.
+        right: Any data on the right side of the operator.
+
+    Returns:
+        DataArray of the result of the operation.
+
+    Raises:
+        UnitsConversionError: Raised if units cannot be converted.
+        UnitsNotFoundError: Raised if units are not found.
+        UnitsNotValidError: Raised if units are not valid.
+
+    """
+    return take(left, "mod", right)
+
+
+def lt(left: TDataArray, right: Any) -> TDataArray:
+    """Perform ``left < right`` considering units.
+
+    When called from an accessor, it does ``accessed < right``.
+
+    Args:
+        left: DataArray with units on the left side of the operator.
+        right: Any data on the right side of the operator.
+
+    Returns:
+        DataArray of the result of the operation.
+
+    Raises:
+        UnitsConversionError: Raised if units cannot be converted.
+        UnitsNotFoundError: Raised if units are not found.
+        UnitsNotValidError: Raised if units are not valid.
+
+    """
+    return take(left, "lt", right)
+
+
+def le(left: TDataArray, right: Any) -> TDataArray:
+    """Perform ``left <= right`` considering units.
+
+    When called from an accessor, it does ``accessed <= right``.
+
+    Args:
+        left: DataArray with units on the left side of the operator.
+        right: Any data on the right side of the operator.
+
+    Returns:
+        DataArray of the result of the operation.
+
+    Raises:
+        UnitsConversionError: Raised if units cannot be converted.
+        UnitsNotFoundError: Raised if units are not found.
+        UnitsNotValidError: Raised if units are not valid.
+
+    """
+    return take(left, "le", right)
+
+
+def eq(left: TDataArray, right: Any) -> TDataArray:
+    """Perform ``left == right`` considering units.
+
+    When called from an accessor, it does ``accessed == right``.
+
+    Args:
+        left: DataArray with units on the left side of the operator.
+        right: Any data on the right side of the operator.
+
+    Returns:
+        DataArray of the result of the operation.
+
+    Raises:
+        UnitsConversionError: Raised if units cannot be converted.
+        UnitsNotFoundError: Raised if units are not found.
+        UnitsNotValidError: Raised if units are not valid.
+
+    """
+    return take(left, "eq", right)
+
+
+def ne(left: TDataArray, right: Any) -> TDataArray:
+    """Perform ``left != right`` considering units.
+
+    When called from an accessor, it does ``accessed != right``.
+
+    Args:
+        left: DataArray with units on the left side of the operator.
+        right: Any data on the right side of the operator.
+
+    Returns:
+        DataArray of the result of the operation.
+
+    Raises:
+        UnitsConversionError: Raised if units cannot be converted.
+        UnitsNotFoundError: Raised if units are not found.
+        UnitsNotValidError: Raised if units are not valid.
+
+    """
+    return take(left, "ne", right)
+
+
+def ge(left: TDataArray, right: Any) -> TDataArray:
+    """Perform ``left >= right`` considering units.
+
+    When called from an accessor, it does ``accessed >= right``.
+
+    Args:
+        left: DataArray with units on the left side of the operator.
+        right: Any data on the right side of the operator.
+
+    Returns:
+        DataArray of the result of the operation.
+
+    Raises:
+        UnitsConversionError: Raised if units cannot be converted.
+        UnitsNotFoundError: Raised if units are not found.
+        UnitsNotValidError: Raised if units are not valid.
+
+    """
+    return take(left, "ge", right)
+
+
+def gt(left: TDataArray, right: Any) -> TDataArray:
+    """Perform ``left > right`` considering units.
+
+    When called from an accessor, it does ``accessed > right``.
+
+    Args:
+        left: DataArray with units on the left side of the operator.
+        right: Any data on the right side of the operator.
+
+    Returns:
+        DataArray of the result of the operation.
+
+    Raises:
+        UnitsConversionError: Raised if units cannot be converted.
+        UnitsNotFoundError: Raised if units are not found.
+        UnitsNotValidError: Raised if units are not valid.
+
+    """
+    return take(left, "gt", right)
---
+++ packages/xarray-units/xarray_units-0.4.0.tar.gz/xarray_units-0.4.0/xarray_units/quantity.py
@@ -0,0 +1,243 @@
+__all__ = [
+    "apply",
+    "decompose",
+    "format",
+    "like",
+    "set",
+    "to",
+    "unset",
+]
+
+
+# standard library
+from types import MethodType, MethodWrapperType
+from typing import Any
+
+
+# dependencies
+from astropy.units import Quantity
+from xarray import DataArray
+from .utils import (
+    TESTER,
+    UNITS,
+    Equivalencies,
+    TDataArray,
+    UnitsConversionError,
+    UnitsExistError,
+    UnitsLike,
+    units_of,
+)
+
+
+def apply(
+    da: TDataArray,
+    method: str,
+    /,
+    *args: Any,
+    **kwargs: Any,
+) -> TDataArray:
+    """Apply a method of Astropy Quantity to a DataArray.
+
+    Args:
+        da: Input DataArray with units.
+        method: Method (or property) name of Astropy Quantity.
+        *args: Positional arguments of the method.
+        *kwargs: Keyword arguments of the method.
+
+    Returns:
+        DataArray with the method (or property) applied.
+
+    Raises:
+        UnitsConversionError: Raised if units cannot be converted.
+        UnitsNotFoundError: Raised if units are not found.
+        UnitsNotValidError: Raised if units are not valid.
+
+    See Also:
+        https://docs.astropy.org/en/stable/units/quantity.html
+
+    """
+    units = units_of(da, strict=True)
+
+    def per_block(block: TDataArray) -> TDataArray:
+        data = apply_any(block, units, method, *args, **kwargs)
+        return block.copy(data=data)
+
+    try:
+        test = apply_any(TESTER, units, method, *args, **kwargs)
+    except Exception as error:
+        raise UnitsConversionError(error)
+
+    try:
+        result = da.map_blocks(per_block)
+    except Exception as error:
+        raise UnitsConversionError(error)
+
+    return set(result, units_of(test, strict=True), overwrite=True)
+
+
+def apply_any(
+    data: Any,
+    units: UnitsLike,
+    method: str,
+    /,
+    *args: Any,
+    **kwargs: Any,
+) -> Any:
+    """Apply a method of Astropy Quantity to any data."""
+    attr = getattr(Quantity(data, units), method)
+
+    if isinstance(attr, (MethodType, MethodWrapperType)):
+        return attr(*args, **kwargs)
+    else:
+        return attr
+
+
+def decompose(da: TDataArray, /) -> TDataArray:
+    """Convert a DataArray with units to decomposed ones.
+
+    Args:
+        da: Input DataArray with units.
+
+    Returns:
+        DataArray with the decomposed units.
+
+    Raises:
+        UnitsConversionError: Raised if units cannot be converted.
+        UnitsNotFoundError: Raised if units are not found.
+        UnitsNotValidError: Raised if units are not valid.
+
+    See Also:
+        https://docs.astropy.org/en/stable/units/decomposing_and_composing.html
+
+    """
+    return apply(da, "decompose")
+
+
+def format(
+    da: TDataArray,
+    format: str,
+    /,
+    **kwargs: Any,
+) -> TDataArray:
+    """Format units of a DataArray.
+
+    Args:
+        da: Input DataArray with units.
+        format: Format of units (e.g. ``"console"``, ``"latex"``).
+        **kwargs: Keyword arguments of the formatting.
+
+    Returns:
+        DataArray with formatted units.
+
+    Raises:
+        UnitsConversionError: Raised if units cannot be converted.
+        UnitsNotFoundError: Raised if units are not found.
+        UnitsNotValidError: Raised if units are not valid.
+
+    See Also:
+        https://docs.astropy.org/en/stable/units/format.html
+
+    """
+    units = units_of(da, format=format, strict=True, **kwargs)
+    return set(da, units, overwrite=True)
+
+
+def like(
+    da: TDataArray,
+    other: DataArray,
+    /,
+    equivalencies: Equivalencies = None,
+) -> TDataArray:
+    """Convert a DataArray with units to those of the other.
+
+    Args:
+        da: Input DataArray with units.
+        other: DataArray with units to which the input is converted.
+        equivalencies: Optional Astropy equivalencies.
+
+    Returns:
+        DataArray with the converted units.
+
+    Raises:
+        UnitsConversionError: Raised if units cannot be converted.
+        UnitsNotFoundError: Raised if units are not found.
+        UnitsNotValidError: Raised if units are not valid.
+
+    See Also:
+        https://docs.astropy.org/en/stable/units/quantity.html
+
+    """
+    units = units_of(other, strict=True)
+    return apply(da, "to", units, equivalencies)
+
+
+def set(
+    da: TDataArray,
+    units: UnitsLike,
+    /,
+    *,
+    overwrite: bool = False,
+) -> TDataArray:
+    """Set units to a DataArray.
+
+    Args:
+        da: Input DataArray.
+        units: Units to be set to the input.
+        overwrite: Whether to overwrite existing units.
+
+    Returns:
+        DataArray with given units in ``attrs["units"]``.
+
+    Raises:
+        UnitsExistError: Raised if units already exist.
+            Not raised when ``overwrite`` is ``True``.
+        UnitsNotValidError: Raised if units are not valid.
+
+    """
+    if not overwrite and units_of(da) is not None:
+        raise UnitsExistError(repr(da))
+
+    return da.assign_attrs({UNITS: units})
+
+
+def to(
+    da: TDataArray,
+    units: UnitsLike,
+    /,
+    equivalencies: Equivalencies = None,
+) -> TDataArray:
+    """Convert a DataArray with units to other units.
+
+    Args:
+        da: Input DataArray with units.
+        units: Units to which the input is converted.
+        equivalencies: Optional Astropy equivalencies.
+
+    Returns:
+        DataArray with the converted units.
+
+    Raises:
+        UnitsConversionError: Raised if units cannot be converted.
+        UnitsNotFoundError: Raised if units are not found.
+        UnitsNotValidError: Raised if units are not valid.
+
+    See Also:
+        https://docs.astropy.org/en/stable/units/quantity.html
+
+    """
+    return apply(da, "to", units, equivalencies)
+
+
+def unset(da: TDataArray, /) -> TDataArray:
+    """Remove units from a DataArray.
+
+    Args:
+        da: Input DataArray.
+
+    Returns:
+        DataArray with units removed.
+
+    """
+    da = da.copy(data=da.data)
+    da.attrs.pop(UNITS, None)
+    return da
