3ab3d4b7e141e63069a4e582c5c89749457e73c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 12:04:22 +0000

    Add package portkey-ai/1.1.6/portkey_ai-1.1.6-py3-none-any.whl

---
+++ packages/portkey-ai/portkey_ai-1.1.6-py3-none-any.whl/portkey_ai/api_resources/apis/embeddings.py
@@ -0,0 +1,38 @@
+from typing import Optional
+from portkey_ai.api_resources.apis.api_resource import APIResource
+from portkey_ai.api_resources.base_client import APIClient
+from portkey_ai.api_resources.utils import PortkeyApiPaths, GenericResponse
+
+
+class Embeddings(APIResource):
+    def __init__(self, client: APIClient) -> None:
+        super().__init__(client)
+
+    def create(
+        self,
+        *,
+        input: str,
+        model: Optional[str] = None,
+        dimensions: Optional[int] = None,
+        encoding_format: Optional[str] = None,
+        user: Optional[str] = None,
+        **kwargs
+    ) -> GenericResponse:
+        body = dict(
+            input=input,
+            model=model,
+            dimensions=dimensions,
+            encoding_format=encoding_format,
+            user=user,
+            **kwargs,
+        )
+
+        return self._post(
+            PortkeyApiPaths.EMBEDDING_API,
+            body=body,
+            params=None,
+            cast_to=GenericResponse,
+            stream_cls=None,
+            stream=False,
+            headers={},
+        )
