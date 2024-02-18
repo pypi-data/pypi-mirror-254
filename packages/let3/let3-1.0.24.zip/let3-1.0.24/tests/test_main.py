c731db0dd1f0cc1eba4c66b221fc9ad8eb7d0d46
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 14:06:28 +0000

    Add package let3/1.0.24/let3-1.0.24.zip

---
+++ packages/let3/let3-1.0.24.zip/let3-1.0.24/tests/test_main.py
@@ -0,0 +1,17 @@
+# Copyright (c) 2016 Adam Karpierz
+# Licensed under the MIT License
+# https://opensource.org/license/mit
+
+import unittest
+
+import let
+from let import let
+
+
+class MainTestCase(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def test_main(self):
+        pass
