d4f6ffb623d406508f26065b8249f6c8bd2a906f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 19:04:09 +0000

    Add package rbl-distributions/1.1/rbl_distributions-1.1.tar.gz

---
+++ packages/rbl-distributions/rbl_distributions-1.1.tar.gz/rbl_distributions-1.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='rbl_distributions',
+      version='1.1',
+      description='Gaussian and Binomial distributions',
+      packages=['rbl_distributions'],
+      author='Ochieng Billy',
+      zip_safe=False)
