0a2481ca290e37c34a59cece7c5962210b189a02
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 11:04:21 +0000

    Add package passninja/1.2.0/passninja-1.2.0-py3-none-any.whl

---
+++ packages/passninja/passninja-1.2.0-py3-none-any.whl/passninja/__init__.py
@@ -0,0 +1,7 @@
+from __future__ import absolute_import
+
+__version__ = '1.2.0'
+
+from .passninja import (
+    PassNinjaClient, SimplePassObject, PassNinjaException, PassNinjaInvalidArgumentsException
+)
