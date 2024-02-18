36145b70ada1610715939f738bd94cbcb2d19b12
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 06:04:46 +0000

    Add package mtf/1.0.0/mtf-1.0.0.tar.gz

---
+++ packages/mtf/mtf-1.0.0.tar.gz/mtf-1.0.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(name='mtf',
+      version='1.0.0',
+      description='none',
+      packages=['mtf'],
+      author_email='mhivanov4@gmail.com',
+      install_requires=['pyperclip','scikit-learn','numpy','IPython'],
+      zip_safe=False)
