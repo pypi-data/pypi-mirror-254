e4c5f0ea08332dd88b491909783fc37f7e021746
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 22:07:19 +0000

    Add package NumberAnggi/1.2/NumberAnggi-1.2.tar.gz

---
+++ packages/NumberAnggi/NumberAnggi-1.2.tar.gz/NumberAnggi-1.2/NumberAnggi/main.py
@@ -0,0 +1,8 @@
+from .service.kurangdarilima import KurangDariLima
+
+class Number:
+
+    def isKurangLima(self, angka) -> bool:
+        #jjjjj
+        return KurangDariLima.isKuranglima(self, angka)
+    
---
+++ packages/NumberAnggi/NumberAnggi-1.2.tar.gz/NumberAnggi-1.2/setup.py
@@ -0,0 +1,3 @@
+from setuptools import setup
+
+setup(name="NumberAnggi", version="1.2", description="Deskripsi angka", packages=['NumberAnggi'], zip_safe=False)
