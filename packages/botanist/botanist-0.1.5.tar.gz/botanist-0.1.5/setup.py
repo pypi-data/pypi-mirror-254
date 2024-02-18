e4c87a56f3341f7b9c8bdc7df5a615331ed72513
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 03:03:34 +0000

    Add package botanist/0.1.5/botanist-0.1.5.tar.gz

---
+++ packages/botanist/botanist-0.1.5.tar.gz/botanist-0.1.5/setup.py
@@ -0,0 +1,34 @@
+
+#
+#	https://setuptools.pypa.io/en/latest/userguide/quickstart.html
+#
+
+from setuptools import setup, find_packages
+
+name = 'botanist'
+
+description = ''
+try:
+	with open ('botanist.s.HTML') as f:
+		description = f.read ()
+
+except Exception as E:
+	pass;
+
+setup (
+    name = name,
+    version = '0.1.5',
+    install_requires = [
+		'psutil'
+	],	
+	package_dir = { name: 'fields/gardens/' + name },
+	
+	license = "GPL 3.0",
+	long_description = description,
+	long_description_content_type = "text/plain"
+	
+	
+	#package_data = {
+	#	NAME: [ 'DATA/**/*' ]
+	#}
+)
