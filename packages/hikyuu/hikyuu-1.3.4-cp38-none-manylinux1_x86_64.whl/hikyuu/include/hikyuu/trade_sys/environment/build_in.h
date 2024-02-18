6295143d019d0a446808e1d892268791eeb930b5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 05:03:57 +0000

    Add package hikyuu/1.3.4/hikyuu-1.3.4-cp38-none-manylinux1_x86_64.whl

---
+++ packages/hikyuu/hikyuu-1.3.4-cp38-none-manylinux1_x86_64.whl/hikyuu/include/hikyuu/trade_sys/environment/build_in.h
@@ -0,0 +1,14 @@
+/*
+ * build_in.h
+ *
+ *  Created on: 2013-5-5
+ *      Author: fasiondog
+ */
+
+#pragma once
+#ifndef ENVIRONMENT_BUILD_IN_H_
+#define ENVIRONMENT_BUILD_IN_H_
+
+#include "crt/EV_TwoLine.h"
+
+#endif /* ENVIRONMENT_BUILD_IN_H */
