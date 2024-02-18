4a8247a88552ff9d405f4f8f5e5a75e633dd0a2f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 11:04:51 +0000

    Add package arfx/2.6.15/arfx-2.6.15.tar.gz

---
+++ packages/arfx/arfx-2.6.15.tar.gz/arfx-2.6.15/test/test_splitter.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+# -*- mode: python -*-
+import pytest
+
+
+def test_combine_jill_logs():
+    """Test whether two jill_log datasets can be combined and written to new file"""
+    pass
