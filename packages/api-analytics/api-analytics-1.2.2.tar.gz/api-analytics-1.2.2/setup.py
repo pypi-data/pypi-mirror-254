3abc8dfd2289002b4b13f08f66c99a2a1c7f8425
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 00:05:57 +0000

    Add package api-analytics/1.2.2/api-analytics-1.2.2.tar.gz

---
+++ packages/api-analytics/api-analytics-1.2.2.tar.gz/api-analytics-1.2.2/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+long_description = open("README.md").read()
+
+setup(
+    name="api-analytics",
+    version="1.2.2",
+    author="Tom Draper",
+    author_email="tomjdraper1@gmail.com",
+    license="MIT",
+    description="Monitoring and analytics for Python API frameworks.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/tom-draper/api-analytics",
+    key_words="analytics api dashboard django fastapi flask tornado middleware",
+    install_requires=["Django", "fastapi", "Flask", "tornado", "requests"],
+    packages=["api_analytics"],
+    python_requires=">=3.6",
+)
