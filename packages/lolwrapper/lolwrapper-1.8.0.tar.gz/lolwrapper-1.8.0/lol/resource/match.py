eeeb534d128ed41cc4e78083930c6d198393454a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 22:06:08 +0000

    Add package lolwrapper/1.8.0/lolwrapper-1.8.0.tar.gz

---
+++ packages/lolwrapper/lolwrapper-1.8.0.tar.gz/lolwrapper-1.8.0/lol/resource/match.py
@@ -0,0 +1,95 @@
+import requests
+from datetime import datetime
+from typing import Optional
+
+
+class OverCountLimitError(Exception):
+    pass
+
+
+class InvalidQueueType(Exception):
+    pass
+
+
+class Match:
+    """
+    MATCH-V5
+    https://developer.riotgames.com/api-methods/#match-v5
+    """
+
+    def __init__(self, base):
+        self.base = base
+        self.version = 'v5'
+
+    def get(self, match_id, region=None):
+        """Get a match by ID.
+
+        Parameters
+        ----------
+        match_id : ID
+
+        Returns
+        -------
+        JSON
+            match data
+
+        """
+        base_url = self.base.get_base_url(region, use_v5_region=True)
+        url = f'{base_url}/lol/match/{self.version}/matches/{match_id}'
+        r = requests.get(url, headers=self.base.headers)
+        return r
+
+    def filter(
+        self,
+        puuid: str,
+        region: Optional[str] = None,
+        startTime: Optional[datetime] = None,
+        endTime: Optional[datetime] = None,
+        queue: Optional[int] = None,
+        queueType: Optional[str] = None,
+        start: Optional[int] = None,
+        count: Optional[int] = None,
+    ):
+        """Get list of matches by account_id."""
+        base_url = self.base.get_base_url(region, use_v5_region=True)
+        params = {}
+        if startTime:
+            value = int(startTime.timestamp() * 1000)
+            params['startTime'] = value
+        if endTime:
+            value = int(endTime.timestamp() * 1000)
+            params['endTime'] = value
+        if queue:
+            params['queue'] = queue
+        if queueType:
+            if queueType not in ['ranked', 'normal', 'tourney', 'tutorial']:
+                raise InvalidQueueType
+            params['type'] = queueType
+        if start:
+            params['start'] = start
+        if count:
+            if count > 100:
+                raise OverCountLimitError('Count must be <= 100.')
+            params['count'] = count
+        url = f'{base_url}/lol/match/{self.version}/matches/by-puuid/{puuid}/ids'
+        r = requests.get(url, params=params, headers=self.base.headers)
+        return r
+
+    def timeline(self, match_id, region=None):
+        """Get timeline data for a match.
+
+        Parameters
+        ----------
+        match_id : ID
+        region : str
+
+        Returns
+        -------
+        JSON
+            timeline data
+
+        """
+        base_url = self.base.get_base_url(region, use_v5_region=True)
+        url = f'{base_url}/lol/match/{self.version}/matches/{match_id}/timeline'
+        r = requests.get(url, headers=self.base.headers)
+        return r
---
+++ packages/lolwrapper/lolwrapper-1.8.0.tar.gz/lolwrapper-1.8.0/lol/riot.py
@@ -0,0 +1,110 @@
+import sys
+import os
+
+PACKAGE_PARENT = '..'
+SCRIPT_DIR = os.path.dirname(
+    os.path.realpath(
+        os.path.join(
+            os.getcwd(),
+            os.path.expanduser(__file__)
+        )
+    )
+)
+package_path = os.path.normpath(os.path.join(SCRIPT_DIR, PACKAGE_PARENT))
+sys.path.append(package_path)
+
+from lol import resource
+
+
+class RiotBase:
+
+    base_url = {
+        'br': 'https://br1.api.riotgames.com',
+        'br1': 'https://br1.api.riotgames.com',
+
+        'eune': 'https://eun1.api.riotgames.com',
+        'eun': 'https://eun1.api.riotgames.com',
+        'eun1': 'https://eun1.api.riotgames.com',
+
+        'euw': 'https://euw1.api.riotgames.com',
+        'euw1': 'https://euw1.api.riotgames.com',
+
+        'jp': 'https://jp1.api.riotgames.com',
+        'jp1': 'https://jp1.api.riotgames.com',
+
+        'kr': 'https://kr.api.riotgames.com',
+        'kr1': 'https://kr.api.riotgames.com',
+
+        'lan': 'https://la1.api.riotgames.com',
+        'la1': 'https://la1.api.riotgames.com',
+
+        'las': 'https://la2.api.riotgames.com',
+        'la2': 'https://la2.api.riotgames.com',
+
+        'na': 'https://na1.api.riotgames.com',
+        'na1': 'https://na1.api.riotgames.com',
+
+        'oce': 'https://oc1.api.riotgames.com',
+        'oc1': 'https://oc1.api.riotgames.com',
+
+        'tr': 'https://tr1.api.riotgames.com',
+        'tr1': 'https://tr1.api.riotgames.com',
+
+        'ru': 'https://ru.api.riotgames.com',
+
+        'pbe': 'https://pbe1.api.riotgames.com',
+        'pbe1': 'https://pbe1.api.riotgames.com',
+
+        'americas': 'https://americas.api.riotgames.com',
+        'europe': 'https://europe.api.riotgames.com',
+        'asia': 'https://asia.api.riotgames.com',
+        'sea': 'https://sea.api.riotgames.com',
+    }
+
+    routes = {
+        'americas': {'na', 'na1', 'br', 'br1', 'lan', 'la1', 'las', 'la2'},
+        'europe': {'eune', 'eun', 'eun1', 'euw', 'euw1', 'tr', 'tr1', 'ru'},
+        'asia': {'kr', 'kr1', 'jp', 'jp1'},
+        'sea': {'oce', 'ph2', 'sg2', 'th2', 'tw2', 'vn2'},
+    }
+
+    def __init__(self, key):
+        self.key = key
+        self.headers = self.get_headers()
+
+    def get_headers(self):
+        headers = {
+            "Accept-Charset": "application/x-www-form-urlencoded; charset=UTF-8",
+            "X-Riot-Token": self.key,
+            "Accept-Language": "en-US,en;q=0.8",
+        }
+        return headers
+
+    def get_base_url(self, region: str, use_v5_region=False):
+        if use_v5_region:
+            for new_region, val in self.routes.items():
+                if region in val:
+                    return self.base_url[new_region]
+        return self.base_url[region]
+
+
+class Riot:
+    def __init__(self, key):
+        self.key = key
+        self.base = RiotBase(key)
+
+        self.account = resource.Account(self.base)
+        self.champion = resource.Champion(self.base)
+        self.championmastery = resource.ChampionMastery(self.base)
+        self.league = resource.League(self.base)
+        self.lolstaticdata = resource.LolStaticData(self.base)
+        self.lolstatus = resource.LolStatus(self.base)
+        self.masteries = resource.Masteries(self.base)
+        self.match = resource.Match(self.base)
+        self.spectator = resource.Spectator(self.base)
+        self.summoner = resource.Summoner(self.base)
+        self.thirdpartycode = resource.ThirdPartyCode(self.base)
+        self.tftleague = resource.TftLeague(self.base)
+        self.tftmatch = resource.TftMatch(self.base)
+        self.tftsummoner = resource.TftSummoner(self.base)
+        self.pro = resource.Pro(self.base)
