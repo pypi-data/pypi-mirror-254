7246e117ff629c6b46f8cce4d3ecd3d93c883511
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 13:04:04 +0000

    Add package fyodorov-utils/0.2.9/fyodorov_utils-0.2.9-py3-none-any.whl

---
+++ packages/fyodorov-utils/fyodorov_utils-0.2.9-py3-none-any.whl/fyodorov_utils/config/config.py
@@ -0,0 +1,14 @@
+from pydantic_settings import BaseSettings
+import os
+
+print(f"Loading settings from .env")
+print(f"Supabase project URL: {os.getenv('SUPABASE_PROJECT_URL')}")
+
+class Settings(BaseSettings):
+    SUPABASE_URL: str = os.getenv("SUPABASE_PROJECT_URL")
+    SUPABASE_KEY: str = os.getenv("SUPABASE_API_KEY")
+    JWT_SECRET: str = os.getenv("JWT_SECRET")
+    
+    class Config:
+        env_file = '.env'
+        env_file_encoding = 'utf-8'
