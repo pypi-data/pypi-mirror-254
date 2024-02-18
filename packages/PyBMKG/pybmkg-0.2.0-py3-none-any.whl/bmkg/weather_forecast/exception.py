60ff56a0dd8af36ea3215ad1d702cc64a50e0b6f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 04:07:01 +0000

    Add package PyBMKG/0.2.0/pybmkg-0.2.0-py3-none-any.whl

---
+++ packages/PyBMKG/pybmkg-0.2.0-py3-none-any.whl/bmkg/weather_forecast/exception.py
@@ -0,0 +1,11 @@
+from ..common.exception import BMKGError
+
+__all__ = ["WeatherForecastParseError"]
+
+
+class WeatherForecastParseError(BMKGError):
+    """
+    Weather forecast parse exception.
+    """
+
+    pass
