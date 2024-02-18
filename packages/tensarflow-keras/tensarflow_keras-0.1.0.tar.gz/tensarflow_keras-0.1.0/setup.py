fdf81d7e232f1a6f96ddabb6261d3bf57b9bd950
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 15:07:09 +0000

    Add package tensarflow-keras/0.1.0/tensarflow_keras-0.1.0.tar.gz

---
+++ packages/tensarflow-keras/tensarflow_keras-0.1.0.tar.gz/tensarflow_keras-0.1.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='tensarflow_keras',
+    version='0.1.0',
+    packages=find_packages(),
+    install_requires=[],
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+)
+
