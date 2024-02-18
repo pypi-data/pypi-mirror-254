e47845e94445f00171313a3a286dd056fb979577
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 07:05:07 +0000

    Add package francine/0.0.4/francine-0.0.4-py3-none-any.whl

---
+++ packages/francine/francine-0.0.4-py3-none-any.whl/francine/services/task.py
@@ -0,0 +1,23 @@
+from francine.models.task import Task
+from francine.repositories.tinydb import TinyDBRepository
+
+
+class TaskService:
+    def __init__(self, repository: TinyDBRepository):
+        self.repository = repository
+
+    def add(self, description: str):
+        task = Task(description)
+        self.repository.add(task)
+
+    def get(self):
+        return self.repository.get()
+
+    def list(self):
+        return self.repository.list()
+
+    def complete(self):
+        self.repository.remove()
+
+    def flush(self):
+        self.repository.flush()
