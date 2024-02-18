d35b1304078cd949a4284fbdfa63a28fefa5350f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 12:05:30 +0000

    Add package mpsiemlib-axelpro/1.5.3.0/mpsiemlib_axelpro-1.5.3.0-py3-none-any.whl

---
+++ packages/mpsiemlib-axelpro/mpsiemlib_axelpro-1.5.3.0-py3-none-any.whl/mpsiemlib/__init__.py
@@ -0,0 +1,9 @@
+from mpsiemlib.modules import MPSIEMWorker
+
+__title__ = 'mpsiemlib_axelpro'
+__description__ = 'MaxPatrol SIEM API SDK'
+__url__ = 'https://github.com/axel-pro/mpsiemlib'
+__version__ = '1.5.3.0'
+__author__ = 'nikolaiav'
+__license__ = 'GPLv3'
+__copyright__ = '2023, Nikolai Arefiev'
