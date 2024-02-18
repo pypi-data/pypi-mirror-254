c7ab41bc37ba4b022db2900a2261967b70521b24
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 07:05:34 +0000

    Add package econnect-python/0.10.0/econnect_python-0.10.0.tar.gz

---
+++ packages/econnect-python/econnect_python-0.10.0.tar.gz/econnect_python-0.10.0/tests/conftest.py
@@ -0,0 +1,40 @@
+import pytest
+import responses
+from fixtures import responses as r
+
+from elmo.api.client import ElmoClient
+
+
+@pytest.fixture(scope="function")
+def client():
+    """Creates an instance of `ElmoClient` which emulates the behavior of a real client for
+    testing purposes.
+
+    Although this client instance operates with mocked calls, it is designed to function as
+    if it were genuine. This ensures that the client's usage in tests accurately mirrors how it
+    would be employed in real scenarios.
+
+    Use it for integration tests where a realistic interaction with the `ElmoClient` is required
+    without actual external calls.
+    """
+    client = ElmoClient(base_url="https://example.com", domain="domain")
+    with responses.RequestsMock(assert_all_requests_are_fired=False) as server:
+        server.add(responses.GET, "https://example.com/api/login", body=r.LOGIN, status=200)
+        server.add(responses.POST, "https://example.com/api/updates", body=r.UPDATES, status=200)
+        server.add(responses.POST, "https://example.com/api/panel/syncLogin", body=r.SYNC_LOGIN, status=200)
+        server.add(responses.POST, "https://example.com/api/panel/syncLogout", body=r.SYNC_LOGOUT, status=200)
+        server.add(
+            responses.POST, "https://example.com/api/panel/syncSendCommand", body=r.SYNC_SEND_COMMAND, status=200
+        )
+        server.add(responses.POST, "https://example.com/api/strings", body=r.STRINGS, status=200)
+        server.add(responses.POST, "https://example.com/api/areas", body=r.AREAS, status=200)
+        server.add(responses.POST, "https://example.com/api/inputs", body=r.INPUTS, status=200)
+        server.add(responses.POST, "https://example.com/api/outputs", body=r.OUTPUTS, status=200)
+        yield client
+
+
+@pytest.fixture
+def server():
+    """Create a `responses` mock."""
+    with responses.RequestsMock() as resp:
+        yield resp
