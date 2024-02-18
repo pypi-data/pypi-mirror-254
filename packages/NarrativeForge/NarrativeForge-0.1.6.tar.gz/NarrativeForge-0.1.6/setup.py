189de0714d993a701a5339369815f4f648ee7d41
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 12:04:20 +0000

    Add package NarrativeForge/0.1.6/NarrativeForge-0.1.6.tar.gz

---
+++ packages/NarrativeForge/NarrativeForge-0.1.6.tar.gz/NarrativeForge-0.1.6/setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup, find_packages
+
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
+setup(
+    name="NarrativeForge",
+    version="0.1.6",
+    description='A package for adding text and ai generated voice to videos.',
+    author='supun nadeera',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/supunnadeera/NarrativeForge',
+    packages=find_packages(),
+    install_requires=[
+        "numpy",
+        "moviepy",
+        "openai==1.10.0",
+        "click",
+    ],
+    entry_points={
+        "console_scripts": [
+            "NarrativeForge=NarrativeForge:main",
+        ],
+    },
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python :: 3.10',
+    ],
+    project_urls={ 
+        'Source': 'https://github.com/supunnadeera/NarrativeForge',
+    },
+)
