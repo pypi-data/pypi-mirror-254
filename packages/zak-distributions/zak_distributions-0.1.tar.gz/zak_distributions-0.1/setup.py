defd638cdd386c0d30d54223fc0e7633472ecee5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 23:05:52 +0000

    Add package zak-distributions/0.1/zak_distributions-0.1.tar.gz

---
+++ packages/zak-distributions/zak_distributions-0.1.tar.gz/zak_distributions-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='zak_distributions',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['zak_distributions'],
+      author='Abdul Razak Husin',
+      author_email = 'husinabdulrazak@gmail.com',
+      zip_safe=False)
