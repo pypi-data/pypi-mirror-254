783981df8bb0c1255395a0639b54526a8a6fb049
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 22:04:47 +0000

    Add package DeskripsiAngka/0.1/DeskripsiAngka-0.1.tar.gz

---
+++ packages/DeskripsiAngka/DeskripsiAngka-0.1.tar.gz/DeskripsiAngka-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name="DeskripsiAngka", 
+ version="0.1",
+ description="[ID] library untuk cek deskripsi dari suatu angka", 
+ packages=['DeskripsiAngka'],
+ zip_safe=False)
