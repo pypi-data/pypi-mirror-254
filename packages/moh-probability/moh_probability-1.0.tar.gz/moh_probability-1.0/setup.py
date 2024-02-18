a070f5112f638a43578cc7f785c55e3c213568e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 07:04:27 +0000

    Add package moh-probability/1.0/moh_probability-1.0.tar.gz

---
+++ packages/moh-probability/moh_probability-1.0.tar.gz/moh_probability-1.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='moh_probability',
+      version='1.0',
+      description='Gaussian and Binomial distributions',
+      packages=['moh_probability'],
+      author = 'Mohamed Mahmoud',
+      author_email = 'moh2661978@gmail.com',
+      zip_safe=False)
