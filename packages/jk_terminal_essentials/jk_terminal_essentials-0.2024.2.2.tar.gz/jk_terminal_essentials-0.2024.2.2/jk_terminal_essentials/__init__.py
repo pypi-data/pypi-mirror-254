971bfefaa287a3aef6cb614f18d5c3cd8e7f43c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 13:04:54 +0000

    Add package jk_terminal_essentials/0.2024.2.2/jk_terminal_essentials-0.2024.2.2.tar.gz

---
+++ packages/jk_terminal_essentials/jk_terminal_essentials-0.2024.2.2.tar.gz/jk_terminal_essentials-0.2024.2.2/jk_terminal_essentials/__init__.py
@@ -0,0 +1,12 @@
+﻿
+
+
+__version__ = "0.2024.2.2"
+
+
+
+from .constants import *
+
+from .functions import checkTerminalSupportsColors, getTerminalSize, stripColors
+
+from .Spinner import Spinner
