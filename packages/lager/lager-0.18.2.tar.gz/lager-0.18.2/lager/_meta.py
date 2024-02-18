5c167450288215c5b17bbf9f11165b084b831a1a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 09:05:46 +0000

    Add package lager/0.18.2/lager-0.18.2.tar.gz

---
+++ packages/lager/lager-0.18.2.tar.gz/lager-0.18.2/lager/_meta.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""Package metadata/info"""
+from __future__ import annotations
+
+import warnings
+
+from lager.__about__ import __description__, __pkgroot__, __title__, __version__
+
+warnings.warn(
+    "lager._meta is deprecated, use lager.__about__ instead",
+    DeprecationWarning,
+    stacklevel=2,
+)
+
+__all__ = ("__title__", "__description__", "__pkgroot__", "__version__")
