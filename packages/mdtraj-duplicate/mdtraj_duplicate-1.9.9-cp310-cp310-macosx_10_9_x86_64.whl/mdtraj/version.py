2ce38bc12cf125c334fb848cb2d37dc6c1b661bd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 17:05:22 +0000

    Add package mdtraj-duplicate/1.9.9/mdtraj_duplicate-1.9.9-cp310-cp310-macosx_10_9_x86_64.whl

---
+++ packages/mdtraj-duplicate/mdtraj_duplicate-1.9.9-cp310-cp310-macosx_10_9_x86_64.whl/mdtraj/version.py
@@ -0,0 +1,7 @@
+
+# This file is generated in setup.py at build time.
+version = '1.9.9'
+short_version = '1.9.9'
+full_version = '1.9.9+2d35d88d772aaa982415dfbf98c98b55ebc145ba'
+git_revision = '2d35d88d772aaa982415dfbf98c98b55ebc145ba'
+release = False
