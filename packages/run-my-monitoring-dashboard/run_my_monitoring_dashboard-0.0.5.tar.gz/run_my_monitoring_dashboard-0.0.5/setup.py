88374c08dc9cdcec92ec989be9b36ad6175052b5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 08:05:52 +0000

    Add package run-my-monitoring-dashboard/0.0.5/run_my_monitoring_dashboard-0.0.5.tar.gz

---
+++ packages/run-my-monitoring-dashboard/run_my_monitoring_dashboard-0.0.5.tar.gz/run_my_monitoring_dashboard-0.0.5/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='run_my_monitoring_dashboard',
+    version='0.0.5',
+    description='A very basic monitoring package',
+    author='Ritik Saxena',
+    author_email='ritiksaxena90@gmail.com',
+    packages=find_packages(),
+    install_requires=['pandas', 'streamlit', 'datetime', 'numpy', 'streamlit_option_menu'],
+    entry_points={
+        'console_scripts': [
+            'run_my_monitoring_dashboard = run_my_monitoring_dashboard.main:main',
+        ],
+    },
+)
