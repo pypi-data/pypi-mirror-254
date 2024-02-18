1d15f232da9877860ccfb661f3695d1670d3235d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 14:04:57 +0000

    Add package bosonic-qiskit/11.3/bosonic-qiskit-11.3.tar.gz

---
+++ packages/bosonic-qiskit/bosonic-qiskit-11.3.tar.gz/bosonic-qiskit-11.3/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name="bosonic-qiskit",
+    version_config=True,
+    setup_requires=["setuptools-git-versioning"],
+    url="https://github.com/C2QA/bosonic-qiskit",
+    author="Tim Stavenger",
+    author_email="timothy.stavenger@pnnl.gov",
+    description="National Quantum Initiative Co-design Center for Quantum Advantage bosonic Qiskit simulator",
+    long_description="Qiskit extension supporting simulation of bosonic qumode Fock states reprsented as qubits within Qiskit",
+    python_requires=">=3.7",
+    packages=find_packages(),
+    install_requires=["qiskit==0.39", "matplotlib==3.5.2", "pylatexenc==2.10"],
+)
