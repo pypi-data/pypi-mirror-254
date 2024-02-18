d5b766e1964a972b77f650f38b729f050020f3c1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 15:05:57 +0000

    Add package pybrops/1.0.1/pybrops-1.0.1.tar.gz

---
+++ packages/pybrops/pybrops-1.0.1.tar.gz/pybrops-1.0.1/pybrops/test/__init__.py
@@ -0,0 +1,12 @@
+"""
+Module containing assertion tools for testing.
+"""
+
+__all__ = [
+    "assert_python",
+    "assert_numpy",
+]
+
+# order dependent import
+from pybrops.test import assert_python
+from pybrops.test import assert_numpy
