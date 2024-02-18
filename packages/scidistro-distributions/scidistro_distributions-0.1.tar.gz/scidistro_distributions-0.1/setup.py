1057d1a19d3eed668e6870a0daafd316bad785a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 04:07:11 +0000

    Add package scidistro-distributions/0.1/scidistro_distributions-0.1.tar.gz

---
+++ packages/scidistro-distributions/scidistro_distributions-0.1.tar.gz/scidistro_distributions-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='scidistro_distributions',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['scidistro_distributions'],
+      zip_safe=False)
