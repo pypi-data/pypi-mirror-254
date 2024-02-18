a412e279ad5a2d6282269af1de70e842c6f72996
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 23:06:01 +0000

    Add package formulite/0.0.4/formulite-0.0.4.tar.gz

---
+++ packages/formulite/formulite-0.0.4.tar.gz/formulite-0.0.4/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup,find_packages
+
+with open("README.md",encoding="utf-8",mode="r")as f:
+    long_description = f.read()
+
+setup(
+    name="formulite",
+    version="0.0.4",
+    description='Simple Formula Parser',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    author='Tom0427',
+    author_email='tom.ipynb@gmail.com',
+    license='MIT',
+    keywords='parser, formula, formulite',
+    python_requires='>=3.10',
+    packages=find_packages(),
+    url="https://github.com/Tom-game-project/formulite"
+)
