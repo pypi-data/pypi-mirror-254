0311278a257c1caa670d72940f6ac7166465fcfa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 00:06:09 +0000

    Add package technoxploit/0.1.0/technoxploit-0.1.0.tar.gz

---
+++ packages/technoxploit/technoxploit-0.1.0.tar.gz/technoxploit-0.1.0/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='technoxploit',
+    version='0.1.0',
+    packages=['technoxploit'],
+    install_requires=[
+        # List your dependencies here
+    ],
+)
