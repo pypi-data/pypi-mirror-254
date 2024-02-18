5dcad89db0db1cc2e119b761c9225e0ee6a6d882
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 13:04:49 +0000

    Add package amarillo/0.0.15a2/amarillo-0.0.15a2-py3-none-any.whl

---
+++ packages/amarillo/amarillo-0.0.15a2-py3-none-any.whl/amarillo/services/config.py
@@ -0,0 +1,12 @@
+from typing import List
+from pydantic_settings import BaseSettings
+
+
+class Config(BaseSettings):
+    admin_token: str
+    ride2go_query_data: str
+    env: str = 'DEV'
+    graphhopper_base_url: str = 'https://api.mfdz.de/gh'
+    stop_sources_file: str = 'conf/stop_sources.json'
+
+config = Config(_env_file='config', _env_file_encoding='utf-8')
