a5145348c1ffe0d108dbc5577db4a9b49ae1aeb6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 14:06:28 +0000

    Add package let3/1.0.24/let3-1.0.24-py3-none-any.whl

---
+++ packages/let3/let3-1.0.24-py3-none-any.whl/let/__init__.py
@@ -0,0 +1,7 @@
+# Copyright (c) 2016 Adam Karpierz
+# Licensed under the MIT License
+# https://opensource.org/license/mit
+
+from .__about__ import * ; del __about__  # noqa
+
+from ._let import * ; del _let  # noqa
