23584c8a8a972a7d0c3a41573357ff2f072ffb3a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 01:26:47 +0000

    Add package probability-stlc/1.0/probability_stlc-1.0.tar.gz

---
+++ packages/probability-stlc/probability_stlc-1.0.tar.gz/probability_stlc-1.0/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='probability_stlc',
+      version='1.0',
+      description='Gaussian distributions',
+      packages=['probability_stlc'],
+      zip_safe=False)
