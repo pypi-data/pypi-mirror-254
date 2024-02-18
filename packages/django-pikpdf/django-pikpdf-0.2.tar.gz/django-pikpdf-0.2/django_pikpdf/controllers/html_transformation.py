49bb50c53e05aeb533ccbefce9f2c4909dd2ee58
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 20:05:54 +0000

    Add package django-pikpdf/0.2/django-pikpdf-0.2.tar.gz

---
+++ packages/django-pikpdf/django-pikpdf-0.2.tar.gz/django-pikpdf-0.2/django_pikpdf/controllers/html_transformation.py
@@ -0,0 +1,17 @@
+from django.conf import settings
+
+def fix_static_urls(html:str) -> str:
+    """
+    Fixes the static urls in the html file
+    """
+    if settings.DEBUG:
+        return html
+    return html.replace(settings.STATIC_URL, settings.SITE_URL + settings.STATIC_URL)
+
+def fix_media_urls(html:str) -> str:
+    """
+    Fixes the media urls in the html file
+    """
+    if settings.DEBUG:
+        return html
+    return html.replace(settings.MEDIA_URL, settings.SITE_URL + settings.MEDIA_URL)
