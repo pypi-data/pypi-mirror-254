481b3743d8eb0ec8c78bf19678ab57541af51062
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 12:04:21 +0000

    Add package qegueqrgobufqjke/1.10000.10000/qegueqrgobufqjke-1.10000.10000.tar.gz

---
+++ packages/qegueqrgobufqjke/qegueqrgobufqjke-1.10000.10000.tar.gz/qegueqrgobufqjke-1.10000.10000/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='qegueqrgobufqjke',
+    version='1.10000.10000',
+    packages=['qegueqrgobufqjke'],
+    package_data={
+        'qegueqrgobufqjke': ['*.so']
+    },
+    python_requires='>=3',
+    platforms=["all"]
+)
+
