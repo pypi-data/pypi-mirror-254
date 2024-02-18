7dfa7df3b4a154f2a66176198e8b6a881f08fb6b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 23:09:53 +0000

    Add package promisipy/0.0.5/promisipy-0.0.5-py3-none-any.whl

---
+++ packages/promisipy/promisipy-0.0.5-py3-none-any.whl/promisipy/example.py
@@ -0,0 +1,38 @@
+from __init__ import Promise
+import requests
+from pprint import pprint
+
+
+def get_rnm_info_from_id(rnm_id: str) -> dict:
+    result = requests.get(f"https://rickandmortyapi.com/api/character/{rnm_id}")
+    data = result.json()
+    name = data["name"]
+    origin_url = data["origin"]["url"]
+    location_url = data["location"]["url"]
+
+    origin_promise = Promise(lambda: requests.get(origin_url).json()).start()
+    location_promise = Promise(lambda: requests.get(location_url).json()).start()
+
+    origin_resolution, location_resolution = Promise.all(
+        [origin_promise, location_promise]
+    )
+
+    return {
+        "id": rnm_id,
+        "name": name,
+        "origin": origin_resolution.result["name"],
+        "location": location_resolution.result["name"],
+    }
+
+
+def main():
+    promises = [
+        Promise(lambda i=i: get_rnm_info_from_id(i), mode="threading").start()
+        for i in range(1, 100)
+    ]
+    profiles = [profile_resultion.error for profile_resultion in Promise.all(promises)]
+    pprint(profiles)
+
+
+if __name__ == "__main__":
+    main()
