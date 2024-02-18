a06168ab0e7928fb33f22542b8515bf6028ec222
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 05:06:09 +0000

    Add package FOXFORD-API/0.1.4a1/FOXFORD_API-0.1.4a1-py3-none-any.whl

---
+++ packages/FOXFORD-API/FOXFORD_API-0.1.4a1-py3-none-any.whl/fapi/foxford_api_datatypes.py
@@ -0,0 +1,96 @@
+HOBBY_NAMES = {
+    1: "Аниме",
+    2: "Дорамы",
+    3: "Игры",
+    4: "Животные",
+    5: "Кино",
+    6: "Книги",
+    7: "Комиксы",
+    8: "Коллекционирование",
+    9: "Косплей",
+    10: "Кулинария",
+    11: "Музыка",
+    12: "Мультфильмы",
+    13: "Отдых на природе",
+    14: "Программирование",
+    15: "Психология",
+    16: "Путешествия",
+    17: "Саморазвитие",
+    18: "Сериалы",
+    19: "Спорт",
+    20: "Творчество",
+    21: "Театр",
+    22: "Фотография",
+    23: "Экология",
+    24: "Языки"
+}
+
+HOBBY_TRANSLATIONS = {
+    1: "Anime",
+    2: "Doramas",
+    3: "Games",
+    4: "Animals",
+    5: "Movies",
+    6: "Books",
+    7: "Comics",
+    8: "Collecting",
+    9: "Cosplay",
+    10: "Cooking",
+    11: "Music",
+    12: "Cartoons",
+    13: "Outdoor Recreation",
+    14: "Programming",
+    15: "Psychology",
+    16: "Traveling",
+    17: "Self_development",
+    18: "TV_Series",
+    19: "Sports",
+    20: "Creativity",
+    21: "Theater",
+    22: "Photography",
+    23: "Ecology",
+    24: "Languages",
+}
+
+USER_TYPES = {
+    "pupil": "Ученик",
+    "agent": "Учитель"
+}
+
+SCHOOL_YEARS_TRANSLATIONS = {
+    "2021-god": "2021 год",
+    "2022-god": "2022 год",
+    "2023-god": "2023 год",
+    "2024-god": "2024 год",
+    "2025-god": "2025 год"
+}
+
+NOTIFICATION_TRANSLATIONS = {
+    "вас ждёт домашка!": "homework",
+    "скоро занятие!": "lesson_near",
+    "фоксики": "foxbonus"
+}
+
+# Всё ещё не полный список...
+# Нужен человек с Аккаунтом 9-11 класса.
+DISCIPLINE_TRANSLATIONS = {
+    "русский язык": "russian",
+    "английский язык": "english",
+    "математика": "math",
+    "алгебра": "algebra",
+    "геометрия": "geometry",
+    "история": "history",
+    "программирование": "programming",
+    "физика": "physics",
+    "немецкий язык": "german",
+    "биология": "biology",
+    "физическая культура": "physical_culture",
+    "информатика": "informatics",
+    "география": "geography",
+    "обществознание": "social_science",
+    "межпредметный": "interdisciplinary",
+    "музыка": "music",
+    "общеразвивающие курсы": "general_educational_courses",
+    "литература": "literature",
+    "внеурочная деятельность": "extracurricular_activities"
+}
