9e222a5039a7ea3d3b250843af2899e845a9d714
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 22:07:12 +0000

    Add package cx-Freeze/6.15.14/cx_Freeze-6.15.14.tar.gz

---
+++ packages/cx-Freeze/cx_Freeze-6.15.14.tar.gz/cx_Freeze-6.15.14/tests/test_command_bdist_msi.py
@@ -0,0 +1,64 @@
+"""Tests for cx_Freeze.command.bdist_msi."""
+from __future__ import annotations
+
+from pathlib import Path
+from sysconfig import get_platform
+
+import pytest
+from generate_samples import run_command
+from setuptools import Distribution
+
+bdist_msi = pytest.importorskip(
+    "cx_Freeze.command.bdist_msi", reason="Windows tests"
+).BdistMSI
+
+DIST_ATTRS = {
+    "name": "foo",
+    "version": "0.0",
+    "executables": [],
+    "script_name": "setup.py",
+}
+SAMPLES_DIR = Path(__file__).resolve().parent.parent / "samples"
+
+
+@pytest.mark.datafiles(SAMPLES_DIR / "msi_binary_data")
+def test_bdist_msi(datafiles: Path):
+    """Test the msi_binary_data sample."""
+    run_command(datafiles, "python setup.py bdist_msi")
+    platform = get_platform().replace("win-amd64", "win64")
+    file_created = datafiles / "dist" / f"hello-0.1-{platform}.msi"
+    assert file_created.is_file()
+
+
+def test_bdist_msi_target_name():
+    """Test the bdist_msi with extra target_name option."""
+    dist = Distribution(DIST_ATTRS)
+    cmd = bdist_msi(dist)
+    cmd.target_name = "mytest"
+    cmd.finalize_options()
+    cmd.ensure_finalized()
+    assert cmd.fullname == "mytest-0.0"
+
+
+def test_bdist_msi_target_name_and_version():
+    """Test the bdist_msi with extra target options."""
+    dist = Distribution(DIST_ATTRS)
+    cmd = bdist_msi(dist)
+    cmd.target_name = "mytest"
+    cmd.target_version = "0.1"
+    cmd.finalize_options()
+    cmd.ensure_finalized()
+    assert cmd.fullname == "mytest-0.1"
+
+
+@pytest.mark.datafiles(SAMPLES_DIR / "msi_binary_data")
+def test_bdist_msi_target_name_with_extension(datafiles: Path):
+    """Test the msi_binary_data sample, with a specified target_name that
+    includes an ".msi" extension.
+    """
+    msi_name = "output.msi"
+    run_command(
+        datafiles, f"python setup.py bdist_msi --target-name {msi_name}"
+    )
+    file_created = datafiles / "dist" / msi_name
+    assert file_created.is_file()
