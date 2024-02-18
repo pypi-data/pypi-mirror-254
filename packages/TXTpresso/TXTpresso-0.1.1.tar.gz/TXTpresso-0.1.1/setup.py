d0c23ec28735cdb6caee48898abd41112aca0f22
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 18:04:23 +0000

    Add package TXTpresso/0.1.1/TXTpresso-0.1.1.tar.gz

---
+++ packages/TXTpresso/TXTpresso-0.1.1.tar.gz/TXTpresso-0.1.1/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='TXTpresso',
+    version='0.1.1',
+    author='Uli Hitzel',
+    author_email='uli.hitzel@gmail.com',
+    description='A lightweight DNS-based message broker',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/u1i/TXTpresso',
+    packages=find_packages(),
+    install_requires=[
+        'dnspython',
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
+
