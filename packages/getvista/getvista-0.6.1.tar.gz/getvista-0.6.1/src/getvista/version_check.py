0324607786e82e4c6b9f0d8f8875b3ed88c2b717
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 06:04:54 +0000

    Add package getvista/0.6.1/getvista-0.6.1.tar.gz

---
+++ packages/getvista/getvista-0.6.1.tar.gz/getvista-0.6.1/src/getvista/version_check.py
@@ -0,0 +1,13 @@
+import importlib.metadata
+from packaging import version
+
+# Check for package updates
+def check_for_updates():
+    try:
+        available_version = importlib.metadata.version('getvista')
+        current_version = '0.6.1'  # Your current version from setup.py
+
+        if version.parse(available_version) > version.parse(current_version):
+            print(f"Update available: {available_version}. You are using {current_version}. Run 'pip install getvista --upgrade' to get the latest version.")
+    except importlib.metadata.PackageNotFoundError:
+        pass  # Package is not installed, so version check is not possible
