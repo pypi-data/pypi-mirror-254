b024a5d064e1143b2503365dcf36f055d48cd717
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 12:05:18 +0000

    Add package advik/0.1.1/advik-0.1.1.tar.gz

---
+++ packages/advik/advik-0.1.1.tar.gz/advik-0.1.1/advik/__init__.py
@@ -0,0 +1,7 @@
+def main():
+    print("Hi, I'm Advik. Visit my website at https://advik.one")
+
+
+# Show an easter egg if the user tries importing the package, instead of running it.
+if __name__ != "advik":
+    print("Why are you importing me?")
