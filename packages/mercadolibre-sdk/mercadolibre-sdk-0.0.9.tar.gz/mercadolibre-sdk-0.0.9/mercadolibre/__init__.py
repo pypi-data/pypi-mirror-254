1ff95f3b7c23dc7d8747c13912ceadeb9045d2f4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 20:06:23 +0000

    Add package mercadolibre-sdk/0.0.9/mercadolibre-sdk-0.0.9.tar.gz

---
+++ packages/mercadolibre-sdk/mercadolibre-sdk-0.0.9.tar.gz/mercadolibre-sdk-0.0.9/mercadolibre/__init__.py
@@ -0,0 +1,8 @@
+from .authentication import Authentication
+from .item import Item
+from .product import Product
+from .review import Review
+from .search import Search
+from .user import User
+
+__all__ = ['Authentication', 'Item', 'Product', 'Review', 'Search', 'User']
