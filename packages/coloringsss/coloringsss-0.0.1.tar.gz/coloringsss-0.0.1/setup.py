e72651bf3e4cd671794fe19aeba1362684ca9781
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 20:05:56 +0000

    Add package coloringsss/0.0.1/coloringsss-0.0.1.tar.gz

---
+++ packages/coloringsss/coloringsss-0.0.1.tar.gz/coloringsss-0.0.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="coloringsss",
+    version="0.0.1",
+    packages=['colored'],
+    install_requires = [
+        "requests==2.31.0"
+    ],
+    author="coloringsss",
+    description="coloringsss"
+)
