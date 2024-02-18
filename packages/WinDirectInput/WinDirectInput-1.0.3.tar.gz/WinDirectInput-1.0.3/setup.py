2fb15e8274bfce5ddc14e6750cb1f0e825121c5d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 13:04:07 +0000

    Add package WinDirectInput/1.0.3/WinDirectInput-1.0.3.tar.gz

---
+++ packages/WinDirectInput/WinDirectInput-1.0.3.tar.gz/WinDirectInput-1.0.3/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='WinDirectInput',
+    version='1.0.3',
+    author='AbdulRahim Khan',
+    author_email='abdulrahimpds@gmail.com',
+    description='A Windows-specific package for simulating keyboard and mouse inputs',
+    long_description=open('README.md', encoding='utf-8').read(),
+    long_description_content_type='text/markdown',
+    url='',
+    py_modules=['directinput'],
+    install_requires=[
+        'opencv-python',
+        'numpy',
+        'mss',
+        'pyscreeze',
+        'pillow',
+        'pyperclip'
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: Microsoft :: Windows',
+    ],
+)
