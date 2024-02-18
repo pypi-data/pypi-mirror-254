195a5706836bb959fca5ff4ddb65e83745416ce7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 06:04:29 +0000

    Add package redisbench-admin/0.10.21/redisbench_admin-0.10.21.tar.gz

---
+++ packages/redisbench-admin/redisbench_admin-0.10.21.tar.gz/redisbench_admin-0.10.21/redisbench_admin/run_local/args.py
@@ -0,0 +1,16 @@
+#  Apache License Version 2.0
+#
+#  Copyright (c) 2021., Redis Labs Modules
+#  All rights reserved.
+#
+import os
+
+from redisbench_admin.run.args import common_run_args
+from redisbench_admin.run.common import REDIS_BINARY
+
+
+def create_run_local_arguments(parser):
+    parser = common_run_args(parser)
+    parser.add_argument("--port", type=int, default=6379)
+    parser.add_argument("--redis-binary", type=str, default=REDIS_BINARY)
+    return parser
