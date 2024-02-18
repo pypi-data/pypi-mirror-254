68fda73f9881f77b14e232586091aeefc9018b6b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 05:06:36 +0000

    Add package captchagenert/1.3/captchagenert-1.3.tar.gz

---
+++ packages/captchagenert/captchagenert-1.3.tar.gz/captchagenert-1.3/setup.py
@@ -0,0 +1,17 @@
+import setuptools
+
+setuptools.setup(
+	name='captchagenert',
+	version='1.3',
+	author='danon',
+	author_email='',
+	description='',
+	packages=['captchatoken'],
+	include_package_data=True,
+	classifiers=[
+		"Programming Language :: Python :: 3",
+		"License :: OSI Approved :: MIT License",
+		"Operating System :: OS Independent",
+	],
+	python_requires='>=3.6',
+)
