b056405190c46b27776262273770bdd467196351
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 23:07:17 +0000

    Add package mybox/0.9.17/mybox-0.9.17.tar.gz

---
+++ packages/mybox/mybox-0.9.17.tar.gz/mybox-0.9.17/mybox/state/installed.py
@@ -0,0 +1,17 @@
+from dataclasses import dataclass
+from pathlib import Path
+
+from .base import storage
+
+
+@dataclass(frozen=True)
+class InstalledFile:
+    path: str
+    root: bool
+
+    @property
+    def path_(self) -> Path:
+        return Path(self.path)
+
+
+INSTALLED_FILES = storage("installed_file", InstalledFile)
