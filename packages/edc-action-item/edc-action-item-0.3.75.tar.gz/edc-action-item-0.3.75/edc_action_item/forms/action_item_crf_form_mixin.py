b94f182e1755adac483cfb1c195b5d5de41df82b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 15:06:51 +0000

    Add package edc-action-item/0.3.75/edc-action-item-0.3.75.tar.gz

---
+++ packages/edc-action-item/edc-action-item-0.3.75.tar.gz/edc-action-item-0.3.75/edc_action_item/forms/action_item_crf_form_mixin.py
@@ -0,0 +1,14 @@
+from django import forms
+
+
+class ActionItemCrfFormMixin:
+
+    """Declare with forms.ModelForm."""
+
+    class Meta:
+        help_text = {"action_identifier": "(read-only)"}
+        widgets = {
+            "action_identifier": forms.TextInput(
+                attrs={"required": False, "readonly": "readonly"}
+            ),
+        }
