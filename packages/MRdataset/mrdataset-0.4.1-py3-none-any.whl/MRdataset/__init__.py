e9f284e6dc419b0604f99e994c05137b79c5aace
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 15:06:52 +0000

    Add package MRdataset/0.4.1/mrdataset-0.4.1-py3-none-any.whl

---
+++ packages/MRdataset/mrdataset-0.4.1-py3-none-any.whl/MRdataset/__init__.py
@@ -0,0 +1,26 @@
+"""Top-level package for MRdataset."""
+
+__author__ = """Pradeep Raamana"""
+__email__ = 'raamana@gmail.com'
+
+# __version__ = '0.1.0'
+import logging
+
+from MRdataset.config import configure_logger
+
+logger = logging.getLogger(__name__)
+logger = configure_logger(logger, output_dir=None, mode='w')
+
+from MRdataset.common import import_dataset, load_mr_dataset, save_mr_dataset
+from MRdataset.config import MRDS_EXT, DatasetEmptyException
+from MRdataset.dicom_utils import is_dicom_file
+from MRdataset.utils import valid_dirs
+from MRdataset.base import BaseDataset
+from MRdataset.bids import BidsDataset
+from MRdataset.dicom import DicomDataset
+
+try:
+    from MRdataset._version import __version__
+except ImportError:
+    raise ImportError('It seems MRdataset is not installed correctly. Use pip '
+                      ' to install it first.')
