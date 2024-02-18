6a98b419e7cc5d5d25e25d97eee53f50cc483ea7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 08:04:27 +0000

    Add package py-partiql-parser/0.5.1/py-partiql-parser-0.5.1.tar.gz

---
+++ packages/py-partiql-parser/py-partiql-parser-0.5.1.tar.gz/py-partiql-parser-0.5.1/tests/test_csv_converter.py
@@ -0,0 +1,39 @@
+from py_partiql_parser._internal.csv_converter import csv_to_json
+import json
+
+
+input_csv = """Sam,(949) 555-6701,Irvine,Solutions Architect
+Vinod,(949) 555-6702,Los Angeles,Solutions Architect
+Jeff,(949) 555-6703,Seattle,AWS Evangelist
+Jane,(949) 555-6704,Chicago,Developer
+Sean,(949) 555-6705,Chicago,Developer
+Mary,(949) 555-6706,Chicago,Developer
+Kate,(949) 555-6707,Chicago,Developer"""
+
+
+input_with_header = (
+    """Name,PhoneNumber,City,Occupation
+"""
+    + input_csv
+)
+
+
+def test_csv_to_json() -> None:
+    result = csv_to_json(input_csv)
+    lines = result.split("\n")
+    assert len(lines) == 7
+
+    line0 = json.loads(lines[0])
+    assert line0["_1"] == "Sam"
+
+
+def test_csv_to_json_with_headers() -> None:
+    result = csv_to_json(input_with_header, headers_included=True)
+    lines = result.split("\n")
+    assert len(lines) == 7
+
+    line0 = json.loads(lines[0])
+    assert line0["Name"] == "Sam"
+
+    line2 = json.loads(lines[2])
+    assert line2["City"] == "Seattle"
