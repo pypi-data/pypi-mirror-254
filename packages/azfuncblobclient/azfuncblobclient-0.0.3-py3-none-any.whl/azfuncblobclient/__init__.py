263355e01ae275cd2341130f262272ebaa2ddf06
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 08:03:44 +0000

    Add package azfuncblobclient/0.0.3/azfuncblobclient-0.0.3-py3-none-any.whl

---
+++ packages/azfuncblobclient/azfuncblobclient-0.0.3-py3-none-any.whl/azfuncblobclient/__init__.py
@@ -0,0 +1,5 @@
+from .blobClient import BlobClient
+from .containerClient import ContainerClient
+from .blobClientConverter import BlobClientConverter
+
+__all__ = ['BlobClient', 'BlobClientConverter', 'ContainerClient']
