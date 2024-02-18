8ca9615de82f7e04c2a0dc61734a2070bf2edfc4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 18:04:23 +0000

    Add package stac-fastapi.elasticsearch/1.1.0/stac-fastapi.elasticsearch-1.1.0.tar.gz

---
+++ packages/stac-fastapi.elasticsearch/stac-fastapi.elasticsearch-1.1.0.tar.gz/stac-fastapi.elasticsearch-1.1.0/setup.py
@@ -0,0 +1,71 @@
+"""stac_fastapi: elasticsearch module."""
+
+from setuptools import find_namespace_packages, setup
+
+with open("README.md") as f:
+    desc = f.read()
+
+install_requires = [
+    "fastapi",
+    "attrs",
+    "pydantic[dotenv]<2",
+    "stac_pydantic==2.0.*",
+    "stac-fastapi.types==2.4.9",
+    "stac-fastapi.api==2.4.9",
+    "stac-fastapi.extensions==2.4.9",
+    "elasticsearch[async]==8.11.0",
+    "elasticsearch-dsl==8.11.0",
+    "opensearch-py==2.4.2",
+    "opensearch-py[async]==2.4.2",
+    "pystac[validation]",
+    "uvicorn",
+    "orjson",
+    "overrides",
+    "starlette",
+    "geojson-pydantic",
+    "pygeofilter==0.2.1",
+]
+
+extra_reqs = {
+    "dev": [
+        "pytest",
+        "pytest-cov",
+        "pytest-asyncio",
+        "pre-commit",
+        "requests",
+        "ciso8601",
+        "httpx",
+    ],
+    "docs": ["mkdocs", "mkdocs-material", "pdocs"],
+    "server": ["uvicorn[standard]==0.19.0"],
+}
+
+setup(
+    name="stac-fastapi.elasticsearch",
+    description="An implementation of STAC API based on the FastAPI framework with both Elasticsearch and Opensearch.",
+    long_description=desc,
+    long_description_content_type="text/markdown",
+    python_requires=">=3.8",
+    classifiers=[
+        "Intended Audience :: Developers",
+        "Intended Audience :: Information Technology",
+        "Intended Audience :: Science/Research",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "License :: OSI Approved :: MIT License",
+    ],
+    url="https://github.com/stac-utils/stac-fastapi-elasticsearch",
+    license="MIT",
+    packages=find_namespace_packages(exclude=["alembic", "tests", "scripts"]),
+    zip_safe=False,
+    install_requires=install_requires,
+    tests_require=extra_reqs["dev"],
+    extras_require=extra_reqs,
+    entry_points={
+        "console_scripts": [
+            "stac-fastapi-elasticsearch=stac_fastapi.elasticsearch.app:run"
+        ]
+    },
+)
