7f49cdb8f8866ffc04b0b354f8341dddf488ca11
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 08:06:05 +0000

    Add package abstra/1.29.0/abstra-1.29.0.tar.gz

---
+++ packages/abstra/abstra-1.29.0.tar.gz/abstra-1.29.0/abstra_internals/stdio_monkey_patch.py
@@ -0,0 +1,54 @@
+import sys
+from .env_masker import GLOBAL_MASKER
+from typing import Callable, Literal
+from .utils.environment import IS_PRODUCTION
+
+from .execution.execution import Execution
+from .stdio_skip_request import ESCAPE_STRING
+
+_og_stdout_write = sys.stdout.write
+_og_stderr_write = sys.stderr.write
+
+
+def writeWraper(
+    type: Literal["stdout", "stderr"],
+    write: Callable,
+    text: str,
+    print_exceptions: bool,
+):
+    if IS_PRODUCTION:
+        text = GLOBAL_MASKER.mask(str(text))
+    try:
+        skip_sending = text.startswith(ESCAPE_STRING)
+        execution = Execution.get_current_execution()
+
+        output: str = str(text)
+
+        if execution and not skip_sending:
+            output = (
+                f"[RUN {execution.id.split(sep='-')[0]}] {output}"
+                if output.strip()
+                else output
+            )
+            execution.stdio(type, text)
+
+        write(output)
+    except Exception as e:
+        if print_exceptions:
+            write(f"[WRITE_WRAPPER] ERROR: {str(e)}\n\n")
+    finally:
+        return len(text)
+
+
+def override_stdio(print_exceptions: bool):
+    sys.stdout.write = lambda text: writeWraper(
+        "stdout", _og_stdout_write, text, print_exceptions
+    )
+    sys.stderr.write = lambda text: writeWraper(
+        "stderr", _og_stderr_write, text, print_exceptions
+    )
+
+
+def reset():
+    sys.stdout.write = _og_stdout_write
+    sys.stderr.write = _og_stderr_write
