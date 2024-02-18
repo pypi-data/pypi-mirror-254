268a241025c95b603ab6d782bb068073a54d0a07
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 19:05:48 +0000

    Add package dsnd-distributions-anchelus/1.0/dsnd-distributions-anchelus-1.0.tar.gz

---
+++ packages/dsnd-distributions-anchelus/dsnd-distributions-anchelus-1.0.tar.gz/dsnd-distributions-anchelus-1.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='dsnd-distributions-anchelus',
+      version='1.0',
+      description='Gaussian and Binomial distributions',
+      packages=['distributions'],
+      author = 'Kevin Darmasaputra',
+      author_email = 'kevin@azurebaycapital.com',
+      zip_safe=False)
