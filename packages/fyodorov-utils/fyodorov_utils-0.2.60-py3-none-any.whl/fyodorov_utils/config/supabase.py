ae2aa8ecc61022de82fd0bc1b10fead9be1e675e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 23:08:34 +0000

    Add package fyodorov-utils/0.2.60/fyodorov_utils-0.2.60-py3-none-any.whl

---
+++ packages/fyodorov-utils/fyodorov_utils-0.2.60-py3-none-any.whl/fyodorov_utils/config/supabase.py
@@ -0,0 +1,13 @@
+from supabase import create_client, Client
+from .config import Settings
+
+settings = Settings()
+
+def get_supabase(key: str = None) -> Client:
+    if jwt:
+        return create_client(
+            settings.SUPABASE_URL,
+            key,
+        )
+    else:
+        return create_client(settings.SUPABASE_URL, settings.SUPABASE_KEY)
