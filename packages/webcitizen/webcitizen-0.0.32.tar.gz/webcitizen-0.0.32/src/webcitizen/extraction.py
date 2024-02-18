fffc2895172debc6806ce1b696063d7949aac4c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 22:06:09 +0000

    Add package webcitizen/0.0.32/webcitizen-0.0.32.tar.gz

---
+++ packages/webcitizen/webcitizen-0.0.32.tar.gz/webcitizen-0.0.32/src/webcitizen/extraction.py
@@ -0,0 +1,28 @@
+
+from newspaper import Article
+
+
+def json_nltk2(url:str, article:Article,html:str)->dict:
+    article = Article(url)
+    article.set_html(html)
+    article.parse()
+    article.nlp()
+    _ = {
+            "keywords": article.keywords,
+            "summary": article.summary,
+            "text": article.text,
+            "title": article.title,
+            "authors": article.authors,
+            "publish_date": str(article.publish_date),
+            "top_image": article.top_image,
+            "meta_keywords": article.meta_keywords,
+            "meta_description": article.meta_description,
+            "meta_lang": article.meta_lang,
+            "meta_favicon": article.meta_favicon,
+            "canonical_link": article.canonical_link,
+            "tags": list(article.tags),
+            "movies": article.movies,
+            "imgs": list(article.imgs),
+        }
+    return _
+    
