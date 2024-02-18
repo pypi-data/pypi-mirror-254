358a4a1e48b3d93b5616539d7a4ad66bc41e0918
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 04:07:59 +0000

    Add package my-unique-distribution-name/0.2/my_unique_distribution_name-0.2.tar.gz

---
+++ packages/my-unique-distribution-name/my_unique_distribution_name-0.2.tar.gz/my_unique_distribution_name-0.2/README.md
@@ -0,0 +1,10 @@
+# distribution
+
+A Python library for probability distributions.
+
+## Installation
+
+You can install the library using pip:
+
+```bash
+pip install distribution
---
+++ packages/my-unique-distribution-name/my_unique_distribution_name-0.2.tar.gz/my_unique_distribution_name-0.2/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name='my_unique_distribution_name',
+    version='0.2',
+    description='A library of probability distributions',
+    author='Latif Jan',
+    author_email='latifjan@example.com',
+    license='MIT',
+    packages=['my_unique_distribution_name'],
+    install_requires=[],
+    url='https://example.com/distribution',
+    keywords='distributions probability',
+    python_requires='>=3.6',
+)
