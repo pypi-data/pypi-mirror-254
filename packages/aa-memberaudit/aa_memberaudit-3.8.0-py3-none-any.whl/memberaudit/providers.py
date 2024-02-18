aafb3bf8a4b0fab741a271d69bf41700be511c97
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 00:05:39 +0000

    Add package aa-memberaudit/3.8.0/aa_memberaudit-3.8.0-py3-none-any.whl

---
+++ packages/aa-memberaudit/aa_memberaudit-3.8.0-py3-none-any.whl/memberaudit/providers.py
@@ -0,0 +1,8 @@
+"""Shared ESI client for Member Audit."""
+
+
+from esi.clients import EsiClientProvider
+
+from memberaudit import __version__
+
+esi = EsiClientProvider(app_info_text=f"aa-memberaudit v{__version__}")
