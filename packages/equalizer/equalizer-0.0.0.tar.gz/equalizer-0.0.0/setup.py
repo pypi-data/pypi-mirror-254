4058d9f362e6facc758847538bf5d09a6657f900
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 03:04:22 +0000

    Add package equalizer/0.0.0/equalizer-0.0.0.tar.gz

---
+++ packages/equalizer/equalizer-0.0.0.tar.gz/equalizer-0.0.0/setup.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+
+"""The setup script."""
+
+from setuptools import setup, find_packages
+
+REQUIREMENTS = ["transformers", "torch"]
+
+TEST_REQUIREMENTS = ["pytest"]
+QUALITY_REQUIREMENTS = ["black", "ruff"]
+
+EXTRAS_REQUIREMENTS = {"dev": TEST_REQUIREMENTS + QUALITY_REQUIREMENTS}
+
+setup(
+    author="FairNLP",
+    author_email="info@fairnlp.com",
+    python_requires=">=3.7",
+    classifiers=[
+        "Development Status :: 2 - Pre-Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: Apache Software License",
+        "Natural Language :: English",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+    ],
+    description="Demographic equality for modern NLP algorithms.",
+    install_requires=REQUIREMENTS,
+    license="Apache Software License 2.0",
+    include_package_data=True,
+    keywords="equalizer",
+    name="equalizer",
+    packages=find_packages(include=["equalizer", "equalizer.*"]),
+    test_suite="tests",
+    tests_require=TEST_REQUIREMENTS,
+    extras_require=EXTRAS_REQUIREMENTS,
+    url="https://github.com/FairNLP/equalizer",
+    version="0.0.0",
+    zip_safe=False,
+)
