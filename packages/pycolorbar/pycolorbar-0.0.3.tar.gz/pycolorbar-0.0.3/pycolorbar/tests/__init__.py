72c388ff9d1c1c4d9396d4341498960ba1dbba02
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 20:05:55 +0000

    Add package pycolorbar/0.0.3/pycolorbar-0.0.3.tar.gz

---
+++ packages/pycolorbar/pycolorbar-0.0.3.tar.gz/pycolorbar-0.0.3/pycolorbar/tests/__init__.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Feb  2 21:40:51 2024
+
+@author: ghiggi
+"""
---
+++ packages/pycolorbar/pycolorbar-0.0.3.tar.gz/pycolorbar-0.0.3/pycolorbar/tests/test_foo.py
@@ -0,0 +1,5 @@
+import pytest  # noqa
+
+
+def test_foo():
+    assert True
