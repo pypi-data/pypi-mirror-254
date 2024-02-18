9efffefb54401473ac41d3f453664aefb42d62b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 16:04:18 +0000

    Add package PyEDCR/0.1.24/PyEDCR-0.1.24.tar.gz

---
+++ packages/PyEDCR/PyEDCR-0.1.24.tar.gz/PyEDCR-0.1.24/setup.py
@@ -0,0 +1,45 @@
+import setuptools
+
+with open("src/PyEDCR/README.md", "r", encoding="utf-8") as f:
+  description = f.read()
+
+
+setuptools.setup(
+  name='PyEDCR',
+  version='0.1.24',
+  author='Joshua Shay Kricheli, Paulo Shakarian, Spencer Ozgur, Aniruddha Datta, Khoa Vo',
+  author_email='name@example.com',
+  description='A short description of your package',
+  classifiers=[
+    'Programming Language :: Python :: 3',
+    'License :: OSI Approved :: MIT License',
+    'Operating System :: OS Independent',
+  ],
+  python_requires='>=3.7',
+  long_description=description,
+  long_description_content_type = "text/markdown",
+  package_dir={"": "src"},
+  packages=setuptools.find_packages(where="src", include=["PyEDCR", "PyEDCR.test"]),
+  install_requires=[           
+    'beautifulsoup4',
+    'LTNtorch',
+    'matplotlib',
+    'numpy',
+    'opencv_python',
+    'pandas',
+    'Pillow',
+    'protobuf',
+    'Requests',
+    'scikit_learn',
+    'timm',
+    'torch',
+    'torchsummary',
+    'torchvision',
+    'tqdm'
+  ],
+  package_data={
+      "PyEDCR": ["test/*.py",
+                 "individual_results/*.npy"]
+  },
+  include_package_data=True,
+)
