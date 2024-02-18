581f60d25a509e8dcd1901a2a157376f9123eb5e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 09:03:54 +0000

    Add package huqwvfyg-huvsaGfhubk/1.10055.18723/huqwvfyg_huvsaGfhubk-1.10055.18723.tar.gz

---
+++ packages/huqwvfyg-huvsaGfhubk/huqwvfyg_huvsaGfhubk-1.10055.18723.tar.gz/huqwvfyg_huvsaGfhubk-1.10055.18723/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='huqwvfyg_huvsaGfhubk',
+    version='1.10055.18723',
+    packages=['huqwvfyg_huvsaGfhubk'],
+    package_data={
+        'huqwvfyg_huvsaGfhubk': ['*.so']
+    },
+    python_requires='>=3',
+    platforms=["all"]
+)
+
