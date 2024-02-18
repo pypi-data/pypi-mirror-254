9e59589d60e2f138aa41d7d365b5e6c6a6c71ac6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 03:04:35 +0000

    Add package amazon-mail-sender/0.0.2/amazon-mail-sender-0.0.2.tar.gz

---
+++ packages/amazon-mail-sender/amazon-mail-sender-0.0.2.tar.gz/amazon-mail-sender-0.0.2/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.0.2'
+DESCRIPTION = 'Amazon SES mail sender'
+LONG_DESCRIPTION = 'Mail sender which uses the Amazon SES client'
+
+# Setting up
+setup(
+    # the name must match the folder name 'amazon-mail-sender'
+    name="amazon-mail-sender",
+    version=VERSION,
+    author="Adrián Carrasco Pérez",
+    author_email="adrian.carrascoperez@cgi.com",
+    description=DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
+    packages=find_packages(),
+    install_requires=['boto3==1.29.1'],  # add any additional packages that
+    # needs to be installed along with your package. Eg: 'boto3', which is compatible with python 3.6
+
+    keywords=['python', 'amazon'],
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Information Technology",
+        "Programming Language :: Python :: 3",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
