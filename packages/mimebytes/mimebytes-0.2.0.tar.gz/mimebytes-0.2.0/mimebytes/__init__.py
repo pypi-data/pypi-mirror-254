02d1f82041b0633a2322142f5cf5f1a4b0f748ad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 19:04:10 +0000

    Add package mimebytes/0.2.0/mimebytes-0.2.0.tar.gz

---
+++ packages/mimebytes/mimebytes-0.2.0.tar.gz/mimebytes-0.2.0/mimebytes/__init__.py
@@ -0,0 +1,5 @@
+# flake8: noqa: F401
+from mimebytes._version import __version__
+from mimebytes.base import Bytes, HTMLBytes, JSONBytes
+from mimebytes.image import JPEGBytes, PNGBytes, SVGBytes
+from mimebytes.mime import mime
