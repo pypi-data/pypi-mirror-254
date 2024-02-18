346d75fcebc4532e7315acc2ca15a7332e7d435c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 04:07:58 +0000

    Add package skyctl/1.0.32/skyctl-1.0.32.tar.gz

---
+++ packages/skyctl/skyctl-1.0.32.tar.gz/skyctl-1.0.32/file_config/test.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+import configparser
+
+import requests
+import os
+import click
+import tarfile
+
+
+@click.group()
+def login():
+    """这是登录命令行接口的根命令"""
+    pass
+
+
+@login.command('login')
+@click.option('--username', prompt=True, help='Username for login')
+@click.option('--password', prompt=True, hide_input=True, confirmation_prompt=True, help='Password for login')
+def auth(username, password):
+    if username == 'admin' and password == 'password':
+        click.echo('Login successful!')
+    else:
+        click.echo('Invalid username or password!')
+        exit(1)
+
+
+@login.command('login')
+@click.option('--check', help='Username for login')
+def check():
+    click.echo('You are now in check command')
+
+
+# @login.command('check')
+# def check():
+#     click.echo('You are now in check command')
