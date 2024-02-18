673efdcdc8dd1fe21e12c409f0eacc9307210f24
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 06:04:42 +0000

    Add package coretex/1.0.57/coretex-1.0.57-py3-none-any.whl

---
+++ packages/coretex/coretex-1.0.57-py3-none-any.whl/coretex/cli/main.py
@@ -0,0 +1,15 @@
+import click
+
+from .node import node
+from .project import project
+from .config import config
+
+
+@click.group()
+def cli() -> None:
+    pass
+
+
+cli.add_command(config)
+cli.add_command(project)
+cli.add_command(node)
