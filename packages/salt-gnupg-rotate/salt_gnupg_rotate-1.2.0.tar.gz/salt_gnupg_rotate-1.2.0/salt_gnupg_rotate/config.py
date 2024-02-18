e82cec8f3acbce095cc64d7dd89ec4803c848452
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 17:04:45 +0000

    Add package salt-gnupg-rotate/1.2.0/salt_gnupg_rotate-1.2.0.tar.gz

---
+++ packages/salt-gnupg-rotate/salt_gnupg_rotate-1.2.0.tar.gz/salt_gnupg_rotate-1.2.0/salt_gnupg_rotate/config.py
@@ -0,0 +1,23 @@
+"""Configuration."""
+
+from __future__ import annotations
+
+import os
+
+from rich.console import Console
+
+from salt_gnupg_rotate.logging_mixins import logging  # type: ignore[attr-defined]
+
+APP_NAME = "salt_gnupg_rotate"
+ENV_APP_NAME = "SALT_GNUPG_ROTATE"
+CONSOLE = Console(stderr=True)
+LOG_LEVEL: str = "info"
+LOG_LEVELS: list[str] = [
+    str(level_name).lower()
+    for _, level_name in sorted(
+        getattr(logging, "_levelToName", {}).items(),
+        reverse=True,
+    )
+]
+DECRYPTION_GPG_HOMEDIR: str = os.getenv("GNUPGHOME", "~/.gnupg")
+ENCRYPTION_GPG_HOMEDIR: str = os.getenv("GNUPGHOME", "~/.gnupg")
