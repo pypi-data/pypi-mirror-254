c8e40341daa04df1497624b29a9e0c1e3c734a6d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 18:04:41 +0000

    Add package maize/0.2.4/maize-0.2.4.tar.gz

---
+++ packages/maize/maize-0.2.4.tar.gz/maize-0.2.4/maize/exceptions/spider_exception.py
@@ -0,0 +1,30 @@
+class ResponseVerifyException(Exception):
+    """响应校验错误"""
+
+
+class TransformTypeException(TypeError):
+    pass
+
+
+class OutputException(Exception):
+    pass
+
+
+class SpiderTypeException(TypeError):
+    pass
+
+
+class ItemInitException(Exception):
+    pass
+
+
+class ItemAttributeException(Exception):
+    pass
+
+
+class DecodeException(Exception):
+    pass
+
+
+class EncodeException(Exception):
+    pass
