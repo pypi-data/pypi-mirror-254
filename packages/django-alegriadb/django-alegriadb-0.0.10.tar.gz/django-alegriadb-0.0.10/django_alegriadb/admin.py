94f66b317fa1cd2ec9f64665f725a8b38ae7c27d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 12:04:00 +0000

    Add package django-alegriadb/0.0.10/django-alegriadb-0.0.10.tar.gz

---
+++ packages/django-alegriadb/django-alegriadb-0.0.10.tar.gz/django-alegriadb-0.0.10/django_alegriadb/admin.py
@@ -0,0 +1,28 @@
+from django.contrib import admin, messages
+from alegriadb.alegriadb.models import AppsToVerifyModel, RedirectEmail, Verified
+from django.db.transaction import atomic
+from django_alegriadb.utils.alegria_admin_funcitons import AlegriaVerifier
+
+@admin.register(AppsToVerifyModel)
+class AppsToVerifyModelAdmin(admin.ModelAdmin):
+    list_display = ['name','db_external_name','should_verify']
+
+    @atomic
+    def verify_app(modeladmin: admin.ModelAdmin, request, queryset):
+        try:
+            AlegriaVerifier(queryset=queryset)
+            msg = "OK..."
+            modeladmin.message_user(request, msg, level=messages.SUCCESS)
+        except:
+            msg = 'Alegria'
+            modeladmin.message_user(request, msg, level=messages.ERROR)
+            
+    actions = ["verify_app",]
+
+@admin.register(RedirectEmail)
+class RedirectEmailAdmin(admin.ModelAdmin):
+    list_display = ['email',]
+
+@admin.register(Verified)
+class VerifiedAdmin(admin.ModelAdmin):
+    list_display = ['data','started_at','finished_at']
---
+++ packages/django-alegriadb/django-alegriadb-0.0.10.tar.gz/django-alegriadb-0.0.10/django_alegriadb/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class AlebriadbConfig(AppConfig):
+    name = 'django_alegriadb'
