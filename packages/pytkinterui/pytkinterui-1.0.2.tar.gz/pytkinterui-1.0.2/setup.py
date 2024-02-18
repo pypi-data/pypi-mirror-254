b1a649b0d16b82ba381a203eeac9397aa64a1a97
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 09:04:15 +0000

    Add package pytkinterui/1.0.2/pytkinterui-1.0.2.tar.gz

---
+++ packages/pytkinterui/pytkinterui-1.0.2.tar.gz/pytkinterui-1.0.2/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='pytkinterui',
+    version="1.0.2",
+    packages=find_packages(),
+    author='icarogamer2441',
+    url="https://github.com/Icarogamer2441/pytkinterui",
+    keywords='ui pytkinterui',
+)
