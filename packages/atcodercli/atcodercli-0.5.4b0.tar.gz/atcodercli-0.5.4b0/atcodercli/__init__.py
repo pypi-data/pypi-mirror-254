f21b62dcc4526b98bd93cbfa7712fc984f817386
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 13:04:40 +0000

    Add package atcodercli/0.5.4b0/atcodercli-0.5.4b0.tar.gz

---
+++ packages/atcodercli/atcodercli-0.5.4b0.tar.gz/atcodercli-0.5.4b0/atcodercli/__init__.py
@@ -0,0 +1,23 @@
+"""
+    Load gettext function _() to translate
+"""
+import gettext
+import locale
+import os
+import pathlib
+
+LOCALEDIR = pathlib.Path(__file__).parent / "locales"
+
+LANG = locale.getlocale()[0]
+
+try:
+    lang = gettext.translation(
+        "atcodercli",
+        languages=[
+            LANG,
+        ],
+        localedir=LOCALEDIR,
+    )
+    lang.install()
+except FileNotFoundError:
+    gettext.install("atcodercli")
