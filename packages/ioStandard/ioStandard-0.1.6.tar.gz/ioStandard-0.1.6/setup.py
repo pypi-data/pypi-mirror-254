2122ff9f278dc01d3d3585f3939855785ce95ca2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 21:07:19 +0000

    Add package ioStandard/0.1.6/ioStandard-0.1.6.tar.gz

---
+++ packages/ioStandard/ioStandard-0.1.6.tar.gz/ioStandard-0.1.6/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name='ioStandard',
+    version='0.1.6',
+    author='Austin Gilchrist-Thomas',
+    packages=['ioStandard'],
+    install_requires=['pygame', 'datetime', 'tk'],
+)
