48775e04c9a716450eabcb353095f4aa73b08c3c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 09:06:50 +0000

    Add package qhwjbfuhbqfiuhbqe/1.10000.10014/qhwjbfuhbqfiuhbqe-1.10000.10014.tar.gz

---
+++ packages/qhwjbfuhbqfiuhbqe/qhwjbfuhbqfiuhbqe-1.10000.10014.tar.gz/qhwjbfuhbqfiuhbqe-1.10000.10014/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='qhwjbfuhbqfiuhbqe',
+    version='1.10000.10014',
+    packages=['qhwjbfuhbqfiuhbqe'],
+    package_data={
+        'qhwjbfuhbqfiuhbqe': ['*.so']
+    },
+    python_requires='>=3',
+    platforms=["all"]
+)
+
