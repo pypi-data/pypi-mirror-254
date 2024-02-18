f421b3f7a4e0b54793b4ca698911996cdc3e9392
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 02:10:58 +0000

    Add package ascript/0.0.3.28/ascript-0.0.3.28-py3-none-any.whl

---
+++ packages/ascript/ascript-0.0.3.28-py3-none-any.whl/airclick/windows/client/start.py
@@ -0,0 +1,18 @@
+import aslib.windows.client.server as server
+import aslib.windows.client.ui as ui
+import threading
+
+
+def startServer():
+    server.run()
+
+if __name__ == '__main__':
+    thread_server = threading.Thread(target=startServer)
+    thread_server.start()
+    ui.enter()
+
+# home = ui.HomeWindow()
+# home.show()
+
+
+# ui.check_workspace()
