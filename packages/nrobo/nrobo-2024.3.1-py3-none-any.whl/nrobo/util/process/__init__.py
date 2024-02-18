e72247c84ab1afeebc54a636b75549a59f12267b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 14:07:23 +0000

    Add package nrobo/2024.3.1/nrobo-2024.3.1-py3-none-any.whl

---
+++ packages/nrobo/nrobo-2024.3.1-py3-none-any.whl/nrobo/util/process/__init__.py
@@ -0,0 +1,27 @@
+"""
+subprocess doc: https://docs.python.org/3/library/subprocess.html
+another subprocess doc: https://www.geeksforgeeks.org/python-subprocess-module/
+
+"""
+import os, subprocess as sp
+
+
+def run_command(command=[], stdin=None, input=None, stdout=None, stderr=None, capture_output=False, shell=False,
+                cwd=None, timeout=None, check=False, encoding=None, errors=None, text=None, env=None,
+                universal_newlines=None):
+    """
+    Execute given command, command
+
+    :param capture_output:
+    :param command: command
+    :return: status code
+    """
+
+    try:
+        # status_code = os.system(command)
+        cp = sp.run(command, capture_output)
+    except Exception as e:
+        print(e)
+
+    # return with status_code.
+    return cp.returncode
