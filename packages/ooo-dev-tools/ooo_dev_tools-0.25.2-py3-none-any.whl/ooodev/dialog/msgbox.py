1e488ab267eef05d9a4295d86926c092e8dfe827
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 08:04:14 +0000

    Add package ooo-dev-tools/0.25.2/ooo_dev_tools-0.25.2-py3-none-any.whl

---
+++ packages/ooo-dev-tools/ooo_dev_tools-0.25.2-py3-none-any.whl/ooodev/dialog/msgbox.py
@@ -0,0 +1,53 @@
+# coding: utf-8
+from __future__ import annotations
+import uno  # pylint: disable=unused-import
+
+from com.sun.star.awt import XToolkit2
+
+# pylint: disable=useless-import-alias
+from ooo.dyn.awt.message_box_results import MessageBoxResultsEnum as MessageBoxResultsEnum
+from ooo.dyn.awt.message_box_buttons import MessageBoxButtonsEnum as MessageBoxButtonsEnum
+from ooo.dyn.awt.message_box_type import MessageBoxType as MessageBoxType
+
+from ..utils import lo as mLo
+
+
+class MsgBox:
+    @staticmethod
+    def msgbox(
+        msg: str,
+        title: str = "Message",
+        boxtype: MessageBoxType = MessageBoxType.MESSAGEBOX,
+        buttons: MessageBoxButtonsEnum | int = MessageBoxButtonsEnum.BUTTONS_OK,
+    ) -> MessageBoxResultsEnum:
+        """
+        Simple message box.
+
+        Args:
+            msg (str): the message for display.
+            title (str, optional): the title of the message box. Defaults to "Message".
+            boxtype (MessageBoxType, optional): determines the type of message box to display. Defaults to ``Type.MESSAGEBOX``.
+            buttons (MessageBoxButtonsEnum, int, optional): determines what buttons to display. Defaults to ``Buttons.BUTTONS_OK``.
+
+        Returns:
+            Results: MessageBoxResultsEnum.
+
+            * Button press ``Abort`` return ``MessageBoxResultsEnum.CANCEL``
+            * Button press ``Cancel`` return ``MessageBoxResultsEnum.CANCEL``
+            * Button press ``Ignore`` returns ``MessageBoxResultsEnum.IGNORE``
+            * Button press ``No`` returns ``MessageBoxResultsEnum.NO``
+            * Button press ``OK`` returns ``MessageBoxResultsEnum.OK``
+            * Button press ``Retry`` returns ``MessageBoxResultsEnum.RETRY``
+            * Button press ``Yes`` returns ``MessageBoxResultsEnum.YES``
+        """
+        # sourcery skip: remove-unnecessary-cast
+        if boxtype == MessageBoxType.INFOBOX:
+            # this is the default behavior anyways. So assigning ok to make it official here
+            _buttons = MessageBoxButtonsEnum.BUTTONS_OK.value
+        else:
+            _buttons = buttons
+
+        tk = mLo.Lo.create_instance_mcf(XToolkit2, "com.sun.star.awt.Toolkit", raise_err=True)
+        parent = tk.getDesktopWindow()
+        box = tk.createMessageBox(parent, boxtype, int(_buttons), str(title), str(msg))  # type: ignore
+        return MessageBoxResultsEnum(int(box.execute()))
---
+++ packages/ooo-dev-tools/ooo_dev_tools-0.25.2-py3-none-any.whl/ooodev/utils/partial/prop_partial.py
@@ -0,0 +1,64 @@
+from __future__ import annotations
+from typing import Any, overload
+from ooodev.utils.inst.lo.lo_inst import LoInst
+from ooodev.utils import props as mProps
+from ooodev.utils import gen_util as gUtil
+from ooodev.utils.context.lo_context import LoContext
+
+
+class PropPartial:
+    def __init__(self, component: Any, lo_inst: LoInst):
+        self.__lo_inst = lo_inst  # may be used in future
+        self.__component = component
+
+    @overload
+    def get_property(self, name: str) -> Any:
+        """
+        Get property value
+
+        Args:
+            name (str): Property Name.
+
+        Returns:
+            Any: Property value or default.
+        """
+        ...
+
+    @overload
+    def get_property(self, name: str, default: Any) -> Any:
+        """
+        Get property value
+
+        Args:
+            name (str): Property Name.
+            default (Any, optional): Return value if property value is ``None``.
+
+        Returns:
+            Any: Property value or default.
+        """
+        ...
+
+    def get_property(self, name: str, default: Any = gUtil.NULL_OBJ) -> Any:
+        """
+        Get property value
+
+        Args:
+            name (str): Property Name.
+            default (Any, optional): Return value if property value is ``None``.
+
+        Returns:
+            Any: Property value or default.
+        """
+        with LoContext(self.__lo_inst):
+            result = mProps.Props.get(obj=self.__component, name=name, default=default)
+        return result
+
+    def set_property(self, **kwargs: Any) -> None:
+        """
+        Set property value
+
+        Args:
+            **kwargs: Variable length Key value pairs used to set properties.
+        """
+        with LoContext(self.__lo_inst):
+            mProps.Props.set(self.__component, **kwargs)
