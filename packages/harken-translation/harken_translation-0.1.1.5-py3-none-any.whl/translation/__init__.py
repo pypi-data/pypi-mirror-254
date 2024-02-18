33a72c93399db9b78e4e61c54159b3de43f328f3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 00:06:43 +0000

    Add package harken-translation/0.1.1.5/harken_translation-0.1.1.5-py3-none-any.whl

---
+++ packages/harken-translation/harken_translation-0.1.1.5-py3-none-any.whl/translation/__init__.py
@@ -0,0 +1,10 @@
+import sys
+
+from funcy import lcat
+
+from .engines import *
+from .languages import *
+from .engines_private import *
+
+modules = ("engines","engines_private", "languages",)
+__all__ = lcat(sys.modules["translation." + m].__all__ for m in modules)
