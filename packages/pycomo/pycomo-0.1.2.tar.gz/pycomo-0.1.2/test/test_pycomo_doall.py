c448517864d4a960e6b29b414bd43de0dd4e73d2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 13:04:06 +0000

    Add package pycomo/0.1.2/pycomo-0.1.2.tar.gz

---
+++ packages/pycomo/pycomo-0.1.2.tar.gz/pycomo-0.1.2/test/test_pycomo_doall.py
@@ -0,0 +1,15 @@
+from src import pycomo as pycomo
+import cobra
+
+community_output = "test/data/gut_community.xml"
+flux_output = "test/data/gut_flux.csv"
+toy_folder = "data/toy/gut"
+
+
+def test_doall():
+    community_model = pycomo.doall(toy_folder).model
+    # Compare the output with the reference
+    ref_model = cobra.io.read_sbml_model(community_output)
+    assert len(community_model.metabolites) == len(ref_model.metabolites)
+    assert len(community_model.reactions) == len(ref_model.reactions)
+    assert len(community_model.groups) == len(ref_model.groups)
