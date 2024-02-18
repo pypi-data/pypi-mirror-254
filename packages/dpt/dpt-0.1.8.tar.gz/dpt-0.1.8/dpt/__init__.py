8a626d53b032a62eb01db9b059d08b7cbd9cf675
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 02:11:00 +0000

    Add package dpt/0.1.8/dpt-0.1.8.tar.gz

---
+++ packages/dpt/dpt-0.1.8.tar.gz/dpt-0.1.8/dpt/__init__.py
@@ -0,0 +1,13 @@
+# from .classes.processor_old import Processor
+# from .classes.processor_old import ProcessorParams
+# from .classes.processor_old import ProcessorParam
+# from .classes.processor_old import Secret
+# from .classes.storage import DbConnection
+# from .classes.storage import Database
+# from .classes.storage import DbReader
+# from .classes.storage import DbWriter
+from .processor import Project
+from .processor import Task
+from .processor import Module
+from .processor import Processor
+
---
+++ packages/dpt/dpt-0.1.8.tar.gz/dpt-0.1.8/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name="dpt",
+    version="0.1.8",
+    author="DF",
+    author_email="your_email@example.com",
+    packages=["dpt"],
+    install_requires=[],
+)
