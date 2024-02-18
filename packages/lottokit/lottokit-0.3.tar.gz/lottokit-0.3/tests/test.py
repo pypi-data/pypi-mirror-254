0992f9c251e2a22f51ba3d64a481f3a8a94d3355
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 19:06:09 +0000

    Add package lottokit/0.3/lottokit-0.3.tar.gz

---
+++ packages/lottokit/lottokit-0.3.tar.gz/lottokit-0.3/tests/test.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+@Author  : zhengdongqi
+@Email   : nickdecodes@163.com
+@Usage   :
+@FileName: test.py
+@DateTime: 2024/1/28 20:13
+@SoftWare: 
+"""
+
+from lottokit.daletou import Daletou
+
+if __name__ == '__main__':
+    t = Daletou()
+    t.download_data()
