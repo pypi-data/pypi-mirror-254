411f4c17f63127a71928b5c6c3b934f5a1cbd813
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 16:04:41 +0000

    Add package aiotankerkoenig/0.3.0/aiotankerkoenig-0.3.0-py3-none-any.whl

---
+++ packages/aiotankerkoenig/aiotankerkoenig-0.3.0-py3-none-any.whl/aiotankerkoenig/exceptions.py
@@ -0,0 +1,18 @@
+"""Exceptions for the aiotankerkoenig client."""
+from __future__ import annotations
+
+
+class TankerkoenigError(Exception):
+    """Base exception class for all exceptions raised by this library."""
+
+
+class TankerkoenigConnectionError(TankerkoenigError):
+    """Raised when a connection error occurs."""
+
+
+class TankerkoenigConnectionTimeoutError(TankerkoenigConnectionError):
+    """Raised when a connection times out."""
+
+
+class TankerkoenigInvalidKeyError(TankerkoenigError):
+    """Raised when the API key is invalid."""
