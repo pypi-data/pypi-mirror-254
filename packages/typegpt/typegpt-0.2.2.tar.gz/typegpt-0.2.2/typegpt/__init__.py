6c9b070490813df3bea64b13d99b91cdc6220d89
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 08:05:55 +0000

    Add package typegpt/0.2.2/typegpt-0.2.2.tar.gz

---
+++ packages/typegpt/typegpt-0.2.2.tar.gz/typegpt-0.2.2/typegpt/__init__.py
@@ -0,0 +1,3 @@
+from .base import BaseLLMArrayElement, BaseLLMResponse
+from .fields import LLMArrayElementOutput, LLMArrayOutput, LLMOutput
+from .prompt_definition.prompt_template import PromptTemplate
