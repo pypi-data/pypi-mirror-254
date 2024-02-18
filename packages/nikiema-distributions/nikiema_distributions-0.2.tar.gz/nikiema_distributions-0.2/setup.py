51dc0ac58861539259cb01874fa4581946c1a569
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 16:04:57 +0000

    Add package nikiema-distributions/0.2/nikiema_distributions-0.2.tar.gz

---
+++ packages/nikiema-distributions/nikiema_distributions-0.2.tar.gz/nikiema_distributions-0.2/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='nikiema_distributions',
+      version='0.2',
+      description='Gaussian and Binomial distributions',
+      packages=['distributions'],
+      author = 'NIKIEMA Joshua',
+      author_email = 'joshuanikiema24@gmail.com',
+      zip_safe=False)
