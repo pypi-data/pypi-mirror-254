32b93d5b997deeebdb1edfd3872e64a12804e321
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 13:04:04 +0000

    Add package invenio-workflows-tugraz/0.4.0/invenio_workflows_tugraz-0.4.0-py2.py3-none-any.whl

---
+++ packages/invenio-workflows-tugraz/invenio_workflows_tugraz-0.4.0-py2.py3-none-any.whl/invenio_workflows_tugraz/config.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2022-2023 Graz University of Technology.
+#
+# invenio-workflows-tugraz is free software; you can redistribute it and/or
+# modify it under the terms of the MIT License; see LICENSE file for more
+# details.
+
+"""Configuration file."""
+
+from .openaccess import pure_import_func, pure_sieve_func
+from .theses import (
+    duplicate_func,
+    import_func,
+    theses_create_aggregator,
+    theses_filter_for_open_records,
+    update_func,
+)
+
+WORKFLOW_ALMA_REPOSITORY_RECORDS_UPDATE_AGGREGATORS = []
+""""""
+
+WORKFLOW_ALMA_REPOSITORY_RECORDS_UPDATE_FUNC = update_func
+""""""
+
+WORKFLOW_ALMA_ALMA_RECORDS_CREATE_AGGREGATORS = [
+    theses_create_aggregator,
+]
+""""""
+
+WORKFLOW_CAMPUSONLINE_THESES_FILTERS = [
+    theses_filter_for_open_records(),
+]
+""""""
+
+WORKFLOW_CAMPUSONLINE_IMPORT_FUNC = import_func
+
+WORKFLOW_CAMPUSONLINE_DUPLICATE_FUNC = duplicate_func
+
+
+WORKFLOW_PURE_IMPORT_FUNC = pure_import_func
+"""See corresponding varaible in invenio-pure."""
+
+WORKFLOW_PURE_SIEVE_FUNC = pure_sieve_func
---
+++ packages/invenio-workflows-tugraz/invenio_workflows_tugraz-0.4.0-py2.py3-none-any.whl/invenio_workflows_tugraz/theses/__init__.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2022-2023 Graz University of Technology.
+#
+# invenio-workflows-tugraz is free software; you can redistribute it and/or
+# modify it under the terms of the MIT License; see LICENSE file for more
+# details.
+
+"""Theses Workflows."""
+
+from .theses import (
+    duplicate_func,
+    import_func,
+    theses_create_aggregator,
+    theses_filter_for_open_records,
+    update_func,
+)
+from .views import create_blueprint
+
+__all__ = (
+    "theses_create_aggregator",
+    "theses_filter_for_open_records",
+    "duplicate_func",
+    "import_func",
+    "update_func",
+    "create_blueprint",
+)
