108648f2e4c328d73d9d44878010b291120a5f35
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 13:04:12 +0000

    Add package easy-explain/0.4.1/easy_explain-0.4.1.tar.gz

---
+++ packages/easy-explain/easy_explain-0.4.1.tar.gz/easy_explain-0.4.1/setup.py
@@ -0,0 +1,43 @@
+from distutils.core import setup
+
+long_description = open("README.md", "r").read()
+
+setup(
+    name="easy_explain",
+    packages=["easy_explain"],
+    version="0.4.1",
+    license="MIT",
+    description="A library that helps to explain AI models in a really quick & easy way",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author="Stavros Theocharis",
+    author_email="stavrostheocharis@yahoo.gr",
+    url="https://github.com/stavrostheocharis/stavrostheocharis",
+    download_url="https://github.com/stavrostheocharis/easy_explain/archive/refs/tags/v0.4.1.tar.gz",
+    keywords=[
+        "explainable ai",
+        "xai",
+        "easy explain",
+    ],  # Keywords that define your package best
+    install_requires=[
+        "backports.weakref",
+        "captum",
+        "matplotlib",
+        "opencv-python",
+        "Pillow",
+        "scikit-learn",
+        "scipy",
+        "torch",
+        "torchvision",
+        "ultralytics",
+    ],
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Build Tools",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+    ],
+)
