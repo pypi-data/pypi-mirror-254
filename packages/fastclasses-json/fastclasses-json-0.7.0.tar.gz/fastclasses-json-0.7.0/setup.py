90a1aac7cf545ad3c45d2768c412894b690b5a65
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 23:05:14 +0000

    Add package fastclasses-json/0.7.0/fastclasses-json-0.7.0.tar.gz

---
+++ packages/fastclasses-json/fastclasses-json-0.7.0.tar.gz/fastclasses-json-0.7.0/setup.py
@@ -0,0 +1,48 @@
+from setuptools import setup, find_packages
+
+# Updated by a script
+VERSION = "0.7.0"
+
+with open('README.md', 'r', encoding='utf-8') as f:
+    readme = f.read()
+
+setup(
+    name='fastclasses-json',
+    version=VERSION,
+    packages=find_packages(exclude=('tests*',)),
+    package_data={"fastclasses_json": ["py.typed"]},
+    author='Daniel Golding',
+    description='Quickly serialize dataclasses to and from JSON',
+    long_description=readme,
+    long_description_content_type='text/markdown',
+    url='https://github.com/cakemanny/fastclasses-json',
+    project_urls={
+        'Bug Tracker': 'https://github.com/cakemanny/fastclasses-json/issues'
+    },
+    license='MIT',
+    keywords='fast dataclasses json fastclasses',
+    python_requires='>=3.7',
+    install_requires=[
+        "typing_extensions;python_version=='3.7'"
+    ],
+    extras_require={
+        'dev': [
+            'pytest',
+            'flake8',
+            'mypy',
+        ]
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Intended Audience :: Developers",
+        "Development Status :: 3 - Alpha",
+    ],
+)
