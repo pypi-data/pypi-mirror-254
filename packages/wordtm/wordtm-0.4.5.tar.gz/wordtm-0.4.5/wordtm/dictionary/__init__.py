a801f5b65357de10ee8ecb46d830fb0e5c6bbfce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 23:08:27 +0000

    Add package wordtm/0.4.5/wordtm-0.4.5.tar.gz

---
+++ packages/wordtm/wordtm-0.4.5.tar.gz/wordtm-0.4.5/wordtm/dictionary/__init__.py
@@ -0,0 +1,15 @@
+# __init__.py
+#
+# Folder 'dictionary'
+#
+# Copyright (c) 2022-2024 WordTM Project 
+# Author: Johnny Cheng <drjohnnycheng@gmail.com>
+#
+# Updated: 2 February 2024
+#
+# URL: https://github.com/drjohnnycheng/wordtm.git
+# For license information, see LICENSE.TXT
+
+"""The folder stores the dictionaries of Chinese (Traditional) stopwords
+and Chinese terms for Jieba word segmentation.
+"""
---
+++ packages/wordtm/wordtm-0.4.5.tar.gz/wordtm-0.4.5/wordtm/version.py
@@ -0,0 +1,21 @@
+# version.py
+#    
+# Version information of the module WordTM
+#
+# Copyright (c) 2022-2024 - Johnny Cheng <drjohnnycheng@gmail.com>
+#
+# Updated: 4 February 2024
+#
+# URL: https://github.com/drjohnnycheng/wordtm.git
+# For license information, see LICENSE.TXT
+
+#: Version information: '__author__', '__copyright__', '__credits__',
+#: '_license__', '__version__', '__email__', '__status__', '__url__'
+__author__ = "Johnny Cheng"
+__copyright__ = "Copyright (c) 2022-2024 - Johnny Cheng <drjohnnycheng@gmail.com>"
+__credits__ = ["Jehovah, the Lord"]
+__license__ = ""
+__version__ = "0.4.5"
+__email__ = "drjohnnycheng@gmail.com"
+__status__ = "Development"
+__url__ = "https://github.com/drjohnnycheng/wordtm.git"
