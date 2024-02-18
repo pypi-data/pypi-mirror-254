4bcc73b717ff0445449df26fa63901f2d0c59718
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 12:05:08 +0000

    Add package api-pipe/1.0.2/api_pipe-1.0.2-py3-none-any.whl

---
+++ packages/api-pipe/api_pipe-1.0.2-py3-none-any.whl/api_pipe/manual_test_run.py
@@ -0,0 +1,72 @@
+'''
+    Test API
+'''
+import os
+import logging
+
+from api_pipe.api import Api
+from pathlib import Path
+
+DUMMY_TEST_GROUP_ID = 79866152
+
+def main():
+    '''
+        Main
+    '''
+    api = Api(
+        url="https://gitlab.com/api/v4",
+        headers={
+            "PRIVATE-TOKEN": os.environ["GL_TOKEN"]
+        },
+        timeout=(5.0, 5.0),
+        logs={
+            "level": logging.DEBUG,
+            "pipe_steps" : {
+                "log_dir": Path("../api_pipe_logs"),
+                "convert_to_json": {
+                    "indent": 2
+                }
+            }
+        }
+    )
+
+    api.url = api.url / "groups" / DUMMY_TEST_GROUP_ID / "members"
+
+    api                     \
+        .fetch()            \
+        .python_object()    \
+        .json(indent=2)
+
+    print(api.data)
+    # api = Api(
+    #     url="https://gitlab.com/api/v4",
+    #     headers={
+    #         "PRIVATE-TOKEN": os.environ["GL_TOKEN"]
+    #     },
+    #     logs={
+    #         "level": logging.DEBUG,
+    #         "pipe_steps" : {
+    #             "log_dir": Path("../api_pipe_logs"),
+    #             "convert_to_json": {
+    #                 "indent": 2
+    #             }
+    #         }
+    #     }
+    # )
+
+    # api.url = api.url / "groups" / DUMMY_TEST_GROUP_ID / "variables"
+
+    # api                     \
+    #     .fetch()            \
+    #     .python_object()    \
+    #     .select([
+    #         "key",
+    #         "value",
+    #         "masked",
+    #     ])                  \
+    #     .filter(
+    #         lambda item: item["key"] == "Var2"
+    #     )                   \
+    #     .json(indent=2)
+
+    # print(api.data)
