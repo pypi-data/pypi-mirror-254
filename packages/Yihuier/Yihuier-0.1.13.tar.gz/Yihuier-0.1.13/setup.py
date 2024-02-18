71cfe603eb9dd3f4ab0c501de87655b5cc82b41b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 03:03:40 +0000

    Add package Yihuier/0.1.13/Yihuier-0.1.13.tar.gz

---
+++ packages/Yihuier/Yihuier-0.1.13.tar.gz/Yihuier-0.1.13/setup.py
@@ -0,0 +1,52 @@
+from setuptools import setup
+from setuptools import find_packages
+
+VERSION = '0.1.13'
+
+with open("README.md", "r", encoding='utf-8') as fh:
+    long_description = fh.read()
+
+setup(
+    name='Yihuier',  # package name
+    version=VERSION,  # package version
+    author='encyc',
+    author_email='atomyuangao@gmail.com',
+    description='Package for Logistic Regression Modeling, focus on Credit Risk Management',  # package description
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://github.com/encyc/yihuier',
+    include_package_data=True,
+    packages=find_packages(),
+    zip_safe=False,
+    python_requires='>=3.6',
+
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Programming Language :: Python :: 3.10",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Topic :: Software Development :: Build Tools",
+        "Intended Audience :: Developers"
+    ],
+    install_requires=[
+        'pandas',
+        'numpy',
+        'matplotlib',
+        'seaborn',
+        'scikit-learn',
+        'ydata_profiling==4.6.3',
+    ],
+
+    # data_files=[
+    #     ('', ['conf/*.conf']),
+    #     ('/usr/lib/systemd/system/', ['bin/*.service']),
+    # ],
+    # exclude_package_data={
+    #     'bandwidth_reporter': ['*.txt']
+    # },
+    package_data={
+        'Data': ['*.csv']
+    }
+)
+# python setup.py sdist bdist_wheel
+# twine upload --skip-existing dist/*
