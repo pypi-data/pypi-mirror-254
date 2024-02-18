08d1d32121588cc1650e2966b600de726d2870cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 20:04:59 +0000

    Add package tatara/0.1.6/tatara-0.1.6-py3-none-any.whl

---
+++ packages/tatara/tatara-0.1.6-py3-none-any.whl/tatara/evals/__init__.py
@@ -0,0 +1,5 @@
+from .eval import Eval
+from .recorder import PrintRecorder, FileRecorder
+from .tatara_recorder import TataraRecorder
+
+__all__ = ["Eval", "TataraRecorder", "PrintRecorder", "FileRecorder"]
