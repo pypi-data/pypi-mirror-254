fc245728cefe03b3268c1e0c3669b3b414c6fab7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 13:05:38 +0000

    Add package subprocess-mock/0.1.0/subprocess_mock-0.1.0-py3-none-any.whl

---
+++ packages/subprocess-mock/subprocess_mock-0.1.0-py3-none-any.whl/subprocess_mock/__init__.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+
+"""
+
+subprocess_mock
+
+Base module
+
+
+Copyright (C) 2024 Rainer Schwarzbach
+
+This file is part of subprocess-mock.
+
+subprocess-mock is free software: you can redistribute it and/or modify
+it under the terms of the MIT License.
+
+subprocess-mock is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+See the LICENSE file for more details.
+
+"""
+
+
+from .core import Popen, ModuleFunctions  # noqa: F401
+
+
+__version__ = "0.1.0"
+
+
+# vim: fileencoding=utf-8 ts=4 sts=4 sw=4 autoindent expandtab syntax=python:
