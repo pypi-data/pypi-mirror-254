8dda1b6792acfa86bd970c8acb4e3e182303a2d5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 08:03:51 +0000

    Add package zenity-py/1.0.7/zenity_py-1.0.7.tar.gz

---
+++ packages/zenity-py/zenity_py-1.0.7.tar.gz/zenity_py-1.0.7/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+from pathlib import Path
+
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
+setup(
+    name='zenity_py',
+    long_description_content_type='text/markdown',
+    long_description=long_description,
+    keywords="zenity",
+    version='1.0.7',
+    packages=['zenity'],
+    url="https://github.com/Marko2155/zenity_py",
+    license='MIT',
+    author='Marko Camandioti',
+    author_email='camandiotimarko@gmail.com',
+    description="A Python library for the CLI tool zenity.",
+    project_urls={
+        'Documentation': 'https://github.com/Marko2155/zenity_py#readme',
+        'Source': 'https://github.com/Marko2155/zenity_py',
+        'Tracker': 'https://github.com/Marko2155/zenity_py/issues',
+    }
+)
