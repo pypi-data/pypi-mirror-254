98bcc82b802297beb51173b5cab5fb266e694cd5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 05:06:02 +0000

    Add package nbktools/0.0.10/nbktools-0.0.10.tar.gz

---
+++ packages/nbktools/nbktools-0.0.10.tar.gz/nbktools-0.0.10/setup.py
@@ -0,0 +1,31 @@
+from setuptools import find_packages, setup
+
+with open("app/README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+
+setup(
+    name="nbktools",
+    version="0.0.10",
+    description="TBC ",
+    package_dir={"": "app"},
+    packages=find_packages(where="app"),
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/bouz1/Plot_AS_String",
+    author="Ab BO",
+    author_email="bouziani.abdelaziz@gmail.com",
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.10",
+        "Operating System :: OS Independent",
+    ],
+    
+
+    #install_requires=[],
+    extras_require={
+        "dev": ['os'],
+
+    },
+    python_requires=">=3.1",
+)
