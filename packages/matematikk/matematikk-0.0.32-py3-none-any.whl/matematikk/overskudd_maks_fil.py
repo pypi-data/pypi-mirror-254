d053e8f02a38e0e7ae7456102a4be9c3434c498d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 04:08:01 +0000

    Add package matematikk/0.0.32/matematikk-0.0.32-py3-none-any.whl

---
+++ packages/matematikk/matematikk-0.0.32-py3-none-any.whl/matematikk/overskudd_maks_fil.py
@@ -0,0 +1,16 @@
+# 🚀 programmering.no | 🤓 matematikk.as
+
+from matematikk import ekstremalpunkt_max, Symbol
+
+def overskudd_maks(variabel = Symbol(""),
+                  uttrykk  = Symbol(""),
+                  rund     = None,
+                  debug    = -1):
+
+    # overskudd_max() er en undergruppe av ekstremalpunkt_max()
+    variabel_max = ekstremalpunkt_max(variabel = variabel,
+                                      uttrykk  = uttrykk,
+                                      rund     = rund,
+                                      debug    = debug)
+
+    return variabel_max
