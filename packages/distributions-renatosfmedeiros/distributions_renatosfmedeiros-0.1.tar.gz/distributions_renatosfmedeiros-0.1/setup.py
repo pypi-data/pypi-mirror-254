07a78dc12d7ae2c2eb7d3629181250382a1fe22c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 08:03:47 +0000

    Add package distributions-renatosfmedeiros/0.1/distributions_renatosfmedeiros-0.1.tar.gz

---
+++ packages/distributions-renatosfmedeiros/distributions_renatosfmedeiros-0.1.tar.gz/distributions_renatosfmedeiros-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='distributions_renatosfmedeiros',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['distributions_renatosfmedeiros'],
+      zip_safe=False)
