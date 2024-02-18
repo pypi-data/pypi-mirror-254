047857b88f7d2daa93227721f0d72c2a98ac0dce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 19:04:58 +0000

    Add package resize-img/0.5/resize_img-0.5.tar.gz

---
+++ packages/resize-img/resize_img-0.5.tar.gz/resize_img-0.5/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+with open('requirements.txt') as requirements_file:
+    requirements = requirements_file.read().splitlines()
+
+with open('README.md', 'r') as read_me_file:
+    description = read_me_file.read()
+
+setup(
+    name='resize_img',
+    version='0.5',
+    packages=find_packages(),
+    install_requires=[
+        requirements
+    ],
+    entry_points = {
+        'console_scripts': [
+            'resize-img = img_resizer.main:main'
+        ]
+    },
+    long_description = description,
+    long_description_content_type = 'text/markdown'
+)
