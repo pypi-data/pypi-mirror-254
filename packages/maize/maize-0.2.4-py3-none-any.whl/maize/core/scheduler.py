1fc6752b4b42817f2caa941094cff427bfd0b79a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 18:04:41 +0000

    Add package maize/0.2.4/maize-0.2.4-py3-none-any.whl

---
+++ packages/maize/maize-0.2.4-py3-none-any.whl/maize/core/scheduler.py
@@ -0,0 +1,27 @@
+import typing
+
+from maize.utils.priority_queue import SpiderPriorityQueue
+
+
+if typing.TYPE_CHECKING:
+    from maize import Request
+
+
+class Scheduler:
+    def __init__(self):
+        self.request_queue: typing.Optional[SpiderPriorityQueue] = None
+
+    def open(self):
+        self.request_queue = SpiderPriorityQueue()
+
+    async def next_request(self):
+        return await self.request_queue.get()
+
+    async def enqueue_request(self, request: "Request"):
+        await self.request_queue.put(request)
+
+    def idle(self) -> bool:
+        return len(self) == 0
+
+    def __len__(self):
+        return self.request_queue.qsize()
