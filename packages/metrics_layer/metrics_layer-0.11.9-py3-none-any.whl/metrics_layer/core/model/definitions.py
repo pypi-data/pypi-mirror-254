4aa4e830c7dca00410a268cbed1ebd15718dee11
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 16:04:45 +0000

    Add package metrics_layer/0.11.9/metrics_layer-0.11.9-py3-none-any.whl

---
+++ packages/metrics_layer/metrics_layer-0.11.9-py3-none-any.whl/metrics_layer/core/model/definitions.py
@@ -0,0 +1,14 @@
+class Definitions:
+    snowflake = "SNOWFLAKE"
+    bigquery = "BIGQUERY"
+    redshift = "REDSHIFT"
+    postgres = "POSTGRES"
+    druid = "DRUID"
+    sql_server = "SQL_SERVER"
+    duck_db = "DUCK_DB"
+    databricks = "DATABRICKS"
+    supported_warehouses = [snowflake, bigquery, redshift, postgres, druid, sql_server, duck_db, databricks]
+    supported_warehouses_text = ", ".join(supported_warehouses)
+
+    does_not_exist = "__DOES_NOT_EXIST__"
+    canon_date_join_graph_root = "canon_date_core"
