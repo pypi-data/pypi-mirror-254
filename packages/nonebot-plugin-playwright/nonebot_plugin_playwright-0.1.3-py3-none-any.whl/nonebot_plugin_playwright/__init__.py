8707be8bfd7580dc84f5a3dee293ea24b3485092
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 04:06:53 +0000

    Add package nonebot-plugin-playwright/0.1.3/nonebot_plugin_playwright-0.1.3-py3-none-any.whl

---
+++ packages/nonebot-plugin-playwright/nonebot_plugin_playwright-0.1.3-py3-none-any.whl/nonebot_plugin_playwright/__init__.py
@@ -0,0 +1,28 @@
+from __future__ import annotations
+
+from nonebot.plugin import PluginMetadata
+
+from .browser import get_playwright as get_playwright, get_browser as get_browser
+from .tool import get_new_page as get_new_page, GetNewPage as GetNewPage, NewPage as NewPage
+from .config import Config
+
+__plugin_meta__ = PluginMetadata(
+    name="Playwright",
+    description="NoneBot Playwright插件",
+    usage=(
+        '声明依赖: `require("nonebot_plugin_playwright")\n'
+        "使用: `from nonebot_plugin_playwright import ...`"
+    ),
+    type="library",
+    homepage="https://github.com/eya46/nonebot_plugin_playwright",
+    config=Config,
+    supported_adapters=None,
+)
+
+__all__ = [
+    "get_playwright",
+    "get_browser",
+    "get_new_page",
+    "GetNewPage",
+    "NewPage",
+]
