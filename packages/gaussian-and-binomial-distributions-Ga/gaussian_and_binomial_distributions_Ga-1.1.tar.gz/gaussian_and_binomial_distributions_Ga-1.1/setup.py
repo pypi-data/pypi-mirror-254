62694db51c498ac1af2cc52717e84888d7fdd4c0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 08:04:32 +0000

    Add package gaussian-and-binomial-distributions-Ga/1.1/gaussian_and_binomial_distributions_Ga-1.1.tar.gz

---
+++ packages/gaussian-and-binomial-distributions-Ga/gaussian_and_binomial_distributions_Ga-1.1.tar.gz/gaussian_and_binomial_distributions_Ga-1.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='gaussian_and_binomial_distributions_Ga',
+      version='1.1',
+      description='Gaussian and Binomial distributions',
+      packages=['gaussian_and_binomial_distributions_Ga'],
+      zip_safe=False)
