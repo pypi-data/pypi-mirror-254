c94749c4f194c9ab603ac2f43792cb1ee71c226a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 15:07:15 +0000

    Add package sklearn-keras/1.1/sklearn_keras-1.1.tar.gz

---
+++ packages/sklearn-keras/sklearn_keras-1.1.tar.gz/sklearn_keras-1.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='sklearn_keras',
+    version='1.1',
+    packages=find_packages(),
+    install_requires=[],
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+)
+
