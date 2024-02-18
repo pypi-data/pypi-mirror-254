97ea69e2b4ffafa1e353ac0af800be162d1661d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 16:04:22 +0000

    Add package aimsim/2.1.3/aimsim-2.1.3.tar.gz

---
+++ packages/aimsim/aimsim-2.1.3.tar.gz/aimsim-2.1.3/aimsim/__init__.py
@@ -0,0 +1,6 @@
+from . import ops
+from . import tasks
+from . import chemical_datastructures
+from . import utils
+
+__version__ = "2.1.3"
---
+++ packages/aimsim/aimsim-2.1.3.tar.gz/aimsim-2.1.3/setup.py
@@ -0,0 +1,47 @@
+import os.path
+import codecs
+import pathlib
+from setuptools import setup, find_packages
+
+cwd = pathlib.Path(__file__).parent
+
+README = (cwd / "README.md").read_text()
+
+desc = "Python command line and GUI tool to analyze molecular similarity."
+
+
+def read(rel_path):
+    here = os.path.abspath(os.path.dirname(__file__))
+    with codecs.open(os.path.join(here, rel_path), "r") as fp:
+        return fp.read()
+
+
+def get_version(rel_path):
+    for line in read(rel_path).splitlines():
+        if line.startswith("__version__"):
+            delim = '"' if '"' in line else "'"
+            return line.split(delim)[1]
+    else:
+        raise RuntimeError("Unable to find version string.")
+
+
+setup(
+    name="aimsim",
+    python_requires=">=3.8",
+    version=get_version("aimsim/__init__.py"),
+    description=desc,
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url="https://github.com/VlachosGroup/AIMSim",
+    author="Himaghna Bhattacharjee, Jackson Burns",
+    license="MIT",
+    classifiers=["Programming Language :: Python :: 3"],
+    install_requires=read("requirements.txt").split("\n"),
+    packages=find_packages(exclude=["docs", "test"]),
+    include_package_data=True,
+    entry_points={
+        "console_scripts": [
+            "aimsim=aimsim.__main__:start_AIMSim",
+        ]
+    },
+)
