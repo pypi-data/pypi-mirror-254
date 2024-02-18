b20d57062c8b0592f74418cfb8b7c644c2b7e8f4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 16:06:21 +0000

    Add package doctext/0.1.6/doctext-0.1.6.tar.gz

---
+++ packages/doctext/doctext-0.1.6.tar.gz/doctext-0.1.6/setup.py
@@ -0,0 +1,15 @@
+# your_library_name/setup.py
+from setuptools import setup, find_packages
+
+setup(
+    name='doctext',
+    version='0.1.6',
+    packages=find_packages(),
+    install_requires=['tika', 'Pillow', 'pytesseract', 'termcolor', 'python-magic', 'openai', 'chardet', 'langdetect', 'iso639-lang'],
+    license='MIT',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/luckfamousa/doctext',
+    author='Felix Nensa',
+    author_email='felix.nensa@gmail.com'
+)
