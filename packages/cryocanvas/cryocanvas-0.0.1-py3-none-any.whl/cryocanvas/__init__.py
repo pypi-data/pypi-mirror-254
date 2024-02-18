4434244a7cee54b89454cd91c8c12a462a617620
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 11:04:46 +0000

    Add package cryocanvas/0.0.1/cryocanvas-0.0.1-py3-none-any.whl

---
+++ packages/cryocanvas/cryocanvas-0.0.1-py3-none-any.whl/cryocanvas/__init__.py
@@ -0,0 +1,7 @@
+__version__ = "0.0.1"
+from .app import CryoCanvasWidget, CryoCanvasApp
+
+__all__ = (
+    "CryoCanvasWidget",
+    "CryoCanvasApp",
+)
---
+++ packages/cryocanvas/cryocanvas-0.0.1-py3-none-any.whl/cryocanvas/_tests/test_widget.py
@@ -0,0 +1,22 @@
+import numpy as np
+
+from cryocanvas.app import (
+    CryoCanvasWidget,
+)
+
+
+# capsys is a pytest fixture that captures stdout and stderr output streams
+def test_cryocanvas_widget(make_napari_viewer, capsys):
+    # make viewer and add an image layer using our fixture
+    viewer = make_napari_viewer()
+    viewer.add_image(np.random.random((100, 100)))
+
+    # create our widget, passing in the viewer
+    my_widget = CryoCanvasWidget(viewer)
+
+    # call our widget method
+    my_widget._on_click()
+
+    # read captured output and check that it's as we expected
+    captured = capsys.readouterr()
+    assert captured.out == "napari has 1 layers\n"
