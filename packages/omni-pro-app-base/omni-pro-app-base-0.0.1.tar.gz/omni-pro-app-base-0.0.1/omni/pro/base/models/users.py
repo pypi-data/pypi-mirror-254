24e4a12cb89cb84aa8867a771175cdd9ddfc4a73
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 07:05:10 +0000

    Add package omni-pro-app-base/0.0.1/omni-pro-app-base-0.0.1.tar.gz

---
+++ packages/omni-pro-app-base/omni-pro-app-base-0.0.1.tar.gz/omni-pro-app-base-0.0.1/omni/pro/base/models/users.py
@@ -0,0 +1,19 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+from django.utils.translation import gettext_lazy as _
+
+from .base_model import OmniModel
+from .managers import UserManager
+
+
+class User(AbstractUser, OmniModel):
+    email = models.EmailField(_("email address"), unique=True)
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = ["username", "first_name", "last_name"]
+
+    @property
+    def name(self):
+        return self.get_full_name()
+
+    def __str__(self):
+        return self.username
