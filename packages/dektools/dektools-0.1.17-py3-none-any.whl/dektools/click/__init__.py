ccfa86e7e9bf2ae5b6793dbe28b04340523f4581
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 12:05:30 +0000

    Add package dektools/0.1.17/dektools-0.1.17-py3-none-any.whl

---
+++ packages/dektools/dektools-0.1.17-py3-none-any.whl/dektools/click/__init__.py
@@ -0,0 +1,10 @@
+import typer
+from . import hash as hash_command
+from . import file as file_command
+from . import net as net_command
+
+app = typer.Typer(add_completion=False)
+
+app.add_typer(hash_command.app, name='hash')
+app.add_typer(file_command.app, name='file')
+app.add_typer(net_command.app, name='net')
---
+++ packages/dektools/dektools-0.1.17-py3-none-any.whl/dektools/download.py
@@ -0,0 +1,36 @@
+import os
+from urllib.parse import urlparse
+from .file import sure_dir, remove_path
+from .shell import shell_wrapper
+
+
+def download_from_http(url, path_dest, username=None, password=None):
+    sure_dir(os.path.dirname(path_dest))
+    remove_path(path_dest)
+    auth = ''
+    if username:
+        auth += f' -u {username}'
+        if password:
+            auth += f':{password}'
+    shell_wrapper(f'curl -fLSs {url} -o {path_dest}{auth}')
+
+
+def download_from_git(url, path_dest):
+    sure_dir(path_dest)
+    ul = url.split('+', 1)
+    b = []
+    if len(ul) > 1:
+        b = ['-b', ul[1]]
+    sub = ' '.join([*b, ul[0]])
+    shell_wrapper(f'git clone  {sub} .')
+
+
+def download_tree_from_http(path_dest, url_list, username=None, password=None):
+    result = {}
+    for url in url_list:
+        pr = urlparse(url)
+        path = pr.path[1:]
+        result[url] = path
+        filepath = os.path.join(path_dest, path)
+        download_from_http(url, filepath, username, password)
+    return result
