115030d851a0d5cb76f50208f2d8856dcc824039
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 23:07:46 +0000

    Add package api-pipe/1.0.45/api_pipe-1.0.45-py3-none-any.whl

---
+++ packages/api-pipe/api_pipe-1.0.45-py3-none-any.whl/api_pipe/logger.py
@@ -0,0 +1,41 @@
+'''
+    logger.py
+'''
+from pathlib import Path
+import logging
+
+from api_pipe import config
+from rich.logging import RichHandler
+
+
+def stdout(unique_name: str, log_dir: Path, log_level: int, log_words_to_highlight: list) -> logging.Logger:
+    '''
+        Sets up a logger that logs to stdout
+
+        Uses RichHandler to pretty print logs
+    '''
+    #stdout handler
+    stdout_handler = RichHandler(
+        show_time=config.logger_show_time,
+        keywords=[w.lower() for w in log_words_to_highlight] + \
+                 [w.upper() for w in log_words_to_highlight] + \
+                 [w.capitalize() for w in log_words_to_highlight]
+    )
+    formatter = logging.Formatter(config.logger_formatter)
+    stdout_handler.setFormatter(formatter)
+
+    #file handler
+    file_handler = logging.FileHandler(log_dir / f"stdout.log")
+    file_handler.setFormatter(logging.Formatter(
+        config.logger_formatter
+    ))
+
+    #config logger
+    logger = logging.getLogger(unique_name)
+    logger.setLevel(log_level)
+
+    #add hand;ers
+    logger.addHandler(stdout_handler)
+    logger.addHandler(file_handler)
+
+    return logger
