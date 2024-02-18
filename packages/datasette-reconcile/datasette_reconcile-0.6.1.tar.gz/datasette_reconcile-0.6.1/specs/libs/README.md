709ef5770a539ba0d572bf6cc8e3cc2378a58282
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 13:05:12 +0000

    Add package datasette-reconcile/0.6.1/datasette_reconcile-0.6.1.tar.gz

---
+++ packages/datasette-reconcile/datasette_reconcile-0.6.1.tar.gz/datasette_reconcile-0.6.1/specs/libs/README.md
@@ -0,0 +1,10 @@
+
+This folder contains a custom version of ReSpec, which supports
+citing by DOIs whose metadata are retrieved via Crossref.
+
+The fork can be found here:
+https://github.com/wetneb/respec
+
+If similar functionality is implemented upstream, we should be able
+to use the official version again.
+
---
+++ packages/datasette-reconcile/datasette_reconcile-0.6.1.tar.gz/datasette_reconcile-0.6.1/specs/test.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+set -e
+
+# Test JSON examples against schemas
+for version in "0.1" "0.2" "draft"; do
+	echo "Testing version: $version"
+
+	for schema_file in `ls $version/schemas`; do
+		echo "Testing $schema_file"
+		schema_name=$(echo "$schema_file" | cut -f 1 -d '.')
+		ajv test -s "$version/schemas/$schema_file" -r "$version/schemas/*.json" -d "$version/examples/$schema_name/valid/*.json" --valid
+		ajv test -s "$version/schemas/$schema_file" -r "$version/schemas/*.json" -d "$version/examples/$schema_name/invalid/*.json" --invalid
+		echo ""
+	done
+done
+
+if [ $? -eq 0 ]
+then
+  echo -e "All tests \033[0;32mPASSED\033[0m\n"
+fi
