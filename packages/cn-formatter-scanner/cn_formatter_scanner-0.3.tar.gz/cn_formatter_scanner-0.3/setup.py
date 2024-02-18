e730ae67d5e560b1b5cdf9b036fe7dfeee3172a5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 09:03:58 +0000

    Add package cn-formatter-scanner/0.3/cn_formatter_scanner-0.3.tar.gz

---
+++ packages/cn-formatter-scanner/cn_formatter_scanner-0.3.tar.gz/cn_formatter_scanner-0.3/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='cn_formatter_scanner',
+    version='0.3',
+    packages=find_packages(),
+    install_requires=[
+        'pytest>=7.4.4'
+    ],
+    author='M Usman Tahir',
+    author_email='usman@cuddlynest.com',
+    description='This Python package provides a title formatter for cuddlynest properties',
+    long_description='This Python package provides a title formatter for cuddlynest properties as per the requirment'
+
+)
