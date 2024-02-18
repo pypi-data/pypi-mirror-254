b62bb572bc59670be4d235f6cbcdca09a0c02327
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 20:04:58 +0000

    Add package dbpipe/0.1.0/dbpipe-0.1.0.tar.gz

---
+++ packages/dbpipe/dbpipe-0.1.0.tar.gz/dbpipe-0.1.0/dbpipe/__init__.py
@@ -0,0 +1,24 @@
+__version__ = "0.1.0"
+
+from dbpipe.core.pipes import (
+    Pipe,
+    Job,
+    Schedule
+)
+
+
+from dbpipe.io.jsonreader import(
+    read_job,
+    read_pipe
+
+)
+
+
+__all__ = [
+    Pipe,
+    Job,
+    Schedule,
+    read_pipe,
+    read_pipe
+    
+]
