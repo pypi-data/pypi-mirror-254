c820c243cd4c81cee9f230e592b5a7d67067cdad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 04:07:34 +0000

    Add package Exercise-library/0.2.0/Exercise_library-0.2.0.tar.gz

---
+++ packages/Exercise-library/Exercise_library-0.2.0.tar.gz/Exercise_library-0.2.0/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='Exercise_library',
+    version='0.2.0',
+    author='Mikolaj Kalejta',
+    author_email='kalejtamikolaj@wp.pl',
+    packages=find_packages(),
+    include_package_data=True,
+    description='Very usefull library'
+)
