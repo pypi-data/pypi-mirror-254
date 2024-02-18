2dd470ae9f5798231b80f5ea20da724592f33c32
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 18:04:01 +0000

    Add package softwaretoolbc/0.1/softwaretoolbc-0.1.tar.gz

---
+++ packages/softwaretoolbc/softwaretoolbc-0.1.tar.gz/softwaretoolbc-0.1/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='softwaretoolbc',
+    version='0.1',
+    packages=find_packages(),
+    description='Library written by Dennis Guo for Stefano',
+    author='Dennis Guo',
+    author_email='guohq@bc.edu',
+    license='MIT',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+    ],
+)
