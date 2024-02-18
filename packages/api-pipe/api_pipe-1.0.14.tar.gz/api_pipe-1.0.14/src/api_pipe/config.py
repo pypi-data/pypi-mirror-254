269de6d14300b7ade05a919edf6387d4561763d3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 16:04:11 +0000

    Add package api-pipe/1.0.14/api_pipe-1.0.14.tar.gz

---
+++ packages/api-pipe/api_pipe-1.0.14.tar.gz/api_pipe-1.0.14/src/api_pipe/config.py
@@ -0,0 +1,37 @@
+'''
+    Config
+
+    This file is used to configure the module
+'''
+import logging
+
+#logger
+logger_level = logging.ERROR
+logger_words_to_highlight = [
+    "fetching",
+    "filtering",
+    "converting",
+    "importing",
+    "exporting",
+    "validating",
+    "deleting",
+    "parsing",
+    "getting",
+    "writing",
+    "reading",
+    "found",
+    "making",
+    "removing",
+    "selecting",
+    "multiplexing",
+    "emptying",
+    "creating",
+    "initializing",
+    "logging",
+    "checking",
+    "requesting",
+    "imported",
+    "exported"
+]
+logger_show_time = False
+logger_formatter = "%(message)s"
