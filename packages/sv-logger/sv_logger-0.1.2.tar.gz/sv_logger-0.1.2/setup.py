270c95e12764469233a1707f05eb9227f410cf5b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 05:05:24 +0000

    Add package sv-logger/0.1.2/sv_logger-0.1.2.tar.gz

---
+++ packages/sv-logger/sv_logger-0.1.2.tar.gz/sv_logger-0.1.2/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="sv_logger",
+    version="0.1.2",
+    packages=find_packages(),
+    license="MIT",
+    description="A simple logging library for Python",
+    long_description=open("README.md").read(),
+    author="pternali",
+    author_email="patrizio.ternali15@gmail.com",
+    url="",
+    requires=[]
+)
