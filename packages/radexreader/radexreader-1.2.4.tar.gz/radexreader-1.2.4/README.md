02edcfa8406b1b6191bca565ae64ad955cd2e104
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 19:05:42 +0000

    Add package radexreader/1.2.4/radexreader-1.2.4.tar.gz

---
+++ packages/radexreader/radexreader-1.2.4.tar.gz/radexreader-1.2.4/README.md
@@ -0,0 +1,91 @@
+# RadexReader
+
+The RadexReader is an user-space driver for the [RADEX RD1212 v1/v2](https://quartarad.com/product-category/radiation-detector/) and the [RADEX ONE](https://quartarad.com/product-category/radiation-detector/) Geiger counters. It allow to read and clear stored data via USB. Warning! This tool is completely unrelated with QuartaRad.
+
+This tool was initially developed with RD1212 v2.48.
+
+![RADEX RD1212](images/RD1212.jpg?raw=true)
+![RADEX ONE](images/ONE.jpg?raw=true)
+
+## Screenshots and Usage
+
+[![Screnshot 1](images/thumbs/read.png?raw=true)](images/read.png?raw=true)
+[![Screnshot 2](images/thumbs/compare.png?raw=true)](images/compare.png?raw=true)
+
+* Read `src/radexreader.py` for examples.
+* Run the command `radexreader` available with DEB/RPM packages.
+* Run the command `.../radexreader.py` available with PYPI package.
+
+## Installation
+
+It require *libusb*, *pyusb* and *pyserial*.
+
+#### Installation for Debian and Ubuntu
+
+* `sudo apt install python3-radexreader radexreader`
+
+#### Installation for Fedora
+
+* `sudo dnf install python3-radexreader`
+
+#### Installation for openSUSE
+
+* `sudo zypper install python3-radexreader`
+
+#### Installation with PIP
+
+* With Linux: `sudo python3 -m pip install radexreader` (+libusb)
+* With Mac: `sudo pip install radexreader` (+libusb)
+* With Windows: `python -m pip install radexreader` (+[libusb](https://libusb.info/), put libusb-1.0.dll in system32)
+
+#### Installation with Docker
+
+* `sudo docker run --rm --user root -it --privileged -v /dev:/dev python:3.x-alpine /bin/sh` then: `apk update ; apk add libusb ; python3 -m pip install radexreader`
+
+#### Repository for Debian and Ubuntu
+
+```bash
+# PPA: https://launchpad.net/~luigifab/+archive/ubuntu/packages
+# with Debian 12+ you can use mantic+ instead of focal (https://unix.stackexchange.com/a/669008/364800)
+sudo add-apt-repository ppa:luigifab/packages
+sudo apt update
+sudo apt install python3-radexreader radexreader
+# or
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys FFE5BD439356DF7D
+echo "deb http://ppa.launchpad.net/luigifab/packages/ubuntu focal main" | sudo tee -a /etc/apt/sources.list
+sudo apt update
+sudo apt install python3-radexreader radexreader
+# or
+sudo wget -O /etc/apt/trusted.gpg.d/luigifab.gpg https://www.luigifab.fr/apt.gpg
+echo "deb http://ppa.launchpad.net/luigifab/packages/ubuntu focal main" | sudo tee -a /etc/apt/sources.list
+sudo apt update
+sudo apt install python3-radexreader radexreader
+```
+```bash
+# sha256sum /etc/apt/trusted.gpg.d/luigifab.gpg
+578c89a677048e38007462d543686b53587efba9f93814601169253c45ff9213
+# apt-key list
+/etc/apt/trusted.gpg.d/luigifab.gpg
+pub   rsa4096 2020-10-31 [SC]
+      458B 0C46 D024 FD8C B8BC  99CD FFE5 BD43 9356 DF7D
+```
+
+## Copyright
+
+- Current version: 1.2.4 (02/02/2024)
+- Compatibility: Python 3.3 / 3.4 / 3.5 / 3.6 / 3.7 / 3.8 / 3.9 / 3.10 / 3.11 / 3.12
+- Links: [luigifab.fr](https://www.luigifab.fr/python/radexreader) - [github.com](https://github.com/luigifab/python-radexreader) - [PyPI](https://pypi.org/project/radexreader/)\
+[Arch Linux python-radexreader.zst](https://aur.archlinux.org/packages/python-radexreader)\
+[Debian python-radexreader.deb](https://packages.debian.org/python3-radexreader)
+  *([ITP](https://bugs.debian.org/cgi-bin/bugreport.cgi?bug=973447),
+   [RFS](https://bugs.debian.org/cgi-bin/bugreport.cgi?bug=974217))*\
+[Fedora python-radexreader.rpm](https://src.fedoraproject.org/rpms/python-radexreader)\
+[openSUSE python-radexreader.rpm](https://software.opensuse.org/package/python-radexreader)\
+[Ubuntu PPA](https://launchpad.net/~luigifab/+archive/ubuntu/packages)
+
+This program is provided under the terms of the **GNU GPLv2+** license.\
+If you like, take some of your time to improve some translations, go to https://bit.ly/2HyCCEc.
+
+## Packages in official distros repositories
+
+[![Packages status](https://repology.org/badge/vertical-allrepos/python:radexreader.svg?header=radexreader)](https://repology.org/project/python:radexreader/versions)
---
+++ packages/radexreader/radexreader-1.2.4.tar.gz/radexreader-1.2.4/radexreader/radexreader.py
@@ -0,0 +1,157 @@
+#!/usr/bin/python3
+# -*- coding: utf8 -*-
+# Created L/19/10/2020
+# Updated D/08/10/2023
+#
+# Copyright 2020-2024 | Fabrice Creuzot (luigifab) <code~luigifab~fr>
+# https://github.com/luigifab/python-radexreader
+#
+# This program is free software, you can redistribute it or modify
+# it under the terms of the GNU General Public License (GPL) as published
+# by the free software foundation, either version 2 of the license, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but without any warranty, without even the implied warranty of
+# merchantability or fitness for a particular purpose. See the
+# GNU General Public License (GPL) for more details.
+
+import datetime
+from platform import python_version
+import sys
+import time
+import usb
+import serial
+
+try:
+	datetime.UTC
+except AttributeError:
+	datetime.UTC = None
+
+# https://stackoverflow.com/a/62804772/2980105
+# prevent auto import for "import radexreader" when filename is "radexreader.py"
+sys.path = sys.path[1:]
+
+# python3 .../src/radexreader.py when radexreader is installed or not
+if ".py" in sys.argv[0]:
+	import os
+	sys.path.insert(0, os.path.abspath(sys.argv[0]).replace('radexreader.py', ''))
+	import radexreader
+	msg = 'Information   radexreader ' + radexreader.__version__
+	msg += ' with python ' + python_version() + ' + pyusb ' + usb.__version__ + ' + pyserial ' + serial.__version__
+# standard command radexreader
+else:
+	import radexreader
+	msg = 'Information   python3-radexreader-pypi ' + radexreader.__version__
+	msg += ' with python ' + python_version() + ' + pyusb ' + usb.__version__ + ' + pyserial ' + serial.__version__
+
+if len(sys.argv) > 1:
+
+	if sys.argv[1] == 'erase':
+		print(msg)
+		reader = radexreader.RadexReader()
+		reader.print_info()
+		reader.erase()
+		print('done')
+		exit(0)
+
+	if sys.argv[1] == 'tail':
+		print(msg)
+		reader = radexreader.RadexReader()
+		reader.print_info()
+		prev = None
+		while True:
+			measures = reader.read(True)
+			for timestamp, measure in measures.items():
+				if timestamp != prev:
+					print('%s  %s µSv/h  ±%s%% (%s ≤ %s ≤ %s)' % (
+						str(datetime.datetime.fromtimestamp(timestamp, datetime.UTC)),
+						str('{:.2f}'.format(measure['val'])).rjust(6, ' '),
+						str(int(measure['pct'])),
+						str('{:.2f}'.format(measure['min'])),
+						str('{:.2f}'.format(measure['val'])),
+						str('{:.2f}'.format(measure['max']))
+					))
+					if reader.com == 'ONEv1':
+						print('                     %s µSv accumulated / %s CPM  [±%s%%]' % (
+							str('{:.2f}'.format(measure['acc'])).rjust(6, ' '),
+							str('{:.0f}'.format(measure['cpm'])).rjust(6, ' '),
+							str(int(measure['pct']))
+						))
+				prev = timestamp
+			time.sleep(10)
+		exit(0)
+
+	if sys.argv[1] == 'jsontail' or sys.argv[1] == 'tailjson':
+		import json
+		reader = radexreader.RadexReader()
+		prev   = None
+		while True:
+			measures = reader.read(True)
+			for timestamp, measure in measures.items():
+				if timestamp != prev:
+					print(json.dumps({timestamp: measure}))
+				prev = timestamp
+			time.sleep(10)
+		exit(0)
+
+	if sys.argv[1] == 'last' or sys.argv[1] == 'readlast':
+		print(msg)
+		reader = radexreader.RadexReader()
+		reader.print_info()
+		measures = reader.read(True)
+		for timestamp, measure in measures.items():
+			print('%s  %s µSv/h  ±%s%% (%s ≤ %s ≤ %s)' % (
+				str(datetime.datetime.fromtimestamp(timestamp, datetime.UTC)),
+				str('{:.2f}'.format(measure['val'])).rjust(6, ' '),
+				str(int(measure['pct'])),
+				str('{:.2f}'.format(measure['min'])),
+				str('{:.2f}'.format(measure['val'])),
+				str('{:.2f}'.format(measure['max']))
+			))
+			if reader.com == 'ONEv1':
+				print('                     %s µSv accumulated / %s CPM  [±%s%%]' % (
+					str('{:.2f}'.format(measure['acc'])).rjust(6, ' '),
+					str('{:.0f}'.format(measure['cpm'])).rjust(6, ' '),
+					str(int(measure['pct']))
+				))
+		if not measures:
+			print('no data stored')
+		exit(0)
+
+	if sys.argv[1] == 'jsonlast':
+		import json
+		print(json.dumps(radexreader.RadexReader().read(True)))
+		exit(0)
+
+	if sys.argv[1] == 'all' or sys.argv[1] == 'readall':
+		print(msg)
+		reader = radexreader.RadexReader()
+		reader.print_info()
+		measures = reader.read(False)
+		for timestamp, measure in measures.items():
+			print('%s  %s µSv/h  ±%s%% (%s ≤ %s ≤ %s)' % (
+				str(datetime.datetime.fromtimestamp(timestamp, datetime.UTC)),
+				str('{:.2f}'.format(measure['val'])).rjust(6, ' '),
+				str(int(measure['pct'])),
+				str('{:.2f}'.format(measure['min'])),
+				str('{:.2f}'.format(measure['val'])),
+				str('{:.2f}'.format(measure['max']))
+			))
+			if reader.com == 'ONEv1':
+				print('                     %s µSv accumulated / %s CPM  [±%s%%]' % (
+					str('{:.2f}'.format(measure['acc'])).rjust(6, ' '),
+					str('{:.0f}'.format(measure['cpm'])).rjust(6, ' '),
+					str(int(measure['pct']))
+				))
+		if not measures:
+			print('no data stored')
+		exit(0)
+
+	if sys.argv[1] == 'jsonall':
+		import json
+		print(json.dumps(radexreader.RadexReader().read(False)))
+		exit(0)
+
+print('Usage: radexreader.py erase|tail|jsontail|last|jsonlast|all|jsonall')
+exit(-1)
