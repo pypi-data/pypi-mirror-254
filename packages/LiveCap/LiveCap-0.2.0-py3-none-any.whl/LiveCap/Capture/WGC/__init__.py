238e1a1ccdd248b6fcda63f3aabff5c3d1cadb9f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 13:05:13 +0000

    Add package LiveCap/0.2.0/LiveCap-0.2.0-py3-none-any.whl

---
+++ packages/LiveCap/LiveCap-0.2.0-py3-none-any.whl/LiveCap/Capture/WGC/__init__.py
@@ -0,0 +1,1 @@
+from .screenshot import WindowsGraphicsCapture, get_hwnd
---
+++ packages/LiveCap/LiveCap-0.2.0-py3-none-any.whl/LiveCap/Capture/__init__.py
@@ -0,0 +1,3 @@
+from .DXGI import DXGICapture
+from .WGC import get_hwnd, WindowsGraphicsCapture
+from .GDI import GDICapure
