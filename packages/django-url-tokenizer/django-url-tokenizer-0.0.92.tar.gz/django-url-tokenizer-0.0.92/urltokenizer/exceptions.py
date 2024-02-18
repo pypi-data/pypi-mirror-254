e8321242215f0663d4714ddf1909b275eafdcb78
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 16:04:53 +0000

    Add package django-url-tokenizer/0.0.92/django-url-tokenizer-0.0.92.tar.gz

---
+++ packages/django-url-tokenizer/django-url-tokenizer-0.0.92.tar.gz/django-url-tokenizer-0.0.92/urltokenizer/exceptions.py
@@ -0,0 +1,31 @@
+from enum import Enum, unique
+
+from django.utils.translation import gettext_lazy as _
+
+
+@unique
+class ErrorCodes(Enum):
+    invalid_token_type = _("There is no token type with name '{token_type}'")
+    invalid_method = _("User model has no method '{method_name}'")
+    send_precondition_execution_error = _(
+        "Error during send precondition '{pred}' execution"
+    )
+    check_precondition_execution_error = _(
+        "Error during check precondition '{pred}' execution"
+    )
+    callback_execution_error = _("Error during callback '{callback}' execution")
+
+
+class URLTokenizerError(Exception):
+    def __init__(self, message, code, context=None, *args, **kwargs):
+        self.message = message.format(*args, **kwargs)
+        self.code = code
+        self.context = context or {}
+
+    def __repr__(self):
+        return "URLTokenizerError({}, code={}, context={})".format(
+            self.message, self.code, self.context
+        )
+
+    def __str__(self):
+        return f"{self.message} (code={self.code}, context={self.context})"
