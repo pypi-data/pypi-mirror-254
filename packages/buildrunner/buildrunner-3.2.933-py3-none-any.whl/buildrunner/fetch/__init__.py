5b7d0a7c72dacf99498495a66ef80127b7aac394
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 01:27:20 +0000

    Add package buildrunner/3.2.933/buildrunner-3.2.933-py3-none-any.whl

---
+++ packages/buildrunner/buildrunner-3.2.933-py3-none-any.whl/buildrunner/fetch/__init__.py
@@ -0,0 +1,49 @@
+"""
+Copyright 2021 Adobe
+All Rights Reserved.
+
+NOTICE: Adobe permits you to use, modify, and distribute this file in accordance
+with the terms of the Adobe license agreement accompanying it.
+"""
+
+import urllib.parse
+
+from . import github
+from . import http
+from . import file
+
+
+def fetch_file(url, config):
+    """
+    Fetch a file from a provider.
+    """
+
+    # pylint: disable=fixme
+    # FIXME: the handled checking should be in each handler module (possibly handle_file(parsed_url,
+    # config) => bool)
+
+    parsed_url = urllib.parse.urlparse(url)
+
+    if parsed_url.scheme == "github":
+        file_contents = github.fetch_file(parsed_url, config)
+
+    elif parsed_url.scheme == "file" or (
+        parsed_url.scheme == "" and parsed_url.netloc == ""
+    ):
+        purl = list(parsed_url)
+        purl[0] = "file"
+        parsed_url = urllib.parse.ParseResult(*purl)
+        file_contents = file.fetch_file(parsed_url, config)
+
+    elif parsed_url.scheme in ("http", "https"):
+        file_contents = http.fetch_file(parsed_url, config)
+
+    else:
+        raise NotImplementedError(f"Unknown fetch backend: {parsed_url.scheme}")
+
+    return file_contents
+
+
+# Local Variables:
+# fill-column: 100
+# End:
---
+++ packages/buildrunner/buildrunner-3.2.933-py3-none-any.whl/buildrunner/fetch/file.py
@@ -0,0 +1,19 @@
+"""
+Copyright 2021 Adobe
+All Rights Reserved.
+
+NOTICE: Adobe permits you to use, modify, and distribute this file in accordance
+with the terms of the Adobe license agreement accompanying it.
+"""
+
+import codecs
+
+
+def fetch_file(parsed_url, config):  # pylint: disable=unused-argument
+    """
+    Pull files from the local file system.
+    """
+    with codecs.open(parsed_url.path, "r", encoding="utf-8") as _file:
+        contents = "".join(_file.readlines())
+
+    return contents
