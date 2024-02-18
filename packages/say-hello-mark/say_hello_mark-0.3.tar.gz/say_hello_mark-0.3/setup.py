e6e1f4b172d817f10bc32daa945debc9f5d31c81
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 03:04:02 +0000

    Add package say-hello-mark/0.3/say_hello_mark-0.3.tar.gz

---
+++ packages/say-hello-mark/say_hello_mark-0.3.tar.gz/say_hello_mark-0.3/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+  name="say_hello_mark",
+  version="0.3",
+  packages=find_packages(),
+  install_requires=[
+
+  ],
+  entry_points={
+    "console_scripts": [
+      "say-hello-mark = say_hello_mark:hello" 
+    ]
+  }
+)
