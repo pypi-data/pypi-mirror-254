bb7b4d8ca9d2b2045af450e02b958ac98e4a3e25
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 15:06:28 +0000

    Add package distcode-ex/1.0/distcode_ex-1.0.tar.gz

---
+++ packages/distcode-ex/distcode_ex-1.0.tar.gz/distcode_ex-1.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name="distcode_ex", 
+ version="1.0",
+ description="Gaussian distributions", 
+ packages=['distcode_ex'],
+ author='Opeyemi Oladipupo',
+ author_email='techrystein@gmail.com',
+ zip_safe=False)
