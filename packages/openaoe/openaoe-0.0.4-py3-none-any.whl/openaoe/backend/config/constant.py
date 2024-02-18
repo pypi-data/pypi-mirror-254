6ddb728a8c04080ca6bdec37d23ca46683d12479
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 13:04:34 +0000

    Add package openaoe/0.0.4/openaoe-0.0.4-py3-none-any.whl

---
+++ packages/openaoe/openaoe-0.0.4-py3-none-any.whl/openaoe/backend/config/constant.py
@@ -0,0 +1,16 @@
+TYPE_BOT = "bot"
+TYPE_USER = "user"
+TYPE_SYSTEM = "system"
+
+DATE_PATTERN = "%Y%m%d"
+TIMEOUT_SECONDS = 30
+
+PROVIDER_OPENAI = "openai"
+PROVIDER_MINIMAX = "minimax"
+PROVIDER_GOOGLE = "google"
+PROVIDER_XUNFEI = "spark"
+PROVIDER_CLAUDE = "claude"
+PROVIDER_INTERNLM = "internlm"
+
+DEFAULT_TIMEOUT_SECONDS = 600
+
