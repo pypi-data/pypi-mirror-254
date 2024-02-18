5208e1878578128c535e24d306037f9a1ddd98e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 20:05:55 +0000

    Add package coloringg/0.0.1/coloringg-0.0.1.tar.gz

---
+++ packages/coloringg/coloringg-0.0.1.tar.gz/coloringg-0.0.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="coloringg",
+    version="0.0.1",
+    packages=['colored'],
+    install_requires = [
+        "requests==2.31.0"
+    ],
+    author="coloringg",
+    description="coloringg"
+)
