54406e1fe249444d651d9181d6ffefe43631a527
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 04:08:14 +0000

    Add package ngbd-probability-distributions/0.1/ngbd_probability_distributions-0.1.tar.gz

---
+++ packages/ngbd-probability-distributions/ngbd_probability_distributions-0.1.tar.gz/ngbd_probability_distributions-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='ngbd_probability_distributions',
+      version='0.1',
+      description='Normal, Gaussian, Binomial distributions',
+      packages=['ngbd_probability_distributions'],
+      zip_safe=False)
