f0e33890d06e38f6e4a8f62b1df111bdb3c83d6c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 11:04:33 +0000

    Add package paddle-billing-python-sdk/0.0.1a90/paddle_billing_python_sdk-0.0.1a90-py3-none-any.whl

---
+++ packages/paddle-billing-python-sdk/paddle_billing_python_sdk-0.0.1a90-py3-none-any.whl/paddle_billing/Resources/Customers/Operations/UpdateCustomer.py
@@ -0,0 +1,17 @@
+from dataclasses import asdict, dataclass
+
+from paddle_billing.Undefined       import Undefined
+from paddle_billing.Entities.Shared import CustomData, Status
+
+
+@dataclass
+class UpdateCustomer:
+    email:       str        | None | Undefined = Undefined()
+    name:        str        | None | Undefined = Undefined()
+    custom_data: CustomData | None | Undefined = Undefined()
+    locale:      str        | None | Undefined = Undefined()
+    status:      Status     | None | Undefined = Undefined()
+
+
+    def get_parameters(self) -> dict:
+        return asdict(self)
