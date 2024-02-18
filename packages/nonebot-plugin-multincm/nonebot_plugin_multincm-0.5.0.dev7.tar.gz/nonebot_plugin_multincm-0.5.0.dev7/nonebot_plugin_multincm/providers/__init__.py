31c58eb0313a263470aa1a0632d33b87e60edf7e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 19:05:51 +0000

    Add package nonebot-plugin-multincm/0.5.0.dev7/nonebot_plugin_multincm-0.5.0.dev7.tar.gz

---
+++ packages/nonebot-plugin-multincm/nonebot_plugin_multincm-0.5.0.dev7.tar.gz/nonebot_plugin_multincm-0.5.0.dev7/nonebot_plugin_multincm/providers/__init__.py
@@ -0,0 +1,11 @@
+from .base import BasePlaylist as BasePlaylist
+from .base import BaseSearcher as BaseSearcher
+from .base import BaseSong as BaseSong
+from .base import playlists as playlists
+from .base import searchers as searchers
+from .base import songs as songs
+from .playlist import Playlist as Playlist
+from .song import Song as Song
+from .song import SongSearcher as SongSearcher
+from .voice import Voice as Voice
+from .voice import VoiceSearcher as VoiceSearcher
