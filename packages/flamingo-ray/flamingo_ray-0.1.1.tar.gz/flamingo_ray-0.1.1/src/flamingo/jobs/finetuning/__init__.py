6c60731496541c8fdf987c93acdcab20ceb4b7fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 15:05:57 +0000

    Add package flamingo-ray/0.1.1/flamingo_ray-0.1.1.tar.gz

---
+++ packages/flamingo-ray/flamingo_ray-0.1.1.tar.gz/flamingo_ray-0.1.1/src/flamingo/jobs/finetuning/__init__.py
@@ -0,0 +1,4 @@
+from flamingo.jobs.finetuning.config import FinetuningJobConfig, FinetuningRayConfig
+from flamingo.jobs.finetuning.entrypoint import run_finetuning
+
+__all__ = ["FinetuningJobConfig", "FinetuningRayConfig", "run_finetuning"]
