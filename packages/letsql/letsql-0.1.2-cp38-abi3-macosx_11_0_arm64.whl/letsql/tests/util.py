7d90317c1884202f1e27ff04db16e4fb50d9dc6d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 04:06:23 +0000

    Add package letsql/0.1.2/letsql-0.1.2-cp38-abi3-macosx_11_0_arm64.whl

---
+++ packages/letsql/letsql-0.1.2-cp38-abi3-macosx_11_0_arm64.whl/letsql/tests/util.py
@@ -0,0 +1,24 @@
+from typing import Any
+
+import pandas as pd
+import pandas.testing as tm
+
+
+reduction_tolerance = 1e-7
+
+
+def assert_series_equal(
+    left: pd.Series, right: pd.Series, *args: Any, **kwargs: Any
+) -> None:
+    kwargs.setdefault("check_dtype", True)
+    kwargs.setdefault("check_names", False)
+    tm.assert_series_equal(left, right, *args, **kwargs)
+
+
+def assert_frame_equal(
+    left: pd.DataFrame, right: pd.DataFrame, *args: Any, **kwargs: Any
+) -> None:
+    left = left.reset_index(drop=True)
+    right = right.reset_index(drop=True)
+    kwargs.setdefault("check_dtype", True)
+    tm.assert_frame_equal(left, right, *args, **kwargs)
