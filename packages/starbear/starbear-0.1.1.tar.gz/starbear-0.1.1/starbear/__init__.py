5b605dd1f67ff25da98521aec0db7cb88a9ea2b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 16:05:44 +0000

    Add package starbear/0.1.1/starbear-0.1.1.tar.gz

---
+++ packages/starbear/starbear-0.1.1.tar.gz/starbear-0.1.1/starbear/__init__.py
@@ -0,0 +1,12 @@
+from hrepr import H
+
+from .ref import Reference
+from .serve import (
+    ConfigurableBear,
+    ConfigurableSimpleBear,
+    bear,
+    dev_injections,
+    simplebear,
+)
+from .templating import template
+from .utils import ClientWrap, FeedbackQueue, Queue, VirtualFile
