ca1a336935b4f39bd9c52cd127cfaf5cd38ff88f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 02:10:58 +0000

    Add package ascript/0.0.3.28/ascript-0.0.3.28.tar.gz

---
+++ packages/ascript/ascript-0.0.3.28.tar.gz/ascript-0.0.3.28/aslib/android/action/gesture.py
@@ -0,0 +1,12 @@
+from aslib.action.path import path
+from typing import List
+from aslib.action.path import path
+
+
+def perform(*paths: List[path]) -> None:
+    """
+    执行手势操作
+
+    :param paths: 需要执行的路径对象列表
+    """
+    pass
