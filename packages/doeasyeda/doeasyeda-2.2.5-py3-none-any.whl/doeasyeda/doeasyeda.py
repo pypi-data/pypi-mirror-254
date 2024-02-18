72a66a1e5b7abc6d5e3431eafa08bf4ca9b3407e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 16:04:56 +0000

    Add package doeasyeda/2.2.5/doeasyeda-2.2.5-py3-none-any.whl

---
+++ packages/doeasyeda/doeasyeda-2.2.5-py3-none-any.whl/doeasyeda/doeasyeda.py
@@ -0,0 +1,11 @@
+from .create_area_plot import create_area_plot
+from .create_hist_plot import create_hist_plot
+from .create_line_plot import create_line_plot
+from .create_scatter_plot import create_scatter_plot
+
+__all__ = [
+    'create_area_plot',
+    'create_hist_plot',
+    'create_line_plot',
+    'create_scatter_plot',
+]
