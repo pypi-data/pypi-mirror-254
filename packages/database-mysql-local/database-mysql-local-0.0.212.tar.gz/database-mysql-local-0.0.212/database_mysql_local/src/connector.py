360c14318f0aae18174f621d37ef2b176d6df1ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 22:06:48 +0000

    Add package database-mysql-local/0.0.212/database-mysql-local-0.0.212.tar.gz

---
+++ packages/database-mysql-local/database-mysql-local-0.0.212.tar.gz/database-mysql-local-0.0.212/database_mysql_local/src/connector.py
@@ -0,0 +1,127 @@
+import mysql.connector
+from logger_local.LoggerLocal import Logger
+from logger_local.MetaLogger import MetaLogger
+from python_sdk_remote.utilities import get_sql_hostname, get_sql_username, get_sql_password
+
+from .constants import LOGGER_CONNECTOR_CODE_OBJECT
+from .cursor import Cursor
+
+logger = Logger.create_logger(object=LOGGER_CONNECTOR_CODE_OBJECT)
+connections_pool = {}
+
+
+# TODO Can we add hostname, IPv4, IPv6, sql statement? Can we have the same message format in all cases?
+class Connector(metaclass=MetaLogger, object=LOGGER_CONNECTOR_CODE_OBJECT):
+    @staticmethod
+    def connect(schema_name: str) -> 'Connector':
+        if (schema_name in connections_pool and
+                connections_pool[schema_name] and
+                connections_pool[schema_name].connection):
+            if connections_pool[schema_name].connection.is_connected():
+
+                return connections_pool[schema_name]
+            else:
+                # reconnect
+                connections_pool[schema_name].connection.reconnect()
+                # TODO We should develop retry mechanism to support password rotation and small network issues.
+                if connections_pool[schema_name].connection.is_connected():
+
+                    return connections_pool[schema_name]
+                else:
+                    connector = Connector(schema_name)
+                    logger.warning("Reconnect failed, returning a new connection",
+                                   object={'connections_pool': str(connections_pool[schema_name])})
+
+                    return connector
+        else:
+            connector = Connector(schema_name)
+
+            return connector
+
+    def __init__(self, schema_name: str,
+                 host: str = get_sql_hostname(),
+                 user: str = get_sql_username(),
+                 password: str = get_sql_password()) -> None:
+        logger.start("Connector.__init__", object={
+            "schema_name": schema_name, "host": host, "user": user, "password": "***"})
+        self.host = host
+        self.schema = schema_name
+        self.user = user
+        self.password = password
+
+        # Checking host suffix
+        if not (self.host.endswith("circ.zone") or self.host.endswith("circlez.ai")):
+            logger.error(
+                f"Warning: Your RDS_HOSTNAME={self.host} which is not what is expected")
+        self.connection: mysql.connector = None
+        self._cursor = None
+        self._connect_to_db()
+        connections_pool[schema_name] = self
+        logger.end("Connector.__init__", object={"connections_pool": connections_pool.keys()})
+
+    def __repr__(self):
+        return f"Connector(schema_name={self.schema}, host={self.host}, user={self.user}, password=***)"
+
+    def reconnect(self) -> None:
+        self.connection.reconnect()
+        self._cursor = self.connection.cursor()
+        self.set_schema(self.schema)
+
+    def _connect_to_db(self):
+        self.connection = mysql.connector.connect(
+            host=self.host,
+            user=self.user,
+            password=self.password,
+            database=self.schema
+        )
+        self._cursor = self.connection.cursor()
+        self.set_schema(self.schema)
+
+    def database_info(self):
+        return f"host={self.host}, user={self.user}, schema={self.schema}"
+
+    def close(self) -> None:
+
+        try:
+            if self._cursor:
+                self._cursor.close()
+                logger.info("Cursor closed successfully.")
+        except Exception as exception:
+            logger.error(f"connection.py close() {self.database_info()}", object={"exception": exception})
+
+        if self.connection and self.connection.is_connected():
+            self.connection.close()
+            logger.info("Connection closed successfully.")
+
+    def cursor(self, dictionary=False, buffered=False) -> Cursor:
+
+        cursor_instance = Cursor(self.connection.cursor(
+            dictionary=dictionary, buffered=buffered))
+
+        return cursor_instance
+
+    def commit(self) -> None:
+
+        self.connection.commit()
+
+    def set_schema(self, new_schema: str | None) -> None:
+        if not new_schema:
+            return
+        if self.schema == new_schema:
+            return
+        self.schema = new_schema
+        if self.connection and self.connection.is_connected():
+            use_query = f"USE `{new_schema}`;"
+            self._cursor.execute(use_query)
+            logger.info(f"Switched to schema: {new_schema}")
+        else:
+            raise Exception(
+                "Connection is not established. The database will be used on the next connect.")
+
+    def rollback(self):
+
+        self.connection.rollback()
+
+    def start_tranaction(self):
+
+        self.connection.start_transaction()
