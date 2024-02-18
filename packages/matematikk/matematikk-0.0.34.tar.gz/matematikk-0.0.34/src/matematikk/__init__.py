d24ad6e630a99b0ed2909fe28e817aefaa5b7bba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 04:07:04 +0000

    Add package matematikk/0.0.34/matematikk-0.0.34.tar.gz

---
+++ packages/matematikk/matematikk-0.0.34.tar.gz/matematikk-0.0.34/src/matematikk/__init__.py
@@ -0,0 +1,71 @@
+# ekstremalpunkt_max()
+#from .ekstremalpunkt_max_fil import (
+#    ekstremalpunkt_max,
+#    #superløs,
+#    #Symbol,
+#    #diff
+#    )
+#from .ekstremalpunkt_maks_fil import (
+#    ekstremalpunkt_maks,
+#    superløs,
+#    Symbol,
+#    diff
+#    )
+
+### from sympy import diff
+
+
+
+# reggis()
+from .reggis_fil import (
+    reggis,
+    #polyfit,
+    #Symbol
+    )
+from .reggis_cas_fil import (
+    reggis_cas,
+    #polyfit,
+    #Symbol
+    )
+
+# superlos()
+from .superløs_fil import (
+    superløs,
+    Eq,
+    Reals,
+    core,
+    Symbol,
+    ConditionSet,
+    FiniteSet,
+    Intersection,
+    solve,
+    solveset,
+    nsolve,
+    #diff
+    )
+#from .superlos_fil import (
+#    superlos,
+#    Eq,
+#    Reals,
+#    core,
+#    Symbol,
+#    ConditionSet,
+#    FiniteSet,
+#    Intersection,
+#    solve,
+#    solveset,
+#    nsolve
+#    )
+
+# overskudd_max()
+from .overskudd_max_fil import (
+    overskudd_max,
+    #ekstremalpunkt_max,
+    #Symbol
+    )
+from .overskudd_maks_fil import (
+    overskudd_maks,
+    #ekstremalpunkt_max,
+    #Symbol
+    )
+
