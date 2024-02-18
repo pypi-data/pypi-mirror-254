e5c03a9a59004e6410e87e103e8ef786ace4700c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 08:06:02 +0000

    Add package django-debug-toolbar/4.3.0/django_debug_toolbar-4.3.0.tar.gz

---
+++ packages/django-debug-toolbar/django_debug_toolbar-4.3.0.tar.gz/django_debug_toolbar-4.3.0/tests/panels/test_staticfiles.py
@@ -0,0 +1,84 @@
+import os
+import unittest
+
+import django
+from django.conf import settings
+from django.contrib.staticfiles import finders
+from django.test.utils import override_settings
+
+from ..base import BaseTestCase
+
+PATH_DOES_NOT_EXIST = os.path.join(settings.BASE_DIR, "tests", "invalid_static")
+
+
+class StaticFilesPanelTestCase(BaseTestCase):
+    panel_id = "StaticFilesPanel"
+
+    def test_default_case(self):
+        response = self.panel.process_request(self.request)
+        self.panel.generate_stats(self.request, response)
+        content = self.panel.content
+        self.assertIn(
+            "django.contrib.staticfiles.finders.AppDirectoriesFinder", content
+        )
+        self.assertIn(
+            "django.contrib.staticfiles.finders.FileSystemFinder (2 files)", content
+        )
+        self.assertEqual(self.panel.num_used, 0)
+        self.assertNotEqual(self.panel.num_found, 0)
+        expected_apps = ["django.contrib.admin", "debug_toolbar"]
+        if settings.USE_GIS:
+            expected_apps = ["django.contrib.gis"] + expected_apps
+        self.assertEqual(self.panel.get_staticfiles_apps(), expected_apps)
+        self.assertEqual(
+            self.panel.get_staticfiles_dirs(), finders.FileSystemFinder().locations
+        )
+
+    def test_insert_content(self):
+        """
+        Test that the panel only inserts content after generate_stats and
+        not the process_request.
+        """
+        response = self.panel.process_request(self.request)
+        # ensure the panel does not have content yet.
+        self.assertNotIn(
+            "django.contrib.staticfiles.finders.AppDirectoriesFinder",
+            self.panel.content,
+        )
+        self.panel.generate_stats(self.request, response)
+        # ensure the panel renders correctly.
+        content = self.panel.content
+        self.assertIn(
+            "django.contrib.staticfiles.finders.AppDirectoriesFinder", content
+        )
+        self.assertValidHTML(content)
+
+    @unittest.skipIf(django.VERSION >= (4,), "Django>=4 handles missing dirs itself.")
+    @override_settings(
+        STATICFILES_DIRS=[PATH_DOES_NOT_EXIST] + settings.STATICFILES_DIRS,
+        STATIC_ROOT=PATH_DOES_NOT_EXIST,
+    )
+    def test_finder_directory_does_not_exist(self):
+        """Misconfigure the static files settings and verify the toolbar runs.
+
+        The test case is that the STATIC_ROOT is in STATICFILES_DIRS and that
+        the directory of STATIC_ROOT does not exist.
+        """
+        response = self.panel.process_request(self.request)
+        self.panel.generate_stats(self.request, response)
+        content = self.panel.content
+        self.assertIn(
+            "django.contrib.staticfiles.finders.AppDirectoriesFinder", content
+        )
+        self.assertNotIn(
+            "django.contrib.staticfiles.finders.FileSystemFinder (2 files)", content
+        )
+        self.assertEqual(self.panel.num_used, 0)
+        self.assertNotEqual(self.panel.num_found, 0)
+        expected_apps = ["django.contrib.admin", "debug_toolbar"]
+        if settings.USE_GIS:
+            expected_apps = ["django.contrib.gis"] + expected_apps
+        self.assertEqual(self.panel.get_staticfiles_apps(), expected_apps)
+        self.assertEqual(
+            self.panel.get_staticfiles_dirs(), finders.FileSystemFinder().locations
+        )
---
+++ packages/django-debug-toolbar/django_debug_toolbar-4.3.0.tar.gz/django_debug_toolbar-4.3.0/tests/test_checks.py
@@ -0,0 +1,260 @@
+import os
+import unittest
+from unittest.mock import patch
+
+import django
+from django.conf import settings
+from django.core.checks import Warning, run_checks
+from django.test import SimpleTestCase, override_settings
+
+PATH_DOES_NOT_EXIST = os.path.join(settings.BASE_DIR, "tests", "invalid_static")
+
+
+class ChecksTestCase(SimpleTestCase):
+    @override_settings(
+        MIDDLEWARE=[
+            "django.contrib.messages.middleware.MessageMiddleware",
+            "django.contrib.sessions.middleware.SessionMiddleware",
+            "django.contrib.auth.middleware.AuthenticationMiddleware",
+            "django.middleware.gzip.GZipMiddleware",
+            "debug_toolbar.middleware.DebugToolbarMiddleware",
+        ]
+    )
+    def test_check_good_configuration(self):
+        messages = run_checks()
+        self.assertEqual(messages, [])
+
+    @override_settings(
+        MIDDLEWARE=[
+            "django.contrib.messages.middleware.MessageMiddleware",
+            "django.contrib.sessions.middleware.SessionMiddleware",
+            "django.contrib.auth.middleware.AuthenticationMiddleware",
+        ]
+    )
+    def test_check_missing_middleware_error(self):
+        messages = run_checks()
+        self.assertEqual(
+            messages,
+            [
+                Warning(
+                    "debug_toolbar.middleware.DebugToolbarMiddleware is "
+                    "missing from MIDDLEWARE.",
+                    hint="Add debug_toolbar.middleware.DebugToolbarMiddleware "
+                    "to MIDDLEWARE.",
+                    id="debug_toolbar.W001",
+                )
+            ],
+        )
+
+    @override_settings(
+        MIDDLEWARE=[
+            "django.contrib.messages.middleware.MessageMiddleware",
+            "django.contrib.sessions.middleware.SessionMiddleware",
+            "django.contrib.auth.middleware.AuthenticationMiddleware",
+            "debug_toolbar.middleware.DebugToolbarMiddleware",
+            "django.middleware.gzip.GZipMiddleware",
+        ]
+    )
+    def test_check_gzip_middleware_error(self):
+        messages = run_checks()
+        self.assertEqual(
+            messages,
+            [
+                Warning(
+                    "debug_toolbar.middleware.DebugToolbarMiddleware occurs "
+                    "before django.middleware.gzip.GZipMiddleware in "
+                    "MIDDLEWARE.",
+                    hint="Move debug_toolbar.middleware.DebugToolbarMiddleware "
+                    "to after django.middleware.gzip.GZipMiddleware in "
+                    "MIDDLEWARE.",
+                    id="debug_toolbar.W003",
+                )
+            ],
+        )
+
+    @override_settings(
+        MIDDLEWARE_CLASSES=[
+            "django.contrib.messages.middleware.MessageMiddleware",
+            "django.contrib.sessions.middleware.SessionMiddleware",
+            "django.contrib.auth.middleware.AuthenticationMiddleware",
+            "django.middleware.gzip.GZipMiddleware",
+            "debug_toolbar.middleware.DebugToolbarMiddleware",
+        ]
+    )
+    def test_check_middleware_classes_error(self):
+        messages = run_checks()
+        self.assertIn(
+            Warning(
+                "debug_toolbar is incompatible with MIDDLEWARE_CLASSES setting.",
+                hint="Use MIDDLEWARE instead of MIDDLEWARE_CLASSES",
+                id="debug_toolbar.W004",
+            ),
+            messages,
+        )
+
+    @unittest.skipIf(django.VERSION >= (4,), "Django>=4 handles missing dirs itself.")
+    @override_settings(
+        STATICFILES_DIRS=[PATH_DOES_NOT_EXIST],
+    )
+    def test_panel_check_errors(self):
+        messages = run_checks()
+        self.assertEqual(
+            messages,
+            [
+                Warning(
+                    "debug_toolbar requires the STATICFILES_DIRS directories to exist.",
+                    hint="Running manage.py collectstatic may help uncover the issue.",
+                    id="debug_toolbar.staticfiles.W001",
+                )
+            ],
+        )
+
+    @override_settings(DEBUG_TOOLBAR_PANELS=[])
+    def test_panels_is_empty(self):
+        errors = run_checks()
+        self.assertEqual(
+            errors,
+            [
+                Warning(
+                    "Setting DEBUG_TOOLBAR_PANELS is empty.",
+                    hint="Set DEBUG_TOOLBAR_PANELS to a non-empty list in your "
+                    "settings.py.",
+                    id="debug_toolbar.W005",
+                )
+            ],
+        )
+
+    @override_settings(
+        TEMPLATES=[
+            {
+                "BACKEND": "django.template.backends.django.DjangoTemplates",
+                "APP_DIRS": False,
+                "OPTIONS": {
+                    "context_processors": [
+                        "django.template.context_processors.debug",
+                        "django.template.context_processors.request",
+                        "django.contrib.auth.context_processors.auth",
+                        "django.contrib.messages.context_processors.messages",
+                    ],
+                    "loaders": [
+                        "django.template.loaders.filesystem.Loader",
+                    ],
+                },
+            },
+        ]
+    )
+    def test_check_w006_invalid(self):
+        errors = run_checks()
+        self.assertEqual(
+            errors,
+            [
+                Warning(
+                    "At least one DjangoTemplates TEMPLATES configuration needs "
+                    "to use django.template.loaders.app_directories.Loader or "
+                    "have APP_DIRS set to True.",
+                    hint=(
+                        "Include django.template.loaders.app_directories.Loader "
+                        'in ["OPTIONS"]["loaders"]. Alternatively use '
+                        "APP_DIRS=True for at least one "
+                        "django.template.backends.django.DjangoTemplates "
+                        "backend configuration."
+                    ),
+                    id="debug_toolbar.W006",
+                )
+            ],
+        )
+
+    @override_settings(
+        TEMPLATES=[
+            {
+                "NAME": "use_loaders",
+                "BACKEND": "django.template.backends.django.DjangoTemplates",
+                "APP_DIRS": False,
+                "OPTIONS": {
+                    "context_processors": [
+                        "django.template.context_processors.debug",
+                        "django.template.context_processors.request",
+                        "django.contrib.auth.context_processors.auth",
+                        "django.contrib.messages.context_processors.messages",
+                    ],
+                    "loaders": [
+                        "django.template.loaders.app_directories.Loader",
+                    ],
+                },
+            },
+            {
+                "NAME": "use_app_dirs",
+                "BACKEND": "django.template.backends.django.DjangoTemplates",
+                "APP_DIRS": True,
+                "OPTIONS": {
+                    "context_processors": [
+                        "django.template.context_processors.debug",
+                        "django.template.context_processors.request",
+                        "django.contrib.auth.context_processors.auth",
+                        "django.contrib.messages.context_processors.messages",
+                    ],
+                },
+            },
+        ]
+    )
+    def test_check_w006_valid(self):
+        self.assertEqual(run_checks(), [])
+
+    @override_settings(
+        TEMPLATES=[
+            {
+                "NAME": "use_loaders",
+                "BACKEND": "django.template.backends.django.DjangoTemplates",
+                "APP_DIRS": False,
+                "OPTIONS": {
+                    "context_processors": [
+                        "django.template.context_processors.debug",
+                        "django.template.context_processors.request",
+                        "django.contrib.auth.context_processors.auth",
+                        "django.contrib.messages.context_processors.messages",
+                    ],
+                    "loaders": [
+                        (
+                            "django.template.loaders.cached.Loader",
+                            [
+                                "django.template.loaders.filesystem.Loader",
+                                "django.template.loaders.app_directories.Loader",
+                            ],
+                        ),
+                    ],
+                },
+            },
+        ]
+    )
+    def test_check_w006_valid_nested_loaders(self):
+        self.assertEqual(run_checks(), [])
+
+    @patch("debug_toolbar.apps.mimetypes.guess_type")
+    def test_check_w007_valid(self, mocked_guess_type):
+        mocked_guess_type.return_value = ("text/javascript", None)
+        self.assertEqual(run_checks(), [])
+        mocked_guess_type.return_value = ("application/javascript", None)
+        self.assertEqual(run_checks(), [])
+
+    @patch("debug_toolbar.apps.mimetypes.guess_type")
+    def test_check_w007_invalid(self, mocked_guess_type):
+        mocked_guess_type.return_value = ("text/plain", None)
+        self.assertEqual(
+            run_checks(),
+            [
+                Warning(
+                    "JavaScript files are resolving to the wrong content type.",
+                    hint="The Django Debug Toolbar may not load properly while mimetypes are misconfigured. "
+                    "See the Django documentation for an explanation of why this occurs.\n"
+                    "https://docs.djangoproject.com/en/stable/ref/contrib/staticfiles/#static-file-development-view\n"
+                    "\n"
+                    "This typically occurs on Windows machines. The suggested solution is to modify "
+                    "HKEY_CLASSES_ROOT in the registry to specify the content type for JavaScript "
+                    "files.\n"
+                    "\n"
+                    "[HKEY_CLASSES_ROOT\\.js]\n"
+                    '"Content Type"="application/javascript"',
+                    id="debug_toolbar.W007",
+                )
+            ],
+        )
