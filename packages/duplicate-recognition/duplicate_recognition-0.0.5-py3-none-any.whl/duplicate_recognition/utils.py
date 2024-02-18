228f7eee06500d3bc7fe89c5be76794a884e702d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 14:04:50 +0000

    Add package duplicate-recognition/0.0.5/duplicate_recognition-0.0.5-py3-none-any.whl

---
+++ packages/duplicate-recognition/duplicate_recognition-0.0.5-py3-none-any.whl/duplicate_recognition/utils.py
@@ -0,0 +1,24 @@
+from typing import Dict, Any, Generator, Tuple
+from enum import Enum
+from dataclasses import dataclass, field
+
+
+class Algorithm(Enum):
+    EQUALITY = 1
+    PHONETIC_DISTANCE = 2
+    URL = 3
+    COUNTRY = 4
+    VAT_ID = 5
+    PHONE = 6
+
+
+@dataclass
+class Comparison:
+    entity: Dict[str, Any]
+    other_entity: Dict[str, Any]
+
+    field_scores: Dict[str, float] = field(default_factory=dict)
+    f_score_sum: float = 0
+    count: int = 0
+
+    score: float = 0
