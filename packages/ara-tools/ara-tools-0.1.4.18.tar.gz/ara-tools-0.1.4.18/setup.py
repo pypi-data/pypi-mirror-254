f0e50c11340f166cdfefbeb03b9000ed48ca9647
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 08:03:43 +0000

    Add package ara-tools/0.1.4.18/ara-tools-0.1.4.18.tar.gz

---
+++ packages/ara-tools/ara-tools-0.1.4.18.tar.gz/ara-tools-0.1.4.18/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="ara-tools",
+    version="0.1.4.18",
+    packages=find_packages(),
+    include_package_data=True,  # Add this line
+    entry_points={
+        "console_scripts": [
+            "ara = ara_tools.__main__:cli",
+        ],
+    },
+    install_requires=[
+        'langchain',
+        'langchain-community',
+        'openai',
+        # Add your package dependencies here
+    ],
+)
