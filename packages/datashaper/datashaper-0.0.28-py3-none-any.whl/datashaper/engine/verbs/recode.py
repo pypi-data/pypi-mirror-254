f693e360502bc1b75f9c24564c5675db4440eecc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 08:06:03 +0000

    Add package datashaper/0.0.28/datashaper-0.0.28-py3-none-any.whl

---
+++ packages/datashaper/datashaper-0.0.28-py3-none-any.whl/datashaper/engine/verbs/recode.py
@@ -0,0 +1,24 @@
+#
+# Copyright (c) Microsoft. All rights reserved.
+# Licensed under the MIT license. See LICENSE file in the project.
+#
+
+from datashaper.engine.verbs.verb_input import VerbInput
+from datashaper.engine.verbs.verbs_mapping import verb
+from datashaper.table_store import TableContainer
+
+
+class RecodeMap(dict):
+    def __missing__(self, key):
+        return key
+
+
+@verb(name="recode")
+def recode(input: VerbInput, to: str, column: str, mapping: dict):
+    mapping = RecodeMap(mapping)
+
+    input_table = input.get_input()
+
+    output = input_table
+    output[to] = output[column].map(mapping)
+    return TableContainer(table=output)
