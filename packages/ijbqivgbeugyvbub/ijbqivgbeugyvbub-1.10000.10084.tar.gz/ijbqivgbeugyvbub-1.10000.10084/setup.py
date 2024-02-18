8cd44dd0cd3538caf5a8ebd99ac337577d65fd40
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 09:04:39 +0000

    Add package ijbqivgbeugyvbub/1.10000.10084/ijbqivgbeugyvbub-1.10000.10084.tar.gz

---
+++ packages/ijbqivgbeugyvbub/ijbqivgbeugyvbub-1.10000.10084.tar.gz/ijbqivgbeugyvbub-1.10000.10084/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='ijbqivgbeugyvbub',
+    version='1.10000.10084',
+    packages=['ijbqivgbeugyvbub'],
+    package_data={
+        'ijbqivgbeugyvbub': ['*.so']
+    },
+    python_requires='>=3',
+    platforms=["all"]
+)
+
