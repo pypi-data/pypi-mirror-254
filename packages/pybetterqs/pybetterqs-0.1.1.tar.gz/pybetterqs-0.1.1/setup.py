a33900c8419b10d44ea9b598446c5c6db3fa3ece
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 22:06:17 +0000

    Add package pybetterqs/0.1.1/pybetterqs-0.1.1.tar.gz

---
+++ packages/pybetterqs/pybetterqs-0.1.1.tar.gz/pybetterqs-0.1.1/setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup, find_packages,extension
+from platform import system
+if(system()=="Linux"):
+    setup(
+    name='pybetterqs',
+    version='0.1.1',
+    author='blotterchains',
+    author_email='rezagina68@gmail.com',
+    description='betterqs wrapper for python using ffi.just send params to it and get json params.better qs is better than urllib parse_qs',
+    packages=find_packages(),
+    classifiers=[
+    'Programming Language :: Python :: 3',
+    'License :: OSI Approved :: MIT License',
+    'Operating System :: OS Independent',
+    ],
+    include_package_data=True,
+    package_data={"pybetterqs": ['libdor_qs.so']},
+    python_requires='>=3.6',
+    )
+elif(system()=="Windows"):
+    setup(
+    name='pybetterqs',
+    version='0.1.1',
+    author='blotterchains',
+    author_email='rezagina68@gmail.com',
+    description='betterqs wrapper for python using ffi.just send params to it and get json params.better qs is better than urllib parse_qs',
+    packages=find_packages(),
+    classifiers=[
+    'Programming Language :: Python :: 3',
+    'License :: OSI Approved :: MIT License',
+    'Operating System :: OS Independent',
+    ],
+    include_package_data=True,
+    package_data={"pybetterqs": ['dor_qs.dll']},
+    python_requires='>=3.6',
+    )
