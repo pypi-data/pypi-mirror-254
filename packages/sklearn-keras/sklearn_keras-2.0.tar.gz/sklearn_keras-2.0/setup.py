200219716c9e2ca3d2bb46d9c43eca8d3c3d7825
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 15:06:55 +0000

    Add package sklearn-keras/2.0/sklearn_keras-2.0.tar.gz

---
+++ packages/sklearn-keras/sklearn_keras-2.0.tar.gz/sklearn_keras-2.0/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='sklearn_keras',
+    version='2.0',
+    author='Your Name',
+    description='A short description of your library',
+    packages=find_packages(),
+    install_requires=[
+        # List your dependencies here
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+)
+
