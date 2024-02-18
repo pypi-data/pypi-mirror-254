daabb03408163848211cff6c66d8ce221b449ca0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 14:06:28 +0000

    Add package distributions192/0.2/distributions192-0.2.tar.gz

---
+++ packages/distributions192/distributions192-0.2.tar.gz/distributions192-0.2/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='distributions192',
+      version='0.2',
+      description='Gaussian distributions',
+      packages=['distributions192'],
+      zip_safe=False)
