1ff6aa5256493b29fd867f3b1f613a53cec72c70
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 05:05:52 +0000

    Add package feffery-antd-components/0.3.0rc1/feffery_antd_components-0.3.0rc1.tar.gz

---
+++ packages/feffery-antd-components/feffery_antd_components-0.3.0rc1.tar.gz/feffery_antd_components-0.3.0rc1/feffery_antd_components/AntdForm.py
@@ -0,0 +1,79 @@
+# AUTO GENERATED FILE - DO NOT EDIT
+
+from dash.development.base_component import Component, _explicitize_args
+
+
+class AntdForm(Component):
+    """An AntdForm component.
+
+
+Keyword arguments:
+
+- children (a list of or a singular dash component, string or number; optional):
+    The content of the tab - will only be displayed if this tab is
+    selected.
+
+- id (string; optional)
+
+- className (string | dict; optional)
+
+- colon (boolean; default True)
+
+- key (string; optional)
+
+- labelAlign (a value equal to: 'left', 'right'; default 'right')
+
+- labelCol (dict; optional)
+
+    `labelCol` is a dict with keys:
+
+    - flex (string | number; optional)
+
+    - offset (number; optional)
+
+    - span (number; optional)
+
+- labelWrap (boolean; default False)
+
+- layout (a value equal to: 'horizontal', 'vertical', 'inline'; default 'horizontal')
+
+- loading_state (dict; optional)
+
+    `loading_state` is a dict with keys:
+
+    - component_name (string; optional):
+        Holds the name of the component that is loading.
+
+    - is_loading (boolean; optional):
+        Determines if the component is loading or not.
+
+    - prop_name (string; optional):
+        Holds which property is loading.
+
+- style (dict; optional)
+
+- wrapperCol (dict; optional)
+
+    `wrapperCol` is a dict with keys:
+
+    - flex (string | number; optional)
+
+    - offset (number; optional)
+
+    - span (number; optional)"""
+    _children_props = []
+    _base_nodes = ['children']
+    _namespace = 'feffery_antd_components'
+    _type = 'AntdForm'
+    @_explicitize_args
+    def __init__(self, children=None, id=Component.UNDEFINED, className=Component.UNDEFINED, style=Component.UNDEFINED, key=Component.UNDEFINED, layout=Component.UNDEFINED, labelCol=Component.UNDEFINED, wrapperCol=Component.UNDEFINED, colon=Component.UNDEFINED, labelAlign=Component.UNDEFINED, labelWrap=Component.UNDEFINED, loading_state=Component.UNDEFINED, **kwargs):
+        self._prop_names = ['children', 'id', 'className', 'colon', 'key', 'labelAlign', 'labelCol', 'labelWrap', 'layout', 'loading_state', 'style', 'wrapperCol']
+        self._valid_wildcard_attributes =            []
+        self.available_properties = ['children', 'id', 'className', 'colon', 'key', 'labelAlign', 'labelCol', 'labelWrap', 'layout', 'loading_state', 'style', 'wrapperCol']
+        self.available_wildcard_properties =            []
+        _explicit_args = kwargs.pop('_explicit_args')
+        _locals = locals()
+        _locals.update(kwargs)  # For wildcard attrs and excess named props
+        args = {k: _locals[k] for k in _explicit_args if k != 'children'}
+
+        super(AntdForm, self).__init__(children=children, **args)
---
+++ packages/feffery-antd-components/feffery_antd_components-0.3.0rc1.tar.gz/feffery_antd_components-0.3.0rc1/feffery_antd_components/AntdMentions.py
@@ -0,0 +1,93 @@
+# AUTO GENERATED FILE - DO NOT EDIT
+
+from dash.development.base_component import Component, _explicitize_args
+
+
+class AntdMentions(Component):
+    """An AntdMentions component.
+
+
+Keyword arguments:
+
+- id (string; optional)
+
+- autoFocus (boolean; default False)
+
+- autoSize (dict; default False)
+
+    `autoSize` is a boolean | dict with keys:
+
+    - maxRows (number; optional)
+
+    - minRows (number; optional)
+
+- batchPropsNames (list of strings; optional)
+
+- batchPropsValues (dict; optional)
+
+- className (string | dict; optional)
+
+- defaultValue (string; optional)
+
+- disabled (boolean; default False)
+
+- key (string; optional)
+
+- loading_state (dict; optional)
+
+    `loading_state` is a dict with keys:
+
+    - component_name (string; optional):
+        Holds the name of the component that is loading.
+
+    - is_loading (boolean; optional):
+        Determines if the component is loading or not.
+
+    - prop_name (string; optional):
+        Holds which property is loading.
+
+- options (list of dicts; required)
+
+    `options` is a list of dicts with keys:
+
+    - label (a list of or a singular dash component, string or number; optional)
+
+    - value (string; optional)
+
+- placement (a value equal to: 'top', 'bottom'; default 'bottom')
+
+- popupClassName (string; optional):
+    设置弹框菜单css类名.
+
+- popupContainer (a value equal to: 'parent', 'body'; optional)
+
+- prefix (string; default '@')
+
+- selectedOptions (list of strings; optional)
+
+- status (a value equal to: 'error', 'warning'; optional)
+
+- style (dict; optional)
+
+- value (string; optional)"""
+    _children_props = ['options[].label']
+    _base_nodes = ['children']
+    _namespace = 'feffery_antd_components'
+    _type = 'AntdMentions'
+    @_explicitize_args
+    def __init__(self, id=Component.UNDEFINED, className=Component.UNDEFINED, style=Component.UNDEFINED, popupClassName=Component.UNDEFINED, key=Component.UNDEFINED, autoSize=Component.UNDEFINED, prefix=Component.UNDEFINED, value=Component.UNDEFINED, defaultValue=Component.UNDEFINED, options=Component.REQUIRED, selectedOptions=Component.UNDEFINED, disabled=Component.UNDEFINED, placement=Component.UNDEFINED, status=Component.UNDEFINED, autoFocus=Component.UNDEFINED, popupContainer=Component.UNDEFINED, batchPropsNames=Component.UNDEFINED, batchPropsValues=Component.UNDEFINED, loading_state=Component.UNDEFINED, **kwargs):
+        self._prop_names = ['id', 'autoFocus', 'autoSize', 'batchPropsNames', 'batchPropsValues', 'className', 'defaultValue', 'disabled', 'key', 'loading_state', 'options', 'placement', 'popupClassName', 'popupContainer', 'prefix', 'selectedOptions', 'status', 'style', 'value']
+        self._valid_wildcard_attributes =            []
+        self.available_properties = ['id', 'autoFocus', 'autoSize', 'batchPropsNames', 'batchPropsValues', 'className', 'defaultValue', 'disabled', 'key', 'loading_state', 'options', 'placement', 'popupClassName', 'popupContainer', 'prefix', 'selectedOptions', 'status', 'style', 'value']
+        self.available_wildcard_properties =            []
+        _explicit_args = kwargs.pop('_explicit_args')
+        _locals = locals()
+        _locals.update(kwargs)  # For wildcard attrs and excess named props
+        args = {k: _locals[k] for k in _explicit_args}
+
+        for k in ['options']:
+            if k not in args:
+                raise TypeError(
+                    'Required argument `' + k + '` was not specified.')
+
+        super(AntdMentions, self).__init__(**args)
