e929b06616940820f1c802fc0791efe602070c4c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 05:04:33 +0000

    Add package sv-logger/0.1.4/sv_logger-0.1.4.tar.gz

---
+++ packages/sv-logger/sv_logger-0.1.4.tar.gz/sv_logger-0.1.4/README.md
@@ -0,0 +1,3 @@
+## License
+
+This project is released under the MIT License. For more details, see the [LICENSE] file.
---
+++ packages/sv-logger/sv_logger-0.1.4.tar.gz/sv_logger-0.1.4/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="sv_logger",
+    version="0.1.4",
+    packages=find_packages(),
+    license="MIT",
+    description="A simple logging library for Python",
+    long_description=open("README.md").read(),
+    author="pternali",
+    author_email="patrizio.ternali15@gmail.com",
+    url="",
+    requires=[],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ]
+)
