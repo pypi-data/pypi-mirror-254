50f98d9aaacfad1dae587ec0bb95bbb42fb444a9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 23:05:50 +0000

    Add package xeye/1.6/xeye-1.6.tar.gz

---
+++ packages/xeye/xeye-1.6.tar.gz/xeye-1.6/xeye/__init__.py
@@ -0,0 +1,9 @@
+from .manual_dataset import ManualDataset
+from .dataset import Dataset
+from .build_dataset import BuildDataset
+
+__all__ = [
+    "Dataset",
+    "ManualDataset",
+    "BuildDataset"
+]
