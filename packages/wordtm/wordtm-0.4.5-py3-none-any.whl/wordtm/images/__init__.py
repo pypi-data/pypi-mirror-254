530316aed258be0f626194c4ef2143cd2747cb93
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 23:08:25 +0000

    Add package wordtm/0.4.5/wordtm-0.4.5-py3-none-any.whl

---
+++ packages/wordtm/wordtm-0.4.5-py3-none-any.whl/wordtm/images/__init__.py
@@ -0,0 +1,14 @@
+# __init__.py
+#
+# Folder 'images'
+#
+# Copyright (c) 2022-2024 WordTM Project 
+# Author: Johnny Cheng <drjohnnycheng@gmail.com>
+#
+# Updated: 2 February 2024
+#
+# URL: https://github.com/drjohnnycheng/wordtm.git
+# For license information, see LICENSE.TXT
+
+"""The folder stores an image mask ('heart.jpg') for plotting a wordcloud.
+"""
