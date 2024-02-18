6d7196e97003d2e9bc24a9617179aefcb06bfe99
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 03:05:15 +0000

    Add package intecomm-edc/0.1.99/intecomm_edc-0.1.99-py3-none-any.whl

---
+++ packages/intecomm-edc/intecomm_edc-0.1.99-py3-none-any.whl/intecomm_subject/model_mixins/__init__.py
@@ -0,0 +1,11 @@
+from .clinical_review_model_mixins import (
+    ClinicalReviewBaselineDmModelMixin,
+    ClinicalReviewBaselineHivModelMixin,
+    ClinicalReviewBaselineHtnModelMixin,
+    ClinicalReviewModelMixin,
+)
+from .crf_model_mixin import CrfModelMixin
+from .crf_with_action_model_mixin import CrfWithActionModelMixin
+from .followup_review_model_mixin import FollowupReviewModelMixin
+from .medication_adherence_model_mixin import MedicationAdherenceModelMixin
+from .search_slug_model_mixin import SearchSlugModelMixin
