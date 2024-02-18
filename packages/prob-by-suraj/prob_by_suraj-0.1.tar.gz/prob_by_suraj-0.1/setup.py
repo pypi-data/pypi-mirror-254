ccc0c547069741037537f07bd8f78f5f5ec6f04f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 20:06:10 +0000

    Add package prob-by-suraj/0.1/prob_by_suraj-0.1.tar.gz

---
+++ packages/prob-by-suraj/prob_by_suraj-0.1.tar.gz/prob_by_suraj-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='prob_by_suraj',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['prob_by_suraj'],
+      zip_safe=False)
