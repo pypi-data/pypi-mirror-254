b2195da4025f11ad8741ad831270ae286ac1327e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 16:05:20 +0000

    Add package xtyping/0.8.2/xtyping-0.8.2-py3-none-any.whl

---
+++ packages/xtyping/xtyping-0.8.2-py3-none-any.whl/xtyping/_meta.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""Package metadata/info"""
+from __future__ import annotations
+
+import warnings
+
+from xtyping.__about__ import __description__, __pkgroot__, __title__, __version__
+
+warnings.warn(
+    "xtyping._meta is deprecated, use xtyping.__about__ instead",
+    DeprecationWarning,
+    stacklevel=2,
+)
+
+__all__ = ("__title__", "__description__", "__pkgroot__", "__version__")
