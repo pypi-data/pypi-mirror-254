ad7f7795b5e86b3d78eceb346e82d55da589f00c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 12:04:17 +0000

    Add package one-data-processing/0.0.4/one-data-processing-0.0.4.tar.gz

---
+++ packages/one-data-processing/one-data-processing-0.0.4.tar.gz/one-data-processing-0.0.4/setup.py
@@ -0,0 +1,30 @@
+# make a setup.py for data-processing package
+
+from typing import List
+
+from setuptools import find_packages, setup
+
+
+def load_requirements() -> List[str]:
+    requirements: List[str] = []
+    with open("requirements.txt", encoding="utf-8") as f:
+        requirements.extend(f.readlines())
+    return requirements
+
+setup(
+    name="one-data-processing",
+    description="Data Processing is used for data processing through MinIO, databases, Web APIs, etc.",
+    long_description=open("README_pypi.md", encoding="utf-8").read(),
+    long_description_content_type="text/markdown",
+    keywords="PDF WORD WEB parsing preprocessing",
+    url="https://github.com/kubeagi/arcadia",
+    python_requires=">=3.11",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3.11",
+    ],
+    packages=find_packages(),
+    version="0.0.4",
+    install_requires=load_requirements(),
+)
