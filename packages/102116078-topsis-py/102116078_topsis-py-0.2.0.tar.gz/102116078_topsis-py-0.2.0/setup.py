5dc8698dce308488e047e5896e09ddd8788152fa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 22:06:33 +0000

    Add package 102116078-topsis-py/0.2.0/102116078_topsis-py-0.2.0.tar.gz

---
+++ packages/102116078-topsis-py/102116078_topsis-py-0.2.0.tar.gz/102116078_topsis-py-0.2.0/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+with open('README.md', 'r') as file:
+    description = file.read()
+
+setup(
+    name='102116078_topsis-py',
+    version='0.2.0',
+    author='Dhruv',
+    author_email='dkikan_be21@thapar.edu',
+    description='Implementation of Topsis in Python',
+    packages=find_packages(),
+    install_requires=[
+        'numpy',
+        'pandas',
+    ],
+    entry_points={
+        'console_scripts': [
+            '102116078_topsis = topsis_code.main_code:main',
+        ],
+    },
+    long_description = description, 
+    long_description_content_type = "text/markdown",
+)
