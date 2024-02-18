6d15de4cd5c0a813ccbddfd75436270b10320582
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 01:26:41 +0000

    Add package amsdal_server/0.0.16/amsdal_server-0.0.16.tar.gz

---
+++ packages/amsdal_server/amsdal_server-0.0.16.tar.gz/amsdal_server-0.0.16/src/amsdal_server/apps/common/serializers/objects_response.py
@@ -0,0 +1,9 @@
+from typing import Any
+
+from amsdal_server.apps.common.serializers.base_serializer import SkipNoneBaseModel
+from amsdal_server.apps.common.serializers.column_response import ColumnInfo
+
+
+class ObjectsResponse(SkipNoneBaseModel):
+    columns: list[ColumnInfo]
+    rows: list[Any]
