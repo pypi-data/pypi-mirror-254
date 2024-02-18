91d5917d94290f8bc464d8b5360b023e0055c5ea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 14:05:11 +0000

    Add package hepconvert/1.3.0/hepconvert-1.3.0.tar.gz

---
+++ packages/hepconvert/hepconvert-1.3.0.tar.gz/hepconvert-1.3.0/tests/test_root_to_parquet.py
@@ -0,0 +1,63 @@
+from __future__ import annotations
+
+import awkward as ak
+import pytest
+import uproot
+
+import hepconvert
+
+skhep_testdata = pytest.importorskip("skhep_testdata")
+
+
+def HZZ_test():
+    f = uproot.open(skhep_testdata.data_path("uproot-HZZ.root"))
+    original = f["events"].arrays()
+    hepconvert.root_to_parquet(
+        in_file=skhep_testdata.data_path("uproot-HZZ.root"),
+        out_file="test.parquet",
+        step_size="100 MB",
+        force=True,
+    )
+    from_parquet = ak.from_parquet("/Users/zobil/Documents/hepconvert/test.parquet")
+    for key in f["events"].keys():
+        assert ak.all(from_parquet[0][key] == original[key])
+
+
+def specify_tree():
+    f = uproot.open(skhep_testdata.data_path("uproot-HZZ.root"))
+    original = f["events"].arrays()
+    hepconvert.root_to_parquet(
+        in_file=skhep_testdata.data_path("uproot-HZZ.root"),
+        out_file="test.parquet",
+        tree="events",
+        step_size="100 MB",
+        force=True,
+    )
+    from_parquet = ak.from_parquet("/Users/zobil/Documents/hepconvert/test.parquet")
+    for key in f["events"].keys():
+        assert ak.all(from_parquet[0][key] == original[key])
+
+
+def Zmumu_test():
+    f = uproot.open(skhep_testdata.data_path("uproot-Zmumu.root"))
+    original = f["events"].arrays()
+    hepconvert.root_to_parquet(
+        in_file=skhep_testdata.data_path("uproot-Zmumu.root"),
+        out_file="test1.parquet",
+        step_size="100 MB",
+    )
+    from_parquet = ak.from_parquet("/Users/zobil/Documents/hepconvert/test1.parquet")
+    for key in f["events"].keys():
+        assert ak.all(from_parquet[0][key] == original[key])
+
+
+def break_trees():
+    with pytest.raises(AttributeError):
+        hepconvert.root_to_parquet(
+            in_file=skhep_testdata.data_path("uproot-hepdata-example.root"),
+            out_file="test2.parquet",
+            step_size="100 MB",
+        )
+
+
+HZZ_test()
