7698ae0199803165359496be56c76ea4057f734d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 00:05:08 +0000

    Add package infuzu-python-sdk/0.28/infuzu-python-sdk-0.28.tar.gz

---
+++ packages/infuzu-python-sdk/infuzu-python-sdk-0.28.tar.gz/infuzu-python-sdk-0.28/infuzu/http_requests/base.py
@@ -0,0 +1,21 @@
+import json
+import requests
+from ..authentication.shortcuts import (generate_message_signature, SIGNATURE_HEADER_NAME)
+
+
+class SignatureSession(requests.Session):
+    def request(self, method, url, private_key: str = None, **kwargs):
+        request_body: any = kwargs.get("data") or kwargs.get("json", '')
+        if not isinstance(request_body, str):
+            request_body: str = json.dumps(request_body)
+        signature: str = generate_message_signature(request_body, private_key=private_key)
+
+        if 'headers' in kwargs and isinstance(kwargs['headers'], dict):
+            kwargs['headers'][SIGNATURE_HEADER_NAME] = signature
+        else:
+            kwargs['headers'] = {SIGNATURE_HEADER_NAME: signature}
+
+        return super().request(method, url, **kwargs)
+
+
+signed_requests: SignatureSession = SignatureSession()
