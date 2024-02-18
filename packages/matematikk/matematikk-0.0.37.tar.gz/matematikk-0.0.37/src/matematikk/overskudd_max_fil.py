1d708eccbae71625f8131ec9d887761ecfa041df
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 04:07:35 +0000

    Add package matematikk/0.0.37/matematikk-0.0.37.tar.gz

---
+++ packages/matematikk/matematikk-0.0.37.tar.gz/matematikk-0.0.37/src/matematikk/overskudd_max_fil.py
@@ -0,0 +1,35 @@
+# 🚀 programmering.no | 🤓 matematikk.as
+
+from sympy import Symbol, diff
+from matematikk import superløs
+
+def overskudd_max(variabel = Symbol(""),
+                       uttrykk  = Symbol(""),
+                       rund     = -1,
+                       debug    = -1):
+
+    # Deriverer uttrykk mhp. variabel og får df
+    df = diff(uttrykk, variabel) # f'(x)
+
+    # Løser likningen df = 0
+    variabel_max = superløs(variabel = variabel,
+                            vs       = df,
+                            hs       = 0,
+                            rund     = rund,
+                            debug    = debug)
+
+    return variabel_max
+
+# Syntax > Right
+overskudd_maks          = overskudd_max
+overskudd_maksimalt     = overskudd_max
+overskudd_mest          = overskudd_max
+overskudd_storst        = overskudd_max
+overskudd_størst        = overskudd_max
+
+# Syntax > Reversed
+maks_overskudd          = overskudd_max
+maksimalt_overskudd     = overskudd_max
+mest_overskudd          = overskudd_max
+storst_overskudd        = overskudd_max
+størst_overskudd        = overskudd_max
