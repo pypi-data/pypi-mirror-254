35cfdafe6dbdd40efcc3cd40917353b0f48034f1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 07:03:51 +0000

    Add package mailblaster/0.0.3/mailblaster-0.0.3.tar.gz

---
+++ packages/mailblaster/mailblaster-0.0.3.tar.gz/mailblaster-0.0.3/mailblaster/__init__.py
@@ -0,0 +1,6 @@
+# mailblaster/__init__.py
+
+from .mailblaster import read_credentials, read_recipients, read_html_content, send_emails
+
+__all__ = ['read_credentials', 'read_recipients', 'read_html_content', 'send_emails']
+
