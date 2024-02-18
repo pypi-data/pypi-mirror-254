ed960587a846d54f7d42df66ddb22f2daad274a9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 15:05:45 +0000

    Add package firebirdsql-run/1.0.8/firebirdsql_run-1.0.8-py3-none-any.whl

---
+++ packages/firebirdsql-run/firebirdsql_run-1.0.8-py3-none-any.whl/firebirdsql_run/__init__.py
@@ -0,0 +1,4 @@
+"""Public accessible objects of that module."""
+
+from firebirdsql_run.main import Connection, callproc, connect, connection, execute
+from firebirdsql_run.type import CompletedTransaction, Dataset, ExecuteError, FBTypes
---
+++ packages/firebirdsql-run/firebirdsql_run-1.0.8-py3-none-any.whl/firebirdsql_run/util.py
@@ -0,0 +1,26 @@
+"""Utils."""
+from os import getenv
+
+
+class GetEnvError(Exception):
+    """Exception raised when an environment variable is not found."""
+
+
+def get_env(variable: str) -> str:
+    """
+    Get the value of an environment variable.
+
+    Args:
+        variable (str): The name of the environment variable to retrieve.
+
+    Returns:
+        str: The value of the environment variable, if found.
+
+    Raises:
+        GetEnvError: If the environment variable is not found.
+    """
+    if value := getenv(variable):
+        return value
+
+    msg = f"Please setup {variable} in environment variables."
+    raise GetEnvError(msg)
