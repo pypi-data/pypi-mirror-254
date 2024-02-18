8c9cea1101c7953f5e72df57882d263ae4485a4b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 11:05:00 +0000

    Add package datashaper/0.0.32/datashaper-0.0.32.tar.gz

---
+++ packages/datashaper/datashaper-0.0.32.tar.gz/datashaper-0.0.32/datashaper/engine/verbs/unorder.py
@@ -0,0 +1,16 @@
+#
+# Copyright (c) Microsoft. All rights reserved.
+# Licensed under the MIT license. See LICENSE file in the project.
+#
+
+from datashaper.engine.verbs.verb_input import VerbInput
+from datashaper.engine.verbs.verbs_mapping import verb
+from datashaper.table_store import TableContainer
+
+
+@verb(name="unorder", treats_input_tables_as_immutable=True)
+def unorder(input: VerbInput):
+    input_table = input.get_input()
+
+    output = input_table.sort_index()
+    return TableContainer(table=output)
---
+++ packages/datashaper/datashaper-0.0.32.tar.gz/datashaper-0.0.32/datashaper/engine/verbs/unroll.py
@@ -0,0 +1,15 @@
+#
+# Copyright (c) Microsoft. All rights reserved.
+# Licensed under the MIT license. See LICENSE file in the project.
+#
+
+from datashaper.engine.verbs.verb_input import VerbInput
+from datashaper.engine.verbs.verbs_mapping import verb
+from datashaper.table_store import TableContainer
+
+
+@verb(name="unroll", treats_input_tables_as_immutable=True)
+def unroll(input: VerbInput, column: str):
+    input_table = input.get_input()
+    output = input_table.explode(column)
+    return TableContainer(table=output)
