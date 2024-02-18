2adb3c4f34fe1debba17d48d1e279f59397a067f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 08:05:34 +0000

    Add package cappr/0.9.0/cappr-0.9.0-py3-none-any.whl

---
+++ packages/cappr/cappr-0.9.0-py3-none-any.whl/cappr/llama_cpp/__init__.py
@@ -0,0 +1,11 @@
+"""
+Install the Llama CPP requirements to use this module::
+
+    pip install "cappr[llama-cpp]"
+
+See this section of the documentation:
+
+https://cappr.readthedocs.io/en/latest/select_a_language_model.html#llama-cpp
+"""
+
+from . import _utils, classify, _classify_no_cache
