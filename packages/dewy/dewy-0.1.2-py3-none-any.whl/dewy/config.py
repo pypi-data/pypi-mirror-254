a2cbdab87779444798e84129a82fa320dfd018ac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 08:05:01 +0000

    Add package dewy/0.1.2/dewy-0.1.2-py3-none-any.whl

---
+++ packages/dewy/dewy-0.1.2-py3-none-any.whl/dewy/config.py
@@ -0,0 +1,79 @@
+from typing import Any, Optional
+
+from fastapi.routing import APIRoute
+from pydantic import PostgresDsn
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+from dewy.constants import Environment
+
+# See https://github.com/zhanymkanov/fastapi-best-practices#10-use-pydantics-basesettings-for-configs
+
+
+class Config(BaseSettings):
+    """Application configuration, parsed from environment variables."""
+
+    model_config = SettingsConfigDict(
+        env_file=".env",
+        env_file_encoding="utf-8",
+    )
+
+    SERVE_ADMIN_UI: bool = True
+    """If true, serve the admin UI."""
+
+    DB: Optional[PostgresDsn] = None
+    """The Postgres database to connect to.
+
+    If not provided, none of the CRUD methods will work.
+    """
+
+    APPLY_MIGRATIONS: bool = True
+    """Whether migrations should be applied to the database.
+
+    This should be set to false if there are multiple services running to prevent
+    concurrent migrations.
+    """
+
+    ENVIRONMENT: Environment = Environment.PRODUCTION
+    """The environment the application is running in."""
+
+    OPENAI_API_KEY: Optional[str] = None
+    """ The OpenAI API Key to use for OpenAI models.
+
+    This is required for using openai models.
+    """
+
+
+settings = Config()
+
+
+def convert_snake_case_to_camel_case(string: str) -> str:
+    """Convert snake case to camel case"""
+
+    words = string.split("_")
+    return words[0] + "".join(word.title() for word in words[1:])
+
+
+def custom_generate_unique_id_function(route: APIRoute) -> str:
+    """Custom function to generate unique id for each endpoint"""
+
+    return convert_snake_case_to_camel_case(route.name)
+
+
+API_DESCRIPTION: str = """This API allows ingesting and retrieving knowledge.
+
+Knowledge comes in a variety of forms -- text, image, tables, etc. and
+from a variety of sources -- documents, web pages, audio, etc."""
+
+app_configs: dict[str, Any] = {
+    "title": "Dewy Knowledge Base API",
+    "version": "0.1.2",
+    "summary": "Knowledge curation for Retrieval Augmented Generation",
+    "description": API_DESCRIPTION,
+    "servers": [
+        {"url": "http://localhost:8000", "description": "Local server"},
+    ],
+    "generate_unique_id_function": custom_generate_unique_id_function,
+}
+
+if not settings.ENVIRONMENT.is_debug:
+    app_configs["openapi_url"] = None  # hide docs
---
+++ packages/dewy/dewy-0.1.2-py3-none-any.whl/dewy/main.py
@@ -0,0 +1,79 @@
+import contextlib
+import os
+from pathlib import Path
+from typing import AsyncIterator, Optional, TypedDict
+
+import asyncpg
+import uvicorn
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.staticfiles import StaticFiles
+from loguru import logger
+
+from dewy.common import db
+from dewy.common.db_migration import apply_migrations
+from dewy.config import app_configs, settings
+from dewy.routes import api_router
+
+
+class State(TypedDict):
+    pg_pool: Optional[asyncpg.Pool]
+
+
+# Resolve paths, independent of PWD
+current_file_path = Path(__file__).resolve()
+react_build_path = current_file_path.parent.parent / "frontend" / "dist"
+migrations_path = current_file_path.parent.parent / "migrations"
+
+
+@contextlib.asynccontextmanager
+async def lifespan(_app: FastAPI) -> AsyncIterator[State]:
+    """Function creating instances used during the lifespan of the service."""
+
+    if settings.DB is not None:
+        async with db.create_pool(settings.DB.unicode_string()) as pg_pool:
+            if settings.APPLY_MIGRATIONS:
+                async with pg_pool.acquire() as conn:
+                    await apply_migrations(conn, migration_dir=migrations_path)
+
+            logger.info("Created database connection")
+            state = State(pg_pool=pg_pool)
+            yield state
+    else:
+        logger.warn("No database configured. CRUD methods will fail.")
+        state = State(pg_pool=None)
+        yield state
+
+
+app = FastAPI(lifespan=lifespan, **app_configs)
+
+origins = [
+    "*",
+]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+@app.get("/healthcheck", include_in_schema=False)
+async def healthcheck() -> dict[str, str]:
+    return {"status": "ok"}
+
+
+app.include_router(api_router)
+
+if settings.SERVE_ADMIN_UI and os.path.isdir(react_build_path):
+    logger.info("Running admin UI at http://localhost/admin")
+    # Serve static files from the React app build directory
+    app.mount(
+        "/admin", StaticFiles(directory=str(react_build_path), html=True), name="static"
+    )
+
+
+# Function for running Dewy as a script
+def run(*args):
+    uvicorn.run("dewy.main:app", host="0.0.0.0", port=8000)
