8833f5ac84a1207e924b36fee67c34dcc98958ca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 19:05:43 +0000

    Add package api-fff/1.0.0/api_fff-1.0.0.tar.gz

---
+++ packages/api-fff/api_fff-1.0.0.tar.gz/api_fff-1.0.0/swagger_client/api/__init__.py
@@ -0,0 +1,26 @@
+from __future__ import absolute_import
+
+# flake8: noqa
+
+# import apis into api package
+from swagger_client.api.arbitre_api import ArbitreApi
+from swagger_client.api.arbitre_officiel_categorie_api import ArbitreOfficielCategorieApi
+from swagger_client.api.calcul_bilan_saison_api import CalculBilanSaisonApi
+from swagger_client.api.calcul_match_histo_api import CalculMatchHistoApi
+from swagger_client.api.calcul_serie_cours_api import CalculSerieCoursApi
+from swagger_client.api.cdg_api import CdgApi
+from swagger_client.api.classement_journee_api import ClassementJourneeApi
+from swagger_client.api.club_api import ClubApi
+from swagger_client.api.compet_api import CompetApi
+from swagger_client.api.dossier_api import DossierApi
+from swagger_client.api.engagement_api import EngagementApi
+from swagger_client.api.equipe_api import EquipeApi
+from swagger_client.api.individu_api import IndividuApi
+from swagger_client.api.match_entity_api import MatchEntityApi
+from swagger_client.api.match_feuille_api import MatchFeuilleApi
+from swagger_client.api.match_membre_api import MatchMembreApi
+from swagger_client.api.officiel_categorie_api import OfficielCategorieApi
+from swagger_client.api.phase_api import PhaseApi
+from swagger_client.api.poule_api import PouleApi
+from swagger_client.api.poule_journee_api import PouleJourneeApi
+from swagger_client.api.terrain_api import TerrainApi
