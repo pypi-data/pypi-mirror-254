ba69bc6a312a25fe98188911fad03479ac71a2cf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 19:05:08 +0000

    Add package openskill/5.1.1/openskill-5.1.1.tar.gz

---
+++ packages/openskill/openskill-5.1.1.tar.gz/openskill-5.1.1/openskill/__init__.py
@@ -0,0 +1,16 @@
+from typing import List
+
+# Public API
+__all__: List[str] = []
+
+
+# Metadata
+__version__ = "5.1.1"
+__author__ = "Vivek Joshy"
+__email__ = "vivek@opendebates.net"
+__copyright__ = "Copyright 2023, Vivek Joshy"
+__credits__ = ["Philihp Busby"]
+__deprecated__ = False
+__license__ = "MIT"
+__maintainer__ = "Vivek Joshy"
+__status__ = "Production"
