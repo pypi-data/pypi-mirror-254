34e6fab7c4ff78500c5463096c8f9e1386756d8a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 22:07:24 +0000

    Add package streamlit-auth0-ts/0.2.0/streamlit-auth0-ts-0.2.0.tar.gz

---
+++ packages/streamlit-auth0-ts/streamlit-auth0-ts-0.2.0.tar.gz/streamlit-auth0-ts-0.2.0/setup.py
@@ -0,0 +1,28 @@
+from pathlib import Path
+
+import setuptools
+
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
+setuptools.setup(
+    name="streamlit-auth0-ts",
+    version="0.2.0",
+    author="",
+    author_email="g.dutton@gmail.com",
+    description="Quickly add Auth0 authentication to your Streamlit app.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="",
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+    classifiers=[],
+    python_requires=">=3.7",
+    install_requires=[
+        # By definition, a Custom Component depends on Streamlit.
+        # If your component has other Python dependencies, list
+        # them here.
+        "streamlit >= 0.63",
+        "python-jose == 3.3.0",
+    ],
+)
