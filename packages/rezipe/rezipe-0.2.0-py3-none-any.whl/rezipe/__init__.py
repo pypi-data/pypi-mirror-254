1cefe5cac20247c75641e9bcfd8a9d0e4c314a7f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 07:04:59 +0000

    Add package rezipe/0.2.0/rezipe-0.2.0-py3-none-any.whl

---
+++ packages/rezipe/rezipe-0.2.0-py3-none-any.whl/rezipe/__init__.py
@@ -0,0 +1,5 @@
+# SPDX-FileCopyrightText: 2022-present Nutchanon Ninyawee <nutchanon@codustry.com>
+#
+# SPDX-License-Identifier: MIT
+
+from .ingredient import Ingredient, IngredientGroup, I, ส่วนผสม
