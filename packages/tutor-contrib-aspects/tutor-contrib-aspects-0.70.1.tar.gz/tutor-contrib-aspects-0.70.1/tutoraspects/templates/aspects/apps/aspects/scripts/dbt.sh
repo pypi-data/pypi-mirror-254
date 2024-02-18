591f03cf61180f89f9eac5b31a7e19e3ef7d696e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 16:04:49 +0000

    Add package tutor-contrib-aspects/0.70.1/tutor-contrib-aspects-0.70.1.tar.gz

---
+++ packages/tutor-contrib-aspects/tutor-contrib-aspects-0.70.1.tar.gz/tutor-contrib-aspects-0.70.1/tutoraspects/templates/aspects/apps/aspects/scripts/dbt.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+## WARNING: If you modify this block, make sure to also update the
+##          corresponding block in the init-aspects.sh file.
+
+echo "Installing dbt packages..."
+
+pip install -r /app/aspects/dbt/requirements.txt
+
+rm -rf {{ DBT_REPOSITORY_PATH }}
+
+echo "Installing aspects-dbt"
+echo "git clone -b {{ DBT_BRANCH }} {{ DBT_REPOSITORY }}"
+git clone -b {{ DBT_BRANCH }} {{ DBT_REPOSITORY }}
+
+cd {{ DBT_REPOSITORY_PATH }} || exit
+
+export ASPECTS_EVENT_SINK_DATABASE={{ASPECTS_EVENT_SINK_DATABASE}}
+export ASPECTS_XAPI_DATABASE={{ASPECTS_XAPI_DATABASE}}
+
+echo "Installing dbt dependencies"
+dbt deps --profiles-dir /app/aspects/dbt/
+
+echo "Running dbt $*"
+dbt "$@" --profiles-dir /app/aspects/dbt/
+
+rm -rf {{ DBT_REPOSITORY_PATH }}
