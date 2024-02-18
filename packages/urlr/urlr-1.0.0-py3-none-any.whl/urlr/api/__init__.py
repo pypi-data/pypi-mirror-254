414e9798c24569a6ce24a32111b76f1d1982d280
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 03:04:24 +0000

    Add package urlr/1.0.0/urlr-1.0.0-py3-none-any.whl

---
+++ packages/urlr/urlr-1.0.0-py3-none-any.whl/urlr/api/__init__.py
@@ -0,0 +1,7 @@
+# flake8: noqa
+
+# import apis into api package
+from urlr.api.authentification_api import AuthentificationApi
+from urlr.api.link_api import LinkApi
+from urlr.api.stats_api import StatsApi
+
