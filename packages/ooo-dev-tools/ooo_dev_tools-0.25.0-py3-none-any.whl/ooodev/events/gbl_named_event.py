5cb86c16dfa16cc8aa88f250d2b3d84827a3605c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 00:05:15 +0000

    Add package ooo-dev-tools/0.25.0/ooo_dev_tools-0.25.0-py3-none-any.whl

---
+++ packages/ooo-dev-tools/ooo_dev_tools-0.25.0-py3-none-any.whl/ooodev/events/gbl_named_event.py
@@ -0,0 +1,14 @@
+# coding: utf-8
+"""
+Global Named Events.
+"""
+from __future__ import annotations
+
+
+class GblNamedEvent:
+    """
+    Global Named Events
+    """
+
+    PRINTING = "global_printing"
+    """Global Printing Event."""
