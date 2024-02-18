e80a78420d725737369d4230f222519ac10faa5c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 23:07:51 +0000

    Add package jiminy-py/1.7.19.post1/jiminy_py-1.7.19.post1-cp38-cp38-manylinux_2_28_x86_64.whl

---
+++ packages/jiminy-py/jiminy_py-1.7.19.post1-cp38-cp38-manylinux_2_28_x86_64.whl/jiminy_py/core/pinocchio/__init__.pyi
@@ -0,0 +1,12 @@
+from .pinocchio_pywrap import *
+from .deprecated import *
+from .shortcuts import *
+from . import utils as utils, visualize as visualize
+from .explog import exp as exp, log as log
+from .pinocchio_pywrap import __raw_version__ as __raw_version__, __version__ as __version__
+from .robot_wrapper import RobotWrapper as RobotWrapper
+from _typeshed import Incomplete
+from hppfcl import CachedMeshLoader as CachedMeshLoader, CollisionGeometry as CollisionGeometry, CollisionResult as CollisionResult, Contact as Contact, DistanceResult as DistanceResult, MeshLoader as MeshLoader, StdVec_CollisionResult as StdVec_CollisionResult, StdVec_Contact as StdVec_Contact, StdVec_DistanceResult as StdVec_DistanceResult
+
+submodules: Incomplete
+WITH_HPP_FCL_BINDINGS: bool
