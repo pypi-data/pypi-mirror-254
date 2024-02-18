404785aeaff0d8dd70bac4082cbe1e2b38a3799c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 16:04:49 +0000

    Add package excode-distribution/1.1/excode_distribution-1.1.tar.gz

---
+++ packages/excode-distribution/excode_distribution-1.1.tar.gz/excode_distribution-1.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='excode_distribution',
+      version='1.1',
+      description='Gaussian and Binomial distribution',
+      packages=['excode_distribution'],
+      author = 'Opeyemi Oladipupo',
+      author_email = 'techrystein@gmail.com',
+      zip_safe=False)
