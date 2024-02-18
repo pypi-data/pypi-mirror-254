b12c6d6d4d2f16a11c0fa5eaa5207cd9426783cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 08:06:03 +0000

    Add package datashaper/0.0.28/datashaper-0.0.28.tar.gz

---
+++ packages/datashaper/datashaper-0.0.28.tar.gz/datashaper-0.0.28/datashaper/engine/types.py
@@ -0,0 +1,247 @@
+#
+# Copyright (c) Microsoft. All rights reserved.
+# Licensed under the MIT license. See LICENSE file in the project.
+#
+"""Common types used across the datashaper codebase."""
+
+from enum import Enum
+from typing import Any, Union
+
+from dataclasses import dataclass, field
+
+
+class Bin:
+    """A data bin."""
+
+    min: Union[float, str]
+    count: int
+
+
+class Category:
+    """A category data class."""
+
+    name: str
+    count: int
+
+
+@dataclass
+class DataType(Enum):
+    """Data type of a column."""
+
+    Array = "array"
+    Boolean = "boolean"
+    Date = "date"
+    Time = "time"
+    Datetime = "datetime"
+    Number = "number"
+    Integer = "integer"
+    String = "string"
+    Object = "object"
+    Undefined = "undefined"
+    Unknown = "unknown"
+
+
+@dataclass
+class Step:
+    """A workflow processing step."""
+
+    verb: str
+    input: Union[str, dict[str, str]]
+    output: Union[str, dict[str, str]]
+    args: dict[str, Any] = field(default_factory=dict)
+
+
+class JoinStrategy(Enum):
+    """Table join strategies."""
+
+    Inner = "inner"
+    LeftOuter = "left outer"
+    RightOuter = "right outer"
+    FullOuter = "full outer"
+    AntiJoin = "anti join"
+    SemiJoin = "semi join"
+    Cross = "cross"
+
+
+@dataclass
+class InputColumnArgs:
+    """Column argument for verbs operating on a single column."""
+
+    column: str
+
+
+class FieldAggregateOperation(Enum):
+    """Aggregate operations for fields."""
+
+    Any = "any"
+    Count = "count"
+    CountDistinct = "distinct"
+    Valid = "valid"
+    Invalid = "invalid"
+    Max = "max"
+    Min = "min"
+    Sum = "sum"
+    Product = "product"
+    Mean = "mean"
+    Mode = "mode"
+    Median = "median"
+    StDev = "stdev"
+    StDevPopulation = "stdevp"
+    Variance = "variance"
+    ArrayAgg = "array_agg"
+    ArrayAggDistinct = "array_agg_distinct"
+
+
+class BinStrategy(Enum):
+    """Binning strategy to use for the binning operation."""
+
+    Auto = "auto"
+    Fd = "fd"
+    Doane = "doane"
+    Scott = "scott"
+    Rice = "rice"
+    Sturges = "sturges"
+    Sqrt = "sqrt"
+    FixedCount = "fixed count"
+    FixedWidth = "fixed width"
+
+
+class FilterCompareType(Enum):
+    """Filter compare type."""
+
+    Value = "value"
+    Column = "column"
+
+
+class NumericComparisonOperator(Enum):
+    """Numeric comparison operators."""
+
+    Equals = "="
+    NotEqual = "!="
+    LessThan = "<"
+    LessThanOrEqual = "<="
+    GreaterThan = ">"
+    GreaterThanOrEqual = ">="
+    IsEmpty = "is empty"
+    IsNotEmpty = "is not empty"
+
+
+class StringComparisonOperator(Enum):
+    """String comparison operators."""
+
+    Equals = "equals"
+    NotEqual = "is not equal"
+    Contains = "contains"
+    StartsWith = "starts with"
+    EndsWith = "ends with"
+    IsEmpty = "is empty"
+    IsNotEmpty = "is not empty"
+    RegularExpression = "regex"
+
+
+class BooleanComparisonOperator(Enum):
+    """Boolean comparison operators."""
+
+    Equals = "equals"
+    NotEqual = "is not equal"
+    IsTrue = "is true"
+    IsFalse = "is false"
+    IsEmpty = "is empty"
+    IsNotEmpty = "is not empty"
+
+
+@dataclass
+class Criterion:
+    """A filter criterion."""
+
+    value: Any
+    type: FilterCompareType
+    operator: Union[
+        NumericComparisonOperator, StringComparisonOperator, BooleanComparisonOperator
+    ]
+
+
+class BooleanLogicalOperator(Enum):
+    """Boolean logical operators."""
+
+    OR = "or"
+    AND = "and"
+    NOR = "nor"
+    NAND = "nand"
+    XOR = "xor"
+    XNOR = "xnor"
+
+
+@dataclass
+class FilterArgs(InputColumnArgs):
+    """Filter criteria for a column."""
+
+    criteria: list[Criterion]
+    logical: BooleanLogicalOperator = BooleanLogicalOperator.OR
+
+
+class SetOp(Enum):
+    """Set operations for the difference and intersect verbs."""
+
+    Concat = "concat"
+    Union = "union"
+    Intersect = "intersect"
+    Except = "except"
+
+
+class MathOperator(Enum):
+    """Mathematical operators."""
+
+    Add = "+"
+    Subtract = "-"
+    Multiply = "*"
+    Divide = "/"
+    Concatenate = "concat"
+
+
+class SortDirection(Enum):
+    """Sort direction for order by."""
+
+    Ascending = "asc"
+    Descending = "desc"
+
+
+class ParseType(Enum):
+    """ParseType is used to specify the type of a column."""
+
+    Boolean = "boolean"
+    Date = "date"
+    Integer = "int"
+    Decimal = "float"
+    String = "string"
+
+
+class MergeStrategy(Enum):
+    """Merge strategy for merge verb."""
+
+    FirstOneWins = "first one wins"
+    LastOneWins = "last one wins"
+    Concat = "concat"
+    CreateArray = "array"
+
+
+class WindowFunction(Enum):
+    """Windowing functions for window verb."""
+
+    RowNumber = "row_number"
+    Rank = "rank"
+    PercentRank = "percent_rank"
+    CumulativeDistribution = "cume_dist"
+    FirstValue = "first_value"
+    LastValue = "last_value"
+    FillDown = "fill_down"
+    FillUp = "fill_up"
+    UUID = "uuid"
+
+
+@dataclass
+class OrderByInstruction:
+    """Details regarding how to order a column."""
+
+    column: str
+    direction: SortDirection
---
+++ packages/datashaper/datashaper-0.0.28.tar.gz/datashaper-0.0.28/datashaper/engine/verbs/__init__.py
@@ -0,0 +1,41 @@
+import importlib
+import importlib.util
+import logging
+import os
+import pkgutil
+import sys
+
+from .verb_input import VerbInput
+from .verbs_mapping import VerbDetails, VerbManager, verb
+
+
+logger = logging.getLogger(__name__)
+
+
+def load_verbs(module):
+    """
+    Loads the verbs from the given module path recursively.
+    This is useful to run all the @verb decorators and register the verbs in the verb manager.
+    """
+    module_path = os.path.dirname(module.__file__)
+    module_name = module.__name__
+    for _, sub_module, is_module in pkgutil.iter_modules([module_path]):
+        if not is_module:
+            full_path = os.path.join(module_path, f"{sub_module}.py")
+            module = importlib.util.spec_from_file_location(module_name, full_path)
+            module_to_load = f"{module.name}.{sub_module}"
+            importlib.import_module(module_to_load)
+            logger.info(f"Found module: {module_to_load}")
+        else:
+            full_path = os.path.join(module_path, sub_module)
+            sub_module_name = f"{module_name}.{sub_module}"
+            sub_module_rec = importlib.import_module(sub_module_name)
+            load_verbs(sub_module_rec)
+
+
+# Load core verbs into VerbManager
+mod = sys.modules[__name__]
+load_verbs(mod)
+
+
+__all__ = ["VerbInput", "VerbManager", "load_verbs", "verb", "VerbDetails"]
