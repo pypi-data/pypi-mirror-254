dd3620ebc68cdbf465730d7e649aa3dc57727dfa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 01:27:14 +0000

    Add package invenio-accounts/4.0.1/invenio_accounts-4.0.1-py2.py3-none-any.whl

---
+++ packages/invenio-accounts/invenio_accounts-4.0.1-py2.py3-none-any.whl/invenio_accounts/api.py
@@ -0,0 +1,57 @@
+# -*- coding: utf-8 -*-
+#
+# This file is part of Invenio.
+# Copyright (C) 2022-2023 CERN.
+#
+# Invenio is free software; you can redistribute it and/or modify it
+# under the terms of the MIT License; see LICENSE file for more details.
+
+"""API objects for Invenio Accounts."""
+
+
+class Session:
+    """Session object for DB Users change history."""
+
+    def __init__(self):
+        """Constructor."""
+        self.updated_users = set()
+        self.updated_roles = set()
+        self.deleted_users = set()
+        self.deleted_roles = set()
+
+
+class DBUsersChangeHistory:
+    """DB Users change history storage."""
+
+    def __init__(self):
+        """Constructor."""
+        self.sessions = {}
+
+    def _get_session(self, session_id):
+        """Returns or creates a session for a concrete session id."""
+        return self.sessions.setdefault(session_id, Session())
+
+    def add_updated_user(self, session_id, user_id):
+        """Adds a user to the updated users list."""
+        session = self._get_session(session_id)
+        session.updated_users.add(user_id)
+
+    def add_updated_role(self, session_id, role_id):
+        """Adds a role to the updated roles list."""
+        session = self._get_session(session_id)
+        session.updated_roles.add(role_id)
+
+    def add_deleted_user(self, session_id, user_id):
+        """Adds a user to the deleted users list."""
+        session = self._get_session(session_id)
+        session.deleted_users.add(user_id)
+
+    def add_deleted_role(self, session_id, role_id):
+        """Adds a role to the deleted roles list."""
+        session = self._get_session(session_id)
+        session.deleted_roles.add(role_id)
+
+    def clear_dirty_sets(self, session):
+        """Removes session object."""
+        sid = id(session)
+        self.sessions.pop(sid, None)
---
+++ packages/invenio-accounts/invenio_accounts-4.0.1-py2.py3-none-any.whl/invenio_accounts/datastore.py
@@ -0,0 +1,75 @@
+# -*- coding: utf-8 -*-
+#
+# This file is part of Invenio.
+# Copyright (C) 2015-2024 CERN.
+#
+# Invenio is free software; you can redistribute it and/or modify it
+# under the terms of the MIT License; see LICENSE file for more details.
+
+"""Session-aware datastore."""
+
+from flask_security import SQLAlchemyUserDatastore
+
+from .models import Domain, Role
+from .proxies import current_db_change_history
+from .sessions import delete_user_sessions
+from .signals import datastore_post_commit, datastore_pre_commit
+
+
+class SessionAwareSQLAlchemyUserDatastore(SQLAlchemyUserDatastore):
+    """Datastore which deletes active session when a user is deactivated."""
+
+    def deactivate_user(self, user):
+        """Deactivate a  user.
+
+        :param user: A :class:`invenio_accounts.models.User` instance.
+        :returns: The datastore instance.
+        """
+        res = super(SessionAwareSQLAlchemyUserDatastore, self).deactivate_user(user)
+        if res:
+            delete_user_sessions(user)
+        return res
+
+    def commit(self):
+        """Commit a user to its session."""
+        datastore_pre_commit.send(session=self.db.session)
+        super().commit()
+        datastore_post_commit.send(session=self.db.session)
+        current_db_change_history.clear_dirty_sets(self.db.session)
+
+    def put(self, model):
+        """Put a user to its session."""
+        res = super().put(model)
+        self.mark_changed(id(self.db.session), uid=model.id)
+        return res
+
+    def mark_changed(self, sid, uid=None, rid=None):
+        """Save a user to the changed history."""
+        if uid:
+            current_db_change_history.add_updated_user(sid, uid)
+        elif rid:
+            current_db_change_history.add_updated_role(sid, rid)
+
+    def update_role(self, role):
+        """Updates roles."""
+        role = self.db.session.merge(role)
+        self.mark_changed(id(self.db.session), rid=role.id)
+        return role
+
+    def create_role(self, **kwargs):
+        """Creates and returns a new role from the given parameters."""
+        role = super().create_role(**kwargs)
+        self.mark_changed(id(self.db.session), rid=role.id)
+        return role
+
+    def find_role_by_id(self, role_id):
+        """Fetches roles searching by id."""
+        return self.role_model.query.filter_by(id=role_id).one_or_none()
+
+    def find_domain(self, domain):
+        """Find a domain."""
+        return Domain.query.filter_by(domain=domain).one_or_none()
+
+    def create_domain(self, domain, **kwargs):
+        """Create a new domain."""
+        return Domain.create(domain, **kwargs)
