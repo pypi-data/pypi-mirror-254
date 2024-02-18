2e97fe48baee954416e240b1a1579fc202d1788c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 07:03:46 +0000

    Add package pixano/0.5.0b3/pixano-0.5.0b3.tar.gz

---
+++ packages/pixano/pixano-0.5.0b3.tar.gz/pixano-0.5.0b3/pixano/app/dist/_app/immutable/chunks/datasetStores.RfgKBczp.js
@@ -0,0 +1,14 @@
+import{w as e}from"./entry.LmOfDFuB.js";function S(s){return(s==null?void 0:s.length)!==void 0?s:Array.from(s)}const n=20,t=1;/**
+ * @copyright CEA
+ * @author CEA
+ * @license CECILL
+ *
+ * This software is a collaborative computer program whose purpose is to
+ * generate and explore labeled data for computer vision applications.
+ * This software is governed by the CeCILL-C license under French law and
+ * abiding by the rules of distribution of free software. You can use,
+ * modify and/ or redistribute the software under the terms of the CeCILL-C
+ * license as circulated by CEA, CNRS and INRIA at the following URL
+ *
+ * http://www.cecill.info
+ */const a={currentPage:t,pageSize:n,query:{model:"",search:""}},d=e(),o=e([]),u=e(!1),A=e(a),T=e({shouldSave:!1,canSave:!1});export{t as D,A as a,n as b,d,S as e,u as i,o as m,T as s};
---
+++ packages/pixano/pixano-0.5.0b3.tar.gz/pixano-0.5.0b3/pixano/app/dist/_app/immutable/entry/start.RyfwGOlI.js
@@ -0,0 +1,1 @@
+import{b as a}from"../chunks/entry.LmOfDFuB.js";export{a as start};
