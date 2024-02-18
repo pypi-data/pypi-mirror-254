7ec277ef26493ad854295d44179ad6269ce1ad41
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 14:04:33 +0000

    Add package matematikk/0.0.97/matematikk-0.0.97-py3-none-any.whl

---
+++ packages/matematikk/matematikk-0.0.97-py3-none-any.whl/matematikk/__init__.py
@@ -0,0 +1,90 @@
+# Versjon: 0.0.96
+
+# Chain mal
+from ._ch_1.ch_2.ch_3.ch_4_fil import ch_5_fn
+
+# numpy()
+from numpy import(
+    polyfit
+    )
+
+# sympy()
+from sympy import (
+    ConditionSet,
+    core,
+    diff,
+    Eq,
+    FiniteSet,
+    Intersection,
+    nsolve,
+    Reals,
+    solve,
+    solveset,
+    Symbol
+    )
+
+# reggis()
+from .reggis_fil import (
+    reggis,
+
+    # Alias > Right
+    reggis_cas,
+    regresjon,
+    regresjon_cas,
+    regresjon_polynom,
+    regresjon_polynom_cas,
+
+    # Alias > Reversed
+    cas_regresjon,
+    cas_regresjon_polynom,
+    regresjon_polynom_cas
+    )
+
+# superlos()
+from .superløs_fil import (
+    superløs,
+
+    # Alias > Right
+    los,
+    losning,
+    løs,
+    løsning,
+    superlos,
+    super_los,
+    super_løs,
+
+    # Alias > Reversed
+    los_super,
+    løs_super
+    )
+
+# ekstremalpunkt_max()
+from .python.funksjoner.matematikk.funksjoner._drøfting.ekstremalpunkt.def_ekstremalpunkt_max_mas import (
+    ekstremalpunkt_max,
+
+    # Alias > Right > ...
+    ekstremalpunkt_maks,
+    ekstremalpunkt_maksimalt,
+    toppunkt
+
+    # Alias > Reversed > ...
+    )
+
+# overskudd_max()
+from .python.funksjoner.matematikk.funksjoner._drøfting.ekstremalpunkt.def_overskudd_max_mas import (
+    overskudd_max,
+
+    # Alias > Right
+    overskudd_maks,
+    overskudd_maksimalt,
+    overskudd_mest,
+    overskudd_storst,
+    overskudd_størst,
+
+    # Alias > Reversed
+    maks_overskudd,
+    maksimalt_overskudd,
+    mest_overskudd,
+    storst_overskudd,
+    størst_overskudd
+    )
