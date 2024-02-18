7f9082145321c6745a1dd525677343b19e108b1b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 05:05:56 +0000

    Add package py-utils-khabibullin.m.m/0.0.18/py_utils_khabibullin.m.m-0.0.18-py3-none-any.whl

---
+++ packages/py-utils-khabibullin.m.m/py_utils_khabibullin.m.m-0.0.18-py3-none-any.whl/commands/__init__.py
@@ -0,0 +1,19 @@
+import argparse
+
+from commands.list_command import list_commands
+
+
+def main():
+  parser = argparse.ArgumentParser(
+    prog="commands",
+    description="",
+  )
+
+  parser.add_argument("-l", "--list", action="store_true")
+
+  args = parser.parse_args()
+
+  if args.list:
+    list_commands()
+  else:
+    print(f"No command was found for the arguments: {args}")
