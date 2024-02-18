e65485da0b6fa0d2db82c512f66b3daf94bd3477
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 05:04:51 +0000

    Add package testEijnim-package/0.1/testEijnim_package-0.1.tar.gz

---
+++ packages/testEijnim-package/testEijnim_package-0.1.tar.gz/testEijnim_package-0.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='testEijnim_package',
+    version='0.1',
+    packages=find_packages(),
+    author='Eijnim Jiang',
+    install_requires=[
+        # List your project's dependencies here.
+        # For example: 'numpy>=1.18.1'
+    ],
+    # Add more metadata such as author, license, etc.
+)
