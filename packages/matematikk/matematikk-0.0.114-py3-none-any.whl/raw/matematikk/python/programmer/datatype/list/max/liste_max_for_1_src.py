4f488b7b4a11fd3b9f99f8d91fe8448e925d77b0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 20:05:49 +0000

    Add package matematikk/0.0.114/matematikk-0.0.114-py3-none-any.whl

---
+++ packages/matematikk/matematikk-0.0.114-py3-none-any.whl/raw/matematikk/python/programmer/datatype/list/max/liste_max_for_1_src.py
@@ -0,0 +1,72 @@
+from __init__ import Pref, Datafil
+
+
+##########################################
+# Pref
+##########################################
+
+__   = Pref()
+_  = Datafil()
+
+
+##########################################
+# Create
+##########################################
+
+_fil_navn_src = __._liste_max_for_src_py
+
+
+##########################################
+# Hotkeys
+##########################################
+
+# python /Users/krj/Desktop/program/src/_dir_1/bld/data_fil/_exe/python/programmer/datatype/list/max/liste_max_for_1.py
+
+
+##########################################
+# Fil content
+##########################################
+
+_fil_content = f"""
+{_._h_top_py}
+{_._p_list_max_for_loop}
+
+{_._c_verdier_L}
+{_._v_liste} = {_._e_list_max_for_loop_arr}
+
+{_._c_fn_max_for_loop}
+def {_._f_max_for_loop}({_._v_liste}):
+
+	{_._c_list_max_for_loop_max_tmp}
+	{_._v_max} = {_._v_liste}[0]
+
+	{_._c_iterer_gjennom_listen}
+	for i in range(1, len({_._v_liste})):
+
+		{_._c_list_max_for_loop_update}
+		if {_._v_liste}[i] > {_._v_max}:
+			{_._v_max} = {_._v_liste}[i]
+
+	return {_._v_max}
+
+{_._c_fnc_max_core}
+{_._v_max} = {_._f_max_for_loop}({_._v_liste})
+
+{_._c_print_legacy}
+print(f"{_._c_list_C}             {_._v_kolon} {{{_._v_liste}}}")
+print(f"{_._c_list_max_C} {_._v_kolon} {{{_._v_max}}}")
+"""
+
+
+##########################################
+# Create fil
+##########################################
+
+__.bld_data_fil(
+    _fil_navn_src,
+    _fil_content)
+
+
+##########################################
+# End
+##########################################
---
+++ packages/matematikk/matematikk-0.0.114-py3-none-any.whl/raw/matematikk/python/programmer/matematikk/geometri/sirkel/sirkel_areal_r_enhet_eksponent_2_input_src.py
@@ -0,0 +1,68 @@
+from __init__ import Pref, Datafil
+
+
+##########################################
+# Pref
+##########################################
+
+__   = Pref()
+_  = Datafil()
+
+
+##########################################
+# Create
+##########################################
+
+_fil_navn_src = __._sirkel_areal_r_enhet_eksponent_input_src_py
+
+
+##########################################
+# Hotkeys
+##########################################
+
+# python /Users/krj/Desktop/program/src/_dir_1/bld/data_fil/_exe/python/programmer/matematikk/geometri/sirkel/sirkel_areal_r_enhet_eksponent_2_input.py
+
+
+##########################################
+# Fil content
+##########################################
+
+_fil_content = f"""
+{_._h_top_py}
+{_._p_sirkel_areal_r_enhet}
+
+{_._c_verdier_L}
+{_._v_pi} = {_._v_pi_val}
+
+{_._c_input}
+{_._v_enhet} = input("{_._i_velg_enhet}")
+{_._v_r} = input("{_._i_sirkel_r}")
+
+{_._c_fn_sirkel_areal_core}
+def {_._f_sirkel_areal}({_._v_r}):
+
+    {_._c_fo_sirkel_areal}
+    {_._v_areal} = {_._v_pi} * float({_._v_r})**2
+
+    return {_._v_areal}
+
+{_._c_fnc_sirkel_areal_core}
+{_._v_areal} = {_._f_sirkel_areal}({_._v_r})
+
+{_._c_print_legacy}
+print(f"{_._r_arealet_er}{_._v_kolon} {{{_._v_areal}}} {{{_._v_enhet}}}^2")
+"""
+
+
+##########################################
+# Create fil
+##########################################
+
+__.bld_data_fil(
+    _fil_navn_src,
+    _fil_content)
+
+
+##########################################
+# End
+##########################################
