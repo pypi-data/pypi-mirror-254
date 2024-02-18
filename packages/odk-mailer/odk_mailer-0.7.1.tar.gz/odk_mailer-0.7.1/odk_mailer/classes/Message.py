17ce0b66adba0ff894801444faad14e879c9dd83
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 03:04:23 +0000

    Add package odk-mailer/0.7.1/odk_mailer-0.7.1.tar.gz

---
+++ packages/odk-mailer/odk_mailer-0.7.1.tar.gz/odk_mailer-0.7.1/odk_mailer/classes/Message.py
@@ -0,0 +1,48 @@
+#from odk_mailer.lib import utils
+from types import SimpleNamespace
+
+class Message:
+    sender: str
+    subject: str
+    source: str
+    location: str
+    format: str
+    content: str
+
+    def __init__(self, data) -> None:
+
+        if type(data) is dict:
+            self.sender = data["sender"]
+            self.subject = data["subject"]
+            self.source = data["source"]
+            self.format = data["type"]
+            self.location = data["location"]            
+            self.setContent()
+
+        if type(data) is SimpleNamespace:
+            self.sender = data.sender
+            self.subject = data.subject
+            self.source = data.source
+            self.format = data.format
+            
+            # backwards compatibility
+            try:
+                self.location = data.location
+                self.setContent()
+            except:
+                self.content = data.content
+
+    def setContent(self):
+        if self.source == "stdin":
+                self.content = self.location
+
+        if self.source == "path":            
+            with open(self.location) as f:
+                data_string = f.read()
+                # base64_string = utils.base64_encode_str(data_string)
+                # self.content = base64_string
+                self.content = data_string
+
+        if self.source == "url":
+            #tbd: read from url and store as base64
+            self.content = self.location
