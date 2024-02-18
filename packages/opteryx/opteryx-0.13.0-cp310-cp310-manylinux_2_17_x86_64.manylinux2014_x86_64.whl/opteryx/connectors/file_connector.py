9ff6a668fc6599db0245afbf562b452dcdb56b34
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 19:06:16 +0000

    Add package opteryx/0.13.0/opteryx-0.13.0-cp310-cp310-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/opteryx/opteryx-0.13.0-cp310-cp310-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/opteryx/connectors/file_connector.py
@@ -0,0 +1,89 @@
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""
+The file connector provides the reader for when a file name is provided as the
+dataset name in a query.
+"""
+from typing import Dict
+from typing import Optional
+
+import pyarrow
+from orso.schema import RelationSchema
+from orso.types import OrsoTypes
+
+from opteryx.connectors.base.base_connector import BaseConnector
+from opteryx.connectors.capabilities import PredicatePushable
+from opteryx.exceptions import DatasetNotFoundError
+from opteryx.utils.file_decoders import get_decoder
+
+
+class FileConnector(BaseConnector, PredicatePushable):
+    __mode__ = "Blob"
+    _byte_array: Optional[bytes] = None  # Instance attribute to store file bytes
+
+    PUSHABLE_OPS: Dict[str, bool] = {
+        "Eq": True,
+        "NotEq": True,
+        "Gt": True,
+        "GtEq": True,
+        "Lt": True,
+        "LtEq": True,
+    }
+
+    PUSHABLE_TYPES = {OrsoTypes.BOOLEAN, OrsoTypes.DOUBLE, OrsoTypes.INTEGER, OrsoTypes.VARCHAR}
+
+    @property
+    def interal_only(self):
+        return True
+
+    def __init__(self, *args, **kwargs):
+        BaseConnector.__init__(self, **kwargs)
+        PredicatePushable.__init__(self, **kwargs)
+        if ".." in self.dataset or self.dataset[0] in ("\\", "/", "~"):
+            # Don't find any datasets which look like path traversal
+            raise DatasetNotFoundError(dataset=self.dataset)
+        self.decoder = get_decoder(self.dataset)
+
+    def _read_file(self) -> None:
+        """
+        Reads the dataset file and stores its content in _byte_array attribute.
+        """
+        if self._byte_array is None:
+            with open(self.dataset, mode="br") as file:
+                self._byte_array = bytes(file.read())
+
+    def read_dataset(
+        self, columns: list = None, predicates: list = None, **kwargs
+    ) -> pyarrow.Table:
+        """
+        Reads the dataset file and decodes it.
+
+        Returns:
+            An iterator containing a single decoded pyarrow.Table.
+        """
+        self._read_file()
+        return iter([self.decoder(self._byte_array, projection=columns, selection=predicates)])
+
+    def get_dataset_schema(self) -> RelationSchema:
+        """
+        Retrieves the schema from the dataset file.
+
+        Returns:
+            The schema of the dataset.
+        """
+        if self.schema is not None:
+            return self.schema
+
+        self._read_file()
+        self.schema = self.decoder(self._byte_array, just_schema=True)
+        return self.schema
