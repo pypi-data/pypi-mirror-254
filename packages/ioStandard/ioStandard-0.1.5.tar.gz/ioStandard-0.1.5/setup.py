0f8b97a9b92ec2bfaddd48c2de9ebbf192f77be4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 21:07:19 +0000

    Add package ioStandard/0.1.5/ioStandard-0.1.5.tar.gz

---
+++ packages/ioStandard/ioStandard-0.1.5.tar.gz/ioStandard-0.1.5/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name='ioStandard',
+    version='0.1.5',
+    author='Austin Gilchrist-Thomas',
+    packages=['ioStandard'],
+    install_requires=['pygame', 'datetime', 'tkinter'],
+)
