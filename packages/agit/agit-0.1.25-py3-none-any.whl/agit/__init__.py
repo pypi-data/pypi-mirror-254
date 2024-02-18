be08ed2646223daa38bc8e57230e192afa37de3d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 23:09:09 +0000

    Add package agit/0.1.25/agit-0.1.25-py3-none-any.whl

---
+++ packages/agit/agit-0.1.25-py3-none-any.whl/agit/__init__.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+'''
+@Time    :   2023/08/15 11:11:03
+@Author  :   ChenHao
+@Contact :   jerrychen1990@gmail.com
+'''
+import os
+
+__version__ = "0.1.0"
+AGIT_ENV = os.environ.get("AGIT_ENV", "local")
+AGIT_PROJECT_HOME = os.path.abspath(os.path.dirname(os.path.dirname(__file__)))
+AGIT_DATA_HOME = os.environ.get("AGIT_DATA_HOME", AGIT_PROJECT_HOME)
+
+
+AGIT_TEMP_DIR = os.path.join(AGIT_DATA_HOME, "temp")
+AGIT_KB_DIR = os.path.join(AGIT_DATA_HOME, "kb")
+AGIT_CONFIG_DIR = os.path.join(AGIT_DATA_HOME, "config")
+
+AGIT_LOG_HOME = os.path.join(AGIT_DATA_HOME, "/tmp/logs")
+
+
+assistant_default_prompt_template = '''你是一个知识渊博、风趣幽默的人工智能小助手
+你回答问题的时候可以参考reference标志的信息
+reference
+```
+{{reference}}
+```
+问题:
+{{message}}'''
+
+LLM_MODELS = [
+    "chatglm_lite",
+    "chatglm_std",
+    "chatglm_pro",
+    "gpt-3.5-turbo-0301"
+]
