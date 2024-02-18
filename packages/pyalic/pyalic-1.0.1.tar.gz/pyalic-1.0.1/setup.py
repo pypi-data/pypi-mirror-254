38a69c2e36d2745e5659141f4a061f1fae77d597
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 14:06:13 +0000

    Add package pyalic/1.0.1/pyalic-1.0.1.tar.gz

---
+++ packages/pyalic/pyalic-1.0.1.tar.gz/pyalic-1.0.1/setup.py
@@ -0,0 +1,30 @@
+"""Setup pyalic module"""
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding='utf-8') as readme_file:
+    readme = readme_file.read()
+
+
+def requirements():
+    """Get current requirements"""
+    return ['httpx']
+
+
+setup(
+    name='pyalic',
+    version='1.0.1',
+    author='Yarosvet',
+    description="Licensing system module which allows you to manage access to your products with ease",
+    long_description_content_type="text/markdown",
+    long_description=readme,
+    url="https://github.com/Yarosvet/Pyalic",
+    packages=find_packages(include=['pyalic', 'pyalic.*']),
+    install_requires=[*requirements()],
+    classifiers=[
+        "Programming Language :: Python :: 3.8",
+        "License :: OSI Approved :: GNU Lesser General Public License v2 (LGPLv2)",
+        "Operating System :: OS Independent",
+        "Development Status :: 5 - Production/Stable",
+        "Topic :: Software Development :: Libraries :: Python Modules"
+    ]
+)
