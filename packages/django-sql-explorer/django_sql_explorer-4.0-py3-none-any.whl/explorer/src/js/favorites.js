aaabc483e89eb1f67f4bb8a02cd229f05a827404
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 05:06:16 +0000

    Add package django-sql-explorer/4.0/django_sql_explorer-4.0-py3-none-any.whl

---
+++ packages/django-sql-explorer/django_sql_explorer-4.0-py3-none-any.whl/explorer/src/js/favorites.js
@@ -0,0 +1,36 @@
+import {getCsrfToken} from "./csrf";
+
+export async function toggleFavorite() {
+    // Assuming this function is bound to an element, 'this' refers to that element
+    let queryId = this.dataset.id; // or this.getAttribute('data-id');
+    let favoriteUrl = this.dataset.url; // or this.getAttribute('data-url');
+
+    try {
+        let response = await fetch(favoriteUrl, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'X-CSRFToken': getCsrfToken()
+            },
+            body: JSON.stringify({})
+        });
+
+        let data = await response.json();
+        let is_favorite = data.is_favorite;
+        let selector = `.query_favorite_toggle[data-id='${queryId}']`;
+        let element = document.querySelector(selector);
+
+        if (element) {
+            if (is_favorite) {
+                element.classList.remove("bi-heart");
+                element.classList.add("bi-heart-fill");
+            } else {
+                element.classList.remove("bi-heart-fill");
+                element.classList.add("bi-heart");
+            }
+        }
+    } catch (error) {
+        console.error('Error:', error);
+        alert("error");
+    }
+}
