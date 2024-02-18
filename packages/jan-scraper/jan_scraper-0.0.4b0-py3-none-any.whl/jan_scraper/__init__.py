bf399c117325e7c63e7a73b3a07121a0a883e052
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 16:06:19 +0000

    Add package jan-scraper/0.0.4b0/jan_scraper-0.0.4b0-py3-none-any.whl

---
+++ packages/jan-scraper/jan_scraper-0.0.4b0-py3-none-any.whl/jan_scraper/__init__.py
@@ -0,0 +1,20 @@
+"""
+jan_scraper: interact with Jan.ai by sending messages and retrieving the response
+
+"https://github.com/AstraBert/jan-scraper"
+"""
+
+# Version
+__version__ = "0.0.4b0"
+
+
+##Errors and Warnings
+class UnableToFindLocationError(Exception):
+    """Raise exception if jan_scraper is not installed correctly"""
+
+    def __init__(self):
+        pass
+
+
+class MayActivateOnlyOneModelWarning(Warning):
+    """Raise warning if API activation is automatized: may only activate one of the models you have installed, and it may not be your desired one"""
