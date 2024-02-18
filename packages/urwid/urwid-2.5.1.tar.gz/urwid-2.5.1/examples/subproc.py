73fa9a6f3214aa16bf78ac887d9d72be0cfc34d3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 08:05:55 +0000

    Add package urwid/2.5.1/urwid-2.5.1.tar.gz

---
+++ packages/urwid/urwid-2.5.1.tar.gz/urwid-2.5.1/examples/subproc.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+
+from __future__ import annotations
+
+import os
+import subprocess
+import sys
+
+import urwid
+
+factor_me = 362923067964327863989661926737477737673859044111968554257667
+run_me = os.path.join(os.path.dirname(sys.argv[0]), "subproc2.py")
+
+output_widget = urwid.Text(f"Factors of {factor_me:d}:\n")
+edit_widget = urwid.Edit("Type anything or press enter to exit:")
+frame_widget = urwid.Frame(
+    header=edit_widget,
+    body=urwid.Filler(output_widget, valign=urwid.BOTTOM),
+    focus_part="header",
+)
+
+
+def exit_on_enter(key):
+    if key == "enter":
+        raise urwid.ExitMainLoop()
+
+
+loop = urwid.MainLoop(frame_widget, unhandled_input=exit_on_enter)
+
+
+def received_output(data):
+    output_widget.set_text(output_widget.text + data.decode("utf8"))
+
+
+write_fd = loop.watch_pipe(received_output)
+proc = subprocess.Popen(
+    ["python", "-u", run_me, str(factor_me)],  # noqa: S603,S607  # Example can be insecure
+    stdout=write_fd,
+    close_fds=True,
+)
+
+loop.run()
+proc.kill()
