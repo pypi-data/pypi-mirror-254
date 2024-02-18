88cba74e2fb9df67b3e5f7a9b5f9c5ce483a026a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 02:10:41 +0000

    Add package creolenltk/0.1.5/creolenltk-0.1.5-py3-none-any.whl

---
+++ packages/creolenltk/creolenltk-0.1.5-py3-none-any.whl/creolenltk/__init__.py
@@ -0,0 +1,7 @@
+import os
+from .text_cleaner import TextCleaner
+from .contraction_expansion import ContractionToExpansion
+from .spelling_checker import SpellingChecker
+from .stopwords import Stopword
+from .tokenizer import Tokenizer
+
