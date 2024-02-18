6b2e17df786803b50080baeac686aeec1851cce9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 16:04:08 +0000

    Add package lamindb/0.67.3/lamindb-0.67.3-py3-none-any.whl

---
+++ packages/lamindb/lamindb-0.67.3-py3-none-any.whl/lamindb/_storage.py
@@ -0,0 +1,18 @@
+from lamindb_setup.dev._docs import doc_args
+from lamindb_setup.dev.upath import UPath, create_path
+from lnschema_core import Storage
+
+
+def root_as_path(self) -> UPath:
+    return create_path(self.root)
+
+
+@property  # type: ignore
+@doc_args(Storage.path.__doc__)
+def path(self) -> UPath:
+    """{}."""
+    return create_path(self.root)
+
+
+Storage.root_as_path = root_as_path
+Storage.path = path
