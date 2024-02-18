0793775871cc899d737ddf4c0d27e5f80434e012
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 03:04:30 +0000

    Add package hky3535/0.1.2/hky3535-0.1.2.tar.gz

---
+++ packages/hky3535/hky3535-0.1.2.tar.gz/hky3535-0.1.2/hky3535/generate.py
@@ -0,0 +1,18 @@
+import numpy
+import random
+
+def blank_frame(width, height):
+    frame = numpy.zeros((height, width, 3), dtype=numpy.uint8)
+    return frame
+
+def moving_rectangles():
+    return
+
+def moving_points():
+    return
+
+def blinking_rectangles():
+    return
+
+def blinking_points(regions, hot_spots=False):
+    return
