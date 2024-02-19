9431f4c229f94dd3bc1de3be458bb4b8d41533ee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-19 00:08:30 +0000

    Add package instructor/0.5.0/instructor-0.5.0-py3-none-any.whl

---
+++ packages/instructor/instructor-0.5.0-py3-none-any.whl/instructor/__init__.py
@@ -0,0 +1,28 @@
+from .distil import FinetuneFormat, Instructions
+from .dsl import (
+    CitationMixin,
+    Maybe,
+    Partial,
+    IterableModel,
+    llm_validator,
+    openai_moderation,
+)
+from .function_calls import OpenAISchema, openai_schema, Mode
+from .patch import apatch, patch
+
+__all__ = [
+    "OpenAISchema",
+    "CitationMixin",
+    "IterableModel",
+    "Maybe",
+    "Partial",
+    "openai_schema",
+    "Mode",
+    "patch",
+    "apatch",
+    "llm_validator",
+    "openai_moderation",
+    "FinetuneFormat",
+    "Instructions",
+    "unpatch",
+]
