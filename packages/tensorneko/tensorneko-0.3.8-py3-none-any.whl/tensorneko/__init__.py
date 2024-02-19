0edc0be55cc0a2f9d64aa9c3e8d593cd617ae2ae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-19 02:12:26 +0000

    Add package tensorneko/0.3.8/tensorneko-0.3.8-py3-none-any.whl

---
+++ packages/tensorneko/tensorneko-0.3.8-py3-none-any.whl/tensorneko/__init__.py
@@ -0,0 +1,39 @@
+import os.path
+
+from tensorneko_util import io
+from . import backend
+from . import callback
+from . import dataset
+from . import debug
+from . import evaluation
+from . import layer
+from . import module
+from . import notebook
+from . import optim
+from . import preprocess
+from . import util
+from . import visualization
+from .neko_model import NekoModel
+from .neko_module import NekoModule
+from .neko_trainer import NekoTrainer
+
+__all__ = [
+    "callback",
+    "dataset",
+    "backend",
+    "evaluation",
+    "io",
+    "layer",
+    "module",
+    "notebook",
+    "optim",
+    "preprocess",
+    "util",
+    "visualization",
+    "debug",
+    "NekoModel",
+    "NekoTrainer",
+    "NekoModule"
+]
+
+__version__ = io.read.text(os.path.join(util.get_tensorneko_path(), "version.txt"))
