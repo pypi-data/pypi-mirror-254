ac1970f36bedc58506f05f2aefbfbd696c9355f3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-19 01:26:47 +0000

    Add package view.py/1.0.0a9/view.py-1.0.0a9-cp312-cp312-win32.whl

---
+++ packages/view.py/view.py-1.0.0a9-cp312-cp312-win32.whl/view/__init__.py
@@ -0,0 +1,29 @@
+# flake8: noqa
+"""
+view.py - The Batteries-Detachable Web Framework
+
+Docs: https://view.zintensity.dev
+GitHub: https://github.com/zerointensity/view.py
+"""
+try:
+    import _view
+except ImportError as e:
+    raise ImportError(
+        "_view has not been built, did you forget to compile it?"
+    ) from e
+
+from _view import Context  # re-export
+from _view import InvalidStatusError  # re-export
+
+from . import _codec
+from .__about__ import *
+from .app import *
+from .components import *
+from .exceptions import *
+from .logging import *
+from .patterns import *
+from .response import *
+from .routing import *
+from .templates import *
+from .typecodes import *
+from .util import *
