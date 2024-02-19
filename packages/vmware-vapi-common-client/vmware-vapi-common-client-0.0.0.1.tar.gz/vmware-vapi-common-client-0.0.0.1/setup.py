a6a3241ed28b52fa87c52cd3f42ce8026efca2ce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-19 02:12:57 +0000

    Add package vmware-vapi-common-client/0.0.0.1/vmware-vapi-common-client-0.0.0.1.tar.gz

---
+++ packages/vmware-vapi-common-client/vmware-vapi-common-client-0.0.0.1.tar.gz/vmware-vapi-common-client-0.0.0.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="vmware-vapi-common-client",
+    version="0.0.0.1",
+    author="VMware Inc.",
+    description="Python bindings for vapi common client",
+    packages=find_packages(),
+    install_requires=[
+        # List your package dependencies here
+    ],
+)
+
