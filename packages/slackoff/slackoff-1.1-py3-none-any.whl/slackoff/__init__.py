af3c60583df9b6dec02a9c44ce5b33a6c8449a1e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-19 01:26:36 +0000

    Add package slackoff/1.1/slackoff-1.1-py3-none-any.whl

---
+++ packages/slackoff/slackoff-1.1-py3-none-any.whl/slackoff/__init__.py
@@ -0,0 +1,9 @@
+from pkg_resources import DistributionNotFound, get_distribution
+
+try:
+    __version__ = get_distribution("slackoff").version
+except DistributionNotFound:
+    __version__ = "(local)"
+
+del DistributionNotFound
+del get_distribution
