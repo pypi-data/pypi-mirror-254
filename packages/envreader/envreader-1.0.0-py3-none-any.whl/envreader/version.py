cf332d5bb4166de40e73646165380aeaa7f93773
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-19 03:05:41 +0000

    Add package envreader/1.0.0/envreader-1.0.0-py3-none-any.whl

---
+++ packages/envreader/envreader-1.0.0-py3-none-any.whl/envreader/version.py
@@ -0,0 +1,7 @@
+# Copyright (C) 2020-2024 by Vd.
+# This file is part of EnvReader, the modern environment variables processor.
+# EnvReader is released under the MIT License (see LICENSE).
+
+
+def version():
+    return "1.0.0"
