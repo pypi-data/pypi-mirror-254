32c12a7689cb765eb5b6f07619551c3eeea660cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-19 02:13:00 +0000

    Add package vmware-vcenter/0.0.0.1/vmware-vcenter-0.0.0.1.tar.gz

---
+++ packages/vmware-vcenter/vmware-vcenter-0.0.0.1.tar.gz/vmware-vcenter-0.0.0.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="vmware-vcenter",
+    version="0.0.0.1",
+    author="VMware Inc.",
+    description="Python bindings for vCenter",
+    packages=find_packages(),
+    install_requires=[
+        # List your package dependencies here
+    ],
+)
+
