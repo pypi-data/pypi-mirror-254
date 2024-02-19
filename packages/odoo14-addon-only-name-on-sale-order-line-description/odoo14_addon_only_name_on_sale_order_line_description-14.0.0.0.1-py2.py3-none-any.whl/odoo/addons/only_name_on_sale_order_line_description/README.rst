c9521bf2bda976f8597c9518ffbdd36f4ea3af21
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-19 03:05:10 +0000

    Add package odoo14-addon-only-name-on-sale-order-line-description/14.0.0.0.1/odoo14_addon_only_name_on_sale_order_line_description-14.0.0.0.1-py2.py3-none-any.whl

---
+++ packages/odoo14-addon-only-name-on-sale-order-line-description/odoo14_addon_only_name_on_sale_order_line_description-14.0.0.0.1-py2.py3-none-any.whl/odoo/addons/only_name_on_sale_order_line_description/README.rst
@@ -0,0 +1,2 @@
+This module is a modification of the sale_order_line_description module
+https://apps.odoo.com/apps/modules/14.0/sale_order_line_description/
---
+++ packages/odoo14-addon-only-name-on-sale-order-line-description/odoo14_addon_only_name_on_sale_order_line_description-14.0.0.0.1-py2.py3-none-any.whl/odoo/addons/only_name_on_sale_order_line_description/__manifest__.py
@@ -0,0 +1,14 @@
+# Talaios coop.
+# Original code by: Copyright 2013-15 Agile Business Group sagl (<http://www.agilebg.com>)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "Only Name on sale order line description",
+    "version": "14.0.0.0.1",
+    "category": "Sales Management",
+    "author": "Talaios",
+    "website": "https://talaios.coop",
+    "license": "AGPL-3",
+    "depends": ["sale"],
+    "data": ["security/sale_security.xml", "views/res_config_settings_views.xml"],
+    "installable": True,
+}
