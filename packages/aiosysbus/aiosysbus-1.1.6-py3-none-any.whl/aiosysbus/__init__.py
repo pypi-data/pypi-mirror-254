915e55ffd34a509f177a2bbcb7e9d528b2d68751
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-19 00:07:13 +0000

    Add package aiosysbus/1.1.6/aiosysbus-1.1.6-py3-none-any.whl

---
+++ packages/aiosysbus/aiosysbus-1.1.6-py3-none-any.whl/aiosysbus/__init__.py
@@ -0,0 +1,24 @@
+"""
+Provides authentication and raw access to Livebox.
+"""
+from .aiosysbus import AIOSysbus
+from .exceptions import (
+    AiosysbusException,
+    AuthenticationFailed,
+    HttpRequestFailed,
+    InsufficientPermissionsError,
+    RetrieveFailed,
+    TimeoutExceededError,
+    UnexpectedResponse,
+)
+
+__all__ = [
+    "AIOSysbus",
+    "AuthenticationFailed",
+    "AiosysbusException",
+    "HttpRequestFailed",
+    "RetrieveFailed",
+    "TimeoutExceededError",
+    "InsufficientPermissionsError",
+    "UnexpectedResponse",
+]
