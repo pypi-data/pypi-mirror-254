5de00774ab9794c61f31d14eb71e6999a6429f23
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-19 01:29:04 +0000

    Add package dbhydra/2.0.18/dbhydra-2.0.18-py3-none-any.whl

---
+++ packages/dbhydra/dbhydra-2.0.18-py3-none-any.whl/dbhydra/mongo.py
@@ -0,0 +1,41 @@
+import pymongo
+from random import randint
+import certifi
+"""
+client = pymongo.MongoClient("mongodb+srv://dominik:mongodominik@cluster0.oz6pa.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",tlsCAFile=certifi.where())
+db = client.test
+print(db)
+
+
+
+names = ['Kitchen','Animal','State', 'Tastey', 'Big','City','Fish', 'Pizza','Goat', 'Salty','Sandwich','Lazy', 'Fun']
+company_type = ['LLC','Inc','Company','Corporation']
+company_cuisine = ['Pizza', 'Bar Food', 'Fast Food', 'Italian', 'Mexican', 'American', 'Sushi Bar', 'Vegetarian']
+for x in range(1, 501):
+    business = {
+        'name' : names[randint(0, (len(names)-1))] + ' ' + names[randint(0, (len(names)-1))]  + ' ' + company_type[randint(0, (len(company_type)-1))],
+        'rating' : randint(1, 5),
+        'cuisine' : company_cuisine[randint(0, (len(company_cuisine)-1))] 
+    }
+    #Step 3: Insert business object directly into MongoDB via insert_one
+    result=db.reviews.insert_one(business)
+    #Step 4: Print to the console the ObjectID of the new document
+    print('Created {0} of 500 as {1}'.format(x,result.inserted_id))
+#Step 5: Tell us that you are done
+print('finished creating 500 business reviews')
+"""
+
+
+import pymongo as pymongo
+# pprint library is used to make the output look more pretty
+from pprint import pprint
+# connect to MongoDB, change the << MONGODB URL >> to reflect your own connection string
+
+
+client = pymongo.MongoClient("mongodb+srv://dominik:mongodominik@cluster0.oz6pa.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
+
+#client = pymongo.MongoClient("mongodb+srv://admin:admin@cluster0.sqds7.mongodb.net/Cluster0?retryWrites=true&w=majority")
+db=client.admin
+# Issue the serverStatus command and print the results
+serverStatusResult=db.command("serverStatus")
+pprint(serverStatusResult)
