1a391037f4db3c727594d1497a2bfb3efe8118f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-19 01:26:32 +0000

    Add package decancer-py/0.3.0/decancer_py-0.3.0.tar.gz

---
+++ packages/decancer-py/decancer_py-0.3.0.tar.gz/decancer_py-0.3.0/src/lib.rs
@@ -0,0 +1,73 @@
+extern crate decancer;
+
+use pyo3::{basic::CompareOp, prelude::*};
+
+/// A small wrapper around the str datatype used for comparison reasons.
+#[pyclass]
+pub struct CuredString(decancer::CuredString);
+
+#[pymethods]
+impl CuredString {
+    /// Checks if this string similarly starts with another string.
+    #[pyo3(text_signature = "($self, other: str) -> bool")]
+    fn starts_with(&self, other: &str) -> bool {
+        self.0.starts_with(other)
+    }
+
+    /// Checks if this string similarly ends with another string.
+    #[pyo3(text_signature = "($self, other: str) -> bool")]
+    fn ends_with(&self, other: &str) -> bool {
+        self.0.ends_with(other)
+    }
+
+    /// Checks if this string similarly contains another string.
+    #[pyo3(text_signature = "($self, other: str) -> bool")]
+    fn contains(&self, other: &str) -> bool {
+        self.0.contains(other)
+    }
+
+    fn __richcmp__(&self, other: &str, op: CompareOp) -> PyResult<bool> {
+        Ok(match op {
+            CompareOp::Eq => self.0 == other,
+            CompareOp::Ne => self.0 != other,
+            _ => false,
+        })
+    }
+
+    fn __contains__(&self, other: &str) -> PyResult<bool> {
+        Ok(self.contains(other))
+    }
+
+    fn __bool__(&self) -> PyResult<bool> {
+        Ok(!self.0.is_empty())
+    }
+
+    fn __str__(&self) -> PyResult<&str> {
+        Ok(&self.0)
+    }
+
+    fn __repr__(&self) -> PyResult<String> {
+        Ok(format!("{:?}", self.0))
+    }
+}
+
+/// Parses a jank string into a less toxic lowercase string wrapped in CuredString object.
+#[pyfunction]
+#[pyo3(text_signature = "(text: str) -> CuredString")]
+pub fn parse<'a>(text: String) -> PyResult<CuredString> {
+    match decancer::cure(&text) {
+        Ok(res) => Ok(CuredString(res)),
+        Err(err) => Err(Python::with_gil(|_| {
+            pyo3::exceptions::PyRuntimeError::new_err(err.to_string())
+        })),
+    }
+}
+
+/// The module we export to python
+#[pymodule]
+fn decancer_py(_py: Python, m: &PyModule) -> PyResult<()> {
+    m.add("__version__", std::env!("CARGO_PKG_VERSION"))?;
+
+    m.add_class::<CuredString>()?;
+    m.add_function(wrap_pyfunction!(parse, m)?)
+}
