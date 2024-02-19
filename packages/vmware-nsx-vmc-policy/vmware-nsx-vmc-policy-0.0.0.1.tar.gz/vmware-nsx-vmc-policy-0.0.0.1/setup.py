b4f31a8bf29763337fdd9a5d29653564a66a0db3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-19 02:12:59 +0000

    Add package vmware-nsx-vmc-policy/0.0.0.1/vmware-nsx-vmc-policy-0.0.0.1.tar.gz

---
+++ packages/vmware-nsx-vmc-policy/vmware-nsx-vmc-policy-0.0.0.1.tar.gz/vmware-nsx-vmc-policy-0.0.0.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="vmware-nsx-vmc-policy",
+    version="0.0.0.1",
+    author="VMware Inc.",
+    description="Python bindings for nsx vmc policy",
+    packages=find_packages(),
+    install_requires=[
+        # List your package dependencies here
+    ],
+)
+
