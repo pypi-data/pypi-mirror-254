cd33f4d720908951cb418667111eac631ff4e55c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-19 03:05:43 +0000

    Add package basen-encoder/1.0.0/basen-encoder-1.0.0.tar.gz

---
+++ packages/basen-encoder/basen-encoder-1.0.0.tar.gz/basen-encoder-1.0.0/basen/__init__.py
@@ -0,0 +1,11 @@
+# Copyright (C) 2017-2024 by Ivan.
+# This file is part of BaseN package.
+# BaseN is released under the MIT License (see LICENSE).
+
+
+from .basen import BaseN
+from .int2base import int2base, base2int
+
+
+def version():
+    return "1.0.0"
---
+++ packages/basen-encoder/basen-encoder-1.0.0.tar.gz/basen-encoder-1.0.0/setup.py
@@ -0,0 +1,43 @@
+# Copyright (C) 2017-2024 by Ivan.
+# This file is part of BaseN package.
+# BaseN is released under the MIT License (see LICENSE).
+
+
+from os.path import join, dirname
+
+import setuptools
+
+setuptools.setup(
+    name='basen-encoder',
+    version='1.0.0',
+    author='Vd',
+    author_email='vd@vd2.org',
+    url='https://github.com/vd2org/basen',
+    license='MIT',
+    description='Custom encoder that encodes any binary data to given alphabet.',
+    long_description=open(join(dirname(__file__), 'README.md')).read(),
+    long_description_content_type='text/markdown',
+    packages=setuptools.find_packages(),
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Education',
+        'Intended Audience :: Information Technology',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12',
+        'Programming Language :: Python :: 3 :: Only',
+        'Topic :: Database',
+        'Topic :: Internet',
+        'Topic :: Utilities',
+        'Topic :: Software Development',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+)
