75f2719995ee316986f3891464ecc3f69f1ea531
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-19 01:26:40 +0000

    Add package distributions-desm/0.1/distributions_desm-0.1.tar.gz

---
+++ packages/distributions-desm/distributions_desm-0.1.tar.gz/distributions_desm-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='distributions_desm',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['distributions_desm'],
+      zip_safe=False)
