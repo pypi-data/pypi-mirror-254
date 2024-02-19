a89dd1edf33829730172ffcb079b49828afda391
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-19 02:12:58 +0000

    Add package vmware-cloud-aws-cho-provider/0.0.0.1/vmware-cloud-aws-cho-provider-0.0.0.1.tar.gz

---
+++ packages/vmware-cloud-aws-cho-provider/vmware-cloud-aws-cho-provider-0.0.0.1.tar.gz/vmware-cloud-aws-cho-provider-0.0.0.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="vmware-cloud-aws-cho-provider",
+    version="0.0.0.1",
+    author="VMware Inc.",
+    description="Python bindings for vmwarecloud aws cho provider",
+    packages=find_packages(),
+    install_requires=[
+        # List your package dependencies here
+    ],
+)
+
