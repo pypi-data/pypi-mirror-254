b3caa19d520d54294cb51ff0daeb61fea212e46b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-19 00:05:27 +0000

    Add package fntypes/0.1.0.dev1/fntypes-0.1.0.dev1-py3-none-any.whl

---
+++ packages/fntypes/fntypes-0.1.0.dev1-py3-none-any.whl/fntypes/option.py
@@ -0,0 +1,26 @@
+from __future__ import annotations
+
+import typing
+
+from fntypes.result import Ok, Error
+
+Value = typing.TypeVar("Value", covariant=True)
+
+
+class Nothing(Error[None]):
+    def __init__(self) -> None:
+        super().__init__(None)
+
+
+class Some(typing.Generic[Value], Ok[Value]):
+    pass
+
+
+Option: typing.TypeAlias = Some[Value] | Nothing
+
+
+__all__ = (
+    "Nothing",
+    "Some",
+    "Option"
+)
---
+++ packages/fntypes/fntypes-0.1.0.dev1-py3-none-any.whl/fntypes/protocols/__init__.py
@@ -0,0 +1,3 @@
+from .wrapped import Wrapped, UnwrapError
+
+__all__ = ("Wrapped", "UnwrapError")
