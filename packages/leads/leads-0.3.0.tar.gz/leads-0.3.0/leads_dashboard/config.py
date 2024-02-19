feb979778f5019ae5d082bea6da912f4a7e9c24d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-19 00:06:42 +0000

    Add package leads/0.3.0/leads-0.3.0.tar.gz

---
+++ packages/leads/leads-0.3.0.tar.gz/leads-0.3.0/leads_dashboard/config.py
@@ -0,0 +1,79 @@
+from json import load as _load, dumps as _dumps
+from typing import TextIO as _TextIO, Any as _Any
+
+from leads_dashboard.system import get_system_platform
+
+
+class Config(object):
+    def __init__(self, base: dict[str, _Any]) -> None:
+        """
+        :param base: base dictionary
+        """
+        self._d: dict[str, _Any] = base
+        self.srw_mode: bool = True
+        self.width: int = 720
+        self.height: int = 480
+        self.fullscreen: bool = False
+        self.no_title_bar: bool = False
+        self.manual_mode: bool = False
+        self.refresh_rate: int = 30
+        self.font_size_small: int = 8
+        self.font_size_medium: int = 16
+        self.font_size_large: int = 32
+        self.font_size_x_large: int = 48
+        self.scaling_factor: float = .8 if get_system_platform() == "linux" else 1
+        self.comm_addr: str = "127.0.0.1"
+        self.comm_port: int = 16900
+        self.data_dir: str = "./data"
+        self.enable_data_persistence: bool = True
+        self.refresh()
+
+    def __getitem__(self, name: str) -> _Any | None:
+        return self.get(name)
+
+    def __getattr__(self, name: str) -> _Any | None:
+        return self.get(name)
+
+    def __setitem__(self, name: str, value: _Any) -> None:
+        self._d[name] = value
+        self.refresh()
+
+    def __str__(self) -> str:
+        return _dumps(self)
+
+    def load(self, d: dict[str, _Any]) -> None:
+        """
+        Load a new dictionary and refresh.
+        :param d: the dictionary to load
+        """
+        self._d = d
+        self.refresh()
+
+    def get(self, name: str, default: _Any | None = None) -> _Any:
+        """
+        Get the value of a given name from the dictionary.
+        :param name: dictionary key
+        :param default: default value if the value does not exist
+        :return: the value if it exists or else the default value
+        """
+        if r := self._d.get(name):
+            return r
+        return default
+
+    def refresh(self) -> None:
+        """
+        Write the dictionary into the instance attributes.
+        """
+        for name in dir(self):
+            if not name.startswith("_") and (v := self.get(name)):
+                self.__setattr__(name, v)
+
+
+DEFAULT_CONFIG: Config = Config({})
+
+
+def load_config(file: str | _TextIO) -> Config:
+    if isinstance(file, str):
+        with open(file) as f:
+            return Config(_load(f))
+    return Config(_load(file))
