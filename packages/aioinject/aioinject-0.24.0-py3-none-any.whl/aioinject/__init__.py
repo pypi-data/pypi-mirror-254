f143c63ca8762c090d5ae4f2e486a35383996666
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-19 02:13:19 +0000

    Add package aioinject/0.24.0/aioinject-0.24.0-py3-none-any.whl

---
+++ packages/aioinject/aioinject-0.24.0-py3-none-any.whl/aioinject/__init__.py
@@ -0,0 +1,31 @@
+from aioinject.containers import Container
+from aioinject.context import InjectionContext, SyncInjectionContext
+from aioinject.decorators import inject
+from aioinject.markers import Inject
+from aioinject.providers import (
+    Callable,
+    Factory,
+    Object,
+    Provider,
+    Scoped,
+    Singleton,
+    Transient,
+)
+
+
+__all__ = [
+    "Scoped",
+    "Factory",
+    "Callable",
+    "Container",
+    "Inject",
+    "InjectionContext",
+    "Object",
+    "Provider",
+    "Singleton",
+    "SyncInjectionContext",
+    "inject",
+    "Transient",
+]
+
+__version__ = "0.24.0"
