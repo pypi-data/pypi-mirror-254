403f60fe77ab9e7388ba96d04fa6ed0796d56550
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-19 02:12:58 +0000

    Add package vmware-cloud-org/0.0.0.1/vmware-cloud-org-0.0.0.1.tar.gz

---
+++ packages/vmware-cloud-org/vmware-cloud-org-0.0.0.1.tar.gz/vmware-cloud-org-0.0.0.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="vmware-cloud-org",
+    version="0.0.0.1",
+    author="VMware Inc.",
+    description="Python bindings for vmwarecloud org",
+    packages=find_packages(),
+    install_requires=[
+        # List your package dependencies here
+    ],
+)
+
