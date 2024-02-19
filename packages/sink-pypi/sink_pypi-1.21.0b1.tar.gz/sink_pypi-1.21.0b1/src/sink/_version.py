30058a66b0269dc034de9695e368f5047bf18da7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-19 00:06:34 +0000

    Add package sink-pypi/1.21.0b1/sink_pypi-1.21.0b1.tar.gz

---
+++ packages/sink-pypi/sink_pypi-1.21.0b1.tar.gz/sink_pypi-1.21.0b1/src/sink/_version.py
@@ -0,0 +1,4 @@
+# File generated from our OpenAPI spec by Stainless.
+
+__title__ = "sink"
+__version__ = "1.21.0-beta.1"  # x-release-please-version
---
+++ packages/sink-pypi/sink_pypi-1.21.0b1.tar.gz/sink_pypi-1.21.0b1/src/sink/types/card_provision_foo_params.py
@@ -0,0 +1,41 @@
+# File generated from our OpenAPI spec by Stainless.
+
+from __future__ import annotations
+
+from typing_extensions import Literal, TypedDict
+
+__all__ = ["CardProvisionFooParams"]
+
+
+class CardProvisionFooParams(TypedDict, total=False):
+    account_token: str
+    """Only required for multi-account users.
+
+    Token identifying the account the card will be associated with. Only applicable
+    if using account holder enrollment. See
+    [Managing Your Program](https://docs.lithic.com/docs/managing-your-program) for
+    more information.
+    """
+
+    certificate: str
+    """Required for `APPLE_PAY`.
+
+    Apple's public leaf certificate. Base64 encoded in PEM format with headers
+    `(-----BEGIN CERTIFICATE-----)` and trailers omitted. Provided by the device's
+    wallet.
+    """
+
+    digital_wallet: Literal["APPLE_PAY", "GOOGLE_PAY", "SAMSUNG_PAY"]
+    """Name of digital wallet provider."""
+
+    nonce: str
+    """Required for `APPLE_PAY`.
+
+    Base64 cryptographic nonce provided by the device's wallet.
+    """
+
+    nonce_signature: str
+    """Required for `APPLE_PAY`.
+
+    Base64 cryptographic nonce provided by the device's wallet.
+    """
