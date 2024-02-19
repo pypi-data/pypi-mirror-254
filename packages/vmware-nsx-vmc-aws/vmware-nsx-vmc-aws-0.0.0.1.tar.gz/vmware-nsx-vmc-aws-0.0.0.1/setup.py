1d22587e03e10ec1f1b512c1a9d0893d183ba455
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-19 02:12:59 +0000

    Add package vmware-nsx-vmc-aws/0.0.0.1/vmware-nsx-vmc-aws-0.0.0.1.tar.gz

---
+++ packages/vmware-nsx-vmc-aws/vmware-nsx-vmc-aws-0.0.0.1.tar.gz/vmware-nsx-vmc-aws-0.0.0.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="vmware-nsx-vmc-aws",
+    version="0.0.0.1",
+    author="VMware Inc.",
+    description="Python bindings for nsx vmc aws",
+    packages=find_packages(),
+    install_requires=[
+        # List your package dependencies here
+    ],
+)
+
