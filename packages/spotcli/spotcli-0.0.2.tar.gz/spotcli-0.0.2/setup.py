52fac8b43a46f24f6f77c602969ec7af0396d6c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-19 00:08:25 +0000

    Add package spotcli/0.0.2/spotcli-0.0.2.tar.gz

---
+++ packages/spotcli/spotcli-0.0.2.tar.gz/spotcli-0.0.2/setup.py
@@ -0,0 +1,35 @@
+# setup.py
+from setuptools import setup, find_packages
+
+try:
+   import pypandoc
+   long_description = pypandoc.convert_file('README.md', 'rst')
+except(IOError, ImportError):
+   long_description = open('README.md').read()
+
+setup(
+    name='spotcli',
+    version='0.0.2',
+    author="micheledinelli",
+    author_email="dinellimichele00@gmail.com",
+
+    long_description_content_type="text/markdown",
+
+    long_description=long_description,
+
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        'typer',
+        'spotipy',
+        'python-dotenv',
+        'halo',
+        'inquirer',
+        'Pygments',
+    ],
+    entry_points={
+        'console_scripts': [
+            'spoticli = spoticli.main:app',
+        ],
+    },
+)
---
+++ packages/spotcli/spotcli-0.0.2.tar.gz/spotcli-0.0.2/spoticli/main.py
@@ -0,0 +1,40 @@
+import typer
+from typing_extensions import Annotated
+
+from modules import auth, user
+
+import spotipy
+
+from halo import Halo
+
+app = typer.Typer(add_completion=False)
+
+# Loading modules
+app.add_typer(auth.auth_app, name="auth")
+
+@app.command(short_help="returns info about the current user")
+def whoami():
+    # Create a Spotify object with the access information from oauth
+    sp = spotipy.Spotify(auth_manager=auth.sp_oauth)
+
+    # Call the whoami function from the user module
+    user.whoami(sp)    
+
+@app.command(short_help="search for a song")
+def search(query: Annotated[str, typer.Argument()], limit: int = typer.Option(5, help="number of results")):
+    # Create a Spotify object with the access information from oauth
+    sp = spotipy.Spotify(auth_manager=auth.sp_oauth)
+
+    # Call the search function from the user module
+    user.search(sp=sp, query=query, limit=limit)
+
+@app.command(short_help="show my devices")
+def devices():
+    # Create a Spotify object with the access information from oauth
+    sp = spotipy.Spotify(auth_manager=auth.sp_oauth)
+
+    # Call the search function from the user module
+    user.devices(sp=sp)
+
+if __name__ == '__main__':
+    app()
