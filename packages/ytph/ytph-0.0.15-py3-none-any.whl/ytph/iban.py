5edaf9bfd82a5883c6d5df02f6081fe44f60881d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-19 00:07:11 +0000

    Add package ytph/0.0.15/ytph-0.0.15-py3-none-any.whl

---
+++ packages/ytph/ytph-0.0.15-py3-none-any.whl/ytph/iban.py
@@ -0,0 +1,58 @@
+from pydantic import BaseModel, computed_field
+from schwifty import IBAN, exceptions
+import re
+
+
+class IbanResponse(BaseModel):
+    iban: str
+    is_valid: bool
+    message: str = ""
+    parsed: IBAN | str = ""
+
+    @computed_field
+    def account(self) -> dict:
+        if self.parsed:
+            return {
+                "bban": self.parsed.bban.__str__(),
+                "checksum_digits": self.parsed.checksum_digits,
+                "account_code": self.parsed.account_code,
+                "account_type": self.parsed.account_type,
+                "in_sepa_zone": self.parsed.in_sepa_zone,
+                "national_checksum_digits": self.parsed.national_checksum_digits,
+                "formatted": self.parsed.formatted,
+            }
+        return {}
+
+    @computed_field
+    def bank(self) -> dict:
+        if self.parsed:
+            _bank = getattr(self.parsed, "bank", {})
+            return _bank if _bank is not None else {}
+        return {}
+
+    @computed_field
+    def country(self) -> dict:
+        if self.parsed:
+            _country = getattr(self.parsed, "country", {})
+            return dict(_country) if _country is not None else {}
+        return {}
+
+    @computed_field
+    def specification(self) -> dict:
+        if self.parsed:
+            _specification = getattr(self.parsed, "spec", {})
+            if _specification is None:
+                return {}
+            if isinstance(_specification.get("regex"), re.Pattern):
+                _specification["regex"] = _specification["regex"].pattern
+            return _specification
+        return {}
+
+
+def validate_iban(iban: str) -> IbanResponse:
+    try:
+        _iban = IBAN(iban, allow_invalid=False, validate_bban=True)
+    except exceptions.SchwiftyException as e:
+        return IbanResponse(iban=iban, is_valid=False, message=str(e))
+    else:
+        return IbanResponse(iban=_iban.compact, is_valid=True, parsed=_iban)
