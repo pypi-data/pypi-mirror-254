752b2ff7d8fbe345fe20064e0c7ea3135abf3c64
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-19 01:26:27 +0000

    Add package cmr05-distributions/0.1/cmr05_distributions-0.1.tar.gz

---
+++ packages/cmr05-distributions/cmr05_distributions-0.1.tar.gz/cmr05_distributions-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='cmr05_distributions',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['cmr05_distributions'],
+      author= 'Christian Relling',
+      author_email='mail@relling.com',
+      zip_safe=False)
