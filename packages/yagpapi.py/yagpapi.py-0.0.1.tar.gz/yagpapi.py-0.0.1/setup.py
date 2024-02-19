ffad05c5098e8015255ae8dfbe86d3e694a5f825
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-19 00:05:21 +0000

    Add package yagpapi.py/0.0.1/yagpapi.py-0.0.1.tar.gz

---
+++ packages/yagpapi.py/yagpapi.py-0.0.1.tar.gz/yagpapi.py-0.0.1/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup
+import re
+
+version = ''
+with open('yagpapi/__init__.py') as f:
+    version = re.search(r'^__version__\s*=\s*[\'"]([^\'"]*)[\'"]', f.read(), re.MULTILINE).group(1)
+
+packages = [
+    'yagpapi',
+]
+
+setup(
+    name='yagpapi.py',
+    url='https://github.com/gxskpo/yagpapi.py',
+    version=version,
+    license='MIT',
+    packages=packages,
+    python_requires='>=3.9.0',
+    classifiers=[
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'Topic :: Internet',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+)
