dbeff7bb19d9f37df0f43cba2cb6df828b516f35
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-19 02:12:59 +0000

    Add package vmware-cloud-inventory/0.0.0.1/vmware-cloud-inventory-0.0.0.1.tar.gz

---
+++ packages/vmware-cloud-inventory/vmware-cloud-inventory-0.0.0.1.tar.gz/vmware-cloud-inventory-0.0.0.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="vmware-cloud-inventory",
+    version="0.0.0.1",
+    author="VMware Inc.",
+    description="Python bindings for vmwarecloud inventory",
+    packages=find_packages(),
+    install_requires=[
+        # List your package dependencies here
+    ],
+)
+
