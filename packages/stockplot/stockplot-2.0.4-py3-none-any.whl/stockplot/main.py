37df9541a93d7dc767e04ec2e17163bfc38c5f3b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-19 00:05:21 +0000

    Add package stockplot/2.0.4/stockplot-2.0.4-py3-none-any.whl

---
+++ packages/stockplot/stockplot-2.0.4-py3-none-any.whl/stockplot/main.py
@@ -0,0 +1,36 @@
+import sys
+import yfinance as yf
+from mplfinance import show
+from stockplot.calculate_price_movement import calculate_price_movement
+from stockplot.create_candlestick_plot import create_in_memory_candlestick_plot
+
+
+def main(ticker: str = sys.argv[1]) -> None:
+    ticker = ticker.upper()
+
+    (
+        current_price,
+        currency,
+        percentage_change_1day,
+        percentage_change_7day,
+        percentage_change_30day,
+    ) = calculate_price_movement(ticker)
+
+    data_90day = yf.download(ticker, period="90d", interval="1d", progress=False)
+    data_1day = yf.download(ticker, period="1d", interval="30m", progress=False)
+
+    create_in_memory_candlestick_plot(
+        ticker,
+        currency,
+        data_1day,
+        data_90day,
+        current_price,
+        percentage_change_1day,
+        percentage_change_7day,
+        percentage_change_30day,
+    )
+    show()
+
+
+if __name__ == "__main__":
+    main()
