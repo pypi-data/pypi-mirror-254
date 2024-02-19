ca2fff9ca13531c532f56e881d7bbc5d801afdbb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-19 01:28:44 +0000

    Add package pciSeq/0.0.60.dev1/pciSeq-0.0.60.dev1-py3-none-any.whl

---
+++ packages/pciSeq/pciSeq-0.0.60.dev1-py3-none-any.whl/pciSeq/src/core/logger.py
@@ -0,0 +1,24 @@
+import sys
+import logging
+
+
+def attach_to_log():
+    """
+    exists only for backwards compatibility.
+    Replaced by logger_setup
+    """
+    logger_setup()
+
+
+def logger_setup():
+
+    logging.basicConfig(
+        level=logging.INFO,
+        format="%(asctime)s [%(levelname)s] %(message)s",
+        handlers=[
+            logging.StreamHandler(sys.stdout)
+        ]
+    )
+    logger = logging.getLogger(__name__)
+
+    return logger
