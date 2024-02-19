b28fe0a727be145beb2d267af88e58352b578f0a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-19 00:05:19 +0000

    Add package ezsam/0.2.1/ezsam-0.2.1-py3-none-any.whl

---
+++ packages/ezsam/ezsam-0.2.1-py3-none-any.whl/ezsam/config/defaults.py
@@ -0,0 +1,13 @@
+from ezsam.formats import OutputImageFormat, OutputVideoCodec
+
+
+DEFAULT_SAM_MODEL = 'vit_h'
+DEFAULT_GROUNDING_DINO_CONFIG_PATH = './config/GroundingDINO_SwinT_OGC.py'
+DEFAULT_GROUNDING_DINO_CONFIG_PATH_TMP = '.gdconf.py'
+DEFAULT_OUTPUT_DIR = '.'
+DEFAULT_OUTPUT_SUFFIX = '.out'
+DEFAULT_BOX_THRESHOLD = 0.3
+DEFAULT_TEXT_THRESHOLD = 0.3
+DEFAULT_NMS_THRESHOLD = 0.8
+DEFAULT_IMAGE_FORMAT = OutputImageFormat.png.value
+DEFAULT_VIDEO_CODEC = OutputVideoCodec.vp9.value
