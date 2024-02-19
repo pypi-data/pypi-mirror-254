c0d10a7b0d53b945fc649a6b469f2ee02255c6fa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-19 02:12:27 +0000

    Add package traces/0.6.3/traces-0.6.3.tar.gz

---
+++ packages/traces/traces-0.6.3.tar.gz/traces-0.6.3/traces/__init__.py
@@ -0,0 +1,11 @@
+from infinity import inf
+
+from . import decorators, operations
+from .eventseries import EventSeries
+from .histogram import Histogram
+from .timeseries import TimeSeries
+from .utils import datetime_range
+
+__author__ = "Mike Stringer"
+__email__ = "mike.stringer@datascopeanalytics.com"
+__version__ = "0.6.3"
