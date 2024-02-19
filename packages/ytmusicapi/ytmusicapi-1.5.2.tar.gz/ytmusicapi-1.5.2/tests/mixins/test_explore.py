09d8d2632f5a0ba69b10d809d6a0e40a38bb06b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-19 00:05:27 +0000

    Add package ytmusicapi/1.5.2/ytmusicapi-1.5.2.tar.gz

---
+++ packages/ytmusicapi/ytmusicapi-1.5.2.tar.gz/ytmusicapi-1.5.2/tests/mixins/test_explore.py
@@ -0,0 +1,18 @@
+class TestExplore:
+    def test_get_mood_playlists(self, yt):
+        categories = yt.get_mood_categories()
+        assert len(list(categories)) > 0
+        cat = next(iter(categories))
+        assert len(categories[cat]) > 0
+        playlists = yt.get_mood_playlists(categories[cat][0]["params"])
+        assert len(playlists) > 0
+
+    def test_get_charts(self, yt, yt_oauth):
+        charts = yt_oauth.get_charts()
+        assert len(charts) == 4
+        charts = yt.get_charts(country="US")
+        assert len(charts) == 4
+        charts = yt_oauth.get_charts(country="US")
+        assert len(charts) == 5
+        charts = yt.get_charts(country="BE")
+        assert len(charts) == 3
