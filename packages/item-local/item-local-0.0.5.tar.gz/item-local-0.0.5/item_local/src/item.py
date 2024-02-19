c8c6541a6d6731f8ae0be3f7d495bcb7c0b5d56e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-19 01:27:04 +0000

    Add package item-local/0.0.5/item-local-0.0.5.tar.gz

---
+++ packages/item-local/item-local-0.0.5.tar.gz/item-local-0.0.5/item_local/src/item.py
@@ -0,0 +1,12 @@
+from abc import ABC, abstractmethod
+
+from logger_local.MetaLogger import ABCMetaLogger
+
+
+class Item(ABC, metaclass=ABCMetaLogger):
+    @abstractmethod
+    def get_id(self):
+        pass
+
+    def __str__(self):
+        return f"Item id= {self.get_id()}"
