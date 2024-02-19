8c082166528af426224276b538a1081687c0ce26
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-19 00:08:19 +0000

    Add package haupt/2.1.1/haupt-2.1.1.tar.gz

---
+++ packages/haupt/haupt-2.1.1.tar.gz/haupt-2.1.1/haupt/polyconf/urls.py
@@ -0,0 +1,6 @@
+from haupt.polyconf.settings import PLATFORM_CONFIG
+
+if PLATFORM_CONFIG.is_streams_service:
+    from haupt.streams.patterns import *  # noqa
+else:
+    from haupt.apis.patterns import *  # noqa
